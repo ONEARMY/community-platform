737688cd243fd0139a797c82cf1aaa376e3ec372
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2018-09-27 17:43:42 +0200

    add yarn.lock - fix smal compile errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/App/App";
+import App from "./components/app/App";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 

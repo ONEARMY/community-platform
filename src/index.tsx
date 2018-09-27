bdc64570fc86fe56717b2075a3b09e60640f0a12
laptop\chris <chris.m.clarke@live.co.uk>
2018-09-27 15:03:33 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./components/App/App";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+registerServiceWorker();
---
+++ src/models/models.tsx
@@ -0,0 +1,4 @@
+import { IProject } from "./project.models";
+import { IUser } from "./user.models";
+
+export { IUser, IProject };

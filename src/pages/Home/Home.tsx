579545115edcf932698dc1b448ddc379c9b0d53c
Chris\Laptop <chris.m.clarke@live.co.uk>
2018-10-08 18:45:09 +0200

    login component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,19 +1,41 @@
 import * as React from "react";
 import { Lock } from "@material-ui/icons";
-import { IconButton } from "@material-ui/core";
+import { IconButton, Modal } from "@material-ui/core";
 import "./Home.scss";
+import { LoginComponent } from "../../components/login";
 
 export class HomePage extends React.Component {
+  public state = {
+    showLoginModal: false
+  };
+  public handleOpen = () => {
+    this.setState({ showLoginModal: true });
+  };
+
+  public handleClose = () => {
+    this.setState({ showLoginModal: false });
+  };
   public render() {
     return (
       <div id="HomePage">
         <div className="bgimg-1">
           <div className="unlock">
-            <IconButton color="primary">
+            <IconButton color="primary" onClick={this.handleOpen}>
               <Lock />
             </IconButton>
           </div>
         </div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.state.showLoginModal}
+          onClose={this.handleClose}
+        >
+          <div className="login-modal">
+            Modal text?
+            <LoginComponent />
+          </div>
+        </Modal>
       </div>
     );
   }

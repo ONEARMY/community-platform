8c4be820370a7e065c8e5f7fb52ae24a7e39a66e
Chris\Laptop <chris.m.clarke@live.co.uk>
2018-10-08 12:40:46 +0200

    add placeholder 404 and home pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 
 import Docs from "./Docs/index";
+import { HomePage } from "./Home";
+import { NotFoundPage } from "./NotFound";
 import { Switch, Route } from "react-router-dom";
 
-class Routes extends React.Component {
+export class Routes extends React.Component {
   public shouldComponentUpdate() {
     // Without this the app won't update on route changes, we've tried using
     // `withRouter`, but it caused the app to remount on every route change.
@@ -14,12 +16,12 @@ class Routes extends React.Component {
     return (
       <Switch>
         <Route path="/docs" component={Docs} />
-
+        <Route exact path="/" component={HomePage} />
         {/* TODO add notFound page */}
-        {/* <Route component={NotFound} /> */}
+        <Route component={NotFoundPage} />
       </Switch>
     );
   }
+  // more examples of router config and behaviours can be found at:
+  // https://reacttraining.com/react-router/web/example/basic
 }
-
-export default Routes;

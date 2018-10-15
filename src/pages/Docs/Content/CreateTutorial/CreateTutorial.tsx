0e7ec8767759991f3003e400c659915c9352abef
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2018-10-15 17:08:50 +0200

    fix add step button - some css improvement - save data in firebase with right formating

--- src/pages/Docs/Content/CreateTutorial/CreateTutorial.tsx
+++ src/pages/Docs/Content/CreateTutorial/CreateTutorial.tsx
@@ -22,21 +22,21 @@ export interface IState {
   formValues: IFormValues;
   _isUploading: boolean;
   _imgUploadProgress: number;
-  _uploadPath: string;
+  _uploadImgPath: string;
+  _uploadTutorialPath: string;
   _currentStepIndex: number;
 }
 interface IFormValues {
   workspace_name: string;
   tutorial_title: string;
   tutorial_description: string;
-  tutorial_time: number;
+  tutorial_time: string;
   tutorial_cost: number;
   difficulty_level: string;
   id: string;
   slug: string;
   steps: ITutorialStep[];
   cover_image_url: string;
-  cover_image_filename: string;
   tutorial_files_url: string;
 }
 
@@ -77,8 +77,8 @@ class CreateTutorial extends React.PureComponent<
     this.state = {
       formValues: {
         tutorial_description: "this is test description",
-        tutorial_title: "my tutorial yolo title",
-        tutorial_time: 20,
+        tutorial_title: "",
+        tutorial_time: "20 hours",
         tutorial_cost: 50,
         difficulty_level: "easy",
         cover_image_url: "",
@@ -102,33 +102,28 @@ class CreateTutorial extends React.PureComponent<
             images: []
           }
         ],
-        workspace_name: "test",
-        cover_image_filename: ""
+        workspace_name: "test"
       },
       _isUploading: false,
       _imgUploadProgress: 0,
-      _uploadPath: "uploads/test",
-      _currentStepIndex: 0
+      _uploadImgPath: "uploads/test",
+      _currentStepIndex: 0,
+      _uploadTutorialPath: "tutorials/test"
     };
 
     this.onSubmit = this.onSubmit.bind(this);
-    this.preparePayload = this.preparePayload.bind(this);
   }
-
-  public preparePayload = (values: any) => {
-    console.log("values", values);
-  };
 
   public onSubmit = async (values: any) => {
     console.log("submitting", values);
-    // try {
-    //   await db.doc("tutorials/testdocdave").set({
-    //     values
-    //   });
-    //   console.log("doc set successfully");
-    // } catch (error) {
-    //   console.log("error while saving the tutorial");
-    // }
+    try {
+      await db.doc(this.state._uploadTutorialPath).set({
+        values
+      });
+      console.log("doc set successfully");
+    } catch (error) {
+      console.log("error while saving the tutorial");
+    }
   };
 
   public handleUploadStart = () => {
@@ -147,7 +142,7 @@ class CreateTutorial extends React.PureComponent<
       _isUploading: false
     });
     storage
-      .ref(this.state._uploadPath)
+      .ref(this.state._uploadImgPath)
       .child(filename)
       .getDownloadURL()
       .then(url => {
@@ -168,11 +163,10 @@ class CreateTutorial extends React.PureComponent<
     const currentSteps: any = this.state.formValues.steps;
     // get the step at the index where the new image will go
     const url = await storage
-      .ref(this.state._uploadPath)
+      .ref(this.state._uploadImgPath)
       .child(filename)
       .getDownloadURL();
     // use the spread operator to merge the existing images with the new url
-    // it should create a new array if one doesn't already exist
     currentSteps[this.state._currentStepIndex].images = [
       ...currentSteps[this.state._currentStepIndex].images,
       url
@@ -191,7 +185,7 @@ class CreateTutorial extends React.PureComponent<
       _isUploading: false
     });
     storage
-      .ref(this.state._uploadPath)
+      .ref(this.state._uploadImgPath)
       .child(filename)
       .getDownloadURL()
       .then(url => {
@@ -201,36 +195,12 @@ class CreateTutorial extends React.PureComponent<
       });
   };
 
-  public onChangeHandlerStepImg = (event: any) => {
-    const {
-      target: { files }
-    } = event;
-    const filesToStore: any = [];
-    console.log("files :", files);
-    for (const f of files) {
-      console.log("f :", f);
-      filesToStore.push(f);
-    }
-    // this.setState({ _stepsImgUrl: filesToStore });
-  };
-
   public onTitleChange = (event: any) => {
     // *** TODO the event.target.value needs to be formated as the article id
-    this.setState({ _uploadPath: "uploads/" + event.target.value });
-  };
-
-  public displayStepImgUpload = (stepIndex: any) => {
-    return (
-      <FileUploader
-        accept="image/*"
-        name={"step" + stepIndex + "_image"}
-        storageRef={storage.ref(this.state._uploadPath)}
-        onUploadStart={this.handleUploadStart}
-        onUploadError={this.handleUploadError}
-        onUploadSuccess={this.handleUploadStepImgSuccess}
-        onProgress={this.handleProgress}
-      />
-    );
+    this.setState({
+      _uploadImgPath: "uploads/" + event.target.value,
+      _uploadTutorialPath: "tutorials/" + event.target.value
+    });
   };
 
   public render() {
@@ -241,14 +211,7 @@ class CreateTutorial extends React.PureComponent<
         mutators={{
           ...arrayMutators
         }}
-        render={({
-          handleSubmit,
-          mutators: { push, pop }, // injected from final-form-arrays above
-          pristine,
-          reset,
-          submitting,
-          values
-        }) => {
+        render={({ handleSubmit, pristine, submitting, values, form }) => {
           return (
             <form className="tutorial-form" onSubmit={handleSubmit}>
               <Typography variant="h4" component="h4">
@@ -307,7 +270,7 @@ class CreateTutorial extends React.PureComponent<
                         hidden
                         accept="image/png, image/jpeg"
                         name="coverImg"
-                        storageRef={storage.ref(this.state._uploadPath)}
+                        storageRef={storage.ref(this.state._uploadImgPath)}
                         onUploadStart={this.handleUploadStart}
                         onUploadError={this.handleUploadError}
                         onUploadSuccess={this.handleUploadCoverSuccess}
@@ -331,6 +294,7 @@ class CreateTutorial extends React.PureComponent<
                           <textarea
                             {...input}
                             placeholder="Tutorial description"
+                            style={{ width: "400px", height: "150px" }}
                           />
                           {meta.error &&
                             meta.touched && <span>{meta.error}</span>}
@@ -341,7 +305,7 @@ class CreateTutorial extends React.PureComponent<
                       {({ input, meta }) => (
                         <div>
                           <Typography component="label">
-                            How much time does it take ?
+                            How much time does it take ? (hours/week)
                           </Typography>
                           <input
                             {...input}
@@ -357,7 +321,7 @@ class CreateTutorial extends React.PureComponent<
                       {({ input, meta }) => (
                         <div>
                           <Typography component="label">
-                            How much does it take ?
+                            How much does it cost ?
                           </Typography>
                           <input
                             {...input}
@@ -385,7 +349,7 @@ class CreateTutorial extends React.PureComponent<
                         hidden
                         multiple={true}
                         name="files"
-                        storageRef={storage.ref(this.state._uploadPath)}
+                        storageRef={storage.ref(this.state._uploadImgPath)}
                         onUploadStart={this.handleUploadStart}
                         onUploadError={this.handleUploadError}
                         onUploadSuccess={this.handleUploadFilesSuccess}
@@ -407,38 +371,36 @@ class CreateTutorial extends React.PureComponent<
                   fields.map((step, index) => (
                     <Card key={step} style={styles.card}>
                       <CardContent>
-                        {this.state.formValues.steps[index] && (
-                          <div>
-                            <Typography variant="h5" component="h2">
-                              Step {index + 1} -{" "}
-                              {this.state.formValues.steps[index].title}
-                            </Typography>
-                            <Typography component="label">
-                              Pick a title for this step
-                            </Typography>
-                            <Field
-                              name={"step_" + index + 1 + "_title"}
-                              component="input"
-                              placeholder="Step title"
-                              validate={required}
-                            />
-                            <Typography component="label">
-                              Describe this step
-                            </Typography>
-                            <Field
-                              name={"step_" + index + 1 + "_description"}
-                              component="textarea"
-                              placeholder="Description"
-                              validate={required}
-                            />
-                          </div>
-                        )}
-                        <span
-                          onClick={() => fields.remove(index)}
-                          style={{ cursor: "pointer" }}
-                        >
-                          ❌
-                        </span>
+                        <div>
+                          <span
+                            onClick={() => fields.remove(index)}
+                            style={{ cursor: "pointer", float: "right" }}
+                          >
+                            ❌
+                          </span>
+                          <Typography variant="h5" component="h2">
+                            Step {index + 1}
+                          </Typography>
+                          <Typography component="label">
+                            Pick a title for this step
+                          </Typography>
+                          <Field
+                            name={`${step}.title`}
+                            component="input"
+                            placeholder="Step title"
+                            validate={required}
+                          />
+                          <Typography component="label">
+                            Describe this step
+                          </Typography>
+                          <Field
+                            name={`${step}.text`}
+                            component="textarea"
+                            placeholder="Description"
+                            validate={required}
+                            style={{ width: "400px", height: "150px" }}
+                          />
+                        </div>
 
                         {this.state.formValues.steps[index] &&
                           this.state.formValues.steps[index].images.length >=
@@ -458,8 +420,8 @@ class CreateTutorial extends React.PureComponent<
                         <label style={styles.uploadBtn}>
                           <FileUploader
                             hidden
-                            name="stepImages"
-                            storageRef={storage.ref(this.state._uploadPath)}
+                            name={`${step}.images`}
+                            storageRef={storage.ref(this.state._uploadImgPath)}
                             onUploadStart={this.handleUploadStart}
                             onUploadError={this.handleUploadError}
                             onUploadSuccess={this.handleUploadStepImgSuccess}
@@ -478,66 +440,47 @@ class CreateTutorial extends React.PureComponent<
                   ))
                 }
               </FieldArray>
-              {/* <div className="buttons">
-                <button
-                  type="button"
-                  onClick={() => {
-                    // const emptyStep: any = {
-                    //   ...this.state.formValues.steps,
-                    //   title: "",
-                    //   text: "",
-                    //   images: []
-                    // };
-                    // this.setState({
-                    //   formValues: { ...this.state.formValues, steps: emptyStep }
-                    // });
-                    push("steps", undefined);
-                  }}
-                >
-                  Add Step
-                </button>
-                
-              </div> */}
-
               <Card style={styles.card}>
                 <CardContent>
                   <Button
                     color="primary"
-                    onClick={() => {
-                      // const emptyStep: any = {
-                      //   ...this.state.formValues.steps,
-                      //   title: "",
-                      //   text: "",
-                      //   images: []
-                      // };
-                      // this.setState({
-                      //   formValues: { ...this.state.formValues, steps: emptyStep }
-                      // });
-                      push("steps", undefined);
-                    }}
                     variant="outlined"
+                    onClick={() => {
+                      // create a empty step in the steps form value
+                      form.mutators.push("steps", {
+                        title: "",
+                        text: "",
+                        images: []
+                      });
+                      // update the state with the empty new step
+                      const stepValuesInput: any = form.getFieldState("steps")!
+                        .value;
+                      this.setState({
+                        formValues: {
+                          ...this.state.formValues,
+                          steps: stepValuesInput
+                        }
+                      });
+                    }}
                   >
                     Add step
-                  </Button>
-                  <Button type="button" onClick={() => pop("steps")}>
-                    Remove Step
                   </Button>
                 </CardContent>
               </Card>
               <Button
+                type="submit"
                 color="primary"
                 variant="outlined"
-                type="submit"
+                style={{
+                  margin: "40px auto",
+                  position: "relative",
+                  display: "flex"
+                }}
                 disabled={submitting || pristine}
               >
-                Save
+                Save documentation
                 <SaveIcon />
               </Button>
-              {/* <div className="buttons">
-                <button type="submit" disabled={submitting || pristine}>
-                  Submit
-                </button>
-              </div> */}
             </form>
           );
         }}

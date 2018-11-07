5e6fd2ca1101a2e432baf06e1ae81e25d4efa3f4
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-11-07 16:22:43 +0000

    fix production deploy config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,17 @@
-// switch firebase config depending on dev or production environment
-let config
-const env = process.env.NODE_ENV
-const devConfig = {
+/*************************************************************************************** 
+Switch config dependent on use case
+
+For our use case the production config is stored in environment variables passed from
+Travis-CI. You can replace this with your own config or use the same pattern to keep
+api keys secret. Note, create-react-app only passes environment variables prefixed with
+'REACT_APP'. The required info has been encrypted and stored in travis.
+
+Dev config is hardcoded - it is recommended if changing for production to hide the 
+details via gitignore. You can find more information about potential security risk here:
+https://javebratt.com/hide-firebase-api/
+*****************************************************************************************/
+
+let config = {
   apiKey: 'AIzaSyChVNSMiYxCkbGd9C95aChr9GxRJtW6NRA',
   authDomain: 'precious-plastics-v4-dev.firebaseapp.com',
   databaseURL: 'https://precious-plastics-v4-dev.firebaseio.com',
@@ -9,37 +19,13 @@ const devConfig = {
   projectId: 'precious-plastics-v4-dev',
   storageBucket: 'precious-plastics-v4-dev.appspot.com',
 }
-console.log(`environment: ${process.env.NODE_ENV}`)
 
-/*************************************************************************************** 
-For our use case the production config is stored in environment variables passed from
-Travis-CI. You can replace this with your own config or use the same pattern to keep
-api keys secret. Note, create-react-app only passes environment variables prefixed with
-'REACT_APP'. The required info has been encrypted and stored in travis.
-
-Additionally we have 2 repository branches deployed (dev and master), with configs
-that are identified through an additional environment variable defined in .travis.yml
-
-Dev config is hardcoded - it is recommended if changing for production to hide the 
-details via gitignore. You can find more information about potential security risk here:
-https://javebratt.com/hide-firebase-api/
-*****************************************************************************************/
-
-if (env === 'production') {
-  console.log('production environment', process.env)
-  const branch = process.env.REACT_APP_BRANCH
-  console.log('branch', branch)
-  try {
-    config = JSON.parse(process.env[
-      `REACT_APP_FIREBASE_${branch}_CONFIG`
-    ] as string)
-  } catch (error) {
-    console.error(
-      'could not load production firebase config, reverting to development',
-    )
-    config = devConfig
-  }
-} else {
-  config = devConfig
+// different production site config pulled from environment variable
+const productionSites = ['onearmy.world', 'onearmyworld.firebaseapp.com']
+if (productionSites.indexOf(window.location.host) > -1) {
+  console.log('production site')
+  config = JSON.parse(process.env[
+    `REACT_APP_FIREBASE_PRODUCTION_CONFIG`
+  ] as string)
 }
 export const FIREBASE_CONFIG = config

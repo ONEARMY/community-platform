cd69ff482038e9cf4afb87273466763d27ae6ec8
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2018-11-07 13:07:20 +0100

    save tags in database #52

--- src/models/tutorial.models.tsx
+++ src/models/tutorial.models.tsx
@@ -1,23 +1,18 @@
 export interface ITutorial {
-  workspace_name: string;
-  cover_image_url: string;
-  tutorial_title: string;
-  tutorial_description: string;
-  difficulty_level: "easy" | "medium" | "difficult";
-  tutorial_time: string;
-  tutorial_cost: number;
-  steps: ITutorialStep[];
-  id: string;
-  slug: string;
+  workspace_name: string
+  cover_image_url: string
+  tutorial_title: string
+  tutorial_description: string
+  difficulty_level: 'easy' | 'medium' | 'difficult'
+  tutorial_time: string
+  tutorial_cost: number
+  steps: ITutorialStep[]
+  id: string
+  slug: string
 }
 
 export interface ITutorialStep {
-  images: string[];
-  title: string;
-  text: string;
-}
-
-export interface ITutorialTag {
-  value: string;
-  isSelected: boolean;
+  images: string[]
+  title: string
+  text: string
 }

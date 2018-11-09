d8cdb12c0aa2299b98bcc318df84ee8acfc4ae7d
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-11-09 20:56:38 +0000

    split router to support subdomin rendering of a single component (closes #80)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* tslint:disable:no-eval */
 import * as React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import DevTools from 'mobx-react-devtools'
@@ -8,9 +9,22 @@ import { NotFoundPage } from './NotFound/NotFound'
 import { TemplatePage } from './_Template/Template'
 import ScrollToTop from './../components/ScrollToTop/ScrollToTop'
 
-export class Routes extends React.Component {
+interface IState {
+  singlePageMode: boolean
+  displayPageComponent?: any
+}
+
+export class Routes extends React.Component<any, IState> {
+  constructor(props: any) {
+    super(props)
+    this.state = this.getDisplayState()
+  }
+
   public render() {
-    return (
+    // we are rendering different pages and navigation dependent on whether the user has navigated directly to view the
+    // entire site, or just one page of it via subdomains. This is so we can effectively integrate just parts of this
+    // platform into other sites. The first case is direct nav
+    return !this.state.singlePageMode ? (
       <div>
         <DevTools />
         <BrowserRouter>
@@ -21,10 +35,45 @@ export class Routes extends React.Component {
               <Route path="/template" component={TemplatePage} />
               <Route exact path="/" component={HomePage} />
               <Route component={NotFoundPage} />
+              <Route component={Docs} />
             </Switch>
           </ScrollToTop>
         </BrowserRouter>
       </div>
+    ) : (
+      // case display just a single component if viewing on subdomain
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route component={this.state.displayPageComponent} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     )
+  }
+
+  // identify the current url subdomain, if it matches one of the designated subdomains then we only want
+  // to render that component isolated
+  private getDisplayState() {
+    const availableSubdomains = ['documentation', 'map']
+    const subdomain = window.location.hostname.split('.')[0]
+    const showSinglePage = availableSubdomains.indexOf(subdomain) > -1
+    const state: IState = {
+      singlePageMode: showSinglePage,
+      displayPageComponent: showSinglePage
+        ? this.getSubdomainComponent(subdomain)
+        : null,
+    }
+    return state
+  }
+
+  // once we know we are only rendering a single page, identify the page component to render depending on subdomain
+  private getSubdomainComponent(subdomain: string) {
+    switch (subdomain) {
+      case 'documentation':
+        return Docs
+      default:
+        return NotFoundPage
+    }
   }
 }

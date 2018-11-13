9f677ba187e579ba233a22922eb9ffa190306a04
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2018-11-13 12:42:13 +0000

    add _key metadata to tags collection query, provide additional tagsByKey in store

--- src/stores/Tags/tags.store.tsx
+++ src/stores/Tags/tags.store.tsx
@@ -2,18 +2,27 @@ import { observable, action, computed } from 'mobx'
 import { db } from '../../utils/firebase'
 import { ITag } from 'src/models/models'
 import { TAGS_MOCK } from 'src/mocks/tags.mock'
+import { ITagQuery } from 'src/models/tags.model'
+import helpers from '../../utils/helpers'
 
 export class TagsStore {
   @observable
   public tags: ITag[] = []
+  public tagsByKey: { [key: string]: ITag } = {}
 
   constructor() {
     this.subscribeToTags()
   }
 
+  // when tags are received from the database we want to populate the _key field and
+  // dispatch back to the observable tags property
   public subscribeToTags() {
     db.collection('tags').onSnapshot(snapshot => {
-      const tags: ITag[] = snapshot.docs.map(doc => doc.data() as ITag)
+      const tags: ITag[] = snapshot.docs.map(doc => {
+        const data = doc.data() as ITagQuery
+        const tag: ITag = { ...data, _key: doc.id }
+        return tag
+      })
       this.updateTags(tags)
     })
   }
@@ -21,6 +30,9 @@ export class TagsStore {
   @action
   public updateTags(tags: ITag[]) {
     this.tags = tags
+    this.tagsByKey = helpers.arrayToJson(tags, '_key')
+    console.log('tags', tags)
+    console.log('tagsByKey', this.tagsByKey)
   }
 
   // sometimes during testing we might want to put the mock data in the database

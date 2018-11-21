88ee4c9d2c55a3c4f2bdf86223f26b90a31f28fc
Chris\Laptop <chris.m.clarke@live.co.uk>
2018-11-21 12:31:56 +0100

    db backup function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,17 +3,19 @@ import * as bodyParser from 'body-parser'
 import * as cors from 'cors'
 import * as express from 'express'
 import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp()
 
 // custom module imports
 import * as DB from './databaseBackup'
 
-// dev only
-const buildNumber = 1
-const app = express()
+// update on change logging purposes
+const buildNumber = 1.03
 
+// express settings to handle api
+const app = express()
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }))
-
 // use bodyparse to create json object from body
 app.use(
   bodyParser.json({
@@ -23,13 +25,11 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: false }))
 
 /************ GET and POST requests ************************************************
-Redirect requests so that if a custom endpoint function exists on koboApi call it,
-otherwise pipe request directly to kobo native API
+Redirect requests so that if a custom endpoint function exists we can call them
+at /api/[endpoint]
 ************************************************************************************/
 
 app.all('*', async (req, res, next) => {
-  // log the version number for dev / tracking:
-  console.log('api build number', buildNumber)
   // get the endpoint based on the request path
   const endpoint = req.path.split('/')[1]
   // *** NOTE currently all request types handled the same, i.e. GET/POST
@@ -38,17 +38,35 @@ app.all('*', async (req, res, next) => {
     case 'db-test':
       const token = await DB.AuthTest()
       res.send(token)
+      break
+    case 'backup':
+      const response = await DB.BackupDatabase()
+      res.send(response)
+      break
     default:
       res.send('invalid api endpoint')
   }
 })
-
-// Expose Express API as a single Cloud Function:
 exports.api = functions.https.onRequest(app)
 
 app.listen(3000, 'localhost', listen => {
-  console.log('API listening on port 3000')
+  console.log(`API v${buildNumber} listening on port 3000`)
 })
+
+/************ Cron tasks ***********************************************************
+Use pubsub to automatically subscribe to messages sent from cron.
+Add/change schedule from `./functions-cron/appengine/cron.yaml`
+************************************************************************************/
+
+exports.backupFirestore = functions.pubsub
+  .topic('firebase-backup')
+  .onPublish(async (message, context) => {
+    console.log('initiating backup')
+    // run our daily db backup task
+    const backup = await DB.BackupDatabase()
+    console.log('backup:', backup)
+    return true
+  })
 
 // add export so can be used by test
 export default app

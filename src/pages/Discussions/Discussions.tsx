c73bde2208b34285a5fcb93b19f119adefd03812
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-01-21 19:00:27 +0100

    rought integration of discussion section landing page with list and row

--- src/pages/Discussions/Discussions.tsx
+++ src/pages/Discussions/Discussions.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import moment from 'moment'
 import { inject, observer } from 'mobx-react'
 import { DISCUSSIONS_MOCK } from 'src/mocks/discussions.mock'
 
@@ -6,7 +7,23 @@ import MaxWidth from 'src/components/Layout/MaxWidth.js'
 import Margin from 'src/components/Layout/Margin.js'
 import FilterBar from 'src/pages/common/FilterBar/FilterBar'
 
-import { Content } from './elements'
+import {
+  Content,
+  Main,
+  Avatar,
+  Post,
+  List,
+  Title,
+  TitleAndTagsContaier,
+  TagsContainer,
+  Tag,
+  InteractionNb,
+  UsefullCount,
+  ViewCount,
+  PostDate,
+  DiscussIcon,
+  QaIcon,
+} from './elements'
 
 import { DocStore } from 'src/stores/Docs/docs.store'
 import { withRouter } from 'react-router'
@@ -30,12 +47,56 @@ class DiscussionsPageClass extends React.Component<IProps, any> {
     console.log('mocks:', DISCUSSIONS_MOCK)
   }
 
+  public durationSincePosted(postDate: string) {
+    // TODO : return the freshness, with minutes/hours/days etc
+    const formatedPostDate = moment(postDate)
+    const now: any = moment()
+    const duration = moment.duration(now.diff(formatedPostDate))
+    return Math.round(duration.as('years'))
+  }
+
   public render() {
     return (
       <MaxWidth>
         <Margin vertical={1.5}>
           <Content>
             <FilterBar />
+            <Margin vertical={1.5}>
+              <Main alignItems="flex-start">
+                <List>
+                  {DISCUSSIONS_MOCK.map((post, i) => (
+                    <Post key={i}>
+                      <Avatar src={post.avatar} alt="avatar" />
+                      <TitleAndTagsContaier>
+                        <Title href={'/discussions/post/' + post._id}>
+                          {post.postTitle}
+                        </Title>
+                        <TagsContainer>
+                          {post.tags.map((tag, j) => (
+                            <Tag key={j}>{tag}</Tag>
+                          ))}
+                        </TagsContainer>
+                      </TitleAndTagsContaier>
+                      <InteractionNb>
+                        {post.postType === 'discussion'
+                          ? post.commentNumber + ' comments'
+                          : post.commentNumber + ' answers'}
+                      </InteractionNb>
+                      <UsefullCount>{post.usefullCount}</UsefullCount>
+                      <ViewCount>{post.viewCount}</ViewCount>
+                      <PostDate>
+                        {this.durationSincePosted(post.date)} years
+                      </PostDate>
+                      {post.postType === 'discussion' ? (
+                        <DiscussIcon />
+                      ) : (
+                        <QaIcon />
+                      )}
+                    </Post>
+                  ))}
+                </List>
+              </Main>
+            </Margin>
           </Content>
         </Margin>
       </MaxWidth>
--- src/pages/Discussions/elements.js
+++ src/pages/Discussions/elements.js
@@ -1,7 +1,132 @@
 import styled from 'styled-components'
 
+import Row from 'src/components/Layout/Row.js'
+
+import { MdRecordVoiceOver, MdQuestionAnswer } from 'react-icons/md'
+
 export const Content = styled.div`
   margin-top: 5%;
   text-align: left;
   color: black;
+`
+
+export const Main = styled(Row)`
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`
+
+export const Avatar = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+  margin: 0 20px;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const List = styled.ul`
+  list-style: none;
+  width: 100%;
+  display: inline-block;
+  padding: 0;
+`
+export const Post = styled.li`
+  height: 100px;
+  margin: 0 25px;
+  border-bottom: 1px grey solid;
+  &:before {
+    content: '';
+    display: inline-block;
+    vertical-align: middle;
+    height: 100%;
+  }
+`
+
+export const TitleAndTagsContaier = styled.div`
+  width: 45%;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const Title = styled.a`
+  text-decoration: none;
+  font-size: 1.1em;
+  color: black;
+  display: block;
+  max-width: 100%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+export const TagsContainer = styled.div`
+  display: inline-block;
+  margin-top: 10px;
+`
+
+export const Tag = styled.span`
+  background-color: green;
+  margin: 5px 10px;
+  border-radius: 2px;
+  padding: 3px;
+  &:first-child {
+    margin-left: 0;
+  }
+`
+
+export const InteractionNb = styled.span`
+  font-size: 0.8em;
+  margin-left: 20px;
+  width: 10%;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const ViewCount = styled.span`
+  font-size: 0.8em;
+  margin-left: 60px;
+  width: 5%;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const UsefullCount = styled.span`
+  font-size: 0.8em;
+  padding: 20px;
+  margin-left: 30px;
+  display: inline-block;
+  vertical-align: middle;
+  border: 1px solid black;
+`
+
+export const PostDate = styled.span`
+  font-size: 0.8em;
+  margin-left: 60px;
+  width: 5%;
+  display: inline-block;
+  vertical-align: middle;
+`
+
+export const DiscussIcon = styled(MdRecordVoiceOver)`
+  font-size: 0.8em;
+  margin-left: 60px;
+  width: 5%;
+  display: inline-block;
+  vertical-align: middle;
+  width: 20px;
+  height: 20px;
+`
+
+export const QaIcon = styled(MdQuestionAnswer)`
+  font-size: 0.8em;
+  margin-left: 60px;
+  width: 5%;
+  display: inline-block;
+  vertical-align: middle;
+  width: 20px;
+  height: 20px;
 `

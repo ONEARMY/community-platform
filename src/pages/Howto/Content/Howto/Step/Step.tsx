813ded08a1ac8d476ec0e532eff924ef1ecde321
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-01-30 14:03:25 +0100

    fix the step.images[0].downloadUrl undefined error

--- src/pages/Howto/Content/Howto/Step/Step.tsx
+++ src/pages/Howto/Content/Howto/Step/Step.tsx
@@ -18,30 +18,36 @@ export default class Step extends React.PureComponent<any, any> {
     super(props)
   }
 
-  public renderMultipleImages(step: IHowtoStep) {
-    const preloadedImages: any[] = []
-    for (const image of step.images) {
-      const imageObj = new Image()
-      imageObj.src = image.downloadUrl
-      preloadedImages.push({
-        src: imageObj.src,
-      })
+  public renderImages(step: IHowtoStep) {
+    // If we have multiple images
+    if (step.images.length > 1) {
+      const preloadedImages: any[] = []
+      // Preload images and add them to preloadedImages array
+      for (const image of step.images) {
+        const imageObj = new Image()
+        imageObj.src = image.downloadUrl
+        preloadedImages.push({
+          src: imageObj.src,
+        })
+      }
+      // Return the <img> element from preloaded images
+      return preloadedImages.map((image: any, index: number) =>
+        index !== 1 ? (
+          <StepImage src={image.src} key={index} />
+        ) : (
+          <StepImage
+            style={{ margin: '20px auto' }}
+            src={image.src}
+            key={index}
+          />
+        ),
+      )
+      // else if only one image
+    } else if (step.images.length === 1) {
+      return <StepImage src={step.images[0].downloadUrl} />
+    } else {
+      return null
     }
-    return preloadedImages.map((image: any, index: number) =>
-      index !== 1 ? (
-        <StepImage src={image.src} key={index} />
-      ) : (
-        <StepImage
-          style={{ margin: '20px auto' }}
-          src={image.src}
-          key={index}
-        />
-      ),
-    )
-  }
-
-  public renderUniqueImage(url: string) {
-    return <StepImage src={url} />
   }
 
   public render() {
@@ -58,9 +64,7 @@ export default class Step extends React.PureComponent<any, any> {
               {step.title}
             </StepTitle>
             <StepDescription component="p">{step.text}</StepDescription>
-            {step.images.length > 1
-              ? this.renderMultipleImages(step)
-              : this.renderUniqueImage(step.images[0].downloadUrl)}
+            {this.renderImages(step)}
           </CardContent>
         </StepCard>
       </Container>

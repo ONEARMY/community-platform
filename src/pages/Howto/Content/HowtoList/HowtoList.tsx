e0c58254a10c61a4d10e3d8cda5c20e0a88cb1d2
Charleen Fei <fei.charleen@gmail.com>
2019-02-03 23:02:51 +0100

    render tags if any

--- src/pages/Howto/Content/HowtoList/HowtoList.tsx
+++ src/pages/Howto/Content/HowtoList/HowtoList.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { ClampLines } from '../../../../components/ClampLines/ClampLines'
 import CardActions from '@material-ui/core/CardActions'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
@@ -12,19 +11,16 @@ import LinearProgress from '@material-ui/core/LinearProgress'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import CommentIcon from '@material-ui/icons/Comment'
 import TurnedInIcon from '@material-ui/icons/TurnedIn'
-import { theme } from '../../../../themes/app.theme'
 import { IHowto } from 'src/models/models'
 
 import { CreateButton } from './elements'
-
-import { Button } from 'src/components/Button'
+import { TagDisplay } from 'src/pages/common/Tags';
 
 const styles: any = {
   cards: {
-    borderRadius: '0px',
     height: '100%',
     display: 'flex',
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
   layout: {
     width: 'auto',
@@ -34,10 +30,10 @@ const styles: any = {
     color: 'black',
   },
   cardMedia: {
-    paddingTop: '60%', // 16:9
+    paddingTop: '70%',
   },
-  cardContent: {
-  },
+  cardContent: {},
+  labelContent: {}
 }
 
 interface IProps {
@@ -48,20 +44,27 @@ export class HowtoList extends React.Component<IProps, any> {
     super(props)
   }
 
+  renderCardActions() {
+    return (
+      <CardActions>
+      <Typography>username</Typography>
+        <IconButton>
+          <TurnedInIcon />
+        </IconButton>
+        <IconButton>
+          <CommentIcon />
+        </IconButton>
+        <IconButton>
+          <MoreVertIcon />
+        </IconButton>
+      </CardActions>
+    )
+  }
+
   public render() {
     const { allHowtos } = this.props
     return (
       <div>
-        <Typography
-          style={{ margin: '30px auto', display: 'table' }}
-          variant="h4"
-          component="h4"
-        >
-          How-To
-        </Typography>
-        <Link to={'/how-to/create'}>
-          <CreateButton icon={'add'}>create how-to</CreateButton>
-        </Link>
         <React.Fragment>
           <div style={styles.layout}>
             {allHowtos.length === 0 ? (
@@ -76,6 +79,7 @@ export class HowtoList extends React.Component<IProps, any> {
                     >
                       <Card
                         style={styles.cards}
+                        raised={false}
                       >
                         <CardMedia
                           style={styles.cardMedia}
@@ -87,22 +91,19 @@ export class HowtoList extends React.Component<IProps, any> {
                           title="Image title"
                         />
                         <CardContent style={styles.cardContent}>
-                          <Typography variant='h6'>
+                          <Typography 
+                            variant='h6'>
                             {howto.tutorial_title}
                           </Typography>
                         </CardContent>
-                        <CardActions>
-                          <Typography>username</Typography>
-                            <IconButton>
-                              <TurnedInIcon />
-                            </IconButton>
-                            <IconButton>
-                              <CommentIcon />
-                            </IconButton>
-                            <IconButton>
-                              <MoreVertIcon />
-                            </IconButton>
-                        </CardActions>
+                        <CardContent style={styles.labelContent}>
+                        <div>
+                          {Object.keys(howto.tags).map(k => (
+                            <TagDisplay tagKey={k} key={k} />
+                          ))}
+                        </div>
+                        </CardContent>
+                        {this.renderCardActions()}
                       </Card>
                     </Link>
                   </Grid>

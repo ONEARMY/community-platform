215bba49dfdc3a0a9a9a01977eb37104a2ea7070
Daria Chernobaieva <daria@douleutaras.com>
2019-02-10 21:48:18 +0200

    #232. Fixed JWT token issue.

---
+++ src/utils/gAPIJwtAccessToken.ts
@@ -0,0 +1,13 @@
+import * as config from '../config/config.json'
+import { TokenCache } from 'google-oauth-jwt'
+
+export const getAccessToken = async (accessScopes: string[], callback) => {
+  const tokens = new TokenCache()
+  await tokens.get({
+    email: config.client_email,
+    key: config.private_key,
+    scopes: accessScopes
+  }, (err, token) => {
+    return callback(token)
+  })
+}

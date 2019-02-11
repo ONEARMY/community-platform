0cb35c493fbc9f395a226b0d8d8ab9ff954078c9
suvya <suvyamanchale@gmail.com>
2019-02-10 19:37:08 -0600

    Add formlink to display the selected link description on page
    Add labelText to localstorage to persist the state
    
    Add onClick event to links to pass the clicked item
    
    Add handleOnClick function to set labelText state

--- src/pages/common/Header/CommunityHeader/CommunityHeader.tsx
+++ src/pages/common/Header/CommunityHeader/CommunityHeader.tsx
@@ -11,6 +11,7 @@ import IconButton from '@material-ui/core/IconButton'
 import MailOutlinedIcon from '@material-ui/icons/MailOutlined'
 import { MdNotifications } from 'react-icons/md'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import FormLabel from '@material-ui/core/FormLabel'
 import {
   Content,
   LogoText,
@@ -33,7 +34,8 @@ interface IProps {
 }
 interface IState {
   moreMenuAnchor: any
-  profileMenuAnchor: any
+  profileMenuAnchor: any,
+  labelText: string
 }
 
 @observer
@@ -43,6 +45,7 @@ export class CommunityHeader extends React.Component<IProps, IState> {
     this.state = {
       moreMenuAnchor: null,
       profileMenuAnchor: null,
+      labelText: this.getInitialState()
     }
   }
 
@@ -53,6 +56,15 @@ export class CommunityHeader extends React.Component<IProps, IState> {
       moreMenuAnchor: e.currentTarget,
     })
   }
+
+  getInitialState = () => {
+    let labelText  = localStorage.getItem('labelText');
+    if(labelText === null){
+      labelText=''
+    }
+    return labelText;
+  }
+
   openProfileMenu = (e: React.MouseEvent) => {
     this.setState({
       profileMenuAnchor: e.currentTarget,
@@ -81,91 +93,105 @@ export class CommunityHeader extends React.Component<IProps, IState> {
     this.closeProfileMenu()
   }
 
-  render() {
-    const { moreMenuAnchor, profileMenuAnchor } = this.state
-    const user = this.props.userStore.user
-    return (
-      <Content>
-        <LogoText>One Army</LogoText>
-        <Logo src="https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png.png" />
-        <Links>
-          {COMMUNITY_PAGES.map(page => (
-            <LinkButton
-              className="nav-link"
-              to={page.path}
-              activeClassName="link-active"
-              key={page.path}
-            >
-              {page.title}
-            </LinkButton>
-          ))}
-          <ListButton
-            className="nav-link"
-            variant="text"
-            onClick={this.openMoreMenu}
-          >
-            More
-          </ListButton>
-          <Menu
-            open={moreMenuAnchor ? true : false}
-            anchorEl={moreMenuAnchor}
-            className="nav__more-menu"
-            style={{ marginTop: '3em' }}
-          >
-            <ClickAwayListener onClickAway={this.closeMoreMenu}>
-              <div>
-                {COMMUNITY_PAGES_MORE.map(page => (
-                  <MenuItem onClick={this.closeMoreMenu} key={page.path}>
-                    <LinkButton
-                      className="nav-link"
-                      to={page.path}
-                      activeClassName="link-active"
-                    >
-                      {page.title}
-                    </LinkButton>
-                  </MenuItem>
-                ))}
-              </div>
-            </ClickAwayListener>
-          </Menu>
-        </Links>
-        <div>
-          <IconButton component="span">
-            <MailOutlinedIcon />
-          </IconButton>
-          <IconButton component="span">
-            <MdNotifications />
-          </IconButton>
-        </div>
-        <div>
-          {user ? this.getProfile(user) : <LoginComponent user={user} />}
-        </div>
+  handleClick = (page) => {
+    localStorage.setItem('labelText', page.title);
+    this.setState({labelText: page.title})
+  };
 
-        <Menu
-          open={profileMenuAnchor ? true : false}
-          anchorEl={profileMenuAnchor}
-          className="nav__more-menu"
-          style={{ marginTop: '3em' }}
-        >
-          <ClickAwayListener onClickAway={this.closeProfileMenu}>
-            <div>
-              {COMMUNITY_PAGES_PROFILE.map(page => (
-                <MenuItem onClick={this.closeProfileMenu} key={page.path}>
-                  <LinkButton
-                    className="nav-link"
-                    to={page.path}
-                    activeClassName={'link-active'}
-                  >
-                    {page.title}
-                  </LinkButton>
-                </MenuItem>
+
+  render() {
+    const { moreMenuAnchor, profileMenuAnchor, labelText} = this.state
+    return (
+      <div>
+          <Content>
+            <LogoText>One Army</LogoText>
+            <Logo src="https://pngimage.net/wp-content/uploads/2018/06/logo-placeholder-png.png" />
+            <Links>
+              {COMMUNITY_PAGES.map(page => (
+                <LinkButton
+                  className="nav-link"
+                  to={page.path}
+                  activeClassName="link-active"
+                  key={page.path}
+                  onClick={this.handleClick.bind(this, page)}
+                >
+                  {page.title}
+                </LinkButton>
               ))}
-              <MenuItem onClick={this.logout}>Logout</MenuItem>
-              <MenuItem onClick={this.closeProfileMenu}>Main Site</MenuItem>
+              <ListButton
+                className="nav-link"
+                variant="text"
+                onClick={this.openMoreMenu}
+              >
+                More
+              </ListButton>
+              <Menu
+                open={moreMenuAnchor ? true : false}
+                anchorEl={moreMenuAnchor}
+                className="nav__more-menu"
+                style={{ marginTop: '3em' }}
+              >
+                <ClickAwayListener onClickAway={this.closeMoreMenu}>
+                  <div>
+                    {COMMUNITY_PAGES_MORE.map(page => (
+                      <MenuItem onClick={this.closeMoreMenu} key={page.path}>
+                        <LinkButton
+                          className="nav-link"
+                          to={page.path}
+                          activeClassName="link-active"
+                          onClick={this.handleClick.bind(this, page)}
+                        >
+                          {page.title}
+                        </LinkButton>
+                      </MenuItem>
+                    ))}
+                  </div>
+                </ClickAwayListener>
+              </Menu>
+            </Links>
+            <div>
+              <IconButton component="span">
+                <MailOutlinedIcon />
+              </IconButton>
+              <IconButton component="span">
+                <MdNotifications />
+              </IconButton>
             </div>
-          </ClickAwayListener>
-        </Menu>
-      </Content>
+            <Profile onClick={this.openProfileMenu}>
+              <Avatar
+                alt="Remy Sharp"
+                src="http://i.pravatar.cc/200"
+                className="header__avatar"
+              />
+              <KeyboardArrowDownIcon />
+            </Profile>
+            <Menu
+              open={profileMenuAnchor ? true : false}
+              anchorEl={profileMenuAnchor}
+              className="nav__more-menu"
+              style={{ marginTop: '3em' }}
+            >
+              <ClickAwayListener onClickAway={this.closeProfileMenu}>
+                <div>
+                  {COMMUNITY_PAGES_PROFILE.map(page => (
+                    <MenuItem onClick={this.closeProfileMenu} key={page.path}>
+                      <LinkButton
+                        className="nav-link"
+                        to={page.path}
+                        activeClassName={'link-active'}
+                      >
+                        {page.title}
+                      </LinkButton>
+                    </MenuItem>
+                  ))}
+                  <MenuItem onClick={this.closeProfileMenu}>Logout</MenuItem>
+                  <MenuItem onClick={this.closeProfileMenu}>Main Site</MenuItem>
+                </div>
+              </ClickAwayListener>
+            </Menu>
+          </Content>
+          <FormLabel style={{ backgroundColor: '#2d5786', display: 'block', color: 'white'}}>{labelText}</FormLabel>
+      </div>
     )
   }
 }

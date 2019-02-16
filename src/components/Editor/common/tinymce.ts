6d8421ed0c35cd8b8c8f608625610de9e85cbb74
xamiro <baumgart.guenter@gmail.com>
2019-02-16 11:20:47 +0100

    Added TinyMCe script loader

--- src/components/Editor/common/tinymce.ts
+++ src/components/Editor/common/tinymce.ts
@@ -1,3 +1,6 @@
+import { TinyMCEUrl } from '../config'
+
+declare var tinymce
 export interface IImageData {
   src: string
   alt: string
@@ -29,3 +32,23 @@ export const toImageData = (src: string, extra = {}): IImageData => {
     ...extra,
   }
 }
+const addScript = (relativeUrl: string) => {
+  const script = document.createElement('script')
+  script.src = relativeUrl
+  script.async = true
+
+  const promise = new Promise((resolve, reject) => {
+    script.addEventListener('load', () => setTimeout(resolve, 0))
+    script.addEventListener('error', e => reject(e))
+  }).catch(e => {
+    throw e
+  })
+  document.body.appendChild(script)
+
+  return promise
+}
+
+export const load = async () =>
+  isLoaded() ? Promise.resolve() : addScript(TinyMCEUrl())
+
+export const isLoaded = () => typeof tinymce !== 'undefined'

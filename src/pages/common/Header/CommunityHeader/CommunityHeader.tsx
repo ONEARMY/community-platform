5b3bcf7e55cefab537248b1c416c3760bf4ba32a
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-02-19 15:02:45 +0100

    change FormLabel into a SectionDescription element for clarity

--- src/pages/common/Header/CommunityHeader/CommunityHeader.tsx
+++ src/pages/common/Header/CommunityHeader/CommunityHeader.tsx
@@ -11,7 +11,6 @@ import IconButton from '@material-ui/core/IconButton'
 import MailOutlinedIcon from '@material-ui/icons/MailOutlined'
 import { MdNotifications } from 'react-icons/md'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
-import FormLabel from '@material-ui/core/FormLabel'
 import {
   Content,
   LogoText,
@@ -21,6 +20,7 @@ import {
   ListButton,
   LinkButton,
   Avatar,
+  SectionDescription,
 } from './elements'
 import { LoginComponent } from '../../Login/Login'
 import { UserStore } from 'src/stores/User/user.store'
@@ -176,16 +176,10 @@ export class CommunityHeader extends React.Component<IProps, IState> {
             </ClickAwayListener>
           </Menu>
         </Content>
-        <FormLabel
-          style={{
-            backgroundColor: '#2d5786',
-            display: 'block',
-            color: 'white',
-          }}
-        >
+        <SectionDescription>
           {this.props.title}
           {this.props.description}
-        </FormLabel>
+        </SectionDescription>
       </div>
     )
   }

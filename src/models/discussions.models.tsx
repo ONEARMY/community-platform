6e7521f36cba07b68cd4a59fb41455f921d676c9
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-02-20 20:47:09 +0100

    update discussion mock and models

--- src/models/discussions.models.tsx
+++ src/models/discussions.models.tsx
@@ -10,8 +10,9 @@ export interface IDiscussionQuestion {
   // user id only saved for created by
   createdBy: string
   tags: string[]
-  postTitle: string
-  postType: 'discussion'
+  title: string
+  content: string
+  type: 'discussionQuestion'
 }
 
 export interface IDiscussionComment {
@@ -19,7 +20,12 @@ export interface IDiscussionComment {
   _discussionID: string
   _created: Date
   _modified: Date
+  // replies will be built recursively from repliesTo field
   replies: IDiscussionComment[]
+  // repliesTo references a specific comment ID marked as a reply to
+  // if not a reply to another comment by default will be the discussionID
+  repliesTo: string
   // comment including images saved as html
   comment: string
+  type: 'discussionComment'
 }

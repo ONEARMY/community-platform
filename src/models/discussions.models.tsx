f0e9fe4bd8d4335041310a452f0587838beeb4ce
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-02-20 11:43:11 +0100

    remove comments

--- src/models/discussions.models.tsx
+++ src/models/discussions.models.tsx
@@ -23,31 +23,3 @@ export interface IDiscussionComment {
   // comment including images saved as html
   comment: string
 }
-
-// // The discussion question class can be used to generate a new discussion object,
-// // and contains common methods for discussion objects
-// export class DiscussionQuestion {
-//   public value: IDiscussionQuestion
-//   constructor(values?: IDiscussionQuestion) {
-//     this._init(values)
-//   }
-//   // when initialising a question either load existing data or generate placeholder
-//   private _init(values?: IDiscussionQuestion) {
-//     this.value = values
-//       ? values
-//       : {
-//           _commentCount: 0,
-//           _created: new Date(),
-//           _id: 'WILL GENERATE',
-//           _last3Comments: [],
-//           _lastResponse: null,
-//           _modified: new Date(),
-//           _usefullCount: 0,
-//           _viewCount: 0,
-//           createdBy: '',
-//           postTitle: '',
-//           postType: 'discussion',
-//           tags: [],
-//         }
-//   }
-// }

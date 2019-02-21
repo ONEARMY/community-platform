76f030f66009cee0202b3943eaef129af4832de8
Sanjay <sanju.skm@gmail.com>
2019-02-21 04:07:10 -0500

    272: Added tooltip to dev-notice icon

--- src/components/Dev/DevNotice.tsx
+++ src/components/Dev/DevNotice.tsx
@@ -4,10 +4,12 @@ import { FaExclamationTriangle } from 'react-icons/fa'
 import { isDebug } from 'src/config/config'
 export const DevNotice = () =>
   isDebug ? (
-    <FaExclamationTriangle
-      color={colors.green}
-      style={{ position: 'absolute', right: 175, padding: '5px 5px' }}
-    />
+    <div title="This is the dev site and all datas posted here can be public">
+      <FaExclamationTriangle
+        color={colors.green}
+        style={{ position: 'absolute', right: 175, padding: '5px 5px' }}
+      />
+    </div>
   ) : (
     <div />
   )

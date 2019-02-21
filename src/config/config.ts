5852c0d78fb82dea2f55d0df792fca8891a1b406
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-02-21 19:13:06 +0000

    update sentry config

--- src/config/config.ts
+++ src/config/config.ts
@@ -11,12 +11,7 @@ details via gitignore. You can find more information about potential security ri
 https://javebratt.com/hide-firebase-api/
 *****************************************************************************************/
 
-let firebaseConfig
-let analyticsConfig: {
-  viewId: string
-  trackingCode: string
-}
-firebaseConfig = {
+let firebaseConfig = {
   apiKey: 'AIzaSyChVNSMiYxCkbGd9C95aChr9GxRJtW6NRA',
   authDomain: 'precious-plastics-v4-dev.firebaseapp.com',
   databaseURL: 'https://precious-plastics-v4-dev.firebaseio.com',
@@ -24,9 +19,12 @@ firebaseConfig = {
   projectId: 'precious-plastics-v4-dev',
   storageBucket: 'precious-plastics-v4-dev.appspot.com',
 }
-analyticsConfig = {
+let analyticsConfig = {
   trackingCode: 'UA-133973230-2',
   viewId: '189456245',
+}
+let sentryConfig = {
+  dsn: 'https://8c1f7eb4892e48b18956af087bdfa3ac@sentry.io/1399729',
 }
 
 // different production site config pulled from environment variable
@@ -46,18 +44,22 @@ if (productionSites.indexOf(window.location.host) > -1) {
   siteVariant = 'production'
   const e = process.env
   firebaseConfig = {
-    apiKey: e.REACT_APP_FIREBASE_API_KEY,
-    authDomain: e.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: e.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: e.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: e.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: e.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    apiKey: e.REACT_APP_FIREBASE_API_KEY as string,
+    authDomain: e.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
+    databaseURL: e.REACT_APP_FIREBASE_DATABASE_URL as string,
+    projectId: e.REACT_APP_FIREBASE_PROJECT_ID as string,
+    storageBucket: e.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
+    messagingSenderId: e.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
   }
   analyticsConfig = {
     trackingCode: e.REACT_APP_GA_TRACKING_CODE as string,
     viewId: e.REACT_APP_GA_VIEW_ID as string,
   }
+  sentryConfig = {
+    dsn: e.REACT_APP_SENTRY_DSN as string,
+  }
 }
 export const SITE = siteVariant
 export const FIREBASE_CONFIG = firebaseConfig
 export const GOOGLE_ANALYTICS_CONFIG = analyticsConfig
+export const SENTRY_CONFIG = sentryConfig

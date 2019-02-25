adf106db976e82feb980e43b73bc09d3c8a8b50e
Charly <charly@centrifuge.io>
2019-02-25 21:21:15 +0100

    functional component rendering

--- src/pages/Howto/Content/HowtoList/HowtoList.tsx
+++ src/pages/Howto/Content/HowtoList/HowtoList.tsx
@@ -1,67 +1,101 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { ClampLines } from '../../../../components/ClampLines/ClampLines'
 import CardActions from '@material-ui/core/CardActions'
 import CardMedia from '@material-ui/core/CardMedia'
 import CardContent from '@material-ui/core/CardContent'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
-import IconButton from '@material-ui/core/IconButton'
 import LinearProgress from '@material-ui/core/LinearProgress'
-import MoreVertIcon from '@material-ui/icons/MoreVert'
-import CommentIcon from '@material-ui/icons/Comment'
-import TurnedInIcon from '@material-ui/icons/TurnedIn'
-import { theme } from '../../../../themes/app.theme'
-
+import { UsefulIcon, SaveIcon, TickIcon } from 'src/assets/icons/icon_components'
 import { CreateButton } from './elements'
-
-import { Button } from 'src/components/Button'
 import { IHowto } from 'src/models/howto.models'
+import { TagDisplay } from 'src/pages/common/Tags/TagDisplay/TagDisplay'
 
 const styles: any = {
+  cards: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    boxShadow: 'none'
+  },
   layout: {
     width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    padding: `${theme.spacing.unit * 4}px 0`,
-    // [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-    //   width: 1100,
-    //   marginLeft: 'auto',
-    //   marginRight: 'auto',
-    // },
   },
   link: {
     textDecoration: 'none',
     color: 'black',
   },
   cardMedia: {
-    paddingTop: '56.25%', // 16:9
+    paddingTop: '70%',
   },
-  cardContent: {
-    flexGrow: 1,
-  },
+  cardContent: {},
+  labelContent: {}
 }
 
 interface IProps {
   allHowtos: IHowto[]
 }
+
 export class HowtoList extends React.Component<IProps, any> {
   constructor(props: any) {
     super(props)
+  }
+  
+  renderCardMedia(howto) {
+    return (
+    <CardMedia
+      style={styles.cardMedia}
+      image={
+        howto.cover_image
+        ? howto.cover_image.downloadUrl
+        : howto.cover_image_url
+      } // eslint-disable-line max-len
+      title="Image title"
+    /> 
+    )
+  }
+
+  renderCardContent(howto) {
+    return (
+    <CardContent style={styles.cardContent}>
+      <Typography 
+        variant='h6'>
+        {howto.tutorial_title}
+      </Typography>
+    </CardContent>
+    ) 
+  }
+
+  renderLabelContent(howto) {
+    return (
+      <CardContent style={styles.labelContent}>
+      {/* placeholder for tags */}
+      <div>
+        {Object.keys(howto.tags).map(k => (
+          <TagDisplay tagKey={k} key={k} />
+        ))}
+      </div>
+      </CardContent>
+    )
+  }
+
+  renderCardActions(howto) {
+    // use passed in howto for user info, other relevant props
+    return (
+      <CardActions>
+      <Typography>username and coverimage</Typography>
+        <UsefulIcon></UsefulIcon>
+        <SaveIcon></SaveIcon>
+        <TickIcon></TickIcon>
+      </CardActions>
+    )
   }
 
   public render() {
     const { allHowtos } = this.props
     return (
       <div>
-        <Typography
-          style={{ margin: '30px auto', display: 'table' }}
-          variant="h4"
-          component="h4"
-        >
-          How-To
-        </Typography>
         <Link to={'/how-to/create'}>
           <CreateButton icon={'add'}>create how-to</CreateButton>
         </Link>
@@ -70,7 +104,7 @@ export class HowtoList extends React.Component<IProps, any> {
             {allHowtos.length === 0 ? (
               <LinearProgress />
             ) : (
-              <Grid container spacing={40}>
+              <Grid container spacing={16}>
                 {allHowtos.map((howto: IHowto, index: number) => (
                   <Grid item key={index} xs={4}>
                     <Link
@@ -78,60 +112,16 @@ export class HowtoList extends React.Component<IProps, any> {
                       style={styles.link}
                     >
                       <Card
-                        style={{
-                          borderRadius: '0px',
-                          height: '100%',
-                          display: 'flex',
-                          flexDirection: 'column',
-                        }}
+                        style={ styles.cards }
+                        raised={ false }
+                        square={ true }
                       >
-                        <CardMedia
-                          style={styles.cardMedia}
-                          image={
-                            howto.cover_image
-                              ? howto.cover_image.downloadUrl
-                              : howto.cover_image_url
-                          } // eslint-disable-line max-len
-                          title="Image title"
-                        />
-                        <CardContent style={styles.cardContent}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {howto.tutorial_title}
-                          </Typography>
-                          <div>
-                            <ClampLines
-                              text={howto.tutorial_description}
-                              lines={4}
-                              ellipsis="..."
-                              className="custom-class"
-                            />
-                          </div>
-                          <Typography>by {howto.workspace_name}</Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Typography>PRECIOUS PLASTIC</Typography>
-                          <div
-                            style={{
-                              marginLeft: 'auto',
-                            }}
-                          >
-                            <IconButton>
-                              <Typography style={{ marginRight: '5px' }}>
-                                {Math.trunc(Math.random() * (60 - 4) + 4) + ' '}
-                              </Typography>
-                              <TurnedInIcon />
-                            </IconButton>
-                            <IconButton>
-                              <Typography style={{ marginRight: '5px' }}>
-                                {Math.trunc(Math.random() * (60 - 4) + 4) + ' '}
-                              </Typography>
-                              <CommentIcon />
-                            </IconButton>
-                            <IconButton>
-                              <MoreVertIcon />
-                            </IconButton>
-                          </div>
-                        </CardActions>
+
+                        { this.renderCardMedia(howto) }
+                        { this.renderCardContent(howto) }
+                        { this.renderLabelContent(howto) }
+                        { this.renderCardActions(howto) }
+                    
                       </Card>
                     </Link>
                   </Grid>
@@ -139,11 +129,6 @@ export class HowtoList extends React.Component<IProps, any> {
               </Grid>
             )}
           </div>
-          {allHowtos.length > 15 ? (
-            <Link to={'/how-to/create'}>
-              <CreateButton icon={'add'}>create how-to</CreateButton>
-            </Link>
-          ) : null}
         </React.Fragment>
       </div>
     )

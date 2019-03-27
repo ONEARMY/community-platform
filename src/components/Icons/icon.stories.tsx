a0acb6bf531f92d9012189a1b60d2aebde036ae6
rudolphfunk <jmc.wakefield+1@gmail.com>
2019-03-27 20:51:57 +0100

    Revert: Prettier formatting

--- src/components/Icons/icon.stories.tsx
+++ src/components/Icons/icon.stories.tsx
@@ -5,24 +5,18 @@ import { withKnobs, select } from '@storybook/addon-knobs'
 
 import Icon, { glyphs } from './'
 
-import { wInfo } from '../../../.storybook/wInfo'
+import { wInfo } from '../../../.storybook/wInfo';
 
-const stories = storiesOf('UI/Icons', module)
-stories.addDecorator(withKnobs)
+const stories = storiesOf('UI/Icons', module);
+stories.addDecorator(withKnobs);
 
 const sizes = {
-  XSmall: 8,
-  Small: 16,
-  Medium: 32,
-  Large: 48,
-  XLarge: 64,
+    XSmall: 8,
+    Small: 16,
+    Medium: 32,
+    Large: 48,
+    XLarge: 64
 }
 
-Object.keys(glyphs).map(glyph =>
-  stories.add(
-    glyph,
-    wInfo()(() => (
-      <Icon glyph={glyph} size={select('Size', sizes, 32, 'iconSizes')} />
-    )),
-  ),
-)
+Object.keys(glyphs).map(glyph => (
+    stories.add(glyph, wInfo()(() => <Icon glyph={glyph} size={select('Size', sizes, 32, 'iconSizes')} />))));

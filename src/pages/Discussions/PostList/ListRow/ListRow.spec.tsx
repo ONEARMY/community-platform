dc7d1dd1d2148158028a5276bf5696c3c0ac4525
rudolphfunk <jmc.wakefield+1@gmail.com>
2019-03-29 20:55:40 +0100

    Updating Unit test to calculate the expected value vs hard coding it otherwise it will keep failing

--- src/pages/Discussions/PostList/ListRow/ListRow.spec.tsx
+++ src/pages/Discussions/PostList/ListRow/ListRow.spec.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react'
 
-import subDays from 'date-fns/sub_days'
 import ListRow from '.'
 import { PostDate } from './elements'
 import { shallow } from 'enzyme'
 import { DISCUSSION_QUESTION_MOCKS } from 'src/mocks/discussions.mock'
+import differenceInDays from 'date-fns/difference_in_days'
+
 
 describe('ListRow', () => {
   it('Should calculate duration since posted', () => {
+    // arrange
     const sutPost = DISCUSSION_QUESTION_MOCKS[0]
     const wrapper = shallow(<ListRow post={sutPost} />)
-
-    const daysSince = wrapper.find(PostDate).text()
-
-    expect(daysSince).toEqual('3 days')
+    const expected = `${differenceInDays(new Date(), sutPost._created.toDate())} days`;
+    // act
+    const result = wrapper.find(PostDate).text()
+    // assert
+    expect(result).toEqual(expected)
   })
 })

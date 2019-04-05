f3d69a631ff478606106e5777e4b6a93be3effa9
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-04-05 12:53:19 +0200

    define text & heading size props

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { Text as RebassText, TextProps as RebassTextProps } from 'rebass'
+import { colors } from 'src/themes/styled.theme'
 
 import theme from 'src/themes/styled.theme'
 
@@ -8,6 +9,10 @@ export interface ITextProps {
   caps?: boolean
   regular?: boolean
   bold?: boolean
+  large?: boolean
+  medium?: boolean
+  small?: boolean
+  superSmall?: boolean
 }
 
 export const caps = props =>
@@ -23,10 +28,26 @@ export const regular = props =>
 export const bold = props =>
   props.bold ? { fontWeight: props.theme.bold } : null
 
+export const large = props =>
+  props.large ? { fontSize: props.theme.fontSizes[3] } : null
+
+export const medium = props =>
+  props.medium ? { fontSize: props.theme.fontSizes[2] } : null
+
+export const small = props =>
+  props.small ? { fontSize: props.theme.fontSizes[1] } : null
+
+export const superSmall = props =>
+  props.small ? { fontSize: props.theme.fontSizes[0] } : null
+
 export const BaseText = styled(RebassText)`
     ${caps}
     ${regular}
     ${bold}
+    ${large}
+    ${medium}
+    ${small}
+    ${superSmall}
 `
 
 type TextProps = ITextProps & RebassTextProps
@@ -37,4 +58,8 @@ export const Text = (props: TextProps) => (
 
 Text.defaultProps = {
   theme,
+  color: colors.black,
+  className: 'text',
 }
+
+export default Text

313ff739e91e18a87f686bb54cd852faef8d6e00
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-04-17 00:00:06 +0200

    avoid scroll to top in case you navigate between location.hash in a page

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,16 +1,20 @@
-import * as React from "react";
-import { withRouter } from "react-router-dom";
+import * as React from 'react'
+import { withRouter } from 'react-router-dom'
 
 class ScrollToTop extends React.Component<any, any> {
   public componentDidUpdate(prevProps: any) {
-    if (this.props.location !== prevProps.location) {
-      window.scrollTo(0, 0);
+    // Check the location hash in case you are navigating in the page using hash ancor
+    if (
+      this.props.location !== prevProps.location &&
+      this.props.location.hash === ''
+    ) {
+      window.scrollTo(0, 0)
     }
   }
 
   public render() {
-    return this.props.children;
+    return this.props.children
   }
 }
 
-export default withRouter(ScrollToTop);
+export default withRouter(ScrollToTop)

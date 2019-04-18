1cde974e56fbc5832e7594948ee97167da802aaf
CHRIS-DESKTOP\Chris <chris.m.clarke@live.co.uk>
2019-04-18 14:33:17 +0100

    function scripts

--- scripts/deploy.dev.sh
+++ scripts/deploy.dev.sh
@@ -1,9 +1,13 @@
+# install dependencies
+npm install ./functions
 # set project (default = dev)
 firebase use default
 # assign variables that can be accessed from firebase config method for backend functions
 # NOTE - GA_SERVICE_DEV is json encrypted as base64 string (using https://www.browserling.com/tools/json-to-base64)
 firebase functions:config:set service.json="$SERVICE_ACCOUNT_JSON_DEV"
 firebase functions:config:set analytics.json="$REACT_APP_ANALYTICS_JSON_DEV"
+# ensure functions admin scripts correct
+cp functions/src/Firebase/admin.ci.ts functions/src/Firebase/admin.ts
 # deploy site, functions, rules etc.
 #echo $(firebase functions:config:get)
 firebase deploy

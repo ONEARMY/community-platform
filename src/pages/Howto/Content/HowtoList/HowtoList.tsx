b610a5172169e8f5490b43ba620d4a6ab6df1820
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-04-23 19:43:50 +0200

    remove all material-UI components from how-to list and start building our own grid & Cards

--- src/pages/Howto/Content/HowtoList/HowtoList.tsx
+++ src/pages/Howto/Content/HowtoList/HowtoList.tsx
@@ -1,43 +1,16 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { ClampLines } from '../../../../components/ClampLines/ClampLines'
-import CardActions from '@material-ui/core/CardActions'
-import CardMedia from '@material-ui/core/CardMedia'
-import CardContent from '@material-ui/core/CardContent'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card'
-import IconButton from '@material-ui/core/IconButton'
+import { Card, Image, Box } from 'rebass'
+import { Flex as FlexGrid } from '@rebass/grid'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Icon from 'src/components/Icons'
-import { theme } from '../../../../themes/app.theme'
+import { theme } from 'src/themes/app.theme'
+import Text from 'src/components/Text'
+import Heading from 'src/components/Heading'
 
 import { Button } from 'src/components/Button'
 import { IHowto } from 'src/models/howto.models'
-
-const styles: any = {
-  layout: {
-    width: 'auto',
-    marginLeft: theme.spacing.unit * 3,
-    marginRight: theme.spacing.unit * 3,
-    padding: `${theme.spacing.unit * 4}px 0`,
-    // [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-    //   width: 1100,
-    //   marginLeft: 'auto',
-    //   marginRight: 'auto',
-    // },
-  },
-  link: {
-    textDecoration: 'none',
-    color: 'black',
-  },
-  cardMedia: {
-    paddingTop: '56.25%', // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-}
+import { maxContainerWidth } from 'src/themes/styled.theme'
 
 interface IProps {
   allHowtos: IHowto[]
@@ -52,82 +25,40 @@ export class HowtoList extends React.Component<IProps, any> {
     return (
       <div>
         <Link to={'/how-to/create'}>
-          <Button variant="outline" mx={'auto'} my={50} icon={'add'}>
+          <Button variant="outline" mx={'auto'} my={3} icon={'add'}>
             create
           </Button>
         </Link>
         <React.Fragment>
-          <div style={styles.layout}>
+          <div>
             {allHowtos.length === 0 ? (
               <LinearProgress />
             ) : (
-              <Grid container spacing={40}>
+              <FlexGrid
+                flexWrap={'wrap'}
+                maxWidth={maxContainerWidth}
+                m={'0 auto'}
+              >
                 {allHowtos.map((howto: IHowto, index: number) => (
-                  <Grid item key={index} xs={4}>
-                    <Link
-                      to={`/how-to/${encodeURIComponent(howto.slug)}`}
-                      style={styles.link}
-                    >
-                      <Card
-                        style={{
-                          borderRadius: '0px',
-                          height: '100%',
-                          display: 'flex',
-                          flexDirection: 'column',
-                        }}
-                      >
-                        <CardMedia
-                          style={styles.cardMedia}
-                          image={
-                            howto.cover_image
-                              ? howto.cover_image.downloadUrl
-                              : howto.cover_image_url
-                          } // eslint-disable-line max-len
-                          title="Image title"
-                        />
-                        <CardContent style={styles.cardContent}>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {howto.tutorial_title}
-                          </Typography>
-                          <div>
-                            <ClampLines
-                              text={howto.tutorial_description}
-                              lines={4}
-                              ellipsis="..."
-                              className="custom-class"
-                            />
-                          </div>
-                          <Typography>by {howto.workspace_name}</Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Typography>PRECIOUS PLASTIC</Typography>
-                          <div
-                            style={{
-                              marginLeft: 'auto',
-                            }}
-                          >
-                            <IconButton>
-                              <Typography style={{ marginRight: '5px' }}>
-                                {Math.trunc(Math.random() * (60 - 4) + 4) + ' '}
-                              </Typography>
-                              <Icon glyph={'turned-in'} />
-                            </IconButton>
-                            <IconButton>
-                              <Typography style={{ marginRight: '5px' }}>
-                                {Math.trunc(Math.random() * (60 - 4) + 4) + ' '}
-                              </Typography>
-                              <Icon glyph={'comment'} />
-                            </IconButton>
-                            <IconButton>
-                              <Icon glyph={'more-vert'} />
-                            </IconButton>
-                          </div>
-                        </CardActions>
-                      </Card>
-                    </Link>
-                  </Grid>
+                  <Box m={2}>
+                    <Card borderRadius={1} width={380} bg={'white'}>
+                      <Image
+                        src={
+                          howto.cover_image
+                            ? howto.cover_image.downloadUrl
+                            : howto.cover_image_url
+                        }
+                      />
+                      <Box px={2}>
+                        <Link to={`/how-to/${encodeURIComponent(howto.slug)}`}>
+                          <Heading medium>{howto.tutorial_title}</Heading>
+                        </Link>
+                        <Text small>by {howto.workspace_name}</Text>
+                      </Box>
+                    </Card>
+                  </Box>
                 ))}
-              </Grid>
+              </FlexGrid>
             )}
           </div>
           {allHowtos.length > 15 ? (

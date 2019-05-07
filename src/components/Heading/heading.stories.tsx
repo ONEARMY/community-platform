3c214b87b585e68361e951c463dec284890dd273
tanvir.raj <rajbp02@gmail.com>
2019-05-08 00:09:36 +0600

    Add stories for Heading component

---
+++ src/components/Heading/heading.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
+import { wInfo } from '../../../.storybook/wInfo'

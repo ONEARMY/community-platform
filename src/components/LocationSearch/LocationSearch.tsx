fb4e351e46099c62e92d6660fc9faaee72ef537c
Chris\Laptop <chris.m.clarke@live.co.uk>
2019-05-21 13:12:30 +0100

    add typings, add multiple bindings for dual inputs

--- src/components/LocationSearch/LocationSearch.tsx
+++ src/components/LocationSearch/LocationSearch.tsx
@@ -1,5 +1,7 @@
 /*
-  Component wrapper for Algolia places search with additional input debounce
+  Component wrapper for Algolia places search with additional input debounce.
+  Uses 2 input components, the first to take raw user input, and debounce before
+  updating the second which is bound to algolia's search autocomplete implementation
 */
 import React from 'react'
 import AlgoliaPlaces from 'places.js'
@@ -12,6 +14,7 @@ interface IProps {
 }
 interface IState {
   debouncedInputValue: string
+  rawInputValue: string
 }
 
 export class LocationSearch extends React.Component<IProps, IState> {
@@ -22,7 +25,7 @@ export class LocationSearch extends React.Component<IProps, IState> {
   places: any
   constructor(props: IProps) {
     super(props)
-    this.state = { debouncedInputValue: '' }
+    this.state = { debouncedInputValue: '', rawInputValue: '' }
   }
 
   componentDidMount() {
@@ -32,31 +35,46 @@ export class LocationSearch extends React.Component<IProps, IState> {
       apiKey: ALGOLIA_PLACES_CONFIG.searchOnlyAPIKey,
       container: this.inputContainerRef.current,
     })
+    // add custom handler when place selected from list
+    this.places.on('change', selected =>
+      this.handlePlaceSelectChange(selected as IAlgoliaResponse),
+    )
   }
 
+  // debounce user input to update algolia search input
   handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     this.setState({
       debouncedInputValue: e.target.value,
+      rawInputValue: e.target.value,
     })
-    // manually trigger the onchange method so places picks up the change
-    const event = new Event('change', { bubbles: true })
+    // manually trigger the input method so places picks up the change
+    const event = new Event('input', { bubbles: true })
     this.inputContainerRef.current.dispatchEvent(event)
+  }
+  // this time we need to pass back changes from the algolia dropdown to the initial input box
+  handlePlaceSelectChange(selected: IAlgoliaResponse) {
+    this.setState({
+      rawInputValue: selected.suggestion.value,
+      debouncedInputValue: selected.suggestion.value,
+    })
   }
   render() {
     return (
       <>
+        {/* the first input uses custom component with debounced onChange function */}
         <Input
           placeholder={this.props.placeholder}
           onChange={v => this.handleInputChange(v)}
+          value={this.state.rawInputValue}
+          style={{ marginBottom: 0 }}
         />
+        {/* the second input takes debounced value from the first input and binds to algolia search  */}
         <input
           type="search"
           id="address-input"
           value={this.state.debouncedInputValue}
           ref={this.inputContainerRef}
-          onChange={e => {
-            console.log('change', e)
-          }}
+          style={{ display: 'none' }}
         />
       </>
     )
@@ -64,4 +82,44 @@ export class LocationSearch extends React.Component<IProps, IState> {
 }
 LocationSearch.defaultProps = {
   placeholder: 'Search for a location',
+}
+
+/****************************************************
+ *            Interfaces
+ *****************************************************/
+// algolia doesn't provide typings so taken from
+// https://community.algolia.com/places/documentation.html
+// implementation contains more fields but assumed not relevant
+
+interface IAlgoliaResponse {
+  query: string
+  rawAnswer: any
+  suggestion: IAlgoliaSuggestion
+  suggestionIndex: number
+}
+interface IAlgoliaSuggestion {
+  type:
+    | 'country'
+    | 'city'
+    | 'address'
+    | 'busStop'
+    | 'trainStation'
+    | 'townhall'
+    | 'airport'
+  name: string
+  city: string
+  country: string
+  countryCode: string
+  administrative: string
+  county: string
+  suburb: string
+  latlng: ILatLng
+  postcode: string
+  postcodes: string[]
+  value: string
+  higlight: Partial<IAlgoliaSuggestion>
+}
+interface ILatLng {
+  lat: number
+  lng: number
 }

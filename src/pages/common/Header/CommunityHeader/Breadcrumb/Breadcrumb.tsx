bbacd9b11ef3d8265a197aa2f0f247d94b52ae35
Marcin Kopec <marcin.kopec@socialwifi.com>
2019-07-01 16:47:35 +0200

    Enable moving to higher level in breadcrumb

--- src/pages/common/Header/CommunityHeader/Breadcrumb/Breadcrumb.tsx
+++ src/pages/common/Header/CommunityHeader/Breadcrumb/Breadcrumb.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'src/components/Links'
 import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
 import HowtoBreadcrumb from './HowtoBreadcrumb'
 import DiscussionsBreadcrumb from './DiscussionsBreadcrumb'
@@ -17,7 +18,13 @@ const Breadcrumbs = ({ breadcrumbs }) => (
     {breadcrumbs.map(({ match, breadcrumb }, index) => (
       <span key={match.url}>
         {index > 0 ? ' > ' : null}
-        {breadcrumb}
+        {index < breadcrumbs.length - 1 ? (
+          <Link to={match.url} color={'black'}>
+            {breadcrumb}
+          </Link>
+        ) : (
+          <> {breadcrumb} </>
+        )}
       </span>
     ))}
   </div>

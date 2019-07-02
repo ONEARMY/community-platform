c8d883ea3ac5ae6a34f79bb37a4aa1d98f8a617a
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-07-02 11:19:40 +0200

    remove breadcrumb for Discussion section and add it for Events section

--- src/pages/common/Header/CommunityHeader/Breadcrumb/Breadcrumb.tsx
+++ src/pages/common/Header/CommunityHeader/Breadcrumb/Breadcrumb.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { Link } from 'src/components/Links'
 import withBreadcrumbs from 'react-router-breadcrumbs-hoc'
 import HowtoBreadcrumb from './HowtoBreadcrumb'
-import DiscussionsBreadcrumb from './DiscussionsBreadcrumb'
 
 const routes = [
   { path: '/how-to', breadcrumb: 'How-to' },
   { path: '/how-to/create', breadcrumb: 'Create' },
   { path: '/how-to/:slug', breadcrumb: HowtoBreadcrumb },
-  { path: '/discussions', breadcrumb: 'Discussions' },
-  { path: '/discussions/create', breadcrumb: 'Create' },
-  { path: '/discussions/:slug', breadcrumb: DiscussionsBreadcrumb },
+  { path: '/events', breadcrumb: 'Events' },
+  { path: '/events/create', breadcrumb: 'Create' },
 ]
 
 const Breadcrumbs = ({ breadcrumbs }) => (

a8c99b30c3e962076390257d25362fc14d788a84
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-08-28 12:36:41 +0200

    remove howtoStore from howto router

--- src/pages/Howto/Howto.tsx
+++ src/pages/Howto/Howto.tsx
@@ -1,31 +1,16 @@
 import * as React from 'react'
-import { inject, observer } from 'mobx-react'
-
-import { HowtoStore } from 'src/stores/Howto/howto.store'
 import { Route, Switch, withRouter } from 'react-router-dom'
 import { Howto } from './Content/Howto/Howto'
 import { CreateHowto } from './Content/CreateHowto/CreateHowto'
 import { HowtoList } from './Content/HowtoList/HowtoList'
 import { AuthRoute } from '../common/AuthRoute'
 
-interface IProps {
-  howtoStore?: HowtoStore
-}
-
-// We're connecting to the 'docStore' state object and will pass down through child compoennts
-// First we use the @inject decorator to bind to the docStore state
-@inject('howtoStore')
-// Then we can use the observer component decorator to automatically tracks observables and re-renders on change
-@observer
-class HowtoPageClass extends React.Component<IProps, any> {
-  constructor(props: IProps) {
+class HowtoPageClass extends React.Component<any, any> {
+  constructor(props: any) {
     super(props)
   }
 
   public render() {
-    // mobx will automatically re-render when it sees updates to the following properties
-    // (note 1, use ! to tell typescript that the store will exist (it's an injected prop))
-    // (note 2, mobx seems to behave more consistently when observables are referenced outside of render methods)
     return (
       <div>
         <Switch>

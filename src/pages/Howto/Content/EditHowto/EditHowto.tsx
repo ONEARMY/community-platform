81ba71eb6023d3eb7e5f71b3de7c915f922da860
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-09-05 18:43:17 +0200

    inject existing data in edit form TODO : inject tags and images

--- src/pages/Howto/Content/EditHowto/EditHowto.tsx
+++ src/pages/Howto/Content/EditHowto/EditHowto.tsx
@@ -1,16 +1,17 @@
 import * as React from 'react'
 import { RouteComponentProps } from 'react-router'
 import { IHowtoFormInput } from 'src/models/howto.models'
-import TEMPLATE from './Template'
-import { UploadedFile } from 'src/pages/common/UploadedFile/UploadedFile'
 import { HowtoStore } from 'src/stores/Howto/howto.store'
 import { inject } from 'mobx-react'
+import { toJS } from 'mobx'
 import { HowtoForm } from 'src/pages/Howto/Content/Common/Howto.form'
+import { Flex } from 'rebass'
+import Heading from 'src/components/Heading'
 
 interface IState {
   formValues: IHowtoFormInput
   formSaved: boolean
-  _docID: string
+  isLoading: boolean
   _toDocsList: boolean
   showSubmitModal?: boolean
 }
@@ -21,16 +22,30 @@ interface IInjectedProps extends IProps {
 
 @inject('howtoStore')
 export class EditHowto extends React.Component<IProps, IState> {
-  uploadRefs: { [key: string]: UploadedFile | null } = {}
   constructor(props: any) {
     super(props)
-    // generate unique id for db and storage references and assign to state
-    const docID = this.store.generateID()
     this.state = {
-      formValues: { ...TEMPLATE.INITIAL_VALUES, id: docID } as IHowtoFormInput,
+      formValues: {} as IHowtoFormInput,
       formSaved: false,
-      _docID: docID,
       _toDocsList: false,
+      isLoading: true,
+    }
+  }
+  public async componentWillMount() {
+    if (this.injected.howtoStore.activeHowto! !== undefined) {
+      this.setState({
+        formValues: toJS(
+          this.injected.howtoStore.activeHowto,
+        ) as IHowtoFormInput,
+        isLoading: false,
+      })
+    } else {
+      const slug = this.props.match.params.slug
+      const doc = await this.injected.howtoStore.getDocBySlug(slug)
+      this.setState({
+        formValues: doc as IHowtoFormInput,
+        isLoading: false,
+      })
     }
   }
 
@@ -43,13 +58,32 @@ export class EditHowto extends React.Component<IProps, IState> {
 
   public onSubmit = async (formValues: IHowtoFormInput) => {
     this.setState({ showSubmitModal: true })
-    await this.store.uploadHowTo(formValues, this.state._docID)
+    console.log('onSubmit edit howto', formValues)
+
+    // await this.store.editHowTo(formValues, this.state._docID)
   }
 
   public render() {
-    const { formValues } = this.state
-    return (
-      <HowtoForm formValues={formValues} parentType="edit" {...this.props} />
-    )
+    const { formValues, isLoading } = this.state
+    if (formValues) {
+      return (
+        <HowtoForm
+          onSubmit={v => this.onSubmit(v)}
+          formValues={formValues}
+          parentType="edit"
+          {...this.props}
+        />
+      )
+    } else {
+      return isLoading ? (
+        <Flex>
+          <Heading auxiliary txtcenter width={1}>
+            loading...
+          </Heading>
+        </Flex>
+      ) : (
+        <div>How-to not found</div>
+      )
+    }
   }
 }

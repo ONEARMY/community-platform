d3b3d8416dc91116e4d13e022ef66610ef986aa7
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-09-13 16:35:46 +0200

    remove fieldContainer on fileInputField

--- src/components/Form/FileInput.field.tsx
+++ src/components/Form/FileInput.field.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import { IFieldProps } from './Fields'
 import { FileInput } from '../FileInput/FileInput'
-import { FieldContainer } from './elements'
 
 export const FileInputField = ({ input, meta, ...rest }: IFieldProps) => (
-  <FieldContainer>
-    <FileInput {...rest} onFilesChange={files => input.onChange(files)} />
-  </FieldContainer>
+  <FileInput {...rest} onFilesChange={files => input.onChange(files)} />
 )

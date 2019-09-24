aa8329649339448e83e8ee876ce779189a270e12
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-09-24 15:15:17 +0200

    upgrade rebass to v4 & remove last tinymce dependencies (part of #561)

--- src/components/Editor/common/strings.ts
+++
@@ -1,7 +0,0 @@
-// @TODO: i8n
-export enum TEXT {
-  INSERT = 'Insert Image',
-  UPLOAD_BUTTON = 'Upload image',
-  UPLOAD_OK = 'Save',
-  UPLOAD_CANCEL = 'Cancel',
-}
--- src/components/Editor/plugins/index.ts
+++
@@ -1,2 +0,0 @@
-import { init as initImage } from './image/Plugin'
-export const init = () => initImage()

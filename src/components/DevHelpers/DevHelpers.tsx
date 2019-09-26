3cf1f906bbad6429a134b32894d632a733f09bc2
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-09-26 12:34:23 +0200

    change link color in DevHelper

--- src/components/DevHelpers/DevHelpers.tsx
+++ src/components/DevHelpers/DevHelpers.tsx
@@ -4,13 +4,18 @@ import DevTools from 'mobx-react-devtools'
 import { SITE, VERSION } from 'src/config/config'
 import Text from 'src/components/Text'
 import { Link } from 'rebass/styled-components'
+import theme from 'src/themes/styled.theme'
 
 const DevHelpers = () => (
   <>
     {SITE !== 'production' && <DevTools />}
     <Text bg={'black'} color={'white'} width={1} py={2} medium txtcenter>
       This is the alpha version (v{VERSION}) of onearmy platform,{' '}
-      <Link target="_blank" href="https://build.onearmy.world">
+      <Link
+        color={theme.colors.blue}
+        target="_blank"
+        href="https://build.onearmy.world"
+      >
         click here
       </Link>{' '}
       to help building it. /!\ This site is for development purposes, do not

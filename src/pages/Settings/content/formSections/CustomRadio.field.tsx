dd7ccc26ab3e51af93953c33fa093f479ddd94b9
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-09-30 16:25:37 +0200

    create CustomRadioField component and use it to build focus

---
+++ src/pages/Settings/content/formSections/CustomRadio.field.tsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { InputField } from 'src/components/Form/Fields'
+import { Label, HiddenInput } from './elements'
+import { Image } from 'rebass'
+import Text from 'src/components/Text'
+import { ProfileTypeLabel } from 'src/models/user_pp.models'
+
+interface IProps {
+  value: string
+  index: number
+  onChange: (value: ProfileTypeLabel) => void
+  isSelected: boolean
+  imageSrc?: string
+  btnLabel?: string
+  textLabel?: string
+  name: string
+}
+interface IState {
+  showDeleteModal: boolean
+}
+
+// validation - return undefined if no error (i.e. valid)
+const required = (value: any) => (value ? undefined : 'Required')
+
+class CustomRadioField extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+    this.state = {
+      showDeleteModal: false,
+    }
+  }
+
+  render() {
+    const {
+      value,
+      imageSrc,
+      isSelected,
+      btnLabel,
+      textLabel,
+      name,
+    } = this.props
+    return (
+      <Label htmlFor={value} className={isSelected ? 'selected' : undefined}>
+        <HiddenInput
+          id={value}
+          name={name}
+          value={value}
+          type="radio"
+          component={InputField}
+          checked={isSelected}
+          onChange={v => {
+            this.props.onChange(v.target.value)
+          }}
+        />
+        {imageSrc && <Image px={3} src={imageSrc} />}
+        {btnLabel && (
+          <Text medium m="10px">
+            {btnLabel}
+          </Text>
+        )}
+        {textLabel && (
+          <Text px={1} my={1} txtcenter small>
+            {textLabel}
+          </Text>
+        )}
+      </Label>
+    )
+  }
+}
+
+export { CustomRadioField }
--- src/pages/Settings/content/formSections/Focus.section.tsx
+++ src/pages/Settings/content/formSections/Focus.section.tsx
@@ -8,17 +8,12 @@ import { HiddenInput, Label, FlexSectionContainer } from './elements'
 import { InputField } from 'src/components/Form/Fields'
 import { Link } from 'src/components/Links'
 import { Button } from 'src/components/Button'
-import { ProfileType } from 'src/models/user_pp.models'
-
-// assets
-import CollectionBadge from 'src/assets/images/badges/pt-collection-point.jpg'
-import MemberBadge from 'src/assets/images/badges/pt-member.jpg'
-import MachineBadge from 'src/assets/images/badges/pt-machine-shop.jpg'
-import WorkspaceBadge from 'src/assets/images/badges/pt-workspace.jpg'
-import LocalComBadge from 'src/assets/images/badges/pt-local-community.jpg'
+import { ProfileTypeLabel } from 'src/models/user_pp.models'
+import { PROFILE_TYPES } from 'src/mocks/user_pp.mock'
+import { CustomRadioField } from './CustomRadio.field'
 
 interface IProps {
-  onInputChange: (inputValue: ProfileType) => void
+  onInputChange: (inputValue: ProfileTypeLabel) => void
 }
 interface IState {
   checkedFocusValue?: string
@@ -30,7 +25,9 @@ export class FocusSection extends React.Component<IProps, IState> {
     this.state = {}
   }
 
-  public onInputChange(value: ProfileType) {
+  public onInputChange(value: ProfileTypeLabel) {
+    console.log('value', value)
+
     this.setState({ checkedFocusValue: value })
     this.props.onInputChange(value)
   }
@@ -44,121 +41,18 @@ export class FocusSection extends React.Component<IProps, IState> {
             What is your main Precious Plastic activity?
           </Text>
           <Flex wrap="nowrap">
-            <Label
-              htmlFor="workspace"
-              className={
-                this.state.checkedFocusValue === 'workspace'
-                  ? 'selected'
-                  : undefined
-              }
-            >
-              <HiddenInput
-                id="workspace"
-                value="workspace"
+            {PROFILE_TYPES.map((profile, index: number) => (
+              <CustomRadioField
+                key={index}
+                value={profile.label}
                 name="profileType"
-                type="radio"
-                validateFields={[]}
-                component={InputField}
-                checked={this.state.checkedFocusValue === 'workspace'}
-                onChange={v => this.onInputChange(v.target.value)}
+                index={index}
+                isSelected={this.state.checkedFocusValue === profile.label}
+                onChange={v => this.onInputChange(v)}
+                imageSrc={profile.imageSrc}
+                textLabel={profile.textLabel}
               />
-              <Image px={3} src={WorkspaceBadge} />
-              <Text px={1} my={1} txtcenter small>
-                I run a workspace
-              </Text>
-            </Label>
-            <Label
-              htmlFor="community-builder"
-              className={
-                this.state.checkedFocusValue === 'community-builder'
-                  ? 'selected'
-                  : undefined
-              }
-            >
-              <HiddenInput
-                id="community-builder"
-                value="community-builder"
-                name="profileType"
-                type="radio"
-                validateFields={[]}
-                component={InputField}
-                checked={this.state.checkedFocusValue === 'community-builder'}
-                onChange={v => this.onInputChange(v.target.value)}
-              />
-              <Image px={3} src={LocalComBadge} />
-              <Text px={1} my={1} txtcenter small>
-                I run a local community
-              </Text>
-            </Label>
-            <Label
-              htmlFor="collection-point"
-              className={
-                this.state.checkedFocusValue === 'collection-point'
-                  ? 'selected'
-                  : undefined
-              }
-            >
-              <HiddenInput
-                id="collection-point"
-                value="collection-point"
-                name="profileType"
-                type="radio"
-                validateFields={[]}
-                component={InputField}
-                checked={this.state.checkedFocusValue === 'collection-point'}
-                onChange={v => this.onInputChange(v.target.value)}
-              />
-              <Image px={3} src={CollectionBadge} />
-              <Text px={1} my={1} txtcenter small>
-                I collect & sort plastic
-              </Text>
-            </Label>
-            <Label
-              htmlFor="machine-builder"
-              className={
-                this.state.checkedFocusValue === 'machine-builder'
-                  ? 'selected'
-                  : undefined
-              }
-            >
-              <HiddenInput
-                id="machine-builder"
-                value="machine-builder"
-                name="profileType"
-                type="radio"
-                validateFields={[]}
-                component={InputField}
-                checked={this.state.checkedFocusValue === 'machine-builder'}
-                onChange={v => this.onInputChange(v.target.value)}
-              />
-              <Image px={3} src={MachineBadge} />
-              <Text px={1} my={1} txtcenter small>
-                I build machines
-              </Text>
-            </Label>
-            <Label
-              htmlFor="member"
-              className={
-                this.state.checkedFocusValue === 'member'
-                  ? 'selected'
-                  : undefined
-              }
-            >
-              <HiddenInput
-                id="member"
-                value="member"
-                name="profileType"
-                type="radio"
-                validateFields={[]}
-                component={InputField}
-                checked={this.state.checkedFocusValue === 'member'}
-                onChange={v => this.onInputChange(v.target.value)}
-              />
-              <Image px={3} src={MemberBadge} />
-              <Text px={1} my={1} txtcenter small>
-                I am a member
-              </Text>
-            </Label>
+            ))}
           </Flex>
           <Flex flexWrap="wrap" alignItems="center" mt={4}>
             <Text>Not sure about your focus ?</Text>

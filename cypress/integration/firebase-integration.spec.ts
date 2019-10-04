a5bfef070d2c8a37198c8e47d00a2c7965865d85
Hung Tran <tmhung88@outlook.com>
2019-10-04 15:35:04 +0700

    Fixed the cypress complain that caused the test hang

--- cypress/integration/firebase-integration.spec.ts
+++ cypress/integration/firebase-integration.spec.ts
@@ -11,26 +11,28 @@ describe('FireBase + Cypress', () => {
     cy.get('[data-cy=create]').should('not.exist')
   })
 
-  it('should add/update/get/delete from FireStore', async () => {
-    const firestore = await cy.firestore()
-    const addRef = await firestore
-      .collection('testCollection')
-      .add({ name: 'Tony Stark', age: 45 })
-    console.log('Object added with ref ' + addRef.id)
-    await firestore
-      .collection('testCollection')
-      .doc(addRef.id)
-      .update({ name: 'Iron man', age: 40 })
-    console.log('Object updated ')
-    const snapshot = await firestore.collection('testCollection').get()
-    snapshot.forEach(doc => {
-      console.log(`ID ${doc.id} => `, doc.data())
+  it('should add/update/get/delete from FireStore', () => {
+    cy.firestore().then(async firestore => {
+      const addRef = await firestore
+        .collection('testCollection')
+        .add({ name: 'Tony Stark', age: 45 })
+      console.log('Object added with ref ' + addRef.id)
+      firestore
+        .collection('testCollection')
+        .doc(addRef.id)
+        .update({ name: 'Iron man', age: 40 })
+      console.log('Object updated ')
+      const snapshot = await firestore.collection('testCollection').get()
+      snapshot.forEach(doc => {
+        console.log(`ID ${doc.id} => `, doc.data())
+      })
+      await firestore
+        .collection('testCollection')
+        .doc(addRef.id)
+        .delete()
+      console.log(`Object ${addRef.id} deleted`)
     })
-    await firestore
-      .collection('testCollection')
-      .doc(addRef.id)
-      .delete()
-    console.log(`Object ${addRef.id} deleted`)
     cy.visit('/how-to')
+    cy.get('[data-cy=create]').should('not.exist')
   })
 })

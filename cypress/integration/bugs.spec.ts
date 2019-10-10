6faefbcac756ab79a86f5b3abe9ba99bc2806bd4
Hung Tran <tmhung88@outlook.com>
2019-10-10 10:23:36 +0700

    Added bug#636

---
+++ cypress/integration/bugs.spec.ts
@@ -0,0 +1,10 @@
+describe('[Bugs]', () => {
+  it('[636]', () => {
+    cy.visit('/how-to')
+    cy.log('Tags are shown')
+    cy.get('[data-cy=card]').contains('Create an extruded lamp').within(($card) => {
+      expect($card).to.contain('product')
+      expect($card).to.contain('extrusion')
+    })
+  })
+})

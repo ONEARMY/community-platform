f590178669d2954adab8792d6637e4246b012407
Hung Tran <tmhung88@outlook.com>
2019-10-10 13:18:26 +0700

    Added bug#640

--- cypress/integration/bugs.spec.ts
+++ cypress/integration/bugs.spec.ts
@@ -26,4 +26,14 @@ describe('[Bugs]', () => {
     cy.get('div').contains('loading...').should('not.exist')
   })
 
+  it.skip('[640]', () => {
+    cy.visit('/how-to')
+    cy.logout()
+    cy.login('howto_creator@test.com', 'test1234')
+    cy.log('Open the create-how-to page with its url')
+    cy.visit('/how-to/create')
+      .url().should('include', '/how-to/create')
+    cy.get('div').contains('How-to Guidelines').should('be.exist')
+  })
+
 })

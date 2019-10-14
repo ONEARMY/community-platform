acc3bdaf7169f687e057aaf84dfb9016862607e7
Hung Tran <tmhung88@outlook.com>
2019-10-14 17:47:12 +0700

    Switched to cy commands because they have built-in timeout

--- cypress/integration/bugs.spec.ts
+++ cypress/integration/bugs.spec.ts
@@ -1,16 +1,6 @@
 import { Page } from '../utils/test-utils'
 
 describe('[Bugs]', () => {
-  it.skip('[#636]', () => {
-    cy.visit('/how-to')
-    cy.step('Tags shown on how-to load')
-    cy.get('[data-cy=card]')
-      .contains('Create an extruded lamp')
-      .within($card => {
-        expect($card).to.contain('product')
-        expect($card).to.contain('extrusion')
-      })
-  })
 
   it.skip('[649]', () => {
     cy.step('Wrong email and wrong password')

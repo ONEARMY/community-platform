f01e8546d35f8745ef289753cd3b3023fe417ed8
Hung Tran <tmhung88@outlook.com>
2019-10-15 22:54:19 +0700

    Added bug#686

--- cypress/integration/bugs.spec.ts
+++ cypress/integration/bugs.spec.ts
@@ -2,6 +2,31 @@ import { Page } from '../utils/test-utils'
 
 describe('[Bugs]', () => {
 
+  it.skip('[686]', () => {
+    cy.deleteDocuments('v2_events', 'title', '==', 'Create a test event')
+    cy.visit('/events')
+
+    cy.login('event_creator@test.com', 'test1234')
+    cy.get('[data-cy=create]').click()
+
+    cy.step('Fill up mandatory info')
+    cy.get('[data-cy=title]').type('Create a test event')
+    cy.get('[data-cy=date]').type(Cypress.moment('2019-08-20').format('YYYY-MM-DD'))
+    cy.get('[data-cy=tag-select]').click()
+    cy.get('.data-cy__menu').contains('event_testing').click()
+
+    cy.get('[data-cy=location]').find('input:eq(0)').type('Atucucho')
+    cy.get('[data-cy=location]').find('div').contains('Atucucho').click()
+    cy.get('[data-cy=tag-select]').click()
+    cy.get('[data-cy=url]').type('https://www.meetup.com/pt-BR/cities/br/rio_de_janeiro/')
+
+    cy.step('Publish the event')
+    cy.get('[data-cy=submit]').click()
+
+    cy.step('The new event is shown in /events')
+    cy.get('[data-cy=card]').contains('Create a test event').should('be.exist')
+  })
+
   it.skip('[685]', () => {
     cy.visit('/how-to')
     cy.login('howto_reader@test.com', 'test1234')

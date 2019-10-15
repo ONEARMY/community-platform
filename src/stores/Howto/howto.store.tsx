97f9db582d5143993bcc35a3f278fef1a37e9cbc
Chris <chris.m.clarke@live.co.uk>
2019-10-15 14:29:46 +0100

    refactor status modal, add button status bindings

--- src/stores/Howto/howto.store.tsx
+++ src/stores/Howto/howto.store.tsx
@@ -59,6 +59,8 @@ export class HowtoStore extends ModuleStore {
 
   // upload a new or update an existing how-to
   public async uploadHowTo(values: IHowtoFormInput | IHowtoDB) {
+    console.log('uploading howto')
+    this.updateUploadStatus('Start')
     // create a reference either to the existing document (if editing) or a new document if creating
     const dbRef = this.db
       .collection<IHowto>(COLLECTION_NAME)
@@ -99,10 +101,10 @@ export class HowtoStore extends ModuleStore {
       // set the database document
       await dbRef.set(howTo)
       this.updateUploadStatus('Database')
-      // complete
-      this.updateUploadStatus('Complete')
       console.log('post added')
       this.activeHowto = await dbRef.get()
+      // complete
+      this.updateUploadStatus('Complete')
     } catch (error) {
       console.log('error', error)
       throw new Error(error.message)
@@ -128,6 +130,7 @@ export class HowtoStore extends ModuleStore {
   }
 }
 interface IHowToUploadStatus {
+  Start: boolean
   Cover: boolean
   Files: boolean
   'Step Images': boolean
@@ -137,6 +140,7 @@ interface IHowToUploadStatus {
 
 function getInitialUploadStatus() {
   const status: IHowToUploadStatus = {
+    Start: false,
     Cover: false,
     'Step Images': false,
     Files: false,

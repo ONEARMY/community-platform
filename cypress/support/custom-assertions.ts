fbdc3e7809abc6cfcdac391ba9f75bae1821b777
Hung Tran <tmhung88@outlook.com>
2019-10-19 16:34:39 +0700

    Added a custom chai assertion to verify a How-to

---
+++ cypress/support/custom-assertions.ts
@@ -0,0 +1,39 @@
+import { IHowto, IHowtoStep } from '../../src/models/howto.models'
+import chaiSubset from 'chai-subset'
+
+declare global {
+  namespace Chai {
+    // tslint:disable-next-line:interface-name
+    interface Assertion  {
+      containSubset(expect: any): any;
+      eqHowtoStep(expect: any, index: number)
+    }
+  }
+}
+
+const eqHowto =(chaiObj, utils) => {
+  function shallowCompare(this: any, expected: any) {
+    const subject: IHowto = this._obj
+    const { _createdBy, _deleted, caption, description, difficulty_level, slug, time, title , tags} = expected
+    expect(subject, 'Basic info').to.containSubset({ _createdBy, _deleted, caption, description, difficulty_level, slug, time, title , tags})
+    expect(subject.cover_image, 'Cover images').to.containSubset(expected.cover_image)
+
+    expected.steps.forEach((step, index) => {
+      expect(subject.steps[index], `Have step ${index}`).to.eqHowtoStep(step, index)
+    })
+  }
+  chaiObj.Assertion.addMethod('eqHowto', shallowCompare)
+}
+const eqHowtoStep = (chaiObj, utils) => {
+  function shallowCompare(this: any, expected: any, index: number) {
+    const subject: IHowtoStep = this._obj
+    const {_animationKey, caption, text, title} = expected
+    expect(subject, `Step ${index} with info`).to.containSubset({_animationKey, caption, text, title})
+    expect(subject.images, `Step ${index} with images`).to.containSubset(expected.images)
+  }
+
+  chaiObj.Assertion.addMethod('eqHowtoStep', shallowCompare)
+}
+chai.use(eqHowto);
+chai.use(eqHowtoStep);
+chai.use(chaiSubset)

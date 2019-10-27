40a64093a81c49390e18b83cbd77ab731babfe51
Patricia Wanat <lauvrenn@gmail.com>
2019-10-27 14:39:17 +0100

    Update Profile.tsx

--- src/pages/common/Header/Menu/Profile/Profile.tsx
+++ src/pages/common/Header/Menu/Profile/Profile.tsx
@@ -67,7 +67,7 @@ export default class Profile extends React.Component<IProps, IState> {
                 path={'how-to'}
                 content={'Log out'}
                 style={{ color: theme.colors.silver }}
-                onClick={() => this.injected.userStore.logout()}
+                onClick={() => user.logout()}
               />
             </MenuMobileLinkContainer>
           ) : (

94e666d07b8a61404312fd5261225ca49459b38c
Caio Kinzel Filho <caiokf@gmail.com>
2019-11-03 01:46:46 -0700

    storybook mdx stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.(js)$/), module);
+configure(require.context('../src', true, /\.stories\.(js|jsx|mdx)$/), module);

12f809b1fd70864410fce2631abc9ee48e052248
Hung Tran <tmhung88@outlook.com>
2019-11-04 19:45:41 +0700

    Added 2 more same tests to verify if the server bootstrap affects the 1st test only

--- cypress/integration/academy.spec.ts
+++ cypress/integration/academy.spec.ts
@@ -1,12 +1,22 @@
 import { Page } from '../utils/test-utils'
 
 describe('[Academy]', () => {
-  describe('[List instructions]', () => {
-    it('[By Everyone]', () => {
-      cy.visit(Page.ACADEMY)
-      cy.step('Load instructions from another github repo')
-      const githubDoc = 'https://onearmy.github.io/academy/intro'
-      cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
-    })
+  it('1st', () => {
+    cy.visit(Page.ACADEMY)
+    cy.step('Load instructions from another github repo')
+    const githubDoc = 'https://onearmy.github.io/academy/intro'
+    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
+  })
+  it('2nd', () => {
+    cy.visit(Page.ACADEMY)
+    cy.step('Load instructions from another github repo')
+    const githubDoc = 'https://onearmy.github.io/academy/intro'
+    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
+  })
+  it('3rd', () => {
+    cy.visit(Page.ACADEMY)
+    cy.step('Load instructions from another github repo')
+    const githubDoc = 'https://onearmy.github.io/academy/intro'
+    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
   })
 })

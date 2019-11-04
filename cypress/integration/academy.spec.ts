ae2cca2bf4d809a09ae5dee9999ef3216987a4cf
Hung Tran <tmhung88@outlook.com>
2019-11-04 20:55:08 +0700

    As academy is the first suite, add 60s to ensure the app is ready before it's run

--- cypress/integration/academy.spec.ts
+++ cypress/integration/academy.spec.ts
@@ -1,22 +1,15 @@
 import { Page } from '../utils/test-utils'
 
 describe('[Academy]', () => {
-  it('1st', () => {
-    cy.visit(Page.ACADEMY)
-    cy.step('Load instructions from another github repo')
-    const githubDoc = 'https://onearmy.github.io/academy/intro'
-    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
+  it('Wait for the app to be ready', () => {
+    cy.wait(60000)
   })
-  it('2nd', () => {
-    cy.visit(Page.ACADEMY)
-    cy.step('Load instructions from another github repo')
-    const githubDoc = 'https://onearmy.github.io/academy/intro'
-    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
-  })
-  it('3rd', () => {
-    cy.visit(Page.ACADEMY)
-    cy.step('Load instructions from another github repo')
-    const githubDoc = 'https://onearmy.github.io/academy/intro'
-    cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
+  describe('[List instructions]', () => {
+    it('[By Everyone]', () => {
+      cy.visit(Page.ACADEMY)
+      cy.step('Load instructions from another github repo')
+      const githubDoc = 'https://onearmy.github.io/academy/intro'
+      cy.get('iframe').should('have.attr', 'src').and('equal', githubDoc)
+    })
   })
 })

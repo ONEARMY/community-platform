922f8f3b51b60fa5e45e5fd06d06b25986036ee3
Benjamin Gammaire <benjamin.gammaire@gmail.com>
2019-11-22 18:10:20 +0100

    use only profile badge in Avatar component - remove useless states - rename fn for clarity

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 import { Image, ImageProps } from 'rebass'
 import { inject, observer } from 'mobx-react'
-import { UserStore, getUserAvatar } from 'src/stores/User/user.store'
-import { ProfileTypeLabel, IProfileType } from 'src/models/user_pp.models'
+import { ProfileTypeLabel } from 'src/models/user_pp.models'
 import Workspace from 'src/pages/User/workspace/Workspace'
 
 interface IProps extends ImageProps {
@@ -12,14 +11,8 @@ interface IProps extends ImageProps {
   profileType?: ProfileTypeLabel
 }
 
-interface IInjected extends IProps {
-  userStore: UserStore
-}
-
 interface IState {
-  avatarUrl?: string
-  fallbackBadge?: string
-  showFallback?: boolean
+  badgeProfileType?: string
 }
 
 @inject('userStore')
@@ -29,35 +22,20 @@ export class Avatar extends React.Component<IProps, IState> {
     super(props)
     this.state = {}
   }
-  get injected() {
-    return this.props as IInjected
-  }
 
   // subscribe/unsubscribe from specific user profile message when
   // user updates their avatar (same url so by default will now be aware of change)
   componentDidMount() {
-    this.getAvatar(this.props.userName)
-    this.getFallbackImage(this.props.profileType)
+    this.getProfileTypeBadge(this.props.profileType)
   }
 
-  public getFallbackImage(type?: ProfileTypeLabel) {
+  public getProfileTypeBadge(type?: ProfileTypeLabel) {
     const img = Workspace.findWorkspaceBadge(type, true)
-    this.setState({ fallbackBadge: img })
+    this.setState({ badgeProfileType: img })
   }
-
-  async getAvatar(userName: string) {
-    const url = getUserAvatar(userName)
-    console.log('avatar', url)
-    this.setState({ avatarUrl: url })
-  }
-
   render() {
     const { width } = this.props
-    const { avatarUrl, fallbackBadge } = this.state
-
-    const addFallbackSrc = (ev: any) => {
-      ev.target.src = fallbackBadge
-    }
+    const { badgeProfileType } = this.state
 
     return (
       <>
@@ -66,8 +44,7 @@ export class Avatar extends React.Component<IProps, IState> {
           width={width ? width : 40}
           height={width ? width : 40}
           sx={{ borderRadius: '25px' }}
-          src={avatarUrl}
-          onError={addFallbackSrc}
+          src={badgeProfileType}
         />
       </>
     )

25ed3d426829f7c0654fc18fc5bebfbfa29445e2
Chris <chris.m.clarke@live.co.uk>
2020-01-05 19:06:38 +0000

    tidy upgrade function

--- functions/src/upgrade/dbv2Upgrade.ts
+++ functions/src/upgrade/dbv2Upgrade.ts
@@ -1,19 +1,13 @@
 import { db } from '../Firebase/firestoreDB'
 
-/*  Temporary function used to migrate from db V2 docs to V3
+/**
+ * Temporary function used to migrate from db V2 docs to V3
     Changes:
-    - Add moderation fields to how-tos, events, mappins (#847)
-
-*/
-
-const mappings: { [key in IDBEndpointV2]: IDBEndpointV3 } = {
-  v2_events: 'v3_events',
-  v2_howtos: 'v3_howtos',
-  v2_mappins: 'v3_mappins',
-  v2_tags: 'v3_tags',
-  v2_users: 'v3_users',
-}
-
+    - Add moderation fields to how-tos, events, mappins, users (#847)
+    - Format usernames to slug format
+    - Add username display fields
+    - Update howto and event _createdBy to reflect new users
+ */
 export const upgradeV2DB = async () => {
   const promises = Object.keys(mappings).map(async endpoint => {
     const upgradedDocs = await upgradeDBEndpoint(endpoint as IDBEndpointV2)
@@ -27,7 +21,11 @@ export const upgradeV2DB = async () => {
 const upgradeDBEndpoint = async (endpoint: IDBEndpointV2) => {
   console.log('upgrading endpoint', endpoint)
   const snap = await db.collection(endpoint).get()
-  const docs = snap.empty ? [] : snap.docs.map(d => d.data())
+  const docs = snap.empty
+    ? []
+    : snap.docs.map(d => {
+        return { ...(d.data() as any), _id: d.id }
+      })
   console.log(`|${endpoint}|: [${docs.length}] docs to upgrade`)
   if (docs.length > 500) {
     throw new Error('Too many docs for batch, chunking required')
@@ -79,15 +77,33 @@ function upgrade(doc: any, endpoint: IDBEndpointV2) {
   }
 }
 
-// convert to lower case and remove any special characters
-const formatLowerNoSpecial = (text: string = 'NA') => {
+/*****************************************************************************
+ *  Imported functions - note, direct import not working so copied from src
+ ******************************************************************************/
+// remove special characters from string, also replacing spaces with dashes
+const stripSpecialCharacters = (text: string) => {
   return text
     ? text
         .replace(/[`~!@#$%^&*()_|+\-=÷¿?;:'",.<>\{\}\[\]\\\/]/gi, '')
         .split(' ')
         .join('-')
-        .toLowerCase()
     : ''
+}
+
+// convert to lower case and remove any special characters
+const formatLowerNoSpecial = (text: string) => {
+  return stripSpecialCharacters(text).toLowerCase()
+}
+
+/*****************************************************************************
+ *  Constants & Types
+ ******************************************************************************/
+const mappings: { [key in IDBEndpointV2]: IDBEndpointV3 } = {
+  v2_events: 'v3_events',
+  v2_howtos: 'v3_howtos',
+  v2_mappins: 'v3_mappins',
+  v2_tags: 'v3_tags',
+  v2_users: 'v3_users',
 }
 
 type IDBEndpointV2 =

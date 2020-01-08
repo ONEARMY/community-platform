d3cae5fa030a73971d7865fa36094c66b173f649
Chris <chris.m.clarke@live.co.uk>
2020-01-08 16:43:51 +0000

    refactor webpack config

--- cypress/plugins/cy-ts-preprocessor.js
+++ cypress/plugins/cy-ts-preprocessor.js
@@ -2,18 +2,21 @@ const wp = require('@cypress/webpack-preprocessor')
 
 const options = {
   webpackOptions: {
+    mode: 'development',
+    // make sure the source maps work
+    devtool: 'eval-source-map',
     resolve: {
-      extensions: [".ts", ".tsx", ".js"]
+      extensions: ['.ts', '.tsx', '.js'],
     },
     module: {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: "ts-loader",
-          options: { transpileOnly: true }
-        }
-      ]
-    }
+          loader: 'ts-loader',
+          options: { transpileOnly: true },
+        },
+      ],
+    },
   },
 }
 

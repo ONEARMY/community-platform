086b21100eace5257edee37b0eabc67cde40584d
Chris <chris.m.clarke@live.co.uk>
2020-01-11 14:32:40 +0000

    WiP - troubleshoot discord functions

--- functions/src/Integrations/firebase-discord.ts
+++ functions/src/Integrations/firebase-discord.ts
@@ -1,6 +1,6 @@
 import { CONFIG } from '../config/config'
 import * as functions from 'firebase-functions'
-import * as request from 'request'
+import axios, { AxiosResponse, AxiosError } from 'axios'
 
 const SITE_URL = CONFIG.deployment.site_url
 // e.g. https://dev.onearmy.world or https://community.preciousplastic.com
@@ -9,89 +9,63 @@ const DISCORD_WEBHOOK_URL = CONFIG.integrations.discord_webhook
 
 export const notifyPinAccepted = functions.firestore
   .document('v3_mappins/{pinId}')
-  .onWrite((change, context) => {
+  .onWrite(async (change, context) => {
     const info = change.after.exists ? change.after.data() : null
+    console.log('info', info)
     if (info === null || info.moderation !== 'accepted') {
       return
     }
-
-    const id = info._id
-    const type = info.type
-    const loc = info.location
-    //  console.log(info);
-    request.post(
-      DISCORD_WEBHOOK_URL,
-      {
-        json: {
-          content: `📍 *New ${type}* pin from ${id}. \n Location here ${SITE_URL}/map/#${id}`,
-        },
-      },
-      (err, res) => {
-        if (err) {
-          console.error(err)
-          return
-        } else {
-          return res
-        }
-      },
-    )
+    const { id, type } = info
+    console.log('update', id, type, DISCORD_WEBHOOK_URL)
+    await axios
+      .post(DISCORD_WEBHOOK_URL, {
+        content: `📍 *New ${type}* pin from ${id}. \n Location here ${SITE_URL}/map/#${id}`,
+      })
+      .then(handleResponse)
+      .catch(handleErr)
   })
+
 export const notifyHowToAccepted = functions.firestore
   .document('v3_howtos/{id}')
-  .onWrite((change, context) => {
+  .onWrite(async (change, context) => {
     const info = change.after.exists ? change.after.data() : null
     if (info === null || info.moderation !== 'accepted') {
       return
     }
-
-    const user = info._createdBy
-    const title = info.title
-    const slug = info.slug
-    request.post(
-      DISCORD_WEBHOOK_URL,
-      {
-        json: {
-          content: `📓 Yeah! New How To "* ${title} *" by ${user}
+    const { _createdBy, title, slug } = info
+    await axios
+      .post(DISCORD_WEBHOOK_URL, {
+        content: `📓 Yeah! New How To "* ${title} *" by ${_createdBy}
             check it out: ${SITE_URL}/how-to/${slug}`,
-        },
-      },
-      (err, res) => {
-        if (err) {
-          console.error(err)
-          return
-        } else {
-          return res
-        }
-      },
-    )
+      })
+      .then(handleResponse)
+      .catch(handleErr)
   })
+
 export const notifyEventAccepted = functions.firestore
   .document('v3_events/{id}')
-  .onWrite((change, context) => {
+  .onWrite(async (change, context) => {
     const info = change.after.exists ? change.after.data() : null
     if (info === null || info.moderation !== 'accepted') {
       return
     }
-
     const user = info._createdBy
     const url = info.url
     const location = info.location.country
-    console.info(info)
-    request.post(
-      DISCORD_WEBHOOK_URL,
-      {
-        json: {
-          content: `📅 Jeej new event in *${location}* by ${user} posted here:
+    await axios
+      .post(DISCORD_WEBHOOK_URL, {
+        content: `📅 Jeej new event in *${location}* by ${user} posted here:
             ${url}`,
-        },
-      },
-      (err, res) => {
-        if (err) {
-          console.error(err)
-        } else {
-          console.log('post success')
-          return res
-        }
-      },
-    )
+      })
+      .then(handleResponse)
+      .catch(handleErr)
   })
+
+const handleResponse = (res: AxiosResponse) => {
+  console.log('post success')
+  return res
+}
+const handleErr = (err: AxiosError) => {
+  console.error('error')
+  throw err
+}

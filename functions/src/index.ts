cd00d7d7ebf3f9d9ca6a786d86ed086b1f13cd6f
Chris <chris.m.clarke@live.co.uk>
2020-02-25 14:18:21 +0000

    refactor user revision backend function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import { weeklyTasks, dailyTasks } from './exports/tasks'
 import { DH_Exports } from './DaveHakkensNL'
 import * as IntegrationsSlack from './Integrations/firebase-slack'
 import * as IntegrationsDiscord from './Integrations/firebase-discord'
-import { FirestoreUserRevisions } from './Firebase/userRevisions'
+import { FirebaseUserBackup } from './Integrations/firebase-userBackup'
 
 // the following endpoints are exposed for use by various triggers
 // see individual files for more informaiton
@@ -19,4 +19,4 @@ exports.notifyNewEvent = IntegrationsSlack.notifyNewEvent
 exports.notifyPinAccepted = IntegrationsDiscord.notifyPinAccepted
 exports.notifyHowToAccepted = IntegrationsDiscord.notifyHowToAccepted
 exports.notifyEventAccepted = IntegrationsDiscord.notifyEventAccepted
-exports.firestoreUserRevisions = FirestoreUserRevisions
+exports.firebaseUserBackup = FirebaseUserBackup

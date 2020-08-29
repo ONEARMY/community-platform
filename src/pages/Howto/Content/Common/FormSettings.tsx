1a90ccc80b67f4f092e39cf75ab7073f68276930
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2020-08-29 11:48:08 +0100

    feat: Adds more time options to how-to form

--- src/pages/Howto/Content/Common/FormSettings.tsx
+++ src/pages/Howto/Content/Common/FormSettings.tsx
@@ -1,5 +1,21 @@
 export const TIME_OPTIONS = [
   {
+    value: '< 1 hour',
+    label: '< 1 hour',
+  },
+  {
+    value: '< 5 hours',
+    label: '< 5 hours',
+  },
+  {
+    value: '< 10 hours',
+    label: '< 10 hours',
+  },
+  {
+    value: '< 1 day',
+    label: '< 1 day',
+  },
+  {
     value: '< 1 week',
     label: '< 1 week',
   },

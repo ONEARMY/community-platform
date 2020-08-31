ef0e6663e0815439f6d416f39a33b3feb31a70eb
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2020-08-31 11:05:04 +0100

    feat: Adds makePair to FormSettings.tsx following review

--- src/pages/Howto/Content/Common/FormSettings.tsx
+++ src/pages/Howto/Content/Common/FormSettings.tsx
@@ -1,53 +1,21 @@
+const makePair = (value: string, label?: string) => {
+  return { value, label: label || value }
+}
+
 export const TIME_OPTIONS = [
-  {
-    value: '< 1 hour',
-    label: '< 1 hour',
-  },
-  {
-    value: '< 5 hours',
-    label: '< 5 hours',
-  },
-  {
-    value: '< 10 hours',
-    label: '< 10 hours',
-  },
-  {
-    value: '< 1 day',
-    label: '< 1 day',
-  },
-  {
-    value: '< 1 week',
-    label: '< 1 week',
-  },
-  {
-    value: '1-2 weeks',
-    label: '1-2 weeks',
-  },
-  {
-    value: '3-4 weeks',
-    label: '3-4 weeks',
-  },
-  {
-    value: '1+ months',
-    label: '1+ months',
-  },
+  makePair('< 1 hour', '< 1 hour'),
+  makePair('< 5 hours', '< 5 hours'),
+  makePair('< 10 hours', '< 10 hours'),
+  makePair('< 1 day', '< 1 day'),
+  makePair('< 1 week', '< 1 week'),
+  makePair('1-2 weeks', '1-2 weeks'),
+  makePair('3-4 weeks', '3-4 weeks'),
+  makePair('1+ months', '1+ months'),
 ]
 
 export const DIFFICULTY_OPTIONS = [
-  {
-    value: 'Easy',
-    label: 'Easy',
-  },
-  {
-    value: 'Medium',
-    label: 'Medium',
-  },
-  {
-    value: 'Hard',
-    label: 'Hard',
-  },
-  {
-    value: 'Very Hard',
-    label: 'Very Hard',
-  },
+  makePair('Easy', 'Easy'),
+  makePair('Medium', 'Medium'),
+  makePair('Hard', 'Hard'),
+  makePair('Very Hard', 'Very Hard'),
 ]

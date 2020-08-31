b49c9b5bc6b42c80ed28a722d61d6823642f2d0c
Darigov Research <30328618+darigovresearch@users.noreply.github.com>
2020-08-31 18:22:36 +0100

    refactor: Adjusts makePair to makeEntry in FormSettings.tsx

--- src/pages/Howto/Content/Common/FormSettings.tsx
+++ src/pages/Howto/Content/Common/FormSettings.tsx
@@ -1,21 +1,21 @@
-const makePair = (value: string, label?: string) => {
+const makeEntry = (value: string, label?: string) => {
   return { value, label: label || value }
 }
 
 export const TIME_OPTIONS = [
-  makePair('< 1 hour', '< 1 hour'),
-  makePair('< 5 hours', '< 5 hours'),
-  makePair('< 10 hours', '< 10 hours'),
-  makePair('< 1 day', '< 1 day'),
-  makePair('< 1 week', '< 1 week'),
-  makePair('1-2 weeks', '1-2 weeks'),
-  makePair('3-4 weeks', '3-4 weeks'),
-  makePair('1+ months', '1+ months'),
+  makeEntry('< 1 hour'),
+  makeEntry('< 5 hours'),
+  makeEntry('< 10 hours'),
+  makeEntry('< 1 day'),
+  makeEntry('< 1 week'),
+  makeEntry('1-2 weeks'),
+  makeEntry('3-4 weeks'),
+  makeEntry('1+ months'),
 ]
 
 export const DIFFICULTY_OPTIONS = [
-  makePair('Easy', 'Easy'),
-  makePair('Medium', 'Medium'),
-  makePair('Hard', 'Hard'),
-  makePair('Very Hard', 'Very Hard'),
+  makeEntry('Easy'),
+  makeEntry('Medium'),
+  makeEntry('Hard'),
+  makeEntry('Very Hard'),
 ]

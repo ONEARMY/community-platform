d05aa98e405af6b60657fac0c0316ed53687ec55
chrismclarke <chris.m.clarke@live.co.uk>
2020-10-12 19:20:45 -0700

    fix test integrations for db refactor

--- cypress/support/db/endpoints.ts
+++ cypress/support/db/endpoints.ts
@@ -1,5 +1,4 @@
-// React apps populate a process variable, however it might not always be accessible outside
-// (e.g. cypress will instead use it's own env to populate a prefix)
+// React apps populate a process variable, however it might not always be accessible outside (e.g. cypress)
 const e = process ? process.env : ({} as any)
 /**
  * A prefix can be used to simplify large-scale schema changes or multisite hosting

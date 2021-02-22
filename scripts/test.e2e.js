b4d06ba650c1eea072596749809b3cd663bd2503
chrismclarke <chris.m.clarke@live.co.uk>
2021-02-22 15:34:18 -0800

    tidy ci scripts

--- scripts/test.e2e.js
+++ scripts/test.e2e.js
@@ -1,6 +1,6 @@
 const child = require('child_process')
 const e2eEnv = require('dotenv').config({ path: `${process.cwd()}/.env.e2e` })
-const fs = require('fs')
+const fs = require('fs-extra')
 const waitOn = require('wait-on')
 
 // Prevent unhandled errors being silently ignored
@@ -19,39 +19,33 @@ process.on('unhandledRejection', err => {
  * @example npm run test ci prod
  */
 async function main() {
-  const isCi = process.argv.includes('ci')
-  const useProductionBuild = process.argv.includes('prod')
-  const DB_PREFIX = `${randomString(5)}_`
-  // we want both cypress and the build to use some of the same build configuration, so define and share
-  const sharedEnv = `REACT_APP_DB_PREFIX=${DB_PREFIX} REACT_APP_SITE_VARIANT=test-ci`
   // copy endpoints for use in testing
   fs.copyFileSync(
     'src/stores/databaseV2/endpoints.ts',
     'cypress/support/db/endpoints.ts',
   )
 
-  await startAppServer(sharedEnv, useProductionBuild)
-  runTests(sharedEnv, isCi)
+  await startAppServer()
+  runTests()
 }
 main()
-// .then(() => console.log('complete'))
-// .catch(err => {
-//   console.error(err)
-// })
 
-function runTests(sharedEnv, isCi) {
+function runTests() {
+  const isCi = process.argv.includes('ci')
   console.log(isCi ? 'Start tests' : 'Opening cypress for manual testing')
   const e = process.env
-  const cyEnv = getCypressEnv(sharedEnv)
+  const { CYPRESS_KEY } = e2eEnv.parsed
   const CI_BROWSER = e.CI_BROWSER || 'chrome'
   const CI_GROUP = e.CI_GROUP || '1x-chrome'
+  // cypress env is also shared to the platform via window.cypress
+  const CYPRESS_ENV = `DB_PREFIX=${randomString(5)}_`
   // keep compatibility with both circleci and travisci builds - note, could pass as env variable instead
   const buildId = e.CIRCLE_WORKFLOW_ID || e.TRAVIS_BUILD_ID || randomString(8)
 
   // main testing command, depending on whether running on ci machine or interactive local
   const testCMD = isCi
-    ? `npx cypress run --record --env ${cyEnv.runtime} --key=${cyEnv.CYPRESS_KEY} --parallel --headless --browser ${CI_BROWSER} --group ${CI_GROUP} --ci-build-id ${buildId}`
-    : `npx cypress@6.4.0 open --browser chrome --env ${cyEnv.runtime}`
+    ? `npx cypress run --record --env ${CYPRESS_ENV} --key=${CYPRESS_KEY} --parallel --headless --browser ${CI_BROWSER} --group ${CI_GROUP} --ci-build-id ${buildId}`
+    : `npx cypress@6.4.0 open --browser chrome --env ${CYPRESS_ENV}`
 
   const spawn = child.spawnSync(`cross-env FORCE_COLOR=1 ${testCMD}`, {
     shell: true,
@@ -66,14 +60,13 @@ function runTests(sharedEnv, isCi) {
  * performance in some environments (https://github.com/bahmutov/start-server-and-test/issues/250).
  * Instead manually track via child spawns
  *
- * @param prod - When production specified a production build will be generated and served instead
- * of using development npm run start server
  */
-async function startAppServer(buildEnv, prod = false) {
+async function startAppServer() {
+  const useProductionBuild = process.argv.includes('prod')
   // by default spawns will not respect colours used in stdio, so try to force
-  const crossEnvArgs = `${buildEnv} FORCE_COLOR=1`
+  const crossEnvArgs = `FORCE_COLOR=1 REACT_APP_SITE_VARIANT=test-ci`
   let serverCmd = `cross-env ${crossEnvArgs} BROWSER=none PORT=3456 npm run start`
-  if (prod) {
+  if (useProductionBuild) {
     child.spawnSync(`cross-env ${crossEnvArgs} npm run build`, {
       shell: true,
       stdio: ['inherit', 'inherit', 'pipe'],
@@ -83,6 +76,7 @@ async function startAppServer(buildEnv, prod = false) {
     fs.writeFileSync('build/serve.json', JSON.stringify(opts))
     serverCmd = `npx serve build -l 3456`
   }
+
   // as the spawn will not terminate create non-async, and just listen to and handle messages
   // from the methods
   const spawn = child.spawn(serverCmd, {
@@ -106,18 +100,6 @@ async function startAppServer(buildEnv, prod = false) {
   // give up if not reponsive after 5 minutes (assume uncaught error somewhere)
   const timeout = 5 * 60 * 1000
   await waitOn({ resources: ['http-get://localhost:3456'], timeout })
-}
-
-/**
- * when passing an environment to cypress we will require a mix of both runtime
- * and own e2e env. The runtime env is passed as comma-separated list and will
- * be made available via Cypress.env()
- */
-function getCypressEnv(sharedEnv) {
-  return {
-    ...e2eEnv.parsed,
-    runtime: sharedEnv.replace(/ /g, ','),
-  }
 }
 
 function randomString(length) {

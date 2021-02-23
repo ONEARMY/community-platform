6f4c36f8af060d796c13edd8b61af46e0bfdd17a
danitrod <danitrod@ibm.com>
2021-02-22 22:27:10 -0300

    Fix user last active

--- functions/src/userUpdates/backupUser.ts
+++ functions/src/userUpdates/backupUser.ts
@@ -1,4 +1,21 @@
-import { DBDoc, IDBDocChange } from '../models'
+import { IDBDocChange } from '../models'
+import { IUserDB } from 'src/models/user.models'
+
+/** Helper function to check if the only field changed is lastActive
+ * (updates on login), in which case we will not want
+ * to backup the user again.
+ */
+const hasUserDataUpdated = (
+  prevUser: IUserDB,
+  updatedUser: IUserDB,
+): boolean => {
+  return Object.keys(prevUser).some(
+    key =>
+      key !== '_modified' &&
+      key !== '_lastActive' &&
+      prevUser[key] !== updatedUser[key],
+  )
+}
 
 /**
  * Automatically create user revision on update
@@ -7,8 +24,9 @@ import { DBDoc, IDBDocChange } from '../models'
  */
 export const backupUser = (change: IDBDocChange) => {
   const { before, after } = change
-  const rev = before.data() as DBDoc
-  if (rev && rev._modified) {
+  const rev = before.data() as IUserDB
+  const updated = after.data() as IUserDB
+  if (rev && rev._modified && hasUserDataUpdated(rev, updated)) {
     return before.ref
       .collection('revisions')
       .doc(rev._modified)

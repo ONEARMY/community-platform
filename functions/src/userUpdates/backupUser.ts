af7369e205b1b438b56e1ac8a7d966171e999856
danitrod <danitrod@ibm.com>
2021-03-09 17:06:38 -0300

    pr revision #1105

--- functions/src/userUpdates/backupUser.ts
+++ functions/src/userUpdates/backupUser.ts
@@ -1,14 +1,10 @@
-import { IDBDocChange } from '../models'
-import { IUserDB } from 'src/models/user.models'
+import { DBDoc, IDBDocChange } from '../models'
 
 /** Helper function to check if the only field changed is lastActive
  * (updates on login), in which case we will not want
  * to backup the user again.
  */
-const hasUserDataUpdated = (
-  prevUser: IUserDB,
-  updatedUser: IUserDB,
-): boolean => {
+const hasUserDataUpdated = (prevUser: DBDoc, updatedUser: DBDoc): boolean => {
   return Object.keys(prevUser).some(
     key =>
       key !== '_modified' &&
@@ -24,8 +20,8 @@ const hasUserDataUpdated = (
  */
 export const backupUser = (change: IDBDocChange) => {
   const { before, after } = change
-  const rev = before.data() as IUserDB
-  const updated = after.data() as IUserDB
+  const rev = before.data() as DBDoc
+  const updated = after.data() as DBDoc
   if (rev && rev._modified && hasUserDataUpdated(rev, updated)) {
     return before.ref
       .collection('revisions')

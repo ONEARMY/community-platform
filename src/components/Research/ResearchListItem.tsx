acae1a69b46ffdef94a75ff1e5ba56bc5e5e4b6e
danitrod <danitrod@ibm.com>
2021-03-09 17:55:36 -0300

    pr review #1110

--- src/components/Research/ResearchListItem.tsx
+++ src/components/Research/ResearchListItem.tsx
@@ -1,27 +1,15 @@
 import { format } from 'date-fns'
 import React from 'react'
-import { Box } from 'rebass/styled-components'
 import Flex from 'src/components/Flex'
 import Heading from 'src/components/Heading'
 import { Link } from 'src/components/Links'
 import Text from 'src/components/Text'
 import { IResearch } from 'src/models/research.models'
 import theme from 'src/themes/styled.theme'
-import styled from 'styled-components'
 
 interface IProps {
   item: IResearch.ItemDB
 }
-
-const GridBox = styled(Box)`
-  display: grid;
-  grid-template-columns: 2fr 1fr 1fr;
-
-  @media only screen and (max-width: ${theme.breakpoints[1]}) {
-    grid-template-columns: 1fr;
-    grid-template-rows: 1fr 2em 2em;
-  }
-`
 
 const ResearchListItem: React.FC<IProps> = ({ item }) => (
   <Flex
@@ -39,13 +27,13 @@ const ResearchListItem: React.FC<IProps> = ({ item }) => (
       key={item._id}
       width={1}
     >
-      <GridBox px={3} py={3}>
-        <Flex alignItems="center">
+      <Flex px={3} py={3} flexDirection={['column', 'column', 'row']}>
+        <Flex alignItems="center" width={[1, 1, 1 / 2]}>
           <Heading small color={'black'}>
             {item.title}
           </Heading>
         </Flex>
-        <Flex alignItems="center">
+        <Flex alignItems="center" width={[1, 1, 1 / 4]}>
           <Text
             auxiliary
             my={2}
@@ -55,7 +43,11 @@ const ResearchListItem: React.FC<IProps> = ({ item }) => (
             {item._createdBy}
           </Text>
         </Flex>
-        <Flex alignItems="center" justifyContent="space-between">
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          width={[1, 1, 1 / 4]}
+        >
           <Text color="black">
             {item.updates.length === 1
               ? item.updates.length + ' update'
@@ -63,7 +55,7 @@ const ResearchListItem: React.FC<IProps> = ({ item }) => (
           </Text>
           <Text auxiliary>{format(new Date(item._created), 'DD-MM-YYYY')}</Text>
         </Flex>
-      </GridBox>
+      </Flex>
     </Link>
   </Flex>
 )

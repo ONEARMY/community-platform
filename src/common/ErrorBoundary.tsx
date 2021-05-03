cd9c08098352d7747a97fc5dddda14e557aae972
Chris <chris.m.clarke@live.co.uk>
2021-05-03 13:42:41 +0100

    minor code fixes

--- src/common/ErrorBoundary.tsx
+++ src/common/ErrorBoundary.tsx
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/browser'
-import * as React from 'react'
+import { Component } from 'react'
 
-export default class extends React.Component<any, any> {
+export default class ErrorBoundary extends Component<any, any> {
   constructor(props) {
     super(props)
     this.state = { error: null }

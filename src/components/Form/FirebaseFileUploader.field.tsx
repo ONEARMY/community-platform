8dfd35ed1b32b11f8d387ec44abc781077b5bb8d
chrismclarke <chris.m.clarke@live.co.uk>
2021-05-28 07:18:57 -0700

    tidying

--- src/components/Form/FirebaseFileUploader.field.tsx
+++
@@ -1,8 +0,0 @@
-import { FirebaseFileUploader } from '../FirebaseFileUploader/FirebaseFileUploader'
-
-export const FirebaseFileUploaderField = ({ input, ...rest }) => (
-  <FirebaseFileUploader
-    {...rest}
-    onUploadSuccess={fileInfo => input.onChange(fileInfo)}
-  />
-)

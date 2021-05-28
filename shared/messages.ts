d5112b5b4cd4f423c81b3d922d159995061fce8c
chrismclarke <chris.m.clarke@live.co.uk>
2021-05-27 22:24:21 -0700

    hotfix type issue

--- shared/messages.ts
+++ shared/messages.ts
@@ -1,3 +1,7 @@
+// HACK - in order to be able to lookup any key in `getFriendlyMessage` we need to include
+// a random key so that full typing can't be inferred (leads to index signature error)
+const randomKey = Math.random().toString()
+
 // set of mappings to use for system->friendly messages
 // some are pulled from error messages, others are hardcoded into the platform but kept
 // here to make easier to change in the future
@@ -10,6 +14,7 @@ export const FRIENDLY_MESSAGES = {
   'auth/invalid-email': `That email address doesn't quite look right`,
   'profile saved': 'Profile Saved',
   'sign-up username taken': 'Woops sorry, that Username is already taken',
+  [randomKey]: randomKey,
 }
 
 /**
@@ -18,13 +23,14 @@ export const FRIENDLY_MESSAGES = {
  * This can either be a status code or full message (depending on how saved above)
  */
 export const getFriendlyMessage = (systemMessage = '') => {
-  const friendlyMessage = FRIENDLY_MESSAGES[systemMessage.toLowerCase()]
-  if (!friendlyMessage) {
+  const messageKey = systemMessage.toLowerCase()
+  if (FRIENDLY_MESSAGES.hasOwnProperty(messageKey)) {
+    return FRIENDLY_MESSAGES[messageKey]
+  } else {
     console.log(
-      `%c No friendly message for [${systemMessage}]`,
+      `%c No friendly message for [${messageKey}]`,
       'background: #222; color: #bada55',
     )
     return systemMessage
   }
-  return friendlyMessage
 }

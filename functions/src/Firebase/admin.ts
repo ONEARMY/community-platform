db0ad68a72ee12480d0f442fd0dcc91161ee7fce
chrismclarke <chris.m.clarke@live.co.uk>
2021-08-23 21:42:05 -0700

    update admin config

--- functions/src/Firebase/admin.ts
+++ functions/src/Firebase/admin.ts
@@ -9,8 +9,9 @@ const serviceAccount = SERVICE_ACCOUNT_CONFIG
 const FIREBASE_CONFIG = JSON.parse(
   process.env.FIREBASE_CONFIG,
 ) as IFirebaseConfig
-
+// Use Service Account Config
 if (serviceAccount) {
+  console.log(`[Admin Configuration] - ${FIREBASE_CONFIG.projectId}`)
   const cert = {
     clientEmail: serviceAccount.client_email,
     privateKey: serviceAccount.private_key,
@@ -22,7 +23,19 @@ if (serviceAccount) {
     projectId: FIREBASE_CONFIG.projectId,
   })
 }
+// Use Emulator Config
+else if (process.env.FUNCTIONS_EMULATOR === 'true') {
+  console.log('[Admin Configuration] - emulator')
+  admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    // supplied to env but for some reason still has to be manually specified (possible bug?)
+    databaseURL: process.env.FIREBASE_DATABASE_EMULATOR_HOST,
+    projectId: process.env.GCLOUD_PROJECT,
+  })
+}
+// Use Google Default Environment (only works on google server)
 else {
+  console.log(`[Admin Configuration] - ${FIREBASE_CONFIG.projectId}`)
   admin.initializeApp()
 }
 

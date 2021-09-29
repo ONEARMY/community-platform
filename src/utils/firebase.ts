7bf3ddfb27499a76bc88661bc0c1a7a2f9cc0a7d
Chris <chris.m.clarke@live.co.uk>
2021-09-29 14:42:14 +0100

    update string quotes

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,34 +1,34 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-import "firebase/storage";
-import "firebase/functions";
-import "firebase/database";
-import { FIREBASE_CONFIG, SITE } from "src/config/config";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+import 'firebase/storage'
+import 'firebase/functions'
+import 'firebase/database'
+import { FIREBASE_CONFIG, SITE } from 'src/config/config'
 // initialise with config settings, additional firestore config to support future changes
 
-firebase.initializeApp(FIREBASE_CONFIG);
+firebase.initializeApp(FIREBASE_CONFIG)
 
 // export firebase endpoints to be accessed by other functions
-const firestore = firebase.firestore();
-const rtdb = firebase.database();
-const storage = firebase.storage();
-const auth = firebase.auth();
-const functions = firebase.functions();
+const firestore = firebase.firestore()
+const rtdb = firebase.database()
+const storage = firebase.storage()
+const auth = firebase.auth()
+const functions = firebase.functions()
 
 // use emulators when running on localhost:4000
 // Note, localhost vs 127.0.0.1 potential issues mentioned in running-emulators docs
-if (SITE === "emulated_site") {
-  firestore.useEmulator("localhost", 4003);
-  rtdb.useEmulator("localhost", 4006);
-  storage.useEmulator("localhost", 4007);
-  auth.useEmulator(`http://localhost:4005`);
-  functions.useEmulator("localhost", 4002);
+if (SITE === 'emulated_site') {
+  firestore.useEmulator('localhost', 4003)
+  rtdb.useEmulator('localhost', 4006)
+  storage.useEmulator('localhost', 4007)
+  auth.useEmulator(`http://localhost:4005`)
+  functions.useEmulator('localhost', 4002)
 }
 
-export { firestore, rtdb, storage, auth, functions };
+export { firestore, rtdb, storage, auth, functions }
 
-export const EmailAuthProvider = firebase.auth.EmailAuthProvider;
+export const EmailAuthProvider = firebase.auth.EmailAuthProvider
 
 // want to also expose the default firebase user
-export type IFirebaseUser = firebase.User;
+export type IFirebaseUser = firebase.User

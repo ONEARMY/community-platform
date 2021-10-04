0f0a7d3d8cc1f02512b874a36ab9529cbc9e5a2f
Daniel T. Rodrigues <45438149+danitrod@users.noreply.github.com>
2021-10-04 19:11:13 -0300

    improve code readability
    Co-authored-by: Luke Watts <luke@thisis.la>

--- src/stores/Howto/howto.store.tsx
+++ src/stores/Howto/howto.store.tsx
@@ -151,11 +151,10 @@ export class HowtoStore extends ModuleStore {
           _created: new Date().toISOString(),
           _creatorId: user._id,
           creatorName: user.userName,
-          creatorCountry: user.country
-            ? user.country.toLowerCase()
-            : user.location
-            ? user.location.countryCode
-            : null,
+          creatorCountry:
+            user.country?.toLowerCase() ||
+            user.location?.countryCode?.toLowerCase() ||
+            null,
           text: comment,
         }
 

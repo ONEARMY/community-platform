5f751e9e2aee3a498d942e95407e4267183823ec
Luke Watts <luke@thisis.la>
2021-10-08 19:53:57 +0200

    chore: refactor sidebar.js to use declarative format
    Docusauraus provides a short hand format that is more
    convenient but harder to read as a relies on more familiarity
    with the conventions of the documentation tool.
    
    We should optimise for readability and use their more verbose
    format. This will make it more straight forward for new
    contributors to edit the documentation site.

--- packages/documentation/sidebars.js
+++ packages/documentation/sidebars.js
@@ -1,34 +1,57 @@
 module.exports = {
-  mainSidebar: {
-    'Getting Started': [
-      'Getting Started/setup',
-      'Getting Started/recommended-tools',
-    ],
-    'How To Contribute': [
-      'Contributing/bounties',
-      'Contributing/maintainer',
-      {
-        'Writing Documentation': [
-          'Docs Contribution/running-docs-locally',
-          'Docs Contribution/md-style-guide',
-        ],
-      },
-    ],
-    'Frontend Development': ['Frontend Development/overview'],
-    'Backend Development': [
-      'Backend Development/BackendOverview',
-      'Backend Development/firebase-emulators',
-      'Backend Development/integrations',
-    ],
-    Testing: ['Testing/overview', 'Testing/end-to-end'],
-    'Advanced Guides': [
-      { 'Continuous Integration': ['Deployment/circle-ci'] },
-      {
-        'Server Maintenance': [
-          'Server Maintenance/dataMigration',
-          'Server Maintenance/manualBackups',
-        ],
-      },
-    ],
-  },
+  mainSidebar: [
+    {
+      type: 'category',
+      label: 'Getting Started',
+      items: ['Getting Started/setup', 'Getting Started/recommended-tools'],
+    },
+    {
+      type: 'category',
+      label: 'How To Contribute',
+      items: [
+        'Contributing/bounties',
+        'Contributing/maintainer',
+        {
+          type: 'category',
+          label: 'Writing Documentation',
+          items: [
+            'Docs Contribution/running-docs-locally',
+            'Docs Contribution/md-style-guide',
+          ],
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Frontend Development',
+      items: ['Frontend Development/overview'],
+    },
+    {
+      type: 'category',
+      label: 'Backend Development',
+      items: [
+        'Backend Development/BackendOverview',
+        'Backend Development/firebase-emulators',
+        'Backend Development/integrations',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Testing',
+      items: ['Testing/overview', 'Testing/end-to-end'],
+    },
+    {
+      type: 'category',
+      label: 'Advanced Guides',
+      items: [
+        { 'Continuous Integration': ['Deployment/circle-ci'] },
+        {
+          'Server Maintenance': [
+            'Server Maintenance/dataMigration',
+            'Server Maintenance/manualBackups',
+          ],
+        },
+      ],
+    },
+  ],
 }

920dd3b9547a3a60468112ff043d4527131bbea7
Luke Watts <luke@thisis.la>
2021-11-20 16:13:53 +0100

    feat: add project Kamp theme

--- src/stores/Theme/theme.store.ts
+++ src/stores/Theme/theme.store.ts
@@ -1,11 +1,13 @@
 import { action, makeAutoObservable } from 'mobx'
 import preciousPlasticTheme from 'src/themes/precious-plastic'
 import fixingFashionTheme from 'src/themes/fixing-fashion'
+import projectKampTheme from 'src/themes/project-kamp'
 import unbrandedTheme from 'src/themes/unbranded'
 import type { PlatformTheme } from 'src/themes/types'
 
 const themeMap = {
   'precious-plastic': preciousPlasticTheme,
+  'project-kamp': projectKampTheme,
   'fixing-fashion': fixingFashionTheme,
   unbranded: unbrandedTheme,
 }
@@ -17,6 +19,10 @@ export class ThemeStore {
     {
       value: 'precious-plastic',
       label: 'Precious Plastic',
+    },
+    {
+      value: 'project-kamp',
+      label: 'Project Kamp',
     },
     {
       value: 'fixing-fashion',

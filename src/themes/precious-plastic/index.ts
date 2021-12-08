f8f6073f539e84f0458f34970474d5a45a70d11e
Luke Watts <luke@thisis.la>
2021-12-08 21:36:05 +0100

    feat: introduce theme provider

---
+++ src/themes/precious-plastic/index.ts
@@ -0,0 +1,12 @@
+import logo from 'src/assets/images/precious-plastic-logo-official.svg'
+import type { PlatformTheme } from '../types'
+import styles from './styles'
+
+const Theme: PlatformTheme = {
+  siteName: 'Precious Plastic',
+  logo,
+  howtoHeading: `Learn & share how to recycle, build and work with plastic`,
+  styles,
+}
+
+export default Theme

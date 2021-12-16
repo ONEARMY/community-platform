a22ae9ffc83d31f1555e5543d56053bd50cf6491
Luke Watts <luke@thisis.la>
2021-12-16 14:29:56 +0100

    feat: adds project-kamp theme

---
+++ src/themes/project-kamp/index.ts
@@ -0,0 +1,13 @@
+import logo from 'src/assets/images/themes/project-kamp/project-kamp-header.png'
+import type { PlatformTheme } from '../types'
+import styles from './styles'
+
+const Theme: PlatformTheme = {
+  siteName: 'Project Kamp',
+  logo,
+  howtoHeading: `Learn & share how to recycle, build and work with plastic`,
+  styles,
+}
+
+
+export default Theme

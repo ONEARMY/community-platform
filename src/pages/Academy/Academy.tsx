03fb961855557bb99ce984358bf121def05130ac
Luke Watts <luke@thisis.la>
2021-12-23 07:37:42 +0100

    fix: academy URL for project kamp
    * Correct base path for Project Kamp academy instance
    * Improve path parsing and introduce testing to validate behaviour

--- src/pages/Academy/Academy.tsx
+++ src/pages/Academy/Academy.tsx
@@ -2,18 +2,23 @@ import { Route } from 'react-router'
 import { useCommonStores } from 'src/index'
 import ExternalEmbed from 'src/components/ExternalEmbed/ExternalEmbed'
 
+export function getFrameSrc(base, path): string {
+  return `${base}${path
+    .split('/')
+    .filter(str => str !== 'academy' && Boolean(str))
+    .join('/')}`
+}
+
 export default function Academy() {
   const { stores } = useCommonStores()
   const src = stores.themeStore.currentTheme.academyResource
+
   return (
     <Route
       render={props => (
         // NOTE - for embed to work github.io site also must host at same path, i.e. /academy
         <ExternalEmbed
-          src={`${src}${props.location.pathname
-            .split('/')
-            .filter(str => str !== 'academy')
-            .join('/')}`}
+          src={getFrameSrc(src, props.location.pathname)}
           {...props}
         />
       )}

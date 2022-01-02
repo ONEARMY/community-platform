b16e55f96ca88a49cf8d09b13501c5107587effb
ppraczyk <ppraczyk@expediagroup.com>
2022-01-02 23:14:45 +0100

    feat: notifications -> nits

--- src/components/Notifications/NotificationList.tsx
+++ src/components/Notifications/NotificationList.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import { NotificationItem } from 'src/components/Notifications/NotificationItem'
 import { INotification } from 'src/models'
 
@@ -8,10 +6,7 @@ interface IProps {
     notifications?: INotification[]
 }
 
-
-// TODO: Expect the comments as a prop from the HowTo
 export const NotificationList = ({ notifications }: IProps) => {
-
     return (
         <div style={{padding: '0em 0.9em 0em 0.9em'}}>
             {notifications &&

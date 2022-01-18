d95de4f35908a634bdde65c6dd03be9a05e03f45
Luke Watts <luke@thisis.la>
2022-01-18 06:58:20 +0100

    feat: use OSM instead of Algolia in Workspace pins

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -157,3 +157,10 @@ interface IEditableDoc extends DBDoc {
 
 // Convert theme em string to px number
 export const emStringToPx = width => Number(width.replace('em', '')) * 16
+
+
+export function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+

410d6a798f3b4ed36276c0dfc925178a5ba8e240
Luke Watts <luke@thisis.la>
2022-02-02 22:30:43 +0100

    test: introduce test infrastructure to functions (#1252)
    * test: introduce tests
    
    The aim of this change is to introduce the infrastructure so
    that future contributors to the `functions` workspace can
    be asked to include tests with minimal overhead.
    
    A test has been added to demonstrate how functions triggered
    by changes within the FireStore data can be validated.
    
    If you want to run the tests locally you can do so using
    the [firebase tools](https://www.npmjs.com/package/firebase-tools)
    
    ```bash
    firebase emulators:exec --project default "jest ."
    ```
    
    ```bash
    firebase emulators:exec --project default "jest . --watch"
    ```
    
    * ci: downgrade jest to align with wider project
    
    * feat: introduce tests
    
    * chore: drop commit docs from this branch
    
    * ci: reduce size of resouce class
    
    * chore: align ts-jest with jest version
    
    * docs: clarify wording for job
    
    * fix: add jest types for functions
    
    Co-authored-by: chrismclarke <chris.m.clarke@live.co.uk>

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -18,10 +18,15 @@ const functions = firebase.functions()
 
 // use emulators when running on localhost:4000
 if (SITE === 'emulated_site') {
+  console.log(`Connecting to emulated services:`)
   firestore.useEmulator('localhost', 4003)
+  console.log(`Connecting rtdb on port `, 4006)
   rtdb.useEmulator('localhost', 4006)
+  console.log(`Connecting storage on port `, 4007)
   storage.useEmulator('localhost', 4007)
+  console.log(`Connecting auth on port `, 4005)
   auth.useEmulator(`http://localhost:4005`)
+  console.log(`Connecting functions on port `, 4002)
   functions.useEmulator('localhost', 4002)
 }
 

1bb5e92d40646dc001d41348c2ed77fc8d28085d
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-02 10:16:53 -0800

    feat: add user badges aggregation methods

--- functions/src/Utils/data.utils.ts
+++ functions/src/Utils/data.utils.ts
@@ -3,10 +3,23 @@
  * length provided by @param chunksize
  */
 export function splitArrayToChunks<T>(arr: T[], chunksize: number): T[][] {
-    const chunkedArrays = []
-    for (let i = 0; i < arr.length; i += chunksize) {
-        const chunk = arr.slice(i, i + chunksize);
-        chunkedArrays.push(chunk)
-    }
-    return chunkedArrays
+  const chunkedArrays = []
+  for (let i = 0; i < arr.length; i += chunksize) {
+    const chunk = arr.slice(i, i + chunksize)
+    chunkedArrays.push(chunk)
+  }
+  return chunkedArrays
+}
+
+/**
+ * Compare two json objects nested one level deep for equality (key order not important)
+ * Adapted from https://stackoverflow.com/a/52323412/5693245
+ * */
+export function shallowCompareObjectsEqual(obj1: any, obj2: any) {
+  return (
+    Object.keys(obj1).length === Object.keys(obj2).length &&
+    Object.keys(obj1).every(
+      key => obj2.hasOwnProperty(key) && obj1[key] === obj2[key],
+    )
+  )
 }
--- functions/src/aggregations/user.aggregations.ts
+++ functions/src/aggregations/user.aggregations.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { DB_ENDPOINTS, IUserDB } from '../models'
 import { db } from '../Firebase/firestoreDB'
+import { shallowCompareObjectsEqual } from '../Utils/data.utils'
 
 /** Aggregate a single list of all users with badges */
 exports.badges = functions.firestore
@@ -10,18 +11,35 @@ exports.badges = functions.firestore
     const id = change.after.id
     const user = change.after.data() as IUserDB
     if (user) {
-      const ref = db.collection(DB_ENDPOINTS.aggregations).doc('user_badges')
-      const { exists, data: userBadgeData } = await ref.get()
-      // handle first-time intialisation
-      if (!exists) {
-        await initialiseUserBadges()
-      }
-      const userBadges = userBadgeData()
-      // handle user badges changed - set if user has badges, delete if not
-      if (user.badges !== userBadges[id]) {
-        ref.update({ [id]: user.badges || admin.firestore.FieldValue.delete() })
-      }
+      await updateUserBadges(id, user)
     }
   })
 
-async function initialiseUserBadges() {}
+async function updateUserBadges(id: string, user: IUserDB) {
+  const ref = db.collection(DB_ENDPOINTS.aggregations).doc('user_badges')
+  const doc = await ref.get()
+  if (!doc.exists) {
+    await seedUserBadges(ref)
+  }
+  const userBadges = doc.data()
+  // handle user badges changed - set if user has badges, delete if not
+  if (shallowCompareObjectsEqual(user.badges, userBadges[id])) {
+    ref.update({ [id]: user.badges || admin.firestore.FieldValue.delete() })
+  }
+}
+
+/**
+ * Calculate baseline aggregation across all users instead of incremental update
+ * Useful for first-time intialisation or handling future breaking changes
+ */
+async function seedUserBadges(ref: FirebaseFirestore.DocumentReference) {
+  const snapshot = await db
+    .collection(DB_ENDPOINTS.users)
+    .orderBy('badges') // this will also filter to include only users with badges
+    .get()
+  const allUserBadges = {}
+  for (const user of snapshot.docs) {
+    allUserBadges[user.id] = user.data().badges
+  }
+  return ref.set(allUserBadges, { merge: false })
+}

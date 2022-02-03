63c8d0db32cc20cd111a92e41d74ef2ecbabe8d5
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-02 22:45:00 -0800

    feat: add user vote aggregator

--- functions/src/aggregations/user.aggregations.ts
+++ functions/src/aggregations/user.aggregations.ts
@@ -1,46 +1,69 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
+import { compareObjectDiffs } from '../Utils/data.utils'
 import { DB_ENDPOINTS, IUserDB } from '../models'
-import { db } from '../Firebase/firestoreDB'
-import { objectsAreEqualShallow } from '../Utils/data.utils'
+import { handleDBAggregations, IAggregation } from './common.aggregations'
 
-/** Aggregate a single list of all users with badges */
-exports.badges = functions.firestore
-  .document(`${DB_ENDPOINTS.users}/{id}`)
-  .onUpdate(async change => {
-    const id = change.after.id
-    const user = change.after.data() as IUserDB
-    if (user) {
-      await updateUserBadges(id, user)
-    }
-  })
-
-async function updateUserBadges(id: string, user: IUserDB) {
-  const ref = db.collection(DB_ENDPOINTS.aggregations).doc('user_badges')
-  const doc = await ref.get()
-  if (!doc.exists) {
-    await seedUserBadges(ref)
-    return updateUserBadges(id, user)
-  }
-  const userBadges = doc.data()
-  // handle user badges changed - set if user has badges, delete if not
-  if (!objectsAreEqualShallow(user.badges, userBadges[id])) {
-    ref.update({ [id]: user.badges || admin.firestore.FieldValue.delete() })
-  }
+interface IUserAggregation extends IAggregation {
+  field: keyof IUserDB
 }
 
+const userAggregations: IUserAggregation[] = [
+  // aggregate all users with lists of badges
+  {
+    field: 'badges',
+    aggregationField: 'user_badges',
+  },
+  // The voted useful aggregator is more complex, as it needs to record
+  // changes in nested field and update overall counters instead of just values
+  {
+    field: 'votedUsefulHowtos',
+    aggregationField: 'user_votedUsefulHowtos',
+    handleAggregationSeed: async aggregator => {
+      const snapshot = await aggregator.collectionRef
+        .orderBy('votedUsefulHowtos')
+        .get()
+      const allVotes = snapshot.docs
+        .map(d => d.data() as IUserDB)
+        .map(u => u.votedUsefulHowtos)
+      const votesByHowto = countHowtoVotes(allVotes)
+      return aggregator.aggregationRef.set(votesByHowto)
+    },
+    handleAggregationUpdate: async aggregator => {
+      const { before, after } = aggregator.valueChange
+      const changedVotes = compareObjectDiffs(before, after)
+      for (const [howto_id, change] of Object.entries(changedVotes)) {
+        const counterChange = change.after === true ? 1 : -1
+        await aggregator.aggregationRef.update({
+          [howto_id]: admin.firestore.FieldValue.increment(counterChange),
+        })
+      }
+    },
+  },
+]
+
+/** Watch changes to all user docs and apply aggregations */
+exports.default = functions.firestore
+  .document(`${DB_ENDPOINTS.users}/{id}`)
+  .onUpdate(change => {
+    return handleDBAggregations(change, userAggregations)
+  })
+
 /**
- * Calculate baseline aggregation across all users instead of incremental update
- * Useful for first-time intialisation or handling future breaking changes
+ * Howto votes are stored in key:value pairs corresponding to howto id and
+ * vote true/false. Collate all votes across all users by howto for intial seed stats
  */
-async function seedUserBadges(ref: FirebaseFirestore.DocumentReference) {
-  const snapshot = await db
-    .collection(DB_ENDPOINTS.users)
-    .orderBy('badges') // this will also filter to include only users with badges
-    .get()
-  const allUserBadges = {}
-  for (const user of snapshot.docs) {
-    allUserBadges[user.id] = user.data().badges
+function countHowtoVotes(allVotes: IUserDB['votedUsefulHowtos'][]) {
+  const votesByHowto = {}
+  for (const votes of allVotes) {
+    Object.entries(votes).forEach(([howto_id, voted]) => {
+      if (voted) {
+        if (!votesByHowto.hasOwnProperty(howto_id)) {
+          votesByHowto[howto_id] = 0
+        }
+        votesByHowto[howto_id]++
+      }
+    })
   }
-  return ref.set(allUserBadges, { merge: false })
+  return votesByHowto
 }

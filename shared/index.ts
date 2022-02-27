9058d823885d0ad37e1d3b55aa2f11f587fd750d
chrismclarke <chris.m.clarke@live.co.uk>
2022-02-27 11:21:26 -0800

    chore: add export

--- shared/index.ts
+++ shared/index.ts
@@ -1,5 +1,6 @@
 export * from './db'
 export * from './messages'
+export * from './mocks/auth-users'
 
 /*************************************************************************************
  * Shared Constants and Generators

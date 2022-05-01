8c6856f15a85064b50b6f18e13b4829e696e42ef
Luke Watts <luke@thisis.la>
2022-05-01 21:38:14 +0200

    chore: increment CACHE_NUMBER

--- src/stores/databaseV2/clients/dexie.tsx
+++ src/stores/databaseV2/clients/dexie.tsx
@@ -10,7 +10,7 @@ import { DB_ENDPOINTS } from '../endpoints'
  * or busting cache on db. This is used as the Dexie version number, see:
  * https://dexie.org/docs/Tutorial/Design#database-versioning
  */
-const DB_CACHE_NUMBER = 20220202
+const DB_CACHE_NUMBER = 20220501
 const CACHE_DB_NAME = 'OneArmyCache'
 const db = new Dexie(CACHE_DB_NAME)
 

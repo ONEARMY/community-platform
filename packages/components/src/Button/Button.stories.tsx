de657e71c0217e433b1b0e5892b0024a8822993e
Luke Watts <luke@thisis.la>
2022-05-13 21:25:59 +0200

    docs: use CSF for documenting components

---
+++ packages/components/src/Button/Button.stories.tsx
@@ -0,0 +1,41 @@
+import type { ComponentStory, ComponentMeta } from '@storybook/react'
+import { Button } from './Button'
+
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: 'Base Components/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>
+
+export const Basic: ComponentStory<typeof Button> = () => (
+  <Button>Button Text</Button>
+)
+
+export const Disabled: ComponentStory<typeof Button> = () => (
+  <Button disabled>Disabled</Button>
+)
+
+export const Primary: ComponentStory<typeof Button> = () => (
+  <Button variant={'primary'}>Primary</Button>
+)
+
+export const Secondary: ComponentStory<typeof Button> = () => (
+  <Button variant={'secondary'}>Secondary</Button>
+)
+
+export const Outline: ComponentStory<typeof Button> = () => (
+  <Button variant={'outline'}>Outline</Button>
+)
+
+export const Small: ComponentStory<typeof Button> = () => (
+  <Button small={true}>Small Button</Button>
+)
+export const Medium: ComponentStory<typeof Button> = () => (
+  <Button medium={true}>Medium Button</Button>
+)
+export const Large: ComponentStory<typeof Button> = () => (
+  <Button large={true}>Large Button</Button>
+)

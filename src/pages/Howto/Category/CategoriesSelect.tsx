86f9ded8e608fd49adf7b9a985f193252fa244f8
Luke Watts <luke@thisis.la>
2022-06-05 16:45:25 +0200

    chore: move VirtualizedFlex out to howto section

--- src/pages/Howto/Category/CategoriesSelect.tsx
+++ src/pages/Howto/Category/CategoriesSelect.tsx
@@ -3,7 +3,10 @@ import { observer } from 'mobx-react'
 import { FieldContainer } from '../../../components/Form/FieldContainer'
 import { DropdownIndicator } from '../../../components/DropdownIndicator'
 import Select from 'react-select'
-import { SelectStyles, FilterStyles } from '../../../components/Form/Select.field'
+import {
+  SelectStyles,
+  FilterStyles,
+} from '../../../components/Form/Select.field'
 import { useCommonStores } from 'src'
 
 export const CategoriesSelect = observer(

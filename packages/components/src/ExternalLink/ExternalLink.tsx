fc75d00922b6a4e40721c4978d32618af28a65b4
chrismclarke <chris.m.clarke@live.co.uk>
2022-06-08 10:54:07 -0700

    feat: add ExternalLink component

---
+++ packages/components/src/ExternalLink/ExternalLink.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'theme-ui'
+import type { LinkProps } from 'theme-ui'
+
+/**
+ * Provides a styled `a` tag. Opens in new tab with noopener and noreferrer rel attributes
+ *
+ * https://pointjupiter.com/what-noopener-noreferrer-nofollow-explained/
+ */
+export const ExternalLink = (props: LinkProps) => {
+  return (
+    <>
+      <Link {...props} target="_blank" rel="noopener noreferrer"></Link>
+    </>
+  )
+}

73c49381b243cd45d3ef53bc3e8b80779e5893b3
ThakurKarthik <iamkarthik08@gmail.com>
2022-08-10 00:20:56 +0530

    feat(profile): added a text component to settings
    - initial commit for adding the success/fail text

--- src/themes/precious-plastic/styles.ts
+++ src/themes/precious-plastic/styles.ts
@@ -71,6 +71,23 @@ const regular = 400
 const bold = 600
 // cc - assume standard image widths are 4:3, however not clearly defined
 
+const alerts = {
+  success: {
+    borderRadius: 1,
+    paddingX: 2,
+    paddingY: 3,
+    backgroundColor: colors.green,
+    textAlign: 'center'
+  },
+  failure: {
+    borderRadius: 1,
+    paddingX: 2,
+    paddingY: 3,
+    backgroundColor: colors.red2,
+    textAlign: 'center'
+  }
+}
+
 const buttons = {
   primary: {
     fontFamily: '"Varela Round", Arial, sans-serif',
@@ -212,6 +229,7 @@ const StyledComponentTheme: ThemeWithName = {
     'https://drive.google.com/file/d/1fXTtBbzgCO0EL6G9__aixwqc-Euqgqnd/view',
   communityProgramURL:
     'https://community.preciousplastic.com/academy/guides/community-program',
+  alerts,
   badges: {
     member: {
       lowDetail: memberBadgeLowDetail,

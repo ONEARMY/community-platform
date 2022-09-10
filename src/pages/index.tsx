079626f78b70a2076932494c8bf90c49e6fef47a
Luke Watts <luke@thisis.la>
2022-09-10 21:18:52 +0200

    chore: rename to common/AlertIncompleteProfile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ import { Flex, Box, Button } from 'theme-ui'
 import DevSiteHeader from 'src/pages/common/DevSiteHeader/DevSiteHeader'
 import { getSupportedModules } from 'src/modules'
 import GlobalSiteFooter from './common/GlobalSiteFooter/GlobalSiteFooter'
-import NotificationBanner from 'src/components/NotificationBanner/NotificationBanner'
+import { AlertIncompleteProfile } from 'src/common/AlertIncompleteProfile'
 import { SeoTagsUpdateComponent } from 'src/utils/seo'
 import { ExternalLink } from 'oa-components'
 
@@ -54,7 +54,7 @@ export class Routes extends React.Component<
           <ScrollToTop>
             {/* TODO - add better loading fallback */}
             <DevSiteHeader />
-            <NotificationBanner />
+            <AlertIncompleteProfile />
             <Header />
             <Suspense
               fallback={

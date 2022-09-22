83a8eb7f187372e7ebe3d560b7856661ae6e088f
Luke Watts <luke@thisis.la>
2022-09-22 19:51:35 +0200

    Revert "refactor: limit error handler"

--- src/common/ErrorBoundary.tsx
+++ src/common/ErrorBoundary.tsx
@@ -1,37 +1,30 @@
 import * as Sentry from '@sentry/react'
 import { Component } from 'react'
-import type { ReactNode } from 'react'
-import { SITE } from 'src/config/config'
-
-interface IProps {
-  children: ReactNode
-}
 
 export default class ErrorBoundary extends Component<any, any> {
-  constructor(props: IProps) {
+  constructor(props) {
     super(props)
     this.state = { error: null }
   }
 
-  /** When catching errors automatically report them to sentry if production */
-  componentDidCatch(error: Error, errorInfo: any) {
+  componentDidCatch(error, errorInfo) {
     this.setState({ error })
-    if (SITE === 'production') {
-      Sentry.withScope((scope) => {
-        Object.keys(errorInfo).forEach((key) => {
-          scope.setExtra(key, errorInfo[key])
-        })
-        Sentry.captureException(error)
+    Sentry.withScope((scope) => {
+      Object.keys(errorInfo).forEach((key) => {
+        scope.setExtra(key, errorInfo[key])
       })
-    }
+      Sentry.captureException(error)
+    })
   }
 
-  /**
-   * By default we still just render the content as intended. We could include
-   * fallback screen or redirect but instead expect the error not severe enough
-   * to break (or that our popup will be able to help all that much...)
-   */
-  render(): ReactNode {
-    return this.props.children
+  render() {
+    if (this.state.error) {
+      // render fallback UI
+      // eslint-disable-next-line
+      return <a onClick={() => Sentry.showReportDialog()}>Report feedback</a>
+    } else {
+      // when there's not an error, render children untouched
+      return this.props.children
+    }
   }
 }

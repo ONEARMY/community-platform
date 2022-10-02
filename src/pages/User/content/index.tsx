ad939f34135d5701bd1c06f08e5eafddca4b33cf
robkiebert <rob.kiebert@gmail.com>
2022-10-02 17:57:03 +0200

    fix: member type shows member profile

--- src/pages/User/content/index.tsx
+++ src/pages/User/content/index.tsx
@@ -77,11 +77,10 @@ export class UserPage extends React.Component<
     }
     return (
       <>
-        {user.profileType === ProfileType.SPACE ||
-        user.profileType === ProfileType.WORKSPACE ? (
-          <SpaceProfile data-cy="spaceProfile" user={user} />
-        ) : (
+        {user.profileType === ProfileType.MEMBER ? (
           <MemberProfile data-cy="memberProfile" user={user} />
+        ) : (
+          <SpaceProfile data-cy="spaceProfile" user={user} />
         )}
       </>
     )

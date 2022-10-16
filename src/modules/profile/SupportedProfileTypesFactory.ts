aa6c2c9812e1a5705e3864220a9968e6b0f62b28
Asheer Rizvi <asheer.vil@gmail.com>
2022-10-16 12:43:19 +0530

    fix: tests

--- src/modules/profile/SupportedProfileTypesFactory.ts
+++ src/modules/profile/SupportedProfileTypesFactory.ts
@@ -1,24 +1,23 @@
-import CollectionBadge from 'src/assets/images/badges/pt-collection-point.svg'
-import MemberBadge from 'src/assets/images/badges/pt-member.svg'
-import SpaceBadge from 'src/assets/images/badges/pt-space.svg'
-import MachineBadge from 'src/assets/images/badges/pt-machine-shop.svg'
-import WorkspaceBadge from 'src/assets/images/badges/pt-workspace.svg'
-import LocalComBadge from 'src/assets/images/badges/pt-local-community.svg'
-import LogoWorkspace from 'src/assets/icons/map-workspace.svg'
-import LogoCollection from 'src/assets/icons/map-collection.svg'
-import LogoMachine from 'src/assets/icons/map-machine.svg'
-import LogoCommunity from 'src/assets/icons/map-community.svg'
-import LogoWorkspaceVerified from 'src/assets/icons/map-workspace-verified.svg'
 import LogoCollectionVerified from 'src/assets/icons/map-collection-verified.svg'
-import LogoMachineVerified from 'src/assets/icons/map-machine-verified.svg'
+import LogoCollection from 'src/assets/icons/map-collection.svg'
 import LogoCommunityVerified from 'src/assets/icons/map-community-verified.svg'
-import type { IProfileType } from './types'
-import type { PlatformTheme } from 'src/themes/types'
+import LogoCommunity from 'src/assets/icons/map-community.svg'
+import LogoMachineVerified from 'src/assets/icons/map-machine-verified.svg'
+import LogoMachine from 'src/assets/icons/map-machine.svg'
+import LogoWorkspaceVerified from 'src/assets/icons/map-workspace-verified.svg'
+import LogoWorkspace from 'src/assets/icons/map-workspace.svg'
+import CollectionBadge from 'src/assets/images/badges/pt-collection-point.svg'
+import LocalComBadge from 'src/assets/images/badges/pt-local-community.svg'
+import MachineBadge from 'src/assets/images/badges/pt-machine-shop.svg'
+import SpaceBadge from 'src/assets/images/badges/pt-space.svg'
+import WorkspaceBadge from 'src/assets/images/badges/pt-workspace.svg'
 import FixingFashionMember from 'src/assets/images/themes/fixing-fashion/avatar_member_sm.svg'
 import FixingFashionSpace from 'src/assets/images/themes/fixing-fashion/avatar_space_sm.svg'
 import PreciousPlasticMember from 'src/assets/images/themes/precious-plastic/avatar_member_sm.svg'
 import ProjectKampMember from 'src/assets/images/themes/project-kamp/avatar_member_sm.svg'
 import ProjectKampSpace from 'src/assets/images/themes/project-kamp/avatar_space_sm.svg'
+import type { PlatformTheme } from 'src/themes/types'
+import type { IProfileType } from './types'
 
 export const ProfileType = {
   MEMBER: 'member',
@@ -54,13 +53,13 @@ function getProfileTypes(currentTheme?: PlatformTheme) {
       textLabel: 'I am a member',
       imageSrc: currentTheme
         ? MemberAndSpace[currentTheme.id].member
-        : MemberBadge,
+        : PreciousPlasticMember,
       cleanImageSrc: currentTheme
         ? MemberAndSpace[currentTheme.id].member
-        : MemberBadge,
+        : PreciousPlasticMember,
       cleanImageVerifiedSrc: currentTheme
         ? MemberAndSpace[currentTheme.id].member
-        : MemberBadge,
+        : PreciousPlasticMember,
     },
     {
       label: ProfileType.SPACE,

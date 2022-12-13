972b3593dc2c44cb712ab6ef2784c4ed95bde30a
chrismclarke <chris.m.clarke@live.co.uk>
2022-12-13 15:37:46 -0800

    chore: ci

--- packages/emulators-docker/src/prepare.ts
+++ packages/emulators-docker/src/prepare.ts
@@ -228,7 +228,7 @@ function generateBuildArgs() {
     PATHS.buildArgsFile,
     Object.entries(buildArgs)
       .map(([k, v]) => `${k}=${v}`)
-      .join(','),
+      .join('\n'),
   )
   console.table(buildArgs)
   return buildArgs

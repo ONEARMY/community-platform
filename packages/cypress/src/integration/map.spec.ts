69781e133adecaee9d1f407af5931008168dad21
Luke Watts <luke@thisis.la>
2022-12-16 19:40:31 +0100

    fix: restore Map Pin moderation behaviour

--- packages/cypress/src/integration/map.spec.ts
+++ packages/cypress/src/integration/map.spec.ts
@@ -5,4 +5,12 @@ context('map', () => {
   it('should navigate without log', () => {
     // TODO add futher tests
   })
+
+  it('should offer option to moderate pins for admin users', () => {
+
+  });
+
+  it('should only show accepted pins on the map', () => {
+
+  });
 })

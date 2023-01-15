c2c26a6f9fab97915a8396707934bcf15da9b0fe
chrismclarke <chris.m.clarke@live.co.uk>
2023-01-14 18:40:46 -0800

    fix: comment out incompatible pino-logflare integration

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,30 +1,34 @@
 import Logger from 'pino'
-import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
+// import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
 import { getConfigurationOption } from 'src/config/config'
 
 const logLevel = getConfigurationOption('REACT_APP_LOG_LEVEL', 'info')
 
-let loggerInstance
+/**
+ * TODO - Unsupported in react-scripts v5 (webpack 5)
+ * Will require eject/craco to add support for 'stream' polyfill
+ * */
 
-if (getConfigurationOption('REACT_APP_LOGFLARE_KEY', '')) {
-  const logflareConfiguration = {
-    apiKey: getConfigurationOption('REACT_APP_LOGFLARE_KEY', ''),
-    sourceToken: getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', ''),
-  }
+// let loggerInstance
+// if (getConfigurationOption('REACT_APP_LOGFLARE_KEY', '')) {
+//   const logflareConfiguration = {
+//     apiKey: getConfigurationOption('REACT_APP_LOGFLARE_KEY', ''),
+//     sourceToken: getConfigurationOption('REACT_APP_LOGFLARE_SOURCE', ''),
+//   }
 
-  loggerInstance = Logger(
-    {
-      browser: {
-        transmit: {
-          send: createPinoBrowserSend(logflareConfiguration),
-        },
-      },
-      level: logLevel,
-    },
-    createWriteStream(logflareConfiguration),
-  )
-} else {
-  loggerInstance = Logger({ browser: { asObject: false }, level: logLevel })
-}
+//   loggerInstance = Logger(
+//     {
+//       browser: {
+//         transmit: {
+//           send: createPinoBrowserSend(logflareConfiguration),
+//         },
+//       },
+//       level: logLevel,
+//     },
+//     createWriteStream(logflareConfiguration),
+//   )
+// } else {
+const loggerInstance = Logger({ browser: { asObject: false }, level: logLevel })
+// }
 
 export const logger = loggerInstance

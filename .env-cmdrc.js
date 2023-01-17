0bcb0d8d91d158db964221c62f6eebe0bf20be5e
chrismclarke <chris.m.clarke@live.co.uk>
2023-01-16 16:42:58 -0800

    chore: ci

--- .env-cmdrc.js
+++ .env-cmdrc.js
@@ -4,23 +4,10 @@ if (!process.env.CI) {
   envCheck()
 }
 
-// Depending on node version use different environment variables to fix
-// specific build or run issues
-const NODE_VERSION = process.versions.node.split('.')[0]
-
-// Retain any node_options specified elsewhere
-const NODE_OPTIONS = process.env.NODE_OPTIONS
-
 // Specific settings to use when running anything that requires a webpack compiler
 // Enabled when npm command specifies `env-cmd -e webpack`
 let webpack = {
-  // fix out-of-memory issues - assumes running on machine with 2GB ram (e.g. circle-ci small)
-  NODE_OPTIONS,
-}
-if (NODE_VERSION > '17') {
-  // fix https://github.com/facebook/create-react-app/issues/11708
-  // https://github.com/facebook/create-react-app/issues/12431
-  webpack.NODE_OPTIONS += ' --openssl-legacy-provider --no-experimental-fetch'
+  NODE_OPTIONS: getNodeOptions(),
 }
 
 // Specific env to use with react-scripts / create-react-app
@@ -32,3 +19,24 @@ let cra = {
 
 exports.cra = cra
 exports.webpack = webpack
+
+/** Determine what node_options to provide depending on context */
+function getNodeOptions() {
+  // Depending on node version use different environment variables to fix
+  // specific build or run issues
+  const NODE_VERSION = process.versions.node.split('.')[0]
+
+  let NODE_OPTIONS = process.env.NODE_OPTIONS || ''
+
+  // fix out-of-memory issues - assumes running on machine with 2GB ram (e.g. circle-ci small)
+  if (!NODE_OPTIONS.includes('--max-old-space-size')) {
+    NODE_OPTIONS += ' --max-old-space-size=1536'
+  }
+
+  if (NODE_VERSION > '17') {
+    // fix https://github.com/facebook/create-react-app/issues/11708
+    // https://github.com/facebook/create-react-app/issues/12431
+    NODE_OPTIONS += ' --openssl-legacy-provider --no-experimental-fetch'
+  }
+  return NODE_OPTIONS.trim()
+}

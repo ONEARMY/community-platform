0bfb014db5b914c2086eabcc8ebe5f130a0a760e
Luke Watts <luke@thisis.la>
2023-02-24 19:49:44 +0100

    feat: experiment with plausible

--- src/common/Analytics/index.tsx
+++ src/common/Analytics/index.tsx
@@ -2,9 +2,13 @@ import { withRouter } from 'react-router-dom'
 import { GoogleAnalytics } from './GoogleAnalytics'
 import ReactGA from 'react-ga4'
 import type { UaEventOptions } from 'react-ga4/types/ga4'
+import Plausible from 'plausible-tracker'
+
+const { trackEvent: plausibleTrackEvent } = Plausible()
 
 export const trackEvent = (options: UaEventOptions) => {
   ReactGA.event(options)
+  plausibleTrackEvent(`${options.category} + ${options.action}`)
 }
 
 export const Analytics = withRouter(GoogleAnalytics)

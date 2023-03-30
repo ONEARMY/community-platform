b1a0bd6680b2b716ac6ff669b7fff2b117d2a492
Chris <chris.m.clarke@live.co.uk>
2023-03-30 16:13:17 +0100

    chore: add functions triggers

---
+++ functions/src/emailNotifications/index.ts
@@ -0,0 +1,10 @@
+/** Trigger tasks daily at 2am */
+
+import * as functions from 'firebase-functions'
+import { createNotificationEmails } from './createEmail'
+
+/** Trigger daily process to send any pending email notifications */
+exports.sendDaily = functions.pubsub
+  // Trigger daily at 5pm (https://crontab.guru/#0_17_*_*_*)
+  .schedule('0 17 * * *')
+  .onRun(async () => createNotificationEmails())

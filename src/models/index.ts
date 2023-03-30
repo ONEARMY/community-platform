9412b648d833b9aa26b5f1df01afd416514a28a6
Chris <chris.m.clarke@live.co.uk>
2023-03-30 15:14:48 +0100

    chore: fix merge

--- src/models/index.ts
+++ src/models/index.ts
@@ -11,6 +11,7 @@ export * from './project.models'
 export * from './selectorList.models'
 export * from './tags.model'
 export * from './user.models'
+export * from './userPreciousPlastic.models'
 export * from './notifications.models'
 export interface UserComment extends IComment {
   isEditable: boolean

a7036d424cb612818fadeb2d9bb8e4ae6a884231
Eva Killenberg <evakill@seas.upenn.edu>
2023-04-02 12:52:32 -0600

    chore: fix test

--- functions/src/emailNotifications/createEmail.ts
+++ functions/src/emailNotifications/createEmail.ts
@@ -5,8 +5,8 @@ import { DB_ENDPOINTS, IUserDB, IPendingEmails } from '../models'
 
 export const TEMPLATE_NAME = 'email_digest'
 
-// move to handlebars functions if possible
-const getResourceTypeFromNotificationType = (type: NotificationType) => {
+// TODO: move following 2 fns to handlebars if possible
+const getResourceLabelFromNotificationType = (type: NotificationType) => {
   switch (type) {
     case 'new_comment_research':
     case 'research_mention':
@@ -33,76 +33,74 @@ const getActionTypeFromNotificationType = (type: NotificationType) => {
 }
 
 export async function createNotificationEmails() {
-  const notificationEndpoint = DB_ENDPOINTS.user_notifications
   const pendingEmails = await db
-    .collection(notificationEndpoint)
+    .collection(DB_ENDPOINTS.user_notifications)
     .doc('emails_pending')
     .get()
 
-  Object.entries(pendingEmails.data() || []).forEach(
-    async ([_userId, value]: [string, IPendingEmails]) => {
-      const user = await db.collection[DB_ENDPOINTS.users].doc(_userId).get()
-      if (user.exists) {
-        const { displayName } = user.data() as IUserDB
-        const { comments, usefuls } = await value.notifications.reduce(
-          async (aggPromise, notification) => {
-            const triggeredByUser = await db
-              .collection(DB_ENDPOINTS.users)
-              .doc(notification.triggeredBy.userId)
-              .get()
-            const triggeredByUserName = triggeredByUser.exists
-              ? (triggeredByUser.data() as IUserDB).userName
-              : undefined
+  for (const entry of Object.entries<IPendingEmails>(
+    pendingEmails.data() ?? [],
+  )) {
+    const [_userId, { notifications }] = entry
+    if (!notifications.length) return
+    const user = await db.collection(DB_ENDPOINTS.users).doc(_userId).get()
+    if (user.exists) {
+      const { displayName } = user.data() as IUserDB
+      // Aggregate list of notifications into comment and useful categories and decorate with
+      // additional fields
+      const { comments, usefuls } = await notifications.reduce(
+        async (aggPromise, notification) => {
+          const triggeredByUser = await db
+            .collection(DB_ENDPOINTS.users)
+            .doc(notification.triggeredBy.userId)
+            .get()
+          const triggeredByUserName = triggeredByUser.exists
+            ? (triggeredByUser.data() as IUserDB).userName
+            : undefined
 
-            const templateNotification = {
-              ...notification,
-              triggeredBy: {
-                ...notification.triggeredBy,
-                // would be nice if we could build this into the notification
-                userName: triggeredByUserName,
-              },
-              resourceType: getResourceTypeFromNotificationType(
-                notification.type,
-              ),
-              actionType: getActionTypeFromNotificationType(notification.type),
-            }
-
-            let { comments, usefuls } = await aggPromise
-
-            // should we check if notif is read? update notified field?
-            if (
-              ['howto_useful', 'research_useful'].includes(notification.type)
-            ) {
-              usefuls.push(templateNotification)
-            } else if (
-              [
-                'new_comment',
-                'howto_mention',
-                'new_comment_research',
-                'research_mention',
-              ].includes(notification.type)
-            ) {
-              comments.push(templateNotification)
-            }
-            return { comments, usefuls }
-          },
-          Promise.resolve({ comments: [], usefuls: [] }),
-        )
-
-        const emailEndpoint = DB_ENDPOINTS.emails
-
-        db.collection(emailEndpoint).add({
-          toUids: [_userId],
-          template: {
-            name: TEMPLATE_NAME,
-            data: {
-              displayName,
-              comments,
-              usefuls,
+          const templateNotification = {
+            ...notification,
+            triggeredBy: {
+              ...notification.triggeredBy,
+              userName: triggeredByUserName,
             },
+            resourceLabel: getResourceLabelFromNotificationType(
+              notification.type,
+            ),
+            actionType: getActionTypeFromNotificationType(notification.type),
+          }
+
+          const { comments, usefuls } = await aggPromise
+
+          if (['howto_useful', 'research_useful'].includes(notification.type)) {
+            usefuls.push(templateNotification)
+          } else if (
+            [
+              'new_comment',
+              'howto_mention',
+              'new_comment_research',
+              'research_mention',
+            ].includes(notification.type)
+          ) {
+            comments.push(templateNotification)
+          }
+          return { comments, usefuls }
+        },
+        Promise.resolve({ comments: [], usefuls: [] }),
+      )
+
+      // Adding emails to this collection triggers an email notification to be sent to the user.
+      await db.collection(DB_ENDPOINTS.emails).add({
+        toUids: [_userId],
+        template: {
+          name: TEMPLATE_NAME,
+          data: {
+            displayName,
+            comments,
+            usefuls,
           },
-        })
-      }
-    },
-  )
+        },
+      })
+    }
+  }
 }

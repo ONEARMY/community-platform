605f76dfc34681ec20bc3f70cef057a595dae14b
Luke Watts <luke@thisis.la>
2023-04-07 15:33:35 +0200

    chore(ci): increase timeout for web server

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,5 +73,6 @@ export default defineConfig({
     command: 'export REACT_APP_SITE_VARIANT=test-ci; yarn start',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 })

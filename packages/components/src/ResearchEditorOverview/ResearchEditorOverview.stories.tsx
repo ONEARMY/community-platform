529a57f9ed487cf40995e0854487e191e77f8d58
Luke Watts <luke@thisis.la>
2023-04-14 21:59:00 +0200

    docs: fills out the default story

--- packages/components/src/ResearchEditorOverview/ResearchEditorOverview.stories.tsx
+++ packages/components/src/ResearchEditorOverview/ResearchEditorOverview.stories.tsx
@@ -7,5 +7,26 @@ export default {
 } as ComponentMeta<typeof ResearchEditorOverview>
 
 export const Default: ComponentStory<typeof ResearchEditorOverview> = () => (
-  <ResearchEditorOverview updates={[]} researchSlug={'abc'} />
+  <ResearchEditorOverview
+    updates={[
+      {
+        isActive: true,
+        title: 'Update 1',
+        slug: 'update-1',
+      },
+      {
+        isActive: false,
+        title: 'Update 2',
+        slug: 'update-2',
+      },
+      {
+        isActive: false,
+        title: 'Update 3',
+        slug: 'update-3',
+      },
+    ]}
+    researchSlug={'abc'}
+    showBackToResearchButton
+    showCreateUpdateButton
+  />
 )

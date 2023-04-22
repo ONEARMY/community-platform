3d0a7548a4368f34b86166a641784e59e7f25da6
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2023-04-22 13:07:27 +0200

    chore: convert userpage functional component

--- src/pages/User/content/index.tsx
+++ src/pages/User/content/index.tsx
@@ -1,112 +1,80 @@
-import { inject, observer } from 'mobx-react'
-import React from 'react'
+import { useEffect, useState } from 'react'
 import type { RouteComponentProps } from 'react-router'
 import { Loader } from 'oa-components'
 import { Text } from 'theme-ui'
-import type { IUserPP } from 'src/models'
+import { observer } from 'mobx-react-lite'
+import { useCommonStores } from 'src/index'
 import { ProfileType } from 'src/modules/profile/types'
-import type { ThemeStore } from 'src/stores/Theme/theme.store'
-import type { UserStore } from 'src/stores/User/user.store'
-import type { AggregationsStore } from 'src/stores/Aggregations/aggregations.store'
-// import { useCommonStores } from 'src/index'
-
 import { MemberProfile } from './MemberProfile'
 import { SpaceProfile } from './SpaceProfile'
+import type { IUserPP } from 'src/models'
+import { logger } from '../../../logger'
 
 interface IRouterCustomParams {
   id: string
 }
 
-interface InjectedProps extends RouteComponentProps<IRouterCustomParams> {
-  userStore: UserStore
-  themeStore: ThemeStore
-  aggregationsStore: AggregationsStore
-}
+interface IProps extends RouteComponentProps<IRouterCustomParams> {}
 
-interface IState {
-  user?: IUserPP
-  isLoading: boolean
-}
+const UserPage = observer((props: IProps) => {
+  const { userStore, aggregationsStore } = useCommonStores().stores
+  const [user, setUser] = useState<IUserPP | undefined>()
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
-interface IProps {}
-
-// TODO: Replace this logic with a simpler mobx-react hook: https://mobx-react.js.org/recipes-migration
-@inject('userStore', 'themeStore', 'aggregationsStore')
-@observer
-export class UserPage extends React.Component<
-  RouteComponentProps<IRouterCustomParams>,
-  IState,
-  IProps
-> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      user: undefined,
-      isLoading: true,
+  useEffect(() => {
+    const userid = props.match.params.id
+    const fetchData = async () => {
+      try {
+        const userData = await userStore.getUserProfile(userid)
+        setUser(userData ?? null)
+        setIsLoading(false)
+      } catch (error) {
+        logger.error('Error getting user profile', error)
+      }
     }
-  }
+    fetchData()
+  }, [props.match.params.id])
 
-  get injected() {
-    return this.props as InjectedProps
-  }
-
-  /* eslint-disable @typescript-eslint/naming-convention*/
-  async UNSAFE_componentWillMount() {
-    const userid = this.props.match.params.id
-    const userData = await this.injected.userStore.getUserProfile(userid)
-    this.setState({
-      user: userData || null,
-      isLoading: false,
-    })
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     // Ensure aggregations up-to-date when using any child pages
-    this.injected.aggregationsStore!.updateAggregation(
-      'users_votedUsefulHowtos',
-    )
-    this.injected.aggregationsStore!.updateAggregation(
-      'users_votedUsefulResearch',
-    )
+    aggregationsStore.updateAggregation('users_votedUsefulHowtos')
+    aggregationsStore.updateAggregation('users_votedUsefulResearch')
+    return () => {
+      // Stop receiving updates when navigating away from child pages
+      aggregationsStore.stopAggregationUpdates('users_votedUsefulHowtos')
+      aggregationsStore.stopAggregationUpdates('users_votedUsefulResearch')
+    }
+  }, [aggregationsStore])
+
+  if (isLoading) {
+    return <Loader />
   }
-  componentWillUnmount() {
-    // Stop receiving updates when navigating away from child pages
-    this.injected.aggregationsStore!.stopAggregationUpdates(
-      'users_votedUsefulHowtos',
-    )
-    this.injected.aggregationsStore!.stopAggregationUpdates(
-      'users_votedUsefulResearch',
+
+  if (!user) {
+    return (
+      <Text
+        sx={{
+          width: '100%',
+          textAlign: 'center',
+          display: 'block',
+          marginTop: 10,
+        }}
+      >
+        User not found
+      </Text>
     )
   }
 
-  render() {
-    const { user, isLoading } = this.state
-    if (isLoading) {
-      return <Loader />
-    }
-    if (!user) {
-      return (
-        <Text
-          sx={{
-            width: '100%',
-            textAlign: 'center',
-            display: 'block',
-            marginTop: 10,
-          }}
-        >
-          User not found
-        </Text>
-      )
-    }
-    return (
-      <>
-        {user.profileType === ProfileType.MEMBER ||
-        user.profileType === undefined ? (
-          <MemberProfile data-cy="memberProfile" user={user} />
-        ) : (
-          <SpaceProfile data-cy="spaceProfile" user={user} />
-        )}
-      </>
-    )
-  }
-}
+  return (
+    <>
+      {user.profileType === ProfileType.MEMBER ||
+      user.profileType === undefined ? (
+        <MemberProfile data-cy="memberProfile" user={user} />
+      ) : (
+        <SpaceProfile data-cy="spaceProfile" user={user} />
+      )}
+    </>
+  )
+})
+
+export default UserPage

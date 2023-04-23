a184001bb353d3f12cd31508c7969a1325e66164
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2023-04-23 12:19:58 +0200

    chore: add tolerance parameter

--- packages/cypress/src/support/CustomAssertations.ts
+++ packages/cypress/src/support/CustomAssertations.ts
@@ -14,11 +14,11 @@ declare global {
 }
 
 chai.use((chaiObj) => {
-  function assertIsInViewport() {
+  function assertIsInViewport(tolerance = 0) {
     const subject = this._obj
 
-    const bottom = Cypress.$(cy.state('window')).height() + 100
-    const width = Cypress.$(cy.state('window')).width() + 100
+    const bottom = Cypress.$(cy.state('window')).height() + tolerance
+    const width = Cypress.$(cy.state('window')).width() + tolerance
     const rect = subject[0].getBoundingClientRect()
 
     expect(

58c76ab64905b3391a33821338f252e209c4565d
thisislawatts <thisislawatts@users.noreply.github.com>
2023-04-27 17:03:23 +0000

    deploy: 5086a459b80f7a40c70c5a6a9b664aa7b8b6bb39

---
+++ storybook-static/assets/GlobalStyles-063d0f56.js
@@ -0,0 +1,91 @@
+import{a as k}from"./theme-ui-css.esm-b1576555.js";const p=""+new URL("precious-plastic-logo-official-d09d9267.svg",import.meta.url).href,h=""+new URL("avatar_member_sm-40752705.svg",import.meta.url).href,n={white:"white",offwhite:"#ececec",black:"#1b1b1b",softyellow:"#f5ede2",blue:"#83ceeb",red:"#eb1b1f",red2:"#f58d8e",softblue:"#e2edf7",bluetag:"#5683b0",grey:"#61646b",green:"#00c3a9",error:"red",background:"#f4f6f7",silver:"#c0c0c0",softgrey:"#c2d4e4",lightgrey:"#ababac",darkGrey:"#686868",subscribed:"orange",notSubscribed:"#1b1b1b"},u={auxiliary:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"12px",color:n.grey},paragraph:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"16px",color:n.grey}},R=""+new URL("avatar_member_lg-36037a47.svg",import.meta.url).href,v=""+new URL("pt-collection-point-6446bce1.svg",import.meta.url).href,S=""+new URL("map-collection-dacf3294.svg",import.meta.url).href,$=""+new URL("pt-machine-shop-95cb7e36.svg",import.meta.url).href,I=""+new URL("map-machine-93076cd5.svg",import.meta.url).href,z=""+new URL("pt-workspace-5d0072c6.svg",import.meta.url).href,C=""+new URL("map-workspace-8f5266f0.svg",import.meta.url).href,A=""+new URL("pt-local-community-e2734b83.svg",import.meta.url).href,L=""+new URL("map-community-fc5bfbe8.svg",import.meta.url).href,t={fontFamily:'"Varela Round", Arial, sans-serif',fontSize:3,display:"inline-flex",alignItems:"center",position:"relative",transition:".2s ease-in-out",borderRadius:1,width:"auto",border:"2px solid"},b=a=>({primary:{...t,color:a.black,bg:a.yellow.base,"&:hover":{bg:a.yellow.hover,cursor:"pointer"},"&[disabled]":{opacity:.5,cursor:"not-allowed"},"&[disabled]:hover":{bg:a.yellow.base}},secondary:{...t,border:"2px solid "+a.black,color:a.black,bg:a.softblue,"&:hover":{bg:a.white,cursor:"pointer"},"&[disabled]":{opacity:.5},"&[disabled]:hover":{bg:a.softblue}},outline:{...t,border:"2px solid "+a.black,color:a.black,backgroundColor:"transparent","&:hover":{backgroundColor:a.softblue,cursor:"pointer"}},imageInput:{border:"2px dashed #e0e0e0",color:"#e0e0e0",backgroundColor:"transparent"},subtle:{...t,borderColor:a.softblue,color:a.black,bg:a.softblue,"&:hover":{bg:a.offwhite,borderColor:a.offwhite,cursor:"pointer"},"&[disabled]":{opacity:.5},"&[disabled]:hover":{bg:a.softblue}}}),i={body:"'Inter', Arial, sans-serif"},e={...n,primary:"red",yellow:{base:"#fee77b",hover:"#ffde45"}},F={behind:-1,level:0,default:1,slickArrows:100,modalProfile:900,logoContainer:999,mapFlexBar:2e3,header:3e3},y=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140],U=y,l=[10,12,14,18,22,30,38,42,46,50,58,66,74],_=["40em","52em","70em"],B=1280,W=400,D=600,V={info:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:e.yellow.base,color:e.black,textAlign:"center",fontWeight:"normal"},success:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:e.green,textAlign:"center",fontWeight:"normal"},failure:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:e.red2,textAlign:"center",fontWeight:"normal"}},H={auxiliary:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"12px",color:e.grey},paragraph:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"16px",color:e.grey}},P={name:"Precious Plastic",logo:p,profileGuidelinesURL:"https://drive.google.com/file/d/1fXTtBbzgCO0EL6G9__aixwqc-Euqgqnd/view",communityProgramURL:"https://community.preciousplastic.com/academy/guides/community-program",alerts:V,badges:{member:{lowDetail:h,normal:R},workspace:{lowDetail:C,normal:z},"community-builder":{lowDetail:L,normal:A},"collection-point":{lowDetail:S,normal:v},"machine-builder":{lowDetail:I,normal:$}},bold:D,breakpoints:_,buttons:b(e),cards:{primary:{background:"white",border:`2px solid ${e.black}`,borderRadius:1,overflow:"hidden"}},colors:e,fonts:i,fontSizes:l,forms:{input:{background:e.background,borderRadius:1,border:"1px solid transparent",fontFamily:i.body,fontSize:1,"&:focus":{borderColor:e.blue,outline:"none",boxShadow:"none"}},inputOutline:{background:"white",border:`2px solid ${e.black}`,borderRadius:1,"&:focus":{borderColor:e.blue,outline:"none",boxShadow:"none"}},error:{background:e.background,borderRadius:1,border:`1px solid ${e.error}`,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,"&:focus":{borderColor:e.blue,outline:"none",boxShadow:"none"}},textarea:{background:e.background,border:"1px solid transparent",borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:e.blue,outline:"none",boxShadow:"none"}},textareaError:{background:e.background,border:`1px solid ${e.error}`,borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:e.blue,outline:"none",boxShadow:"none"}}},maxContainerWidth:B,radii:U,regular:W,space:y,text:{heading:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:l[5],fontWeight:"normal"},small:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:l[4],fontWeight:"normal"},body:{fontFamily:i.body},quiet:{fontFamily:i.body,color:"grey"},...u},typography:H,zIndex:F},G={id:"precious-plastic",siteName:"Precious Plastic",logo:p,badge:h,avatar:"",howtoHeading:"Learn & share how to recycle, build and work with plastic",styles:P,academyResource:"https://onearmy.github.io/academy/",externalLinks:[{url:"https://bazar.preciousplastic.com/",label:"Bazar"},{url:"https://preciousplastic.com/",label:"Global Site"}]},N=""+new URL("project-kamp-header-3b363f8e.png",import.meta.url).href,q=""+new URL("avatar_member_sm-d5f614a7.svg",import.meta.url).href,E=""+new URL("avatar_member_lg-45558694.svg",import.meta.url).href,o={...n,primary:"green",yellow:{base:"#8ab57f",hover:"hsl(108, 25%, 68%)"}},T={behind:-1,level:0,default:1,slickArrows:100,modalProfile:900,logoContainer:999,mapFlexBar:2e3,header:3e3},s={body:"'Inter', Arial, sans-serif"},w=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140],X=w,d=[10,12,14,18,22,30,38,42,46,50,58,66,74],M=["40em","52em","70em"],Y=1280,O=400,j=600,K={success:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:o.green,textAlign:"center",fontWeight:"normal"},failure:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:o.red2,textAlign:"center",fontWeight:"normal"}},J={auxiliary:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"12px",color:o.grey},paragraph:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"16px",color:o.grey}},we={name:"Project Kamp",profileGuidelinesURL:"https://drive.google.com/file/d/1fXTtBbzgCO0EL6G9__aixwqc-Euqgqnd/view",communityProgramURL:"https://community.preciousplastic.com/academy/guides/community-program",logo:N,alerts:K,badges:{member:{lowDetail:q,normal:E}},cards:{primary:{background:"white",border:`2px solid ${o.black}`,borderRadius:1,overflow:"hidden"}},colors:o,buttons:b(o),breakpoints:M,space:w,radii:X,fontSizes:d,fonts:s,forms:{input:{background:o.background,borderRadius:1,border:"1px solid transparent",fontFamily:"'Inter', Arial, sans-serif",fontSize:1,"&:focus":{borderColor:o.blue,outline:"none",boxShadow:"none"}},inputOutline:{background:"white",border:`2px solid ${o.black}`,borderRadius:1,"&:focus":{borderColor:o.blue,outline:"none",boxShadow:"none"}},error:{background:o.background,borderRadius:1,border:`1px solid ${o.error}`,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,"&:focus":{borderColor:o.blue,outline:"none",boxShadow:"none"}},textarea:{background:o.background,border:"1px solid transparent",borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:o.blue,outline:"none",boxShadow:"none"}},textareaError:{background:o.background,border:`1px solid ${o.error}`,borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:o.blue,outline:"none",boxShadow:"none"}}},maxContainerWidth:Y,regular:O,bold:j,text:{heading:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:d[5],fontWeight:"normal"},small:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:d[4],fontWeight:"normal"},body:{fontFamily:s.body},quiet:{fontFamily:s.body,color:"grey"},...u},typography:J,zIndex:T},Q=""+new URL("fixing-fashion-header-b915b7a6.png",import.meta.url).href,Z=""+new URL("avatar_member_sm-4d3ee73c.svg",import.meta.url).href,m=""+new URL("avatar_space_lg-f7da92ee.svg",import.meta.url).href,ee=""+new URL("avatar_member_lg-54f0e065.svg",import.meta.url).href,c={body:"'Inter', Arial, sans-serif"},r={...n,primary:"green",yellow:{base:"#E95628",hover:"hsl(14, 81%, 43%)"}},oe={behind:-1,level:0,default:1,slickArrows:100,modalProfile:900,logoContainer:999,mapFlexBar:2e3,header:3e3},x=[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140],re=x,f=[10,12,14,18,22,30,38,42,46,50,58,66,74],ae=["40em","52em","70em"],ne=1280,te=400,ie=600,le={success:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:r.green,textAlign:"center",fontWeight:"normal"},failure:{borderRadius:1,paddingX:3,paddingY:3,backgroundColor:r.red2,textAlign:"center",fontWeight:"normal"}},se={auxiliary:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"12px",color:r.grey},paragraph:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"16px",color:r.grey}},xe={name:"Fixing Fashion",logo:Q,profileGuidelinesURL:"https://community.fixing.fashion/academy/guides/profile",communityProgramURL:"https://community.fixing.fashion/academy/guides/community-program",alerts:le,badges:{member:{lowDetail:Z,normal:ee},space:{lowDetail:m,normal:m}},cards:{primary:{background:"white",border:`2px solid ${r.black}`,borderRadius:1,overflow:"hidden"}},colors:r,buttons:b(r),breakpoints:ae,space:x,radii:re,fontSizes:f,fonts:c,forms:{input:{background:r.background,borderRadius:1,border:"1px solid transparent",fontFamily:"'Inter', Arial, sans-serif",fontSize:1,"&:focus":{borderColor:r.blue,outline:"none",boxShadow:"none"}},inputOutline:{background:"white",border:`2px solid ${r.black}`,borderRadius:1,"&:focus":{borderColor:r.blue,outline:"none",boxShadow:"none"}},error:{background:r.background,borderRadius:1,border:`1px solid ${r.error}`,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,"&:focus":{borderColor:r.blue,outline:"none",boxShadow:"none"}},textarea:{background:r.background,border:"1px solid transparent",borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:r.blue,outline:"none",boxShadow:"none"}},textareaError:{background:r.background,border:`1px solid ${r.error}`,borderRadius:1,fontFamily:"'Inter', Arial, sans-serif",fontSize:1,padding:2,"&:focus":{borderColor:r.blue,outline:"none",boxShadow:"none"}}},maxContainerWidth:ne,regular:te,bold:ie,text:{heading:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:f[5],fontWeight:"normal"},small:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:f[4],fontWeight:"normal"},body:{fontFamily:c.body},quiet:{fontFamily:c.body,color:"grey"},...u},typography:se,zIndex:oe},de=""+new URL("VarelaRound-Regular-65d33a72.woff",import.meta.url).href,ce=""+new URL("VarelaRound-Regular-1f536f47.ttf",import.meta.url).href,fe=""+new URL("Inter-Regular-0e6ff9be.woff2",import.meta.url).href,ue=""+new URL("Inter-Regular-a3daad5d.woff",import.meta.url).href,be=""+new URL("Inter-Regular-0d156db7.ttf",import.meta.url).href,me=""+new URL("Inter-Medium-8340de71.woff2",import.meta.url).href,ge=""+new URL("Inter-Medium-5524ddae.woff",import.meta.url).href,pe=""+new URL("Inter-Medium-697f88e6.ttf",import.meta.url).href,he=`
+  @font-face {
+    font-family: 'Varela Round';
+    font-display: auto;
+    src:  url(${de}) format('woff'),
+          url(${ce}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Inter';
+    font-display: auto;
+    src:  url(${fe}) format('woff2'),
+          url(${ue}) format('woff'),
+          url(${be}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Inter';
+    font-display: auto;
+    src:  url(${me}) format('woff2'),
+          url(${ge}) format('woff'),
+          url(${pe}) format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
+`,g=G.styles,ke=k`
+  ${he}
+  body {
+    font-family: 'Varela Round', Arial, sans-serif;
+    background-color: ${g.colors.background};
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .slick-prev,
+  .slick-next {
+    position: absolute !important;
+    top: 50%;
+    z-index: ${g.zIndex.slickArrows};
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
+
+  .slick-next {
+    left: auto;
+    right: 0;
+  }
+
+  .slick-prev {
+    left: 0;
+    right: auto;
+  }
+
+  .slick-track {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: stretch;
+  }
+
+  /***** Fix for Algolia search Icon *******/
+  .ap-icon-pin {
+    display: none;
+  }
+
+  /* Screen-reader text only - Taken from bootstrap 4 */
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
+  }
+`;export{ke as G,we as S,G as T,Q as a,q as b,Z as c,xe as d,N as l};
+//# sourceMappingURL=GlobalStyles-063d0f56.js.map

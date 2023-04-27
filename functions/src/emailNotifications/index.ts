9c98986fbc37ed96e828460f1e637b90388c9fbc
Eva Killenberg <evakill@seas.upenn.edu>
2023-04-26 22:58:44 -0700

    feat(email-notifs): send emails from admin panel for testing

--- functions/src/emailNotifications/index.ts
+++ functions/src/emailNotifications/index.ts
@@ -1,8 +1,41 @@
 import * as functions from 'firebase-functions'
 import { createNotificationEmails } from './createEmail'
+import { db } from '../Firebase/firestoreDB'
+import { DB_ENDPOINTS, IUserDB } from '../models'
 
 /** Trigger daily process to send any pending email notifications */
 exports.sendDaily = functions.pubsub
   // Trigger daily at 5pm (https://crontab.guru/#0_17_*_*_*)
   .schedule('0 17 * * *')
   .onRun(async () => createNotificationEmails())
+
+exports.sendOnce = functions.https.onCall(async (_, context) => {
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      'failed-precondition',
+      'The function must be called while authenticated.',
+    )
+  }
+  // Validate user exists and has admin status before triggering function.
+  const { uid } = context.auth
+  const user = await db.collection(DB_ENDPOINTS.users).doc(uid).get()
+  if (user.exists) {
+    const { userRoles } = user.data() as IUserDB
+    if (userRoles?.some((role) => ['admin', 'super-admin'].includes(role))) {
+      try {
+        await createNotificationEmails()
+        return 'OK'
+      } catch (error) {
+        console.error(error)
+        throw new functions.https.HttpsError(
+          'internal',
+          'There was an error creating emails.',
+        )
+      }
+    }
+  }
+  throw new functions.https.HttpsError(
+    'permission-denied',
+    'Emails can be triggered by admins only.',
+  )
+})

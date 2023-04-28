0347562188511b68962c36f2c4bde6b60ff0a312
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2023-04-28 16:34:42 +0200

    test: make userStatistic tests less brittle

--- packages/components/src/UserStatistics/UserStatistics.test.tsx
+++ packages/components/src/UserStatistics/UserStatistics.test.tsx
@@ -4,10 +4,62 @@ import type { UserStatisticsProps } from './UserStatistics'
 
 describe('UserStatistics', () => {
   it('renders correctly', () => {
-    const { container } = render(
+    render(<Default {...(Default.args as UserStatisticsProps)} />)
+  })
+
+  it('renders verified icon when isVerified is true', () => {
+    const { getByTestId } = render(
       <Default {...(Default.args as UserStatisticsProps)} />,
     )
+    const verifiedIcon = getByTestId('verified-stat')
 
-    expect(container).toMatchSnapshot()
+    expect(verifiedIcon).toBeInTheDocument()
+  })
+
+  it('renders location link when country and userName are provided', () => {
+    const { getByTestId } = render(
+      <Default {...(Default.args as UserStatisticsProps)} />,
+    )
+    const locationLink = getByTestId('location-link')
+
+    expect(locationLink).toBeInTheDocument()
+    expect(locationLink).toHaveTextContent('Greenland')
+  })
+
+  it('renders supporter icon when isSupporter is true', () => {
+    const { getByTestId } = render(
+      <Default {...(Default.args as UserStatisticsProps)} />,
+    )
+    const supporterLink = getByTestId('supporter-stat')
+
+    expect(supporterLink).toBeInTheDocument()
+  })
+
+  it('renders useful count when usefulCount is provided', () => {
+    const { getByTestId } = render(
+      <Default {...(Default.args as UserStatisticsProps)} />,
+    )
+    const usefulCount = getByTestId('useful-stat')
+
+    expect(usefulCount).toBeInTheDocument()
+    expect(usefulCount).toHaveTextContent('Useful: 20')
+  })
+
+  it('renders howto count when howtoCount is provided', () => {
+    const { getByTestId } = render(
+      <Default {...(Default.args as UserStatisticsProps)} />,
+    )
+    const howtoCount = getByTestId('howto-stat')
+
+    expect(howtoCount).toBeInTheDocument()
+    expect(howtoCount).toHaveTextContent(/^How[-‑]?to: 10$/)
+  })
+
+  it('renders event count when eventCount is provided', () => {
+    const { getByTestId } = render(
+      <Default {...(Default.args as UserStatisticsProps)} />,
+    )
+    const eventCount = getByTestId('event-stat')
+    expect(eventCount).toHaveTextContent('Events: 4')
   })
 })
--- packages/components/src/UserStatistics/UserStatistics.tsx
+++ packages/components/src/UserStatistics/UserStatistics.tsx
@@ -35,14 +35,18 @@ export const UserStatistics = (props: UserStatisticsProps) => {
         }}
       >
         {props.isVerified && (
-          <Flex>
+          <Flex data-testid="verified-stat">
             <Icon glyph="verified" size={22} />
             <Box ml={1}>Verified</Box>
           </Flex>
         )}
 
         {hasLocation ? (
-          <InternalLink to={'/map/#' + props.userName} sx={{ color: 'black' }}>
+          <InternalLink
+            to={'/map/#' + props.userName}
+            sx={{ color: 'black' }}
+            data-testid="location-link"
+          >
             <Flex>
               <Icon glyph="location-on" size={22} />
               <Box ml={1}>{props.country || 'View on Map'}</Box>
@@ -51,7 +55,7 @@ export const UserStatistics = (props: UserStatisticsProps) => {
         ) : null}
 
         {props?.isSupporter ? (
-          <Flex>
+          <Flex data-testid="supporter-stat">
             <Icon glyph={'supporter'} size={22} />
             <Box ml={1}>
               <ExternalLink
@@ -66,21 +70,21 @@ export const UserStatistics = (props: UserStatisticsProps) => {
         ) : null}
 
         {props.usefulCount ? (
-          <Flex>
+          <Flex data-testid="useful-stat">
             <ElWithBeforeIcon icon={starActiveSVG} />
             Useful:&nbsp;{props.usefulCount}
           </Flex>
         ) : null}
 
         {props.howtoCount ? (
-          <Flex>
+          <Flex data-testid="howto-stat">
             <ElWithBeforeIcon icon={HowToCountIcon} />
             How‑to:&nbsp;{props.howtoCount}
           </Flex>
         ) : null}
 
         {props.eventCount ? (
-          <Flex>
+          <Flex data-testid="event-stat">
             <ElWithBeforeIcon icon={EventsIcon} />
             Events:&nbsp;{props.eventCount}
           </Flex>

76935af6d8144e7f411d7389c982acc9d4b1b175
thisislawatts <thisislawatts@users.noreply.github.com>
2023-05-05 12:48:55 +0000

    deploy: baa1b26b281c9c938b2d4bd09237f81fb8a2dbba

---
+++ storybook-static/assets/MapWithDraggablePin.stories-db45864c.js
@@ -0,0 +1,14 @@
+import{a}from"./jsx-runtime-93f93352.js";import{M as n}from"./UsefulStatsButton-196a1fb8.js";import"./index-ba39e096.js";import"./_commonjsHelpers-042e6b4d.js";import"./emotion-styled.browser.esm-70334677.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-789fca20.js";import"./theme-ui-css.esm-915c1cc4.js";import"./index-4d501b15.js";import"./ExternalLink-ff2226a7.js";import"./theme-ui-components.esm-8feed514.js";import"./emotion-react.browser.esm-1405c41e.js";import"./FieldDatepicker-af02139f.js";import"./FieldInput-59eb65f5.js";import"./FieldTextarea-ad75ec4f.js";import"./Button-00db9353.js";import"./Icon-94808b7f.js";import"./theme-ui-core-jsx-runtime.esm-9cced462.js";import"./parseProps-376f43a7.esm-ec1eb83c.js";import"./icon-verified-badge-7d7bdb14.js";import"./icon-star-active-9fadbee7.js";import"./MemberBadge-55f05547.js";import"./Username-89d641b5.js";import"./InternalLink-40581e06.js";import"./react-router-dom-011cacea.js";import"./react-router-5f08007f.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./CreateComment-53bf1257.js";import"./Modal-707656e9.js";import"./index-3e1d4d7f.js";import"./MoreContainer-e8c937b6.js";import"./ModerationStatus-d8dad367.js";import"./ElWithBeforeIcon-787d21dd.js";import"./NotificationItem-a26e661d.js";import"./NotificationList-43a1d044.js";import"./ImageGallery-66ac4aab.js";import"./Loader-2f45ebef.js";import"./CategoryTag-4e50192d.js";import"./OsmGeocoding-e494e1a0.js";import"./TextNotification-a7d282da.js";import"./Select-30db6a58.js";import"./isNativeReflectConstruct-98f790dd.js";import"./MapMemberCard-d127e7b8.js";import"./ProfileLink-f4e3f0c9.js";import"./GlobalStyles-7f24eae8.js";import"./UserStatistics-66e9d924.js";import"./DownloadFiles-d5943091.js";import"./ResearchEditorOverview-f1dd275b.js";const rt={title:"Components/MapWithDraggablePin",component:n},t=()=>{const o={lat:0,lng:0};return a(n,{position:o,updatePosition:i=>{o.lat=i.lat,o.lng=i.lng}})};var r,p,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  const position = {
+    lat: 0,
+    lng: 0
+  };
+  return <MapWithDraggablePin position={position} updatePosition={(_position: {
+    lat: number;
+    lng: number;
+  }) => {
+    position.lat = _position.lat;
+    position.lng = _position.lng;
+  }} />;
+}`,...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const pt=["Default"];export{t as Default,pt as __namedExportsOrder,rt as default};
+//# sourceMappingURL=MapWithDraggablePin.stories-db45864c.js.map

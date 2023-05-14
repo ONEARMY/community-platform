02a0ddfa802c65a7fb21cf06b7e3ca2ee519e736
iSCJT <sean@speckledbanana.com>
2023-05-14 15:22:16 +0100

    test: add research follow button test

---
+++ packages/cypress/src/integration/research/follow.spec.ts
@@ -0,0 +1,53 @@
+describe('[Research]', () => {
+  describe('[Follow a research article]', () => {
+    const researchArticleUrl = '/research/qwerty'
+    beforeEach(() => {
+      cy.visit(researchArticleUrl)
+    })
+
+    describe('[By Everyone]', () => {
+      it('[Follow button]', () => {
+        cy.step('Should redirect to sign in')
+        cy.get('[data-cy="follow-button"]').should('not.exist')
+        cy.get('[data-cy="follow-redirect"]').should('exist')
+        cy.get(`[data-cy="follow-redirect"]`)
+          .should('have.attr', 'href', '/sign-in')
+          .should('have.attr', 'target', '_blank')
+          .should('have.attr', 'rel', 'noopener noreferrer')
+          .then((link) => {
+            cy.request(link.prop('href')).its('status').should('eq', 200)
+          })
+      })
+    })
+
+    describe('[By Authenticated]', () => {
+      it('[Follow button]', () => {
+        cy.step('Should exist')
+        cy.login('demo_beta_tester@example.com', 'demo_beta_tester')
+        cy.get('[data-cy="follow-redirect"]').should('not.exist')
+        cy.get('[data-cy="follow-button"]')
+          .should('exist')
+          .should('contain.text', 'Follow')
+
+        cy.step('Should follow on click')
+        cy.get('[data-cy="follow-button"]')
+          .first()
+          .click()
+          .should('contain.text', 'Following')
+
+        cy.step('Should persist follow status on reload')
+        cy.visit(researchArticleUrl)
+        cy.get('[data-cy="follow-button"]')
+          .first()
+          .click()
+          .should('contain.text', 'Following')
+
+        cy.step('Should unfollow on click')
+        cy.get('[data-cy="follow-button"]')
+          .first()
+          .click()
+          .should('contain.text', 'Follow')
+      })
+    })
+  })
+})

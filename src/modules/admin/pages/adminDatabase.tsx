d29b4fc10d79f9d56657f129bcb4464bead7ce2e
iSCJT <sean@speckledbanana.com>
2023-06-16 12:58:14 +0100

    feat: update admin db page to allow trigger of lastEditTimestamp setting

--- src/modules/admin/pages/adminDatabase.tsx
+++ src/modules/admin/pages/adminDatabase.tsx
@@ -1,40 +1,54 @@
 import { observer } from 'mobx-react'
 import { useState } from 'react'
-import { Button, Flex } from 'theme-ui'
+import { Button, Flex, Heading } from 'theme-ui'
 
 import { functions } from 'src/utils/firebase'
 
 const AdminDB = observer(() => {
   const [triggerMigrationState, setMigrationState] = useState<string>('')
+  const [triggerUpdateState, setUpdateState] = useState<string>('')
+  const [updateRecords, setUpdateRecords] = useState<Record<string, any>>({})
 
-  const [migrationRecords, setMigrationRecords] = useState<Record<string, any>>(
-    {},
-  )
-
-  const howtoList = migrationRecords.howtoUpdates ? (
-    migrationRecords.howtoUpdates.map((howto) => (
+  const howtoList = updateRecords.howtoUpdates ? (
+    updateRecords.howtoUpdates.map((howto) => (
       <li key={howto.id}>
-        {howto.id}
-        <ul>
-          {howto.votedUseful.map((vote) => (
-            <li key={howto.id + '-' + vote}>{vote}</li>
-          ))}
-        </ul>
+        {howto.id}{' '}
+        {howto._lastEditTimestamp ? ' - ' + howto._lastEditTimestamp : ''}
+        {howto.votedUseful && (
+          <ul>
+            {howto.votedUseful.map((vote) => (
+              <li key={howto.id + '-' + vote}>{vote}</li>
+            ))}
+          </ul>
+        )}
       </li>
     ))
   ) : (
     <></>
   )
 
-  const researchList = migrationRecords.researchUpdates ? (
-    migrationRecords.researchUpdates.map((research) => (
+  const researchList = updateRecords.researchUpdates ? (
+    updateRecords.researchUpdates.map((research) => (
       <li key={research.id}>
-        {research.id}
-        <ul>
-          {research.votedUseful.map((vote) => (
-            <li key={research.id + '-' + vote}>{vote}</li>
-          ))}
-        </ul>
+        {research.id}{' '}
+        {research._lastEditTimestamp ? ' - ' + research._lastEditTimestamp : ''}
+        {research.votedUseful && (
+          <ul>
+            {research.votedUseful.map((vote) => (
+              <li key={research.id + '-' + vote}>{vote}</li>
+            ))}
+          </ul>
+        )}
+      </li>
+    ))
+  ) : (
+    <></>
+  )
+  const eventList = updateRecords.eventUpdates ? (
+    updateRecords.eventUpdates.map((event) => (
+      <li key={event.id}>
+        {event.id}{' '}
+        {event._lastEditTimestamp ? ' - ' + event._lastEditTimestamp : ''}
       </li>
     ))
   ) : (
@@ -48,7 +62,7 @@ const AdminDB = observer(() => {
         'aggregations-migrate-userUseful',
       )(true)
       setMigrationState('Dry run successful.')
-      setMigrationRecords({ ...operations?.data?.meta })
+      setUpdateRecords({ ...operations?.data?.meta })
     } catch (error) {
       setMigrationState(`Error during dry run: \n ${error}`)
     }
@@ -60,23 +74,70 @@ const AdminDB = observer(() => {
         'aggregations-migrate-userUseful',
       )(false)
       setMigrationState('Migration successful.')
-      setMigrationRecords({ ...operations?.data?.meta })
+      setUpdateRecords({ ...operations?.data?.meta })
     } catch (error) {
       setMigrationState(`Error migrating useful: \n ${error}`)
+    }
+  }
+
+  const updateLastEditDryRun = async () => {
+    setUpdateState('Performing last edit timestamp dry run...')
+    try {
+      const operations = await functions.httpsCallable(
+        'database-updates-lastEditTimestamp',
+      )(true)
+      setUpdateState('Dry run successful.')
+      setUpdateRecords({ ...operations?.data?.meta })
+    } catch (error) {
+      setUpdateState(`Error during dry run: \n ${error}`)
+    }
+  }
+  const updateLastEdit = async () => {
+    setUpdateState('Setting last edit timestamps...')
+    try {
+      const operations = await functions.httpsCallable(
+        'database-updates-lastEditTimestamp',
+      )(false)
+      setUpdateState('Last edit timestamps set successfully.')
+      setUpdateRecords({ ...operations?.data?.meta })
+    } catch (error) {
+      setUpdateState(`Error setting last edit timestamps: \n ${error}`)
     }
   }
 
   return (
     <>
       <h2>Database Operations</h2>
-      <Flex>
-        <Button sx={{ marginRight: '10px' }} onClick={migrateUsefulDryRun}>
-          Dry run migrate user useful
-        </Button>
-        <Button sx={{ marginRight: '10px' }} onClick={migrateUseful}>
-          Migrate user useful
-        </Button>
-        {triggerMigrationState && <p>{triggerMigrationState}</p>}
+      <Flex mb={6} sx={{ flexDirection: 'column' }}>
+        <Flex mb={4} sx={{ flexDirection: 'column' }}>
+          <Heading mb={2} variant="small" color={'black'}>
+            Set last edit timestamps
+          </Heading>
+          <Flex sx={{ flexDirection: 'row' }}>
+            <Button sx={{ marginRight: '10px' }} onClick={updateLastEditDryRun}>
+              Dry run setting last edit timestamps
+            </Button>
+            <Button sx={{ marginRight: '10px' }} onClick={updateLastEdit}>
+              Set last edit timestamps
+            </Button>
+            {triggerUpdateState && <p>{triggerUpdateState}</p>}
+          </Flex>
+        </Flex>
+
+        <Flex sx={{ flexDirection: 'column' }}>
+          <Heading mb={2} variant="small" color={'black'}>
+            Migrate user useful
+          </Heading>
+          <Flex sx={{ flexDirection: 'row' }}>
+            <Button sx={{ marginRight: '10px' }} onClick={migrateUsefulDryRun}>
+              Dry run migrate user useful
+            </Button>
+            <Button sx={{ marginRight: '10px' }} onClick={migrateUseful}>
+              Migrate user useful
+            </Button>
+            {triggerMigrationState && <p>{triggerMigrationState}</p>}
+          </Flex>
+        </Flex>
       </Flex>
 
       <Flex
@@ -86,18 +147,31 @@ const AdminDB = observer(() => {
           wordWrap: 'break-word',
         }}
       >
-        {migrationRecords.howtoUpdates && (
-          <Flex sx={{ flexDirection: 'column', width: '50%' }}>
-            <h3>How-to Migrations - {migrationRecords.howtoUpdates.length}</h3>
+        {updateRecords.howtoUpdates && (
+          <Flex sx={{ flexDirection: 'column', width: '33%' }}>
+            <h3>
+              How-to {triggerMigrationState != '' ? 'Migrations' : 'Updates'} -{' '}
+              {updateRecords.howtoUpdates.length}
+            </h3>
             <ul>{howtoList}</ul>
           </Flex>
         )}
-        {migrationRecords.researchUpdates && (
-          <Flex sx={{ flexDirection: 'column', width: '50%' }}>
+        {updateRecords.researchUpdates && (
+          <Flex sx={{ flexDirection: 'column', width: '33%' }}>
             <h3>
-              Research Migrations - {migrationRecords.researchUpdates.length}
+              Research {triggerMigrationState != '' ? 'Migrations' : 'Updates'}{' '}
+              - {updateRecords.researchUpdates.length}
             </h3>
             <ul>{researchList}</ul>
+          </Flex>
+        )}
+        {updateRecords.eventUpdates && (
+          <Flex sx={{ flexDirection: 'column', width: '33%' }}>
+            <h3>
+              Event {triggerMigrationState != '' ? 'Migrations' : 'Updates'} -{' '}
+              {updateRecords.eventUpdates.length}
+            </h3>
+            <ul>{eventList}</ul>
           </Flex>
         )}
       </Flex>

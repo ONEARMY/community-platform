42e5a2574225192be35a471bb7d819391e03b349
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-06-19 21:16:33 +0300

    fix: remove use of theme from logo

--- src/pages/common/Header/Menu/Logo/Logo.tsx
+++ src/pages/common/Header/Menu/Logo/Logo.tsx
@@ -1,29 +1,13 @@
 import { Component } from 'react'
-// TODO: Remove direct usage of Theme
-import { preciousPlasticTheme } from 'oa-themes'
-const theme = preciousPlasticTheme.styles
-import { Flex, Image, Text } from 'theme-ui'
-import styled from '@emotion/styled'
+import { Flex, Image, Text, Box } from 'theme-ui'
 import { inject, observer } from 'mobx-react'
 import type { ThemeStore } from 'src/stores/Theme/theme.store'
-
 import { VERSION } from 'src/config/config'
 import { Link } from 'react-router-dom'
 
 interface IProps {
   isMobile?: boolean
 }
-
-const LogoContainer = styled(Flex)`
-  align-items: center;
-  position: relative;
-  padding: 10px 0;
-
-  @media only screen and (min-width: ${theme.breakpoints[1]}) {
-    margin-bottom: -50px;
-    padding: 0;
-  }
-`
 
 interface InjectedProps {
   themeStore: ThemeStore
@@ -44,44 +28,49 @@ export class Logo extends Component<IProps> {
   render() {
     const name = this.injected.themeStore?.currentTheme.siteName
     const logo = this.injected.themeStore?.currentTheme.logo
+
     const nameAndVersion = `${name} logo ${VERSION}`
 
     return (
-      <>
-        <LogoContainer>
-          <Link to="/">
-            <Flex
-              ml={[0, 4]}
+      <Box
+        sx={{
+          py: ['10px', '10px', 0], // padding on y axes ( top & bottom )
+          marginBottom: [0, 0, '-50px'],
+          position: 'relative',
+        }}
+      >
+        <Link to="/">
+          <Flex
+            ml={[0, 4]}
+            sx={{
+              zIndex: 1000,
+              display: 'flex',
+              alignItems: 'center',
+              width: ['50px', '50px', '100px'],
+              height: ['50px', '50px', '100px'],
+            }}
+          >
+            <Image
+              loading="lazy"
+              src={logo}
               sx={{
-                zIndex: 1000,
-                display: 'flex',
-                alignItems: 'center',
-                width: ['50px', '50px', '100px'],
-                height: ['50px', '50px', '100px'],
+                width: [50, 50, 100],
+                height: [50, 50, 100],
               }}
-            >
-              <Image
-                loading="lazy"
-                src={logo}
-                sx={{
-                  width: [50, 50, 100],
-                  height: [50, 50, 100],
-                }}
-                alt={nameAndVersion}
-                title={nameAndVersion}
-              />
-            </Flex>
-            <Text
-              className="sr-only"
-              ml={2}
-              sx={{ display: ['none', 'none', 'block'] }}
-              color="black"
-            >
-              Precious Plastic
-            </Text>
-          </Link>
-        </LogoContainer>
-      </>
+              alt={nameAndVersion}
+              title={nameAndVersion}
+            />
+          </Flex>
+          <Text
+            className="sr-only"
+            ml={2}
+            sx={{ display: ['none', 'none', 'block'] }}
+            color="black"
+          >
+            {name}
+          </Text>
+        </Link>
+      </Box>
     )
   }
 }

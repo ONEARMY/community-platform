b035d144386c6097b338fa3dca3bc5635dd1cece
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-06-28 08:40:22 +0300

    refactor:  feedback implemented

---
+++ packages/themes/src/common/commonStyles.ts
@@ -0,0 +1,46 @@
+export const commonStyles = {
+  input: {
+    borderRadius: 1,
+    '&:focus': {
+      borderColor: '#83ceeb',
+      outline: 'none',
+      boxShadow: 'none',
+    },
+  },
+  colors: {
+    white: 'white',
+    offwhite: '#ececec',
+    black: '#1b1b1b',
+    softyellow: '#f5ede2',
+    blue: '#83ceeb',
+    red: '#eb1b1f',
+    red2: '#f58d8e',
+    softblue: '#e2edf7',
+    bluetag: '#5683b0',
+    grey: '#61646b',
+    green: '#00c3a9',
+    error: 'red',
+    background: '#f4f6f7',
+    silver: '#c0c0c0',
+    softgrey: '#c2d4e4',
+    lightgrey: '#ababac',
+    darkGrey: '#686868',
+    subscribed: 'orange',
+    notSubscribed: '#1b1b1b',
+    betaGreen: '#98cc98',
+  },
+  alert: {
+    borderRadius: 1,
+    paddingX: 3,
+    paddingY: 3,
+    textAlign: 'center',
+    fontWeight: 'normal',
+  },
+  fontFamily: {
+    body: `'Inter', Arial, sans-serif`,
+  },
+  space: [
+    0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90,
+    95, 100, 105, 110, 115, 120, 125, 130, 135, 140,
+  ],
+}
--- packages/themes/src/common/index.ts
+++ packages/themes/src/common/index.ts
@@ -1,55 +1,7 @@
-class AuxiliaryStyle {
-  static _commonInputStyle = {
-    borderRadius: 1,
-    '&:focus': {
-      borderColor: '#83ceeb',
-      outline: 'none',
-      boxShadow: 'none',
-    },
-  }
-
-  static _colors = {
-    white: 'white',
-    offwhite: '#ececec',
-    black: '#1b1b1b',
-    softyellow: '#f5ede2',
-    blue: '#83ceeb',
-    red: '#eb1b1f',
-    red2: '#f58d8e',
-    softblue: '#e2edf7',
-    bluetag: '#5683b0',
-    grey: '#61646b',
-    green: '#00c3a9',
-    error: 'red',
-    background: '#f4f6f7',
-    silver: '#c0c0c0',
-    softgrey: '#c2d4e4',
-    lightgrey: '#ababac',
-    darkGrey: '#686868',
-    subscribed: 'orange',
-    notSubscribed: '#1b1b1b',
-    betaGreen: '#98cc98',
-  }
-
-  static _commonAlertsStyle = {
-    borderRadius: 1,
-    paddingX: 3,
-    paddingY: 3,
-    textAlign: 'center',
-    fontWeight: 'normal',
-  }
-
-  static _commonFontFamily = {
-    body: `'Inter', Arial, sans-serif`,
-  }
-  static _commonSpace = [
-    0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90,
-    95, 100, 105, 110, 115, 120, 125, 130, 135, 140,
-  ]
-}
+import { commonStyles } from './commonStyles'
 
 export const baseTheme = {
-  colors: AuxiliaryStyle._colors,
+  colors: commonStyles.colors,
   zIndex: {
     behind: -1,
     level: 0,
@@ -81,51 +33,51 @@ export const baseTheme = {
     auxiliary: {
       fontFamily: '"Inter", Helvetica Neue, Arial, sans-serif;',
       fontSize: 1,
-      color: AuxiliaryStyle._colors.grey,
+      color: commonStyles.colors.grey,
     },
     paragraph: {
       fontFamily: '"Inter", Helvetica Neue, Arial, sans-serif;',
       fontSize: '16px',
-      color: AuxiliaryStyle._colors.grey,
+      color: commonStyles.colors.grey,
     },
   },
-  space: AuxiliaryStyle._commonSpace,
-  radii: AuxiliaryStyle._commonSpace,
-  fonts: AuxiliaryStyle._commonFontFamily,
+  space: commonStyles.space,
+  radii: commonStyles.space,
+  fonts: commonStyles.fontFamily,
 
   forms: {
     input: {
-      ...AuxiliaryStyle._commonInputStyle,
-      background: AuxiliaryStyle._colors.background,
+      ...commonStyles.input,
+      background: commonStyles.colors.background,
       border: '1px solid transparent',
-      fontFamily: AuxiliaryStyle._commonFontFamily.body,
+      fontFamily: commonStyles.fontFamily.body,
       fontSize: 1,
     },
     inputOutline: {
-      ...AuxiliaryStyle._commonInputStyle,
+      ...commonStyles.input,
       background: 'white',
-      border: `2px solid ${AuxiliaryStyle._colors.black}`,
+      border: `2px solid ${commonStyles.colors.black}`,
     },
     error: {
-      ...AuxiliaryStyle._commonInputStyle,
-      background: AuxiliaryStyle._colors.background,
-      border: `1px solid ${AuxiliaryStyle._colors.error}`,
-      fontFamily: AuxiliaryStyle._commonFontFamily.body,
+      ...commonStyles.input,
+      background: commonStyles.colors.background,
+      border: `1px solid ${commonStyles.colors.error}`,
+      fontFamily: commonStyles.fontFamily.body,
       fontSize: 1,
     },
     textarea: {
-      ...AuxiliaryStyle._commonInputStyle,
-      background: AuxiliaryStyle._colors.background,
+      ...commonStyles.input,
+      background: commonStyles.colors.background,
       border: `1px solid transparent`,
-      fontFamily: AuxiliaryStyle._commonFontFamily.body,
+      fontFamily: commonStyles.fontFamily.body,
       fontSize: 1,
       padding: 2,
     },
     textareaError: {
-      ...AuxiliaryStyle._commonInputStyle,
-      background: AuxiliaryStyle._colors.background,
-      border: `1px solid ${AuxiliaryStyle._colors.error}`,
-      fontFamily: AuxiliaryStyle._commonFontFamily.body,
+      ...commonStyles.input,
+      background: commonStyles.colors.background,
+      border: `1px solid ${commonStyles.colors.error}`,
+      fontFamily: commonStyles.fontFamily.body,
       fontSize: 1,
       padding: 2,
     },
@@ -140,7 +92,7 @@ export const baseTheme = {
   cards: {
     primary: {
       background: 'white',
-      border: `2px solid ${AuxiliaryStyle._colors.black}`,
+      border: `2px solid ${commonStyles.colors.black}`,
       borderRadius: 1,
       overflow: 'hidden',
     },
@@ -148,12 +100,12 @@ export const baseTheme = {
   breakpoints: ['40em', '52em', '70em'], // standard widths: 512px, 768px, 1024px
   alerts: {
     success: {
-      ...AuxiliaryStyle._commonAlertsStyle,
-      backgroundColor: AuxiliaryStyle._colors.green,
+      ...commonStyles.alert,
+      backgroundColor: commonStyles.colors.green,
     },
     failure: {
-      ...AuxiliaryStyle._commonAlertsStyle,
-      backgroundColor: AuxiliaryStyle._colors.red2,
+      ...commonStyles.alert,
+      backgroundColor: commonStyles.colors.red2,
     },
   },
 }

8c50a64c07c5b1e25145cbfcdfb4a727124577bc
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-06-28 19:48:00 +0300

    feat: style the filter & sort research

--- src/pages/Research/Content/ResearchList.tsx
+++ src/pages/Research/Content/ResearchList.tsx
@@ -4,11 +4,12 @@ import { Button, Select } from 'oa-components'
 import { Link, useHistory } from 'react-router-dom'
 import { AuthWrapper } from 'src/common/AuthWrapper'
 import { useResearchStore } from 'src/stores/Research/research.store'
-import { Box, Flex, Grid, Heading, Input } from 'theme-ui'
+import { Box, Flex, Heading, Input } from 'theme-ui'
 import ResearchListItem from './ResearchListItem'
 import type { RouteComponentProps } from 'react-router'
 import { CategoriesSelect } from 'src/pages/Howto/Category/CategoriesSelect'
 import { FieldContainer } from 'src/common/Form/FieldContainer'
+import { useState } from 'react'
 
 // Update query params for categories
 const updateQueryParams = (
@@ -45,7 +46,7 @@ const ResearchList = observer(() => {
       value: label.toLowerCase(),
     }),
   )
-  const state = { value: '' }
+  const [sortState, setSortState] = useState('')
   const { filteredResearches, searchValue } = store
   return (
     <>
@@ -62,91 +63,76 @@ const ResearchList = observer(() => {
         </Heading>
       </Flex>
 
-      <Grid
+      <Flex
         sx={{
-          gridTemplateColumns: ['unset', '2fr minmax(274px, 1fr)', null],
-          gap: '60px',
-          padding: 2,
-          px: 0,
-          mb: 2,
+          flexWrap: 'nowrap',
+          justifyContent: 'space-between',
+          flexDirection: ['column', 'column', 'row'],
+          mb: 3,
         }}
       >
-        <Flex
-          sx={{
-            flexWrap: 'nowrap',
-            gap: '2em',
-            flexDirection: ['column', 'column', 'row'],
-          }}
-        >
-          <Flex sx={{ width: ['100%', '240px', '240px'] }}>
-            <CategoriesSelect
-              value={
-                store.selectedCategory
-                  ? { label: store.selectedCategory }
-                  : null
-              }
-              onChange={(category) => {
-                updateQueryParams(
-                  window.location.href,
-                  'category',
-                  category ? category.label : '',
-                  history,
-                )
-                store.updateSelectedCategory(category ? category.label : '')
-              }}
-              placeholder="Filter by category"
-              isForm={false}
-              type="research"
-            />
-          </Flex>
-          <Flex
-            ml={[0, 0, '8px']}
-            mb={['10px', '10px', 0]}
-            sx={{ width: ['100%', '100%', '20%'] }}
-          >
-            <FieldContainer>
-              <Select
-                options={sortingOptions}
-                placeholder="Sort by"
-                value={state.value}
-                onChange={(value) =>
-                  store.updateActiveSorter(String(value.value))
-                }
-              />
-            </FieldContainer>
-          </Flex>
-          <Flex ml={[0, 0, '8px']} mr={[0, 0, 'auto']} mb={['10px', '10px', 0]}>
-            <Input
-              variant="inputOutline"
-              data-cy="how-to-search-box"
-              value={searchValue}
-              placeholder="Search for a how-to"
-              onChange={(evt) => {
-                const value = evt.target.value
-                updateQueryParams(
-                  window.location.href,
-                  'search',
-                  value,
-                  history,
-                )
-                store.updateSearchValue(value)
-              }}
-            />
-          </Flex>
-
-          <Flex sx={{ justifyContent: ['flex-end', 'flex-end', 'auto'] }}>
-            <Box sx={{ width: '100%', display: 'block' }} mb={[3, 3, 0]}>
-              <Link to={store.activeUser ? '/research/create' : 'sign-up'}>
-                <AuthWrapper roleRequired="beta-tester">
-                  <Button variant={'primary'} data-cy="create">
-                    Add Research
-                  </Button>
-                </AuthWrapper>
-              </Link>
-            </Box>
-          </Flex>
+        <Flex sx={{ width: ['100%', '240px', '240px'] }}>
+          <CategoriesSelect
+            value={
+              store.selectedCategory ? { label: store.selectedCategory } : null
+            }
+            onChange={(category) => {
+              updateQueryParams(
+                window.location.href,
+                'category',
+                category ? category.label : '',
+                history,
+              )
+              store.updateSelectedCategory(category ? category.label : '')
+            }}
+            placeholder="Filter by category"
+            isForm={false}
+            type="research"
+          />
         </Flex>
-      </Grid>
+        <Flex
+          ml={[0, 0, '8px']}
+          mb={['10px', '10px', 0]}
+          sx={{ width: ['100%', '100%', '20%'] }}
+        >
+          <FieldContainer>
+            <Select
+              options={sortingOptions}
+              placeholder="Sort by"
+              value={sortState}
+              onChange={(value) => {
+                store.updateActiveSorter(String(value.value))
+                setSortState(value)
+              }}
+            />
+          </FieldContainer>
+        </Flex>
+        <Flex ml={[0, 0, '8px']} mr={[0, 0, 'auto']} mb={['10px', '10px', 0]}>
+          <Input
+            variant="inputOutline"
+            data-cy="how-to-search-box"
+            value={searchValue}
+            placeholder="Search for a research"
+            onChange={(evt) => {
+              const value = evt.target.value
+              updateQueryParams(window.location.href, 'search', value, history)
+              store.updateSearchValue(value)
+            }}
+          />
+        </Flex>
+
+        <Flex sx={{ justifyContent: ['flex-end', 'flex-end', 'auto'] }}>
+          <Box sx={{ width: '100%', display: 'block' }} mb={[3, 3, 0]}>
+            <Link to={store.activeUser ? '/research/create' : 'sign-up'}>
+              <AuthWrapper roleRequired="beta-tester">
+                <Button variant={'primary'} data-cy="create">
+                  Add Research
+                </Button>
+              </AuthWrapper>
+            </Link>
+          </Box>
+        </Flex>
+      </Flex>
       {filteredResearches?.length !== 0
         ? filteredResearches.map((item) => {
             const votedUsefulCount = (item.votedUsefulBy || []).length

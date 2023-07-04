accfb4825a79a9347df13b9b6bc2a5ea15cdfd5f
iSCJT <sean@speckledbanana.com>
2023-07-04 17:19:50 +0100

    feat: add redirectToSignIn functionality to DownloadFiles component

--- packages/components/src/DownloadFiles/DownloadFiles.tsx
+++ packages/components/src/DownloadFiles/DownloadFiles.tsx
@@ -1,18 +1,20 @@
 import { Flex, Text } from 'theme-ui'
 import { ExternalLink } from '../ExternalLink/ExternalLink'
 import { Icon } from '../Icon/Icon'
+import { useHistory } from 'react-router-dom'
+import { Tooltip } from 'oa-components'
 
 export interface DownloadFilesProps {
   link: string
   handleClick?: () => Promise<void>
+  redirectToSignIn?: boolean
 }
 
-export const DownloadFiles = (props: DownloadFilesProps) => {
+const DownloadButton = (props: { redirectToSignIn: boolean }) => {
+  const history = useHistory()
+
   return (
-    <ExternalLink
-      href={props.link}
-      onClick={() => props.handleClick && props.handleClick()}
-    >
+    <>
       <Flex
         p={2}
         mb={1}
@@ -25,12 +27,34 @@ export const DownloadFiles = (props: DownloadFilesProps) => {
           maxWidth: '300px',
           borderRadius: 1,
         }}
+        onClick={() =>
+          props.redirectToSignIn ? history.push('/sign-in') : undefined
+        }
+        data-tip={props.redirectToSignIn ? 'Login to download' : ''}
       >
         <Icon size={24} glyph={'external-url'} mr={3} />
         <Text sx={{ flex: 1, fontSize: 1, color: 'black' }} mr={3}>
           Download files
         </Text>
       </Flex>
-    </ExternalLink>
+      <Tooltip />
+    </>
+  )
+}
+
+export const DownloadFiles = (props: DownloadFilesProps) => {
+  return (
+    <>
+      {!props.redirectToSignIn ? (
+        <ExternalLink
+          href={props.link}
+          onClick={() => props.handleClick && props.handleClick()}
+        >
+          <DownloadButton redirectToSignIn={false} />
+        </ExternalLink>
+      ) : (
+        <DownloadButton redirectToSignIn={props.redirectToSignIn || false} />
+      )}
+    </>
   )
 }

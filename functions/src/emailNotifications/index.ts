96330174c94a6cad00d0542e7465dbbd6aa5320b
Eva Killenberg <evakill@seas.upenn.edu>
2023-07-18 21:33:16 -0400

    fix: increase memory limit

--- functions/src/emailNotifications/index.ts
+++ functions/src/emailNotifications/index.ts
@@ -5,8 +5,11 @@ import { DB_ENDPOINTS, IUserDB } from '../models'
 import { EmailNotificationFrequency } from 'oa-shared'
 import { withErrorAlerting } from '../alerting/errorAlerting'
 
-exports.sendDaily = functions.pubsub
-  // Trigger daily at 3pm PT (https://crontab.guru/#0_15_*_*_*)
+const EMAIL_FUNCTION_MEMORY_LIMIT = '512MB'
+
+exports.sendDaily = functions
+  .runWith({ memory: EMAIL_FUNCTION_MEMORY_LIMIT })
+  .pubsub // Trigger daily at 3pm PT (https://crontab.guru/#0_15_*_*_*)
   .schedule('0 15 * * *')
   .timeZone('Europe/Lisbon')
   .onRun((context) =>
@@ -15,8 +18,9 @@ exports.sendDaily = functions.pubsub
     ),
   )
 
-exports.sendWeekly = functions.pubsub
-  // Trigger weekly at 3pm PT on Sunday (https://crontab.guru/#0_15_*_*_0)
+exports.sendWeekly = functions
+  .runWith({ memory: EMAIL_FUNCTION_MEMORY_LIMIT })
+  .pubsub // Trigger weekly at 3pm PT on Sunday (https://crontab.guru/#0_15_*_*_0)
   .schedule('0 15 * * 0')
   .timeZone('Europe/Lisbon')
   .onRun(async (context) =>
@@ -25,8 +29,9 @@ exports.sendWeekly = functions.pubsub
     ),
   )
 
-exports.sendMonthly = functions.pubsub
-  // Trigger monthly at 3pm PT on the first day of the month (https://crontab.guru/#0_15_1_*_*)
+exports.sendMonthly = functions
+  .runWith({ memory: EMAIL_FUNCTION_MEMORY_LIMIT })
+  .pubsub // Trigger monthly at 3pm PT on the first day of the month (https://crontab.guru/#0_15_1_*_*)
   .schedule('0 15 1 * *')
   .timeZone('Europe/Lisbon')
   .onRun(async (context) =>
@@ -35,37 +40,39 @@ exports.sendMonthly = functions.pubsub
     ),
   )
 
-exports.sendOnce = functions.https.onCall(async (_, context) => {
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      'failed-precondition',
-      'The function must be called while authenticated.',
-    )
-  }
-  // Validate user exists and has admin status before triggering function.
-  const { uid: authId } = context.auth
-  const user = await db
-    .collection(DB_ENDPOINTS.users)
-    .where('_authID', '==', authId)
-    .get()
+exports.sendOnce = functions
+  .runWith({ memory: EMAIL_FUNCTION_MEMORY_LIMIT })
+  .https.onCall(async (_, context) => {
+    if (!context.auth) {
+      throw new functions.https.HttpsError(
+        'failed-precondition',
+        'The function must be called while authenticated.',
+      )
+    }
+    // Validate user exists and has admin status before triggering function.
+    const { uid: authId } = context.auth
+    const user = await db
+      .collection(DB_ENDPOINTS.users)
+      .where('_authID', '==', authId)
+      .get()
 
-  if (user) {
-    const { userRoles } = user.docs[0].data() as IUserDB
-    if (userRoles?.some((role) => ['admin', 'super-admin'].includes(role))) {
-      try {
-        await createNotificationEmails()
-        return 'OK'
-      } catch (error) {
-        console.error(error)
-        throw new functions.https.HttpsError(
-          'internal',
-          'There was an error creating emails.',
-        )
+    if (user) {
+      const { userRoles } = user.docs[0].data() as IUserDB
+      if (userRoles?.some((role) => ['admin', 'super-admin'].includes(role))) {
+        try {
+          await createNotificationEmails()
+          return 'OK'
+        } catch (error) {
+          console.error(error)
+          throw new functions.https.HttpsError(
+            'internal',
+            'There was an error creating emails.',
+          )
+        }
       }
     }
-  }
-  throw new functions.https.HttpsError(
-    'permission-denied',
-    'Emails can be triggered by admins only.',
-  )
-})
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Emails can be triggered by admins only.',
+    )
+  })

863704c5cc3d508636aab5b6f6ad27677625a078
iSCJT <iSCJT@users.noreply.github.com>
2023-07-31 09:46:48 +0000

    deploy: 0e3ace8e744519f384a070327e6fb68ab9599967

---
+++ storybook-static/assets/MapWithDraggablePin.stories-e001bedf.js
@@ -0,0 +1,14 @@
+import{a}from"./jsx-runtime-913be41c.js";import{M as n}from"./UsefulStatsButton-013849b8.js";import"./index-2506bfc3.js";import"./Button-c5e128ed.js";import"./Icon-3a85d23f.js";import"./theme-ui-core-jsx-runtime.esm-9e16d62c.js";import"./theme-ui-css.esm-b19fe7ec.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-11801e73.js";import"./parseProps-376f43a7.esm-45b791e5.js";import"./emotion-styled.browser.esm-09c6e9f5.js";import"./icon-verified-badge-7d7bdb14.js";import"./icon-star-active-9fadbee7.js";import"./theme-ui-components.esm-a8d58c50.js";import"./emotion-react.browser.esm-aba50b66.js";import"./CategoryTag-627d389c.js";import"./CreateComment-7b36302c.js";import"./react-router-dom-66940fa3.js";import"./react-router-e884db87.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./index-4d501b15.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./MemberBadge-014b757b.js";import"./ExternalLink-443ce632.js";import"./ElWithBeforeIcon-73c64692.js";import"./FieldDatepicker-172be27f.js";import"./FieldInput-930d9e68.js";import"./CharacterCount-16bb8a2c.js";import"./FieldTextarea-33977323.js";import"./Username-70498745.js";import"./InternalLink-902afeab.js";import"./GlobalStyles-53eb644e.js";import"./ImageGallery-255f7915.js";import"./index-0dfb046a.js";import"./Loader-f4e86c64.js";import"./MapMemberCard-19b149ac.js";import"./Modal-f4c8fc37.js";import"./ModerationStatus-bbfa809f.js";import"./MoreContainer-1b7417b0.js";import"./NotificationItem-238a2565.js";import"./NotificationList-83809977.js";import"./OsmGeocoding-74b7ec6b.js";import"./ProfileLink-faa12960.js";import"./ResearchEditorOverview-96b642b4.js";import"./Select-14502d64.js";import"./isNativeReflectConstruct-98f790dd.js";import"./TextNotification-72465045.js";import"./Tooltip-9f520354.js";import"./UserStatistics-6495de38.js";import"./VideoPlayer-39670afb.js";import"./iframe-bbe4f2a1.js";import"../sb-preview/runtime.js";import"./cjs-89e33937.js";const at={title:"Components/MapWithDraggablePin",component:n},t=()=>{const o={lat:0,lng:0};return a(n,{position:o,updatePosition:i=>{o.lat=i.lat,o.lng=i.lng}})};var r,p,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`() => {
+  const position = {
+    lat: 0,
+    lng: 0
+  };
+  return <MapWithDraggablePin position={position} updatePosition={(_position: {
+    lat: number;
+    lng: number;
+  }) => {
+    position.lat = _position.lat;
+    position.lng = _position.lng;
+  }} />;
+}`,...(m=(p=t.parameters)==null?void 0:p.docs)==null?void 0:m.source}}};const st=["Default"];export{t as Default,st as __namedExportsOrder,at as default};
+//# sourceMappingURL=MapWithDraggablePin.stories-e001bedf.js.map
---
+++ storybook-static/assets/SiteFooter.stories-b8be5a6e.js
@@ -0,0 +1,35 @@
+import{j as e,a as o}from"./jsx-runtime-913be41c.js";import{n as p}from"./emotion-styled.browser.esm-09c6e9f5.js";import"./UsefulStatsButton-013849b8.js";import"./Button-c5e128ed.js";import"./CategoryTag-627d389c.js";import"./CreateComment-7b36302c.js";import"./ElWithBeforeIcon-73c64692.js";import{E as x}from"./ExternalLink-443ce632.js";import"./FieldDatepicker-172be27f.js";import"./FieldInput-930d9e68.js";import"./FieldTextarea-33977323.js";import"./Username-70498745.js";import"./GlobalStyles-53eb644e.js";import{I as f}from"./Icon-3a85d23f.js";import"./ImageGallery-255f7915.js";import"./InternalLink-902afeab.js";import"./Loader-f4e86c64.js";import"./MapMemberCard-19b149ac.js";import"./MemberBadge-014b757b.js";import"./Modal-f4c8fc37.js";import"./ModerationStatus-bbfa809f.js";import"./MoreContainer-1b7417b0.js";import"./NotificationItem-238a2565.js";import"./NotificationList-83809977.js";import"./OsmGeocoding-74b7ec6b.js";import"./ProfileLink-faa12960.js";import"./ResearchEditorOverview-96b642b4.js";import"./Select-14502d64.js";import"./TextNotification-72465045.js";import"./Tooltip-9f520354.js";import"./UserStatistics-6495de38.js";import"./VideoPlayer-39670afb.js";import{u as g}from"./theme-ui-css.esm-b19fe7ec.js";import{F as u,T as n}from"./theme-ui-components.esm-a8d58c50.js";import"./index-2506bfc3.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-11801e73.js";import"./react-router-e884db87.js";import"./inheritsLoose-d541526f.js";import"./setPrototypeOf-0bb37fbe.js";import"./index-4d501b15.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./isNativeReflectConstruct-98f790dd.js";import"./react-router-dom-66940fa3.js";import"./CharacterCount-16bb8a2c.js";import"./icon-verified-badge-7d7bdb14.js";import"./emotion-react.browser.esm-aba50b66.js";import"./theme-ui-core-jsx-runtime.esm-9e16d62c.js";import"./parseProps-376f43a7.esm-45b791e5.js";import"./icon-star-active-9fadbee7.js";import"./index-0dfb046a.js";import"./iframe-bbe4f2a1.js";import"../sb-preview/runtime.js";import"./cjs-89e33937.js";const l=()=>{const t=g(),m=310,i=p(x)`
+    color: #fff;
+    text-decoration: underline;
+  `,c=p(u)`
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    margin-top: 45px;
+    line-heigh: 1.5;
+    padding: 45px ${t.space[4]}px;
+    position: relative;
+    text-align: center;
+
+    @media only screen and (min-width: ${t.breakpoints[1]}) and (max-width: ${t.breakpoints[2]}) {
+      align-items: flex-start;
+      padding-top: 35px;
+      padding-bottom: 35px;
+      padding-left: 65px;
+      padding-right: ${m}px;
+      text-align: left;
+    }
+
+    @media only screen and (min-width: ${t.breakpoints[2]}) {
+      flex-direction: row;
+      padding-right: ${m}px;
+      text-align: left;
+    }
+  `,h=p(f)`
+    @media only screen and (min-width: ${t.breakpoints[1]}) and (max-width: ${t.breakpoints[2]}) {
+      position: absolute;
+      top: 45px;
+      left: 30px;
+    }
+  `;return e(c,{bg:"#27272c",sx:{alignItems:"center"},style:{marginTop:"45px"},children:[o(h,{glyph:"star-active",mb:[3,3,0]}),e(n,{ml:[0,0,0,3],mr:1,children:[t.name," is a project by"," ",o(i,{href:"https://onearmy.earth/",children:"One Army"}),"."]}),e(n,{mt:[2,2,0],children:["Please"," ",o(i,{href:"https://www.patreon.com/one_army",children:"sponsor the work"})," ","or"," ",o(i,{href:"https://platform.onearmy.earth/",children:"help us build the software"}),"."]})]})},ut={title:"Components/SiteFooter",component:l},r=()=>o(l,{});var a,s,d;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:"() => <SiteFooter />",...(d=(s=r.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};const yt=["Default"];export{r as Default,yt as __namedExportsOrder,ut as default};
+//# sourceMappingURL=SiteFooter.stories-b8be5a6e.js.map

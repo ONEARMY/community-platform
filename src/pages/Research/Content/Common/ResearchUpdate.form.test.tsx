67ad1987c21fd13524c7300918987d9e5d9e7da7
Guilherme Ramalho <g.ramalho@kigroup.de>
2023-08-23 19:56:58 +0100

    feat: linting

--- src/pages/Research/Content/Common/ResearchUpdate.form.test.tsx
+++ src/pages/Research/Content/Common/ResearchUpdate.form.test.tsx
@@ -1,15 +1,15 @@
-import { fireEvent, render } from "@testing-library/react"
-import { Provider } from "mobx-react"
-import { act } from "react-dom/test-utils"
-import { useCommonStores } from "src"
-import { FactoryResearchItemUpdate } from "src/test/factories/ResearchItem"
+import { fireEvent, render } from '@testing-library/react'
+import { Provider } from 'mobx-react'
+import { act } from 'react-dom/test-utils'
+import { useCommonStores } from 'src'
+import { FactoryResearchItemUpdate } from 'src/test/factories/ResearchItem'
 
-import { ThemeProvider } from "theme-ui"
+import { ThemeProvider } from 'theme-ui'
 import { testingThemeStyles } from 'src/test/utils/themeUtils'
-import { MemoryRouter } from "react-router"
-import { ResearchUpdateForm } from "./ResearchUpdate.form"
-import { useResearchStore } from "src/stores/Research/research.store"
-import { FactoryUser } from "src/test/factories/User"
+import { MemoryRouter } from 'react-router'
+import { ResearchUpdateForm } from './ResearchUpdate.form'
+import { useResearchStore } from 'src/stores/Research/research.store'
+import { FactoryUser } from 'src/test/factories/User'
 
 const Theme = testingThemeStyles
 
@@ -21,33 +21,34 @@ jest.mock('src/stores/Research/research.store', () => {
         Images: false,
         Files: false,
         Database: false,
-        Complete: false
+        Complete: false,
       },
       validateTitleForSlug: jest.fn(),
-    })
+    }),
   }
 })
 
 describe('Research update form', () => {
   describe('Invalid file warning', () => {
     it('Does not appear when submitting only fileLink', async () => {
-      const formValues = FactoryResearchItemUpdate({fileLink: 'www.filedonwload.test'})
-  
+      const formValues = FactoryResearchItemUpdate({
+        fileLink: 'www.filedonwload.test',
+      })
+
       let wrapper
       await act(async () => {
         wrapper = await getWrapper(formValues, 'create', {})
-  
       })
 
-      expect(wrapper.queryByTestId('invalid-file-warning')).not.toBeInTheDocument()
+      expect(
+        wrapper.queryByTestId('invalid-file-warning'),
+      ).not.toBeInTheDocument()
     })
 
     it('Does not appear when submitting only files', async () => {
       // Arrange
       const formValues = FactoryResearchItemUpdate({
-        files: [
-          new File(['test file content'], 'test-file.zip')
-        ]
+        files: [new File(['test file content'], 'test-file.zip')],
       })
 
       // Act
@@ -65,15 +66,10 @@ describe('Research update form', () => {
     it('Appears when submitting 2 file types', async () => {
       // Arrange
       const formValues = FactoryResearchItemUpdate({
-        images: [
-          new File(['hello'], 'hello.png')
-        ],
-        files: [
-          new File(['test file content'], 'test-file.zip')
-        ],
-        fileLink: 'www.filedonwload.test'
+        images: [new File(['hello'], 'hello.png')],
+        files: [new File(['test file content'], 'test-file.zip')],
+        fileLink: 'www.filedonwload.test',
       })
-
 
       // Act
       let wrapper
@@ -83,14 +79,11 @@ describe('Research update form', () => {
         const submitFormButton = wrapper.getByTestId('submit-form')
         fireEvent.click(submitFormButton)
       })
-      
+
       // Assert
       expect(wrapper.queryByTestId('invalid-file-warning')).toBeInTheDocument()
     })
   })
-
-  
-
 })
 
 const getWrapper = async (formValues, parentType, navProps) => {
@@ -103,6 +96,6 @@ const getWrapper = async (formValues, parentType, navProps) => {
           {...navProps}
         />
       </MemoryRouter>
-    </ThemeProvider>
+    </ThemeProvider>,
   )
 }

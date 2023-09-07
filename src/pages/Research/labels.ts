29c9548d5ef6217926a8c1534f89ff530a3b056a
Ben Furber <ben.furber@googlemail.com>
2023-09-07 15:55:16 +0100

    refactor: add labels for files to research update

--- src/pages/Research/labels.ts
+++ src/pages/Research/labels.ts
@@ -12,6 +12,7 @@ export const buttons = {
     confirm: 'Delete',
     message: 'Are you sure you want to delete this update?',
   },
+  files: 'Re-upload files (this will delete the existing ones)',
   publish: 'Publish',
 }
 
@@ -60,6 +61,15 @@ export const update: ILabels = {
   description: {
     placeholder: `Explain what is happening in your research (max ${RESEARCH_MAX_LENGTH} characters)`,
     title: 'Description of this update',
+  },
+  fileLink: {
+    title: 'Add a download link',
+    description: 'Link to Google Drive, Dropbox, Grabcad etc',
+  },
+  files: {
+    title: 'Attach your file(s) for this update',
+    description: 'Maximum file size 50MB',
+    error: 'Please provide either a file link or upload a file, not both.',
   },
   title: {
     placeholder: `Title of this update (max ${RESEARCH_TITLE_MAX_LENGTH} characters)`,

197edb22bfadfc7da5ea52ac8ba28b18a2bc88b7
unknown <goratt12@gmail.com>
2023-09-14 17:48:21 +0300

    fix: fix issue #2744, added static notice message

--- src/pages/Howto/Content/Common/HowtoFieldTitle.tsx
+++ src/pages/Howto/Content/Common/HowtoFieldTitle.tsx
@@ -1,5 +1,6 @@
 import { Field } from 'react-final-form'
 import { FieldInput } from 'oa-components'
+import { Text, Card } from 'theme-ui'
 
 import { COMPARISONS } from 'src/utils/comparisons'
 import { composeValidators, minValue, required } from 'src/utils/validators'
@@ -11,6 +12,7 @@ export const HowtoFieldTitle = () => {
   const { placeholder, title } = intro.title
 
   const name = 'title'
+  const duplicateTitle = true
 
   const titleValidation = (value) => {
     const validators = composeValidators(
@@ -35,8 +37,14 @@ export const HowtoFieldTitle = () => {
         minLength={HOWTO_TITLE_MIN_LENGTH}
         maxLength={HOWTO_TITLE_MAX_LENGTH}
         placeholder={placeholder}
-        showCharacterCount
+        showCharacterCount={!duplicateTitle}
       />
+      {duplicateTitle && (<Card sx={{ bg: 'softblue', padding: 2, marginTop: 2 }}>
+        <Text sx={{ fontSize: 1, margin: 0 }}>
+          Did you know there is an existing how-to with the title Existing how-to Title? 
+          Using a unique title helps readers decide which how-to better meet their needs.
+          </Text>
+      </Card>)}
     </FormFieldWrapper>
   )
 }

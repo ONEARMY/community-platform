a15a621df30552419a105d6c2613facce3ab0751
Luke Watts <luke@thisis.la>
2023-09-23 15:05:39 +0200

    chore(deps): upgrade react-virtualized

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,19 @@ import svgr from 'vite-plugin-svgr'
 import envCompatible from 'vite-plugin-env-compatible'
 import ViteTsConfigPathsPlugin from 'vite-tsconfig-paths'
 
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import url from 'node:url'
+import { createRequire } from 'node:module'
+import type { PluginOption } from 'vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [
+    reactVirtualized(),
     react({
       babel: {
         parserOpts: {
@@ -63,3 +73,31 @@ export default defineConfig({
     },
   },
 })
+
+
+
+const WRONG_CODE = `import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";`
+
+function reactVirtualized(): PluginOption {
+  return {
+    name: 'flat:react-virtualized',
+    // Note: we cannot use the `transform` hook here
+    //       because libraries are pre-bundled in vite directly,
+    //       plugins aren't able to hack that step currently.
+    //       so instead we manually edit the file in node_modules.
+    //       all we need is to find the timing before pre-bundling.
+    configResolved: async () => {
+      const require = createRequire(import.meta.url)
+      const reactVirtualizedPath = require.resolve('react-virtualized')
+      const { pathname: reactVirtualizedFilePath } = new url.URL(reactVirtualizedPath, import.meta.url)
+      const file = reactVirtualizedFilePath
+        .replace(
+          path.join('dist', 'commonjs', 'index.js'),
+          path.join('dist', 'es', 'WindowScroller', 'utils', 'onScroll.js'),
+        )
+      const code = await fs.readFile(file, 'utf-8')
+      const modified = code.replace(WRONG_CODE, '')
+      await fs.writeFile(file, modified)
+    },
+  }
+}

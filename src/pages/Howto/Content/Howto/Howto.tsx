5ad25f93bb5d36666edda1e1c20bc1508e083d9a
Luke Watts <luke@thisis.la>
2023-09-30 15:06:31 +0200

    fix: failback to empty string

--- src/pages/Howto/Content/Howto/Howto.tsx
+++ src/pages/Howto/Content/Howto/Howto.tsx
@@ -289,7 +289,7 @@ export class Howto extends React.Component<
             pathname: '/how-to',
             search:
               '?search=' +
-              (this.props?.match?.params?.slug).replace(/\-/gi, ' ') +
+              (this.props?.match?.params?.slug || '').replace(/\-/gi, ' ') +
               '&source=how-to-not-found',
           }}
         />

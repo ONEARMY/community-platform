3f79c3dd7545dfe1a9271fa142eeb30d2085b2b0
Luke Watts <luke@thisis.la>
2023-10-08 14:05:46 +0200

    docs: use faker to generate tab example

--- packages/components/src/TabbedContent/TabbedContent.stories.tsx
+++ packages/components/src/TabbedContent/TabbedContent.stories.tsx
@@ -1,9 +1,45 @@
 import type { Meta, StoryFn } from '@storybook/react'
-import { TabbedContent } from './TabbedContent'
+import { Tabs, TabsList, Tab, TabPanel } from './TabbedContent'
+import { faker } from '@faker-js/faker'
 
 export default {
   title: 'Components/TabbedContent',
-  component: TabbedContent,
-} as Meta<typeof TabbedContent>
+} as Meta
 
-export const Default: StoryFn<typeof TabbedContent> = () => <TabbedContent />
+const uppercase = (str: string) => str.charAt(0).toUpperCase() + str.slice(1)
+
+export const Default: StoryFn = () => {
+  return (
+    <Tabs defaultValue={0}>
+      <TabsList>
+        <Tab
+          sx={{
+            backgroundColor: 'transparent',
+          }}
+        >
+          {uppercase(faker.lorem.word())}
+        </Tab>
+        <Tab>{uppercase(faker.lorem.word())}</Tab>
+        <Tab>{uppercase(faker.lorem.word())}</Tab>
+        <Tab>{uppercase(faker.lorem.word())}</Tab>
+        <Tab>{uppercase(faker.lorem.word())}</Tab>
+      </TabsList>
+
+      <TabPanel>
+        <p>{faker.lorem.paragraphs(3)}</p>
+      </TabPanel>
+      <TabPanel>
+        <p>{faker.lorem.paragraphs(3)}</p>
+      </TabPanel>
+      <TabPanel>
+        <p>{faker.lorem.paragraphs(3)}</p>
+      </TabPanel>
+      <TabPanel>
+        <p>{faker.lorem.paragraphs(3)}</p>
+      </TabPanel>
+      <TabPanel>
+        <p>{faker.lorem.paragraphs(3)}</p>
+      </TabPanel>
+    </Tabs>
+  )
+}

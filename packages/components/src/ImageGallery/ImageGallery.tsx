5269e5c9f7f10e52b20b8eb911ba9dec3e060b98
Luke Watts <luke@thisis.la>
2023-10-08 21:20:25 +0200

    feat: add next svg

--- packages/components/src/ImageGallery/ImageGallery.tsx
+++ packages/components/src/ImageGallery/ImageGallery.tsx
@@ -135,6 +135,8 @@ export const ImageGallery = (props: ImageGalleryProps) => {
         />
         <button
           style={{
+            background: 'transparent',
+            border: 0,
             position: 'absolute',
             top: 0,
             right: 0,
@@ -146,7 +148,18 @@ export const ImageGallery = (props: ImageGalleryProps) => {
             )
           }
         >
-          Next
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 24 24"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ width: '60px', height: '60px' }}
+          >
+            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+          </svg>
         </button>
         <button
           style={{

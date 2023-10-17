e8a6c4faa5739b507aa7a8916e4cb085cfb078fb
Luke Watts <luke@thisis.la>
2023-10-17 21:10:56 +0200

    chore: address feedback to model file

--- src/models/question.models.tsx
+++ src/models/question.models.tsx
@@ -1,15 +1,15 @@
 import type { DBDoc, IModerable, ISharedFeatures } from '.'
 
 /**
- * Research retrieved from the database also include metadata such as _id, _created and _modified
+ * Question retrieved from the database also include metadata such as _id, _created and _modified
  */
 export type IQuestionDB = DBDoc & IQuestion.Item
 
-/** All typings related to the Research Module can be found here */
+/** All typings related to the Question Module can be found here */
 type UserIdList = string[]
 
 export namespace IQuestion {
-  /** The main research item, as created by a user */
+  /** The main Question item, as created by a user */
   export type Item = {
     _createdBy: string
     _deleted: boolean

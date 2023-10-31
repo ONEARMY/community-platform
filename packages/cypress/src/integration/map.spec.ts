d990661466ce4848625ba4c64503fc1a78a08cb0
Ben Furber <ben.furber@googlemail.com>
2023-10-31 15:29:59 +0000

    feat: hide member pins on pp only

--- packages/cypress/src/integration/map.spec.ts
+++ packages/cypress/src/integration/map.spec.ts
@@ -1,8 +1,19 @@
 context('map', () => {
-  beforeEach(() => {
-    cy.visit('/map')
-  })
-  it('should render an `accepted` pin', () => {
-    cy.get('[class*="leaflet-marker-icon"]').should('exist')
+  describe('member pins', () => {
+    it('by default should render all `accepted` pins', () => {
+      localStorage.setItem('platformTheme', 'project-kamp')
+      cy.visit('/map')
+
+      cy.get('[class*="leaflet-marker-icon"]').should('exist')
+      cy.get('[class*="icon-member"]').should('exist')
+    })
+
+    it('for PP is should render all non-member `accepted` pins', () => {
+      localStorage.setItem('platformTheme', 'precious-plastic')
+      cy.visit('/map')
+
+      cy.get('[class*="leaflet-marker-icon"]').should('exist')
+      cy.get('[class*="icon-member"]').should('not.exist')
+    })
   })
 })

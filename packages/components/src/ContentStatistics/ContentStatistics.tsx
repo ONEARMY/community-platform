c87ddba9143de9d4ba4d4f4a0b7b737f91dd2de9
deniz <yilmaz.deniz.itu@gmail.com>
2023-11-05 14:22:46 +0300

    style: ran prettier

--- packages/components/src/ContentStatistics/ContentStatistics.tsx
+++ packages/components/src/ContentStatistics/ContentStatistics.tsx
@@ -1,7 +1,7 @@
 import { Text, Flex } from 'theme-ui'
 import type { availableGlyphs } from '..'
 import { Icon } from '..'
-import { useState} from 'react'
+import { useState } from 'react'
 import { Button } from '../Button/Button'
 
 export interface IProps {
@@ -12,76 +12,76 @@ export interface IProps {
 }
 
 export const ContentStatistics = (props: IProps) => {
-
   const [showStats, setShowStats] = useState(false)
 
   const handleShowStats = () => {
     setShowStats(!showStats)
   }
-  
+
   return (
-  <Flex
-    data-cy={'ContentStatistics'}
-    py={1}
-    sx={{ 
-      alignItems: ['flex-start','center','center'], 
-      justifyContent: 'center', 
-      gap: 2, 
-      flexDirection: ['column','row', 'row'],
-      pl: [2,0,0]
-    }}
-  >
-    <Flex 
+    <Flex
+      data-cy={'ContentStatistics'}
+      py={1}
       sx={{
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        display: ['flex', 'none', 'none'],
-        width: '100%',
-        pr: 2,
-        pl: 2
+        alignItems: ['flex-start', 'center', 'center'],
+        justifyContent: 'center',
+        gap: 2,
+        flexDirection: ['column', 'row', 'row'],
+        pl: [2, 0, 0],
       }}
-      onClick={handleShowStats}
     >
-      <Text
-        sx={{
-          fontSize: '13px'
-        }}
-      >
-        {showStats?'':'More Information'}
-      </Text>
-      <Button
-        variant={'subtle'}
-        showIconOnly={true}
-        icon={showStats?'chevron-up':'chevron-down'}
-        small={true}
-        sx={{
-          bg:'white',
-          borderWidth: '0px',
-          '&:hover': {
-            bg: 'white'
-          },
-          '&:active': {
-            bg: 'white'
-          }
-        }}
-      />
-    </Flex>
-    {props.statistics.map((statistic, idx) => (
       <Flex
-        key={idx}
-        px={2}
-        py={1}
-        mb={1}
         sx={{
+          flexDirection: 'row',
           alignItems: 'center',
-          fontSize: '1',
-          display: [showStats?'flex':'none','flex','flex']
+          justifyContent: 'space-between',
+          display: ['flex', 'none', 'none'],
+          width: '100%',
+          pr: 2,
+          pl: 2,
         }}
+        onClick={handleShowStats}
       >
-        <Icon glyph={statistic.icon} mr={1} size={'sm'} opacity={'0.5'} />
-        <Text>{statistic.label}</Text>
+        <Text
+          sx={{
+            fontSize: '13px',
+          }}
+        >
+          {showStats ? '' : 'More Information'}
+        </Text>
+        <Button
+          variant={'subtle'}
+          showIconOnly={true}
+          icon={showStats ? 'chevron-up' : 'chevron-down'}
+          small={true}
+          sx={{
+            bg: 'white',
+            borderWidth: '0px',
+            '&:hover': {
+              bg: 'white',
+            },
+            '&:active': {
+              bg: 'white',
+            },
+          }}
+        />
       </Flex>
-    ))}
-  </Flex>
-)}
+      {props.statistics.map((statistic, idx) => (
+        <Flex
+          key={idx}
+          px={2}
+          py={1}
+          mb={1}
+          sx={{
+            alignItems: 'center',
+            fontSize: '1',
+            display: [showStats ? 'flex' : 'none', 'flex', 'flex'],
+          }}
+        >
+          <Icon glyph={statistic.icon} mr={1} size={'sm'} opacity={'0.5'} />
+          <Text>{statistic.label}</Text>
+        </Flex>
+      ))}
+    </Flex>
+  )
+}

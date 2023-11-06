82b6a6e59d30670bc03612d48c8fe037bcd62dc0
Luke Watts <luke@thisis.la>
2023-11-06 21:24:55 +0100

    test: lift resize setup from jest files

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,9 @@
 import { expect, afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import matchers from '@testing-library/jest-dom/matchers'
+import ResizeObserver from 'resize-observer-polyfill'
+global.ResizeObserver = ResizeObserver
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers)

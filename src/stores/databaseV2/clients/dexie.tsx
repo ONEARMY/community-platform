23183a3604b75ac39efcc8bbcaf58e07871bf4ad
Luke Watts <luke@thisis.la>
2023-11-19 21:31:30 +0100

    feat: increment DB number

--- src/stores/databaseV2/clients/dexie.tsx
+++ src/stores/databaseV2/clients/dexie.tsx
@@ -15,7 +15,7 @@ import { DB_QUERY_DEFAULTS } from '../utils/db.utils'
  * or busting cache on db. This is used as the Dexie version number, see:
  * https://dexie.org/docs/Tutorial/Design#database-versioning
  */
-const DB_CACHE_NUMBER = 20231022
+const DB_CACHE_NUMBER = 20231119
 const CACHE_DB_NAME = 'OneArmyCache'
 const db = new Dexie(CACHE_DB_NAME)
 

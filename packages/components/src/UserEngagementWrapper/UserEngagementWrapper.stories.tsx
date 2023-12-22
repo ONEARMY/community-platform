e5c549171dcb85e24f275eabe4393f8564da5148
Ben Furber <ben.furber@googlemail.com>
2023-12-22 17:53:57 +0000

    feat: add userengagementwrapper

---
+++ packages/components/src/UserEngagementWrapper/UserEngagementWrapper.stories.tsx
@@ -0,0 +1,37 @@
+import { Box, Button } from 'theme-ui'
+
+import { ArticleCallToAction, UsefulStatsButton } from '..'
+import { UserEngagementWrapper } from './UserEngagementWrapper'
+
+import type { Meta, StoryFn } from '@storybook/react'
+
+export default {
+  title: 'Components/UserEngagementWrapper',
+  component: UserEngagementWrapper,
+} as Meta<typeof UserEngagementWrapper>
+
+export const Default: StoryFn<typeof UserEngagementWrapper> = () => (
+  <Box sx={{ maxWidth: '1000px', margin: '0 auto' }}>
+    <UserEngagementWrapper>
+      <Box sx={{ margin: 3 }}>
+        <ArticleCallToAction
+          author={{
+            userName: 'howto._createdBy',
+            countryCode: 'howto.creatorCountry',
+            isVerified: true,
+          }}
+        >
+          <Button sx={{ fontSize: 2 }} onClick={() => null}>
+            Leave a comment
+          </Button>
+          <UsefulStatsButton
+            votedUsefulCount={28}
+            hasUserVotedUseful={false}
+            isLoggedIn={false}
+            onUsefulClick={() => null}
+          />
+        </ArticleCallToAction>
+      </Box>
+    </UserEngagementWrapper>
+  </Box>
+)

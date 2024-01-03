1ae062df76c519f8e1e1706971dbc4d2090a905f
Ben Furber <ben.furber@googlemail.com>
2024-01-03 10:36:22 +0000

    chore: alphabetise icon imports

--- packages/components/src/Icon/svgs.tsx
+++ packages/components/src/Icon/svgs.tsx
@@ -1,45 +1,45 @@
-import loadingSVG from '../../assets/images/loading.svg'
-import flagUnknownSVG from '../../assets/icons/flag-unknown.svg'
-import starSVG from '../../assets/icons/icon-star-default.svg'
-import starActiveSVG from '../../assets/icons/icon-star-active.svg'
-import verifiedSVG from '../../assets/icons/icon-verified-badge.svg'
-import usefulSVG from '../../assets/icons/icon-useful.svg'
-import commentSVG from '../../assets/icons/icon-comment.svg'
+import bazarSVG from '../../assets/icons/icon-bazar.svg'
 import chevronLeftSVG from '../../assets/icons/chevron-left.svg'
 import chevronRightSVG from '../../assets/icons/chevron-right.svg'
-import viewSVG from '../../assets/icons/icon-views.svg'
-import supporterSVG from '../../assets/icons/supporter.svg'
-import socialMediaSVG from '../../assets/icons/icon-social-media.svg'
+import commentSVG from '../../assets/icons/icon-comment.svg'
 import discordSVG from '../../assets/icons/icon-discord.svg'
-import websiteSVG from '../../assets/icons/icon-website.svg'
-import bazarSVG from '../../assets/icons/icon-bazar.svg'
 import emailOutlineSVG from '../../assets/icons/icon-email.svg'
-import updateSVG from '../../assets/icons/icon-update.svg'
 import eyeSVG from '../../assets/icons/eye.svg'
 import eyeCrossedSVG from '../../assets/icons/eye-crossed.svg'
+import flagUnknownSVG from '../../assets/icons/flag-unknown.svg'
+import loadingSVG from '../../assets/images/loading.svg'
+import socialMediaSVG from '../../assets/icons/icon-social-media.svg'
+import starSVG from '../../assets/icons/icon-star-default.svg'
+import starActiveSVG from '../../assets/icons/icon-star-active.svg'
+import supporterSVG from '../../assets/icons/supporter.svg'
+import updateSVG from '../../assets/icons/icon-update.svg'
+import usefulSVG from '../../assets/icons/icon-useful.svg'
+import verifiedSVG from '../../assets/icons/icon-verified-badge.svg'
+import viewSVG from '../../assets/icons/icon-views.svg'
+import websiteSVG from '../../assets/icons/icon-website.svg'
 
 const imgStyle = {
   maxWidth: '100%',
 }
 
 export const iconMap = {
-  flagUnknown: <img alt="alt" style={imgStyle} src={flagUnknownSVG} />,
-  loading: <img alt="icon" style={imgStyle} src={loadingSVG} />,
-  star: <img alt="icon" style={imgStyle} src={starSVG} />,
-  starActive: <img alt="icon" style={imgStyle} src={starActiveSVG} />,
-  verified: <img alt="icon" style={imgStyle} src={verifiedSVG} />,
-  useful: <img alt="icon" style={imgStyle} src={usefulSVG} />,
-  comment: <img alt="icon" style={imgStyle} src={commentSVG} />,
-  view: <img alt="icon" style={imgStyle} src={viewSVG} />,
-  supporter: <img alt="icon" style={imgStyle} src={supporterSVG} />,
-  socialMedia: <img alt="icon" style={imgStyle} src={socialMediaSVG} />,
-  discord: <img alt="icon" style={imgStyle} src={discordSVG} />,
-  website: <img alt="icon" style={imgStyle} src={websiteSVG} />,
   bazar: <img alt="icon" style={imgStyle} src={bazarSVG} />,
-  emailOutline: <img alt="icon" style={imgStyle} src={emailOutlineSVG} />,
-  update: <img alt="icon" style={imgStyle} src={updateSVG} />,
-  show: <img alt="icon" style={imgStyle} src={eyeSVG} />,
-  hide: <img alt="icon" style={imgStyle} src={eyeCrossedSVG} />,
   chevronLeft: <img alt="icon" style={imgStyle} src={chevronLeftSVG} />,
   chevronRight: <img alt="icon" style={imgStyle} src={chevronRightSVG} />,
+  comment: <img alt="icon" style={imgStyle} src={commentSVG} />,
+  discord: <img alt="icon" style={imgStyle} src={discordSVG} />,
+  emailOutline: <img alt="icon" style={imgStyle} src={emailOutlineSVG} />,
+  flagUnknown: <img alt="alt" style={imgStyle} src={flagUnknownSVG} />,
+  hide: <img alt="icon" style={imgStyle} src={eyeCrossedSVG} />,
+  loading: <img alt="icon" style={imgStyle} src={loadingSVG} />,
+  show: <img alt="icon" style={imgStyle} src={eyeSVG} />,
+  socialMedia: <img alt="icon" style={imgStyle} src={socialMediaSVG} />,
+  star: <img alt="icon" style={imgStyle} src={starSVG} />,
+  starActive: <img alt="icon" style={imgStyle} src={starActiveSVG} />,
+  supporter: <img alt="icon" style={imgStyle} src={supporterSVG} />,
+  update: <img alt="icon" style={imgStyle} src={updateSVG} />,
+  useful: <img alt="icon" style={imgStyle} src={usefulSVG} />,
+  verified: <img alt="icon" style={imgStyle} src={verifiedSVG} />,
+  view: <img alt="icon" style={imgStyle} src={viewSVG} />,
+  website: <img alt="icon" style={imgStyle} src={websiteSVG} />,
 }

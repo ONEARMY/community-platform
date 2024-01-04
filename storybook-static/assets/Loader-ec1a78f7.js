cb39ea1293b7b5fd45449d85ef96087e4b3d192b
benfurber <benfurber@users.noreply.github.com>
2024-01-04 10:01:48 +0000

    deploy: 41ee9d6de6ad648e7e22aa5be679a39c6a11e807

---
+++ storybook-static/assets/Loader-ec1a78f7.js
@@ -0,0 +1,13 @@
+import{a as t,F as o,j as r}from"./jsx-runtime-913be41c.js";import{k as a}from"./emotion-react.browser.esm-ac7fc07b.js";import{n}from"./emotion-styled.browser.esm-935c8bd8.js";import{u as s}from"./emotion-element-6a883da9.browser.esm-becb4ad2.js";import{I as i,F as m,a as l}from"./theme-ui-components.esm-1dc2aee2.js";const c=a`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`,d=n(i)`
+  animation: ${c} 2s cubic-bezier(0.445, 0.05, 0.55, 0.95) infinite;
+  padding: 1rem;
+`,j=()=>{const e=s().logo||null;return t(o,{children:r(m,{sx:{flexWrap:"wrap",justifyContent:"center"},children:[e&&t(d,{loading:"lazy",src:e,sx:{width:[75,75,100]}}),t(l,{sx:{width:"100%",textAlign:"center"},children:"loading..."})]})})};export{j as L};
+//# sourceMappingURL=Loader-ec1a78f7.js.map

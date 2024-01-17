384c6d15ab2f10a9869c019e5a432b4450da82a9
thisislawatts <thisislawatts@users.noreply.github.com>
2024-01-17 21:10:01 +0000

    deploy: 815084c652bee8c7f75d00e8fb7ee6a74fd5c4b4

---
+++ storybook-static/assets/UserEngagementWrapper.stories-e5443a55.js
@@ -0,0 +1,24 @@
+import{a as o,j as p}from"./jsx-runtime-913be41c.js";import{U as n,A as a,a as s}from"./UserEngagementWrapper-fb97f521.js";import"./Button-7b434845.js";import"./CreateComment-f058f3db.js";import"./ElWithBeforeIcon-678a36ad.js";import"./ExternalLink-cca335b2.js";import"./FieldDatepicker-78d5a4a1.js";import"./FieldInput-9ff512f4.js";import"./FieldTextarea-60f2fca0.js";import"./Username-244e3be3.js";import"./GlobalStyles-2fddc22a.js";import"./Icon-ce3fd2be.js";import"./ImageGallery-850220ff.js";import"./InternalLink-e8b10725.js";import"./Loader-dd43aedd.js";import"./MapMemberCard-e5e75e55.js";import"./MemberBadge-a2685196.js";import"./Modal-b2a7bed1.js";import"./ModerationStatus-67336dc1.js";import"./MoreContainer-364cbc52.js";import"./NotificationItem-254138ce.js";import"./NotificationList-2078b73c.js";import"./OsmGeocoding-370c8c79.js";import"./ProfileLink-bba58633.js";import"./ResearchEditorOverview-81931f67.js";import"./Select-5d991919.js";import"./emotion-styled.browser.esm-935c8bd8.js";import"./Tag-396fe4f3.js";import"./TextNotification-637d6422.js";import"./Tooltip-60f84766.js";import"./UserStatistics-5dbf2b9b.js";import"./VideoPlayer-ce4e76bd.js";import"./BlockedRoute-6cdb1464.js";import"./TabbedContent-dbf187d0.js";import"./DiscussionTitle-2b5a24fb.js";import{B as r,a as l}from"./theme-ui-components.esm-85fd2ce8.js";import"./index-2506bfc3.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-11801e73.js";import"./emotion-element-6a883da9.browser.esm-becb4ad2.js";import"./index-150eb7c2.js";import"./isNativeReflectConstruct-7f9e7527.js";import"./inheritsLoose-0022215d.js";import"./hoist-non-react-statics.cjs-dd442a32.js";import"./index-54304c9c.js";import"./index-0dfb046a.js";import"./CharacterCount-3a9526b1.js";import"./icon-verified-badge-3c009cbf.js";import"./index-4d501b15.js";import"./emotion-react.browser.esm-ac7fc07b.js";import"./theme-ui-core-jsx-runtime.esm-9bc3c749.js";import"./theme-ui-css.esm-a1197288.js";import"./icon-star-active-309631d9.js";import"./iframe-3757c190.js";import"../sb-preview/runtime.js";const uo={title:"Components/UserEngagementWrapper",component:n},t=()=>o(r,{sx:{maxWidth:"1000px",margin:"0 auto"},children:o(n,{children:o(r,{sx:{margin:3},children:p(a,{author:{userName:"howto._createdBy",countryCode:"howto.creatorCountry",isVerified:!0},children:[o(l,{sx:{fontSize:2},onClick:()=>null,children:"Leave a comment"}),o(s,{votedUsefulCount:28,hasUserVotedUseful:!1,isLoggedIn:!1,onUsefulClick:()=>null})]})})})});var e,i,m;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`() => <Box sx={{
+  maxWidth: '1000px',
+  margin: '0 auto'
+}}>
+    <UserEngagementWrapper>
+      <Box sx={{
+      margin: 3
+    }}>
+        <ArticleCallToAction author={{
+        userName: 'howto._createdBy',
+        countryCode: 'howto.creatorCountry',
+        isVerified: true
+      }}>
+          <Button sx={{
+          fontSize: 2
+        }} onClick={() => null}>
+            Leave a comment
+          </Button>
+          <UsefulStatsButton votedUsefulCount={28} hasUserVotedUseful={false} isLoggedIn={false} onUsefulClick={() => null} />
+        </ArticleCallToAction>
+      </Box>
+    </UserEngagementWrapper>
+  </Box>`,...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const co=["Default"];export{t as Default,co as __namedExportsOrder,uo as default};
+//# sourceMappingURL=UserEngagementWrapper.stories-e5443a55.js.map

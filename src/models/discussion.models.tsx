fb28abdeb0d0c2a988b4411d0685962199c4cea9
Luke Watts <luke@thisis.la>
2024-01-22 19:45:10 +0100

    chore: add deprecation flag for IComment

--- src/models/discussion.models.tsx
+++ src/models/discussion.models.tsx
@@ -1,5 +1,9 @@
 import type { IComment } from './comment.model'
 
+/**
+ * Extends IComment with parentCommentId
+ * to support nested comments
+ */
 export type IDiscussionComment = IComment & {
   parentCommentId: string | null
 }

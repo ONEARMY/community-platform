528ef30181f6f3e7fb4bc437ed4503134b11b5f3
Ben Furber <ben.furber@googlemail.com>
2024-02-12 15:01:50 +0000

    feat: add custom message to loader

--- packages/components/src/Loader/Loader.stories.tsx
+++ packages/components/src/Loader/Loader.stories.tsx
@@ -8,3 +8,7 @@ export default {
 } as Meta<typeof Loader>
 
 export const Default: StoryFn<typeof Loader> = () => <Loader />
+
+export const CustomMessage: StoryFn<typeof Loader> = () => (
+  <Loader label="Whatever you want to say! OMG!" />
+)

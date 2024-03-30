d17324cbf3cec076b4bdde538c26fb42ac2c747f
Mário Nunes <mariojsnunes@gmail.com>
2024-03-30 09:13:25 +0000

    feat: improve keywords to remove special characters

--- src/utils/searchHelper.test.ts
+++ src/utils/searchHelper.test.ts
@@ -3,9 +3,9 @@ import '@testing-library/jest-dom'
 import { getKeywords } from './searchHelper'
 
 describe('searchHelper', () => {
-  it('should return all words of a string', () => {
+  it('should return all lowercase words of a string', () => {
     // act
-    const words = getKeywords('test1 test2 test3')
+    const words = getKeywords('Test1 teSt2 teST3')
 
     // assert
     expect(words).toEqual(['test1', 'test2', 'test3'])
@@ -25,5 +25,29 @@ describe('searchHelper', () => {
 
     // assert
     expect(words).toEqual(['test1'])
+  })
+
+  it('should normalize diatrics and filter stopwords', () => {
+    // act
+    const words = getKeywords('I ám test1 and thémselves')
+
+    // assert
+    expect(words).toEqual(['test1'])
+  })
+
+  it('should return normalized words', () => {
+    // act
+    const words = getKeywords('Tésting wórds açaí')
+
+    // assert
+    expect(words).toEqual(['testing', 'words', 'acai'])
+  })
+
+  it('should remove special characters', () => {
+    // act
+    const words = getKeywords('Good morning!')
+
+    // assert
+    expect(words).toEqual(['good', 'morning'])
   })
 })

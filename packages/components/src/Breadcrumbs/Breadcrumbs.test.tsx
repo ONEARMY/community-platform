ded6343836874d1d4550e1884b343bc02c0d16fa
unknown <goratt12@gmail.com>
2024-04-05 11:05:37 +0300

    feat: add Breadcrumbs component tests

--- packages/components/src/Breadcrumbs/Breadcrumbs.test.tsx
+++ packages/components/src/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,13 +1,30 @@
 import { render } from '../tests/utils'
-import { Default } from './Breadcrumbs.stories'
+import { Default, Shortened, NoCategory } from './Breadcrumbs.stories'
 import type { BreadcrumbsProps } from './Breadcrumbs'
 
 describe('Breadcrumbs', () => {
-  it('validates the component behaviour', () => {
+  it('validate full breadcrumbs', () => {
     const { getByText } = render(
       <Default {...(Default.args as BreadcrumbsProps)} />,
     )
 
-    expect(getByText('Breadcrumbs')).toBeInTheDocument();
+    expect(getByText('Category')).toBeInTheDocument()
+  })
+
+  it('validate shortened breadcrumbs', () => {
+    const { getByText } = render(
+      <Shortened {...(Shortened.args as BreadcrumbsProps)} />,
+    )
+
+    expect(getByText('...')).toBeInTheDocument()
+  })
+
+  it('validate no category breadcrumbs', () => {
+    const { getAllByRole } = render(
+      <NoCategory {...(NoCategory.args as BreadcrumbsProps)} />,
+    )
+
+    const chevrons = getAllByRole('img')
+    expect(chevrons).toHaveLength(1)
   })
 })

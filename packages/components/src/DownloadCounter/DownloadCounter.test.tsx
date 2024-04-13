2a0588a6ea5c4712653474c875d9c296e166ad0c
Ben Furber <ben.furber@googlemail.com>
2024-04-13 09:22:38 +0100

    feat: add download counter to components library

---
+++ packages/components/src/DownloadCounter/DownloadCounter.test.tsx
@@ -0,0 +1,24 @@
+import { render } from '../tests/utils'
+import { Default, One, Zero } from './DownloadCounter.stories'
+
+import type { IProps } from './DownloadCounter'
+
+describe('DownloadCounter', () => {
+  it('Adds commas for larger download counts', () => {
+    const { getByText } = render(<Default {...(Default.args as IProps)} />)
+
+    expect(getByText('1,888,999 downloads')).toBeInTheDocument()
+  })
+
+  it('Adds "download" when total is one', () => {
+    const { getByText } = render(<One {...(One.args as IProps)} />)
+
+    expect(getByText('1 download')).toBeInTheDocument()
+  })
+
+  it('Adds a zero for undefined total', () => {
+    const { getByText } = render(<Zero {...(Zero.args as IProps)} />)
+
+    expect(getByText('0 downloads')).toBeInTheDocument()
+  })
+})

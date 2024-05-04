5d076bdb681e04105931c7873abbb14219ef66fd
Ben Furber <ben.furber@googlemail.com>
2024-05-04 15:23:39 +0100

    feat: add donation request modal

---
+++ packages/components/src/DonationRequestModal/DonationRequestModal.tsx
@@ -0,0 +1,36 @@
+import { DonationRequest } from '../DonationRequest/DonationRequest'
+import { Modal } from '../Modal/Modal'
+
+export interface IProps {
+  body: string
+  callback: () => void
+  imageURL: string
+  iframeSrc: string
+  isOpen: boolean
+  link: string
+  onDidDismiss: () => void
+}
+
+export const DonationRequestModal = (props: IProps) => {
+  const { body, callback, iframeSrc, imageURL, isOpen, link, onDidDismiss } =
+    props
+
+  const sx = {
+    minWidth: ['90%', '90%', '750px', '1050px'],
+    border: '0 !important',
+    padding: '0 !important',
+    width: 'inherit',
+  }
+
+  return (
+    <Modal onDidDismiss={onDidDismiss} isOpen={isOpen} sx={sx}>
+      <DonationRequest
+        body={body}
+        callback={callback}
+        iframeSrc={iframeSrc}
+        link={link}
+        imageURL={imageURL}
+      />
+    </Modal>
+  )
+}

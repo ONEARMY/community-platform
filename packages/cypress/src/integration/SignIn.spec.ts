a27cd229d8ada1ab2f383eed47c9ddfce1b3b037
Ben Furber <ben.furber@googlemail.com>
2024-05-06 13:01:30 +0100

    chore: drop unneeded sign in test

--- packages/cypress/src/integration/SignIn.spec.ts
+++ packages/cypress/src/integration/SignIn.spec.ts
@@ -22,10 +22,3 @@ describe('[Sign in]', () => {
     cy.get('[data-cy=user-menu]')
   })
 })
-describe('[Sign-in - authenticated user]', () => {
-  it('redirects to home page', () => {
-    cy.visit('/sign-in')
-    cy.login('howto_reader@test.com', 'test1234')
-    cy.visit('/sign-in').url().should('include', '/')
-  })
-})

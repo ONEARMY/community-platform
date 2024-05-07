030fab37db5465aaa50097085fd8ebc4d8980ef0
Venu G Soganadgi <soganadgivenu@gmail.com>
2024-05-07 09:45:14 +0530

    fix: no warning when editing a comment to an empty comment #3411

--- packages/components/src/EditComment/EditComment.tsx
+++ packages/components/src/EditComment/EditComment.tsx
@@ -1,5 +1,6 @@
 import { Field, Form } from 'react-final-form'
 import { Flex, Label } from 'theme-ui'
+import { object, string } from 'yup'
 
 import { Button } from '../Button/Button'
 import { FieldTextarea } from '../FieldTextarea/FieldTextarea'
@@ -13,6 +14,22 @@ export interface IProps {
 
 export const EditComment = (props: IProps) => {
   const { comment, isReply } = props
+
+  const validationSchema = object({
+    comment: string().required('Make sure this field is filled correctly'),
+  })
+
+  const required = (value: string) =>
+    value && value.trim()
+      ? undefined
+      : 'Comment cannot be blank'
+
+  const handleFormSubmit = (comment: string) => {
+    if (comment.trim()) {
+      props?.handleSubmit(comment)
+    }
+  }
+
   return (
     <Form
       onSubmit={() => {
@@ -21,51 +38,69 @@ export const EditComment = (props: IProps) => {
       initialValues={{
         comment,
       }}
-      render={({ handleSubmit, values }) => (
-        <Flex
-          as="form"
-          sx={{
-            flexDirection: 'column',
-          }}
-          p={2}
-          onSubmit={handleSubmit}
-        >
-          <Label
-            as="label"
-            htmlFor="comment"
-            sx={{ marginBottom: '6px', fontSize: 3 }}
+      validate={async (values: any) => {
+        try {
+          await validationSchema.validate(values, { abortEarly: false })
+        } catch (err: any) {
+          return err.inner.reduce(
+            (acc: any, error: any) => ({
+              ...acc,
+              [error.path]: error.message,
+            }),
+            {},
+          )
+        }
+      }}
+      render={({ invalid, handleSubmit, values }) => {
+        const disabled = invalid
+        return (
+          <Flex
+            as="form"
+            sx={{
+              flexDirection: 'column',
+            }}
+            p={2}
+            onSubmit={handleSubmit}
           >
-            Edit {isReply ? 'Reply' : 'Comment'}
-          </Label>
-          <Field
-            component={FieldTextarea}
-            data-cy="edit-comment"
-            id="comment"
-            name="comment"
-          />
-          <Flex mt={4} ml="auto">
-            <Button
-              small
-              mr={4}
-              variant="secondary"
-              onClick={() => props?.handleCancel()}
+            <Label
+              as="label"
+              htmlFor="comment"
+              sx={{ marginBottom: '6px', fontSize: 3 }}
             >
-              Cancel
-            </Button>
-            <Button
-              data-cy="edit-comment-submit"
-              type={'submit'}
-              aria-label="Save changes"
-              small
-              onClick={() => {
-                props?.handleSubmit(values.comment)
-              }}
-            >
-              Save
-            </Button>
+              Edit {isReply ? 'Reply' : 'Comment'}
+            </Label>
+            <Field
+              component={FieldTextarea}
+              data-cy="edit-comment"
+              id="comment"
+              validate={required}
+              name="comment"
+            />
+            <Flex mt={4} ml="auto">
+              <Button
+                small
+                mr={4}
+                variant="secondary"
+                onClick={() => props?.handleCancel()}
+              >
+                Cancel
+              </Button>
+              <Button
+                data-cy="edit-comment-submit"
+                type={'submit'}
+                aria-label="Save changes"
+                small
+                disabled={disabled}
+                onClick={() => {
+                  handleFormSubmit(values.comment)
+                }}
+              >
+                Save
+              </Button>
+            </Flex>
           </Flex>
-        </Flex>
-      )}
+        )
+      }}
     />
   )
 }

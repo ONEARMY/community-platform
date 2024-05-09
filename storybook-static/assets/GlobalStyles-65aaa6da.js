98d76c9e884a2fcca94e32f4f72c3e08639360cd
benfurber <benfurber@users.noreply.github.com>
2024-05-09 09:55:04 +0000

    deploy: 3b61ab90b3d1fdacfe78c25bed34efe3dc2861ea

---
+++ storybook-static/assets/GlobalStyles-65aaa6da.js
@@ -0,0 +1,99 @@
+import{c as f}from"./emotion-react.browser.esm-ac7fc07b.js";const u=""+new URL("avatar_member_sm-4d3ee73c.svg",import.meta.url).href,b=""+new URL("fixing-fashion-header-b915b7a6.png",import.meta.url).href,e={input:{borderRadius:1,"&:focus":{borderColor:"#83ceeb",outline:"none",boxShadow:"none"}},colors:{white:"white",offwhite:"#ececec",black:"#1b1b1b",softyellow:"#f5ede2",blue:"#83ceeb",red:"#eb1b1f",red2:"#f58d8e",softblue:"#e2edf7",bluetag:"#5683b0",grey:"#61646b",green:"#00c3a9",error:"red",background:"#f4f6f7",silver:"#c0c0c0",softgrey:"#c2d4e4",lightgrey:"#ababac",darkGrey:"#686868",subscribed:"orange",notSubscribed:"#1b1b1b",betaGreen:"#98cc98"},alert:{borderRadius:1,paddingX:3,paddingY:3,textAlign:"center",fontWeight:"normal"},fontFamily:{body:"'Inter', Arial, sans-serif"},space:[0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,105,110,115,120,125,130,135,140]},a={colors:e.colors,zIndex:{behind:-1,level:0,default:1,slickArrows:100,modalProfile:900,logoContainer:999,header:3e3},text:{heading:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:5,fontWeight:"normal"},small:{fontFamily:'"Varela Round", Arial, sans-serif',fontSize:4,fontWeight:"normal"},body:{fontFamily:"'Inter', Arial, sans-serif"},quiet:{fontFamily:"'Inter', Arial, sans-serif",color:"grey"},auxiliary:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:1,color:e.colors.grey},paragraph:{fontFamily:'"Inter", Helvetica Neue, Arial, sans-serif;',fontSize:"16px",color:e.colors.grey}},space:e.space,radii:e.space,fonts:e.fontFamily,forms:{input:{...e.input,background:e.colors.background,border:"1px solid transparent",fontFamily:e.fontFamily.body,fontSize:1},inputOutline:{...e.input,background:"white",border:`2px solid ${e.colors.black}`},error:{...e.input,background:e.colors.background,border:`1px solid ${e.colors.error}`,fontFamily:e.fontFamily.body,fontSize:1},textarea:{...e.input,background:e.colors.background,border:"1px solid transparent",fontFamily:e.fontFamily.body,fontSize:1,padding:2},textareaError:{...e.input,background:e.colors.background,border:`1px solid ${e.colors.error}`,fontFamily:e.fontFamily.body,fontSize:1,padding:2}},maxContainerWidth:1280,sizes:{container:1280},regular:400,bold:600,fontSizes:[10,12,14,18,22,30,38,42,46,50,58,66,74],cards:{primary:{background:"white",border:`2px solid ${e.colors.black}`,borderRadius:1,overflow:"hidden"}},breakpoints:["40em","52em","70em"],alerts:{success:{...e.alert,backgroundColor:e.colors.green},failure:{...e.alert,backgroundColor:e.colors.red2},info:{...e.alert,backgroundColor:e.colors.softblue,color:e.colors.black}}},r={fontFamily:'"Varela Round", Arial, sans-serif',fontSize:3,display:"inline-flex",alignItems:"center",position:"relative",transition:".2s ease-in-out",borderRadius:1,width:"auto",border:"2px solid",height:"2.75rem"},n=o=>({primary:{...r,color:o.black,bg:o.accent.base,"&:hover":{bg:o.accent.hover,cursor:"pointer"},"&[disabled]":{opacity:.5,cursor:"not-allowed"},"&[disabled]:hover":{bg:o.accent.base}},secondary:{...r,border:"2px solid "+o.black,color:o.black,bg:o.softblue,"&:hover":{bg:o.white,cursor:"pointer"},"&[disabled]":{opacity:.5},"&[disabled]:hover":{bg:o.softblue}},destructive:{...r,border:"2px solid "+o.black,backgroundColor:o.red2,color:o.black,cursor:"pointer"},outline:{...r,border:"2px solid "+o.black,color:o.black,backgroundColor:"transparent","&:hover":{backgroundColor:o.softblue,cursor:"pointer"}},imageInput:{border:"2px dashed #e0e0e0",color:"#e0e0e0",backgroundColor:"transparent"},subtle:{...r,borderColor:o.softblue,color:o.black,bg:o.softblue,"&:hover":{bg:o.offwhite,borderColor:o.offwhite,cursor:"pointer"},"&[disabled]":{opacity:.5},"&[disabled]:hover":{bg:o.softblue}},breadcrumb:{...r,padding:"1",border:"1px solid transparent",backgroundColor:"transparent",height:"auto",color:"dimgray",fontSize:15,"&:hover":{backgroundColor:o.softblue,border:"1px solid "+o.lightgrey,cursor:"pointer"}}}),p=""+new URL("avatar_member_lg-54f0e065.svg",import.meta.url).href,i=""+new URL("avatar_space_lg-f7da92ee.svg",import.meta.url).href,l={...a.colors,primary:"green",accent:{base:"#F82F03",hover:"hsl(14, 81%, 63%)"}},M={name:"Fixing Fashion",logo:b,profileGuidelinesURL:"https://community.fixing.fashion/academy/guides/profile",communityProgramURL:"https://community.fixing.fashion/academy/guides/community-program",...a,colors:l,buttons:n(l),badges:{member:{lowDetail:u,normal:p},space:{lowDetail:i,normal:i}}},d=""+new URL("precious-plastic-logo-official-d09d9267.svg",import.meta.url).href,m=""+new URL("avatar_member_sm-40752705.svg",import.meta.url).href,g=""+new URL("map-collection-dacf3294.svg",import.meta.url).href,h=""+new URL("map-community-fc5bfbe8.svg",import.meta.url).href,y=""+new URL("map-machine-93076cd5.svg",import.meta.url).href,w=""+new URL("map-workspace-8f5266f0.svg",import.meta.url).href,k=""+new URL("pt-collection-point-6446bce1.svg",import.meta.url).href,R=""+new URL("pt-local-community-e2734b83.svg",import.meta.url).href,v=""+new URL("pt-machine-shop-95cb7e36.svg",import.meta.url).href,x=""+new URL("pt-workspace-5d0072c6.svg",import.meta.url).href,L=""+new URL("avatar_member_lg-36037a47.svg",import.meta.url).href,s={...a.colors,primary:"red",accent:{base:"#fee77b",hover:"#ffde45"}},U={name:"Precious Plastic",logo:d,profileGuidelinesURL:"https://drive.google.com/file/d/1fXTtBbzgCO0EL6G9__aixwqc-Euqgqnd/view",communityProgramURL:"https://community.preciousplastic.com/academy/guides/community-program",...a,badges:{member:{lowDetail:m,normal:L},workspace:{lowDetail:w,normal:x},"community-builder":{lowDetail:h,normal:R},"collection-point":{lowDetail:g,normal:k},"machine-builder":{lowDetail:y,normal:v}},buttons:n(s),colors:s},_={id:"precious-plastic",siteName:"Precious Plastic",logo:d,badge:m,avatar:"",howtoHeading:"Learn & share how to recycle, build and work with plastic",styles:U,academyResource:"https://onearmy.github.io/academy/",externalLinks:[{url:"https://bazar.preciousplastic.com/",label:"bazar"},{url:"https://preciousplastic.com/",label:"Global Site"}]},$=""+new URL("avatar_member_sm-d5f614a7.svg",import.meta.url).href,I=""+new URL("project-kamp-header-3b363f8e.png",import.meta.url).href,F=""+new URL("avatar_member_lg-45558694.svg",import.meta.url).href,c={...a.colors,primary:"green",accent:{base:"#8ab57f",hover:"hsl(108, 25%, 68%)"}},q={name:"Project Kamp",profileGuidelinesURL:"https://drive.google.com/file/d/1fXTtBbzgCO0EL6G9__aixwqc-Euqgqnd/view",communityProgramURL:"https://community.preciousplastic.com/academy/guides/community-program",logo:I,...a,colors:c,buttons:n(c),badges:{member:{lowDetail:$,normal:F}}},C=""+new URL("Inter-Medium-697f88e6.ttf",import.meta.url).href,S=""+new URL("Inter-Medium-5524ddae.woff",import.meta.url).href,z=""+new URL("Inter-Medium-8340de71.woff2",import.meta.url).href,B=""+new URL("Inter-Regular-0d156db7.ttf",import.meta.url).href,D=""+new URL("Inter-Regular-a3daad5d.woff",import.meta.url).href,A=""+new URL("Inter-Regular-0e6ff9be.woff2",import.meta.url).href,G=""+new URL("VarelaRound-Regular-1f536f47.ttf",import.meta.url).href,T=""+new URL("VarelaRound-Regular-65d33a72.woff",import.meta.url).href,P=`
+  @font-face {
+    font-family: 'Varela Round';
+    font-display: auto;
+    src:  url(${T}) format('woff'),
+          url(${G}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Inter';
+    font-display: auto;
+    src:  url(${A}) format('woff2'),
+          url(${D}) format('woff'),
+          url(${B}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Inter';
+    font-display: auto;
+    src:  url(${z}) format('woff2'),
+          url(${S}) format('woff'),
+          url(${C}) format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
+`,t=_.styles,E=f`
+  ${P}
+  body {
+    font-family: 'Varela Round', Arial, sans-serif;
+    background-color: ${t.colors.background};
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .beta-tester-feature {
+    border: 4px dashed ${t.colors.betaGreen};
+  }
+
+  body:has(.beta-tester-feature) .user-beta-icon > span {
+    background-color: ${t.colors.betaGreen};
+  }
+
+  .slick-prev,
+  .slick-next {
+    position: absolute !important;
+    top: 50%;
+    z-index: ${t.zIndex.slickArrows};
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
+
+  .slick-next {
+    left: auto;
+    right: 0;
+  }
+
+  .slick-prev {
+    left: 0;
+    right: auto;
+  }
+
+  .slick-track {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-items: stretch;
+  }
+
+  /***** Fix for Algolia search Icon *******/
+  .ap-icon-pin {
+    display: none;
+  }
+
+  /* Screen-reader text only - Taken from bootstrap 4 */
+  .sr-only {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    white-space: nowrap;
+    border: 0;
+  }
+`;export{E as G,M as S,_ as T,I as a,u as b,$ as c,q as d,b as l};
+//# sourceMappingURL=GlobalStyles-65aaa6da.js.map

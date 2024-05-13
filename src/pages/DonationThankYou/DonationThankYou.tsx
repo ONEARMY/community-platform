c17524f89226f6c546e5835d77cb6e60a47b05c2
Ben Furber <ben.furber@googlemail.com>
2024-05-13 14:12:06 +0100

    feat: update donation thank you text styling

--- src/pages/DonationThankYou/DonationThankYou.tsx
+++ src/pages/DonationThankYou/DonationThankYou.tsx
@@ -21,9 +21,12 @@ const DonationThankYou = () => {
         Thank you!
       </Text>
       <Text as="p" sx={{ textAlign: 'center' }}>
-        Epic ♡ Your donation is helping more plastic being recycled!
+        Epic ♡ Your donation is helping more plastic being recycled.
       </Text>
-      <Text as="p" sx={{ textAlign: 'center', paddingTop: 4 }}>
+      <Text
+        as="p"
+        sx={{ fontStyle: 'italics', textAlign: 'center', paddingTop: 5 }}
+      >
         (If your download hasn't started yet, please click the button below.)
         <br />
         <Image loading="lazy" src={IconArrowDown} sx={{ width: '20px' }} />

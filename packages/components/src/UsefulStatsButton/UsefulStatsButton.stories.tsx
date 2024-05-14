0c89727eb30ade6edd71aec88d50e517d31e007a
Mário Nunes <mariojsnunes@gmail.com>
2024-05-14 13:57:24 +0100

    feat: improve UsefulStatsButton story book

--- packages/components/src/UsefulStatsButton/UsefulStatsButton.stories.tsx
+++ packages/components/src/UsefulStatsButton/UsefulStatsButton.stories.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import { UsefulStatsButton } from './UsefulStatsButton'
 
 import type { Meta, StoryFn } from '@storybook/react'
@@ -7,38 +9,51 @@ export default {
   component: UsefulStatsButton,
 } as Meta<typeof UsefulStatsButton>
 
-export const LoggedOut: StoryFn<typeof UsefulStatsButton> = () => (
-  <UsefulStatsButton
-    isLoggedIn={false}
-    votedUsefulCount={0}
-    hasUserVotedUseful={false}
-    onUsefulClick={() => new Promise(() => {})}
-  />
-)
-
 export const LoggedOutWithCount: StoryFn<typeof UsefulStatsButton> = () => (
   <UsefulStatsButton
     isLoggedIn={false}
     hasUserVotedUseful={false}
     votedUsefulCount={99}
-    onUsefulClick={() => new Promise(() => {})}
+    onUsefulClick={() => Promise.resolve()}
   />
 )
 
-export const LoggedInWithCount: StoryFn<typeof UsefulStatsButton> = () => (
-  <UsefulStatsButton
-    votedUsefulCount={99}
-    hasUserVotedUseful={false}
-    isLoggedIn={true}
-    onUsefulClick={() => new Promise(() => {})}
-  />
-)
+export const LoggedInWithCount: StoryFn<typeof UsefulStatsButton> = () => {
+  const [count, setCount] = useState<number>(99)
+  const [voted, setVoted] = useState(false)
 
-export const CurrentUserHasVoted: StoryFn<typeof UsefulStatsButton> = () => (
-  <UsefulStatsButton
-    votedUsefulCount={99}
-    hasUserVotedUseful={true}
-    isLoggedIn={true}
-    onUsefulClick={() => new Promise(() => {})}
-  />
-)
+  const clickVote = async () => {
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
+    setCount((val) => (voted ? val - 1 : val + 1))
+    setVoted((val) => !val)
+  }
+
+  return (
+    <UsefulStatsButton
+      votedUsefulCount={count}
+      hasUserVotedUseful={voted}
+      isLoggedIn={true}
+      onUsefulClick={clickVote}
+    />
+  )
+}
+
+export const CurrentUserHasVoted: StoryFn<typeof UsefulStatsButton> = () => {
+  const [count, setCount] = useState<number>(100)
+  const [voted, setVoted] = useState(true)
+
+  const clickVote = async () => {
+    await new Promise<void>((resolve) => setTimeout(() => resolve(), 2000))
+    setCount((val) => (voted ? val - 1 : val + 1))
+    setVoted((val) => !val)
+  }
+
+  return (
+    <UsefulStatsButton
+      votedUsefulCount={count}
+      hasUserVotedUseful={voted}
+      isLoggedIn={true}
+      onUsefulClick={clickVote}
+    />
+  )
+}

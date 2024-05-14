7747429cbb309bf6507aaf7e115f75c9e263bfef
Venu G Soganadgi <soganadgivenu@gmail.com>
2024-05-14 18:50:08 +0530

    fix: test case for edit comment error message

--- packages/components/src/EditComment/EditComment.test.tsx
+++ packages/components/src/EditComment/EditComment.test.tsx
@@ -1,9 +1,14 @@
+import { fireEvent } from '@testing-library/react'
+import { vi } from 'vitest'
+
 import { render } from '../tests/utils'
+import { EditComment, type IProps } from './EditComment'
 import { Default, EditReply } from './EditComment.stories'
 
-import type { IProps } from './EditComment'
-
 describe('EditComment', () => {
+  const mockOnSubmit = vi.fn()
+  const mockOnCancel = vi.fn()
+
   it('showed correct title when a comment', () => {
     const { getByText } = render(<Default {...(Default.args as IProps)} />)
 
@@ -15,5 +20,55 @@ describe('EditComment', () => {
 
     expect(getByText('Edit Reply')).toBeInTheDocument()
     expect(() => getByText('Edit Comment')).toThrow()
+  })
+  it('enables save button when comment is not empty', () => {
+    const screen = render(
+      <EditComment
+        isReply={true}
+        comment="Test comment"
+        handleCancel= {mockOnCancel}
+        handleSubmit= {mockOnSubmit}
+      />,
+    )
+    expect(screen.getByText('Save')).not.toBeDisabled()
+  })
+
+  it('calls onSubmit when the submit button is clicked', () => {
+    const screen = render(
+      <EditComment
+        isReply={true}
+        comment="Test comment"
+        handleCancel= {mockOnCancel}
+        handleSubmit= {mockOnSubmit}
+      />,
+    )
+    const button = screen.getByText('Save')
+    fireEvent.click(button)
+    expect(mockOnSubmit).toHaveBeenCalledWith('Test comment')
+  })
+
+  it('disables save button when comment is empty', () => {
+    const screen = render(
+      <EditComment
+        isReply={true}
+        comment=""
+        handleCancel= {mockOnCancel}
+        handleSubmit= {mockOnSubmit}
+      />,
+    )
+    expect(screen.getByText('Save')).toBeDisabled()
+  })
+  it('should dispaly error message when the comment is empty', () => {
+    const screen = render(
+      <EditComment
+        isReply={true}
+        comment=""
+        handleCancel= {mockOnCancel}
+        handleSubmit= {mockOnSubmit}
+      />,
+    )
+    const inputComment = screen.getByLabelText('Edit Comment')
+    inputComment.blur();
+    expect(screen.getByText('Comment cannot be blank')).toBeInTheDocument()
   })
 })

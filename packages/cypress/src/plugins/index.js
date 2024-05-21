3956dff36e7d98dcd0a97feb6850e303077a86c7
David Germain <davidispizza@gmail.com>
2024-05-21 17:28:46 +0200

    refactor: remove cypress webpack config
    Cypress can take TypeScript as an input

--- packages/cypress/src/plugins/index.js
+++ packages/cypress/src/plugins/index.js
@@ -1,12 +1,6 @@
-/* global require, module, console */
-const wp = require('@cypress/webpack-preprocessor')
+/* global module, console */
 
 module.exports = function (on) {
-  const options = {
-    webpackOptions: require('../webpack.config'),
-  }
-  on('file:preprocessor', wp(options))
-
   /**
    * Log console to terminal
    * https://github.com/cypress-io/cypress/issues/3199

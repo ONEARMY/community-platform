0ac3b1b1cb8996eca719d48b59dd89b6a3bade14
benfurber <benfurber@users.noreply.github.com>
2024-05-21 10:02:43 +0000

    deploy: 2ab7a91bace28763f8b016b6489e8ac36673e5be

---
+++ storybook-static/assets/UserEngagementWrapper.stories-fd328ac7.js
@@ -0,0 +1,24 @@
+import{a as o,j as n}from"./jsx-runtime-913be41c.js";import{A as a}from"./ArticleCallToAction-fe7dd7d7.js";import"./Breadcrumbs-1df41658.js";import"./Button-f228a39f.js";import"./ButtonShowReplies-54557bfc.js";import"./Category-89a2a13a.js";import"./CommentItem-95009eb1.js";import"./CommentList-3decadb2.js";import"./CreateReply-4cdf9d85.js";import"./ConfirmModal-ee5a71bf.js";import"./CreateComment-e25e73ac.js";import"./DownloadButton-2c3272fa.js";import"./DownloadCounter-97681943.js";import"./DownloadFileFromLink-861c3721.js";import"./EditComment-9296671e.js";import"./ElWithBeforeIcon-f4285d4e.js";import"./ExternalLink-da92e34d.js";import"./FieldDatepicker-38c213c8.js";import"./FieldInput-1bda246f.js";import"./FieldTextarea-51366082.js";import"./DownloadStaticFile-8a3d07fe.js";import"./Username-d4b4b02e.js";import"./FollowButton-f40de773.js";import"./GlobalStyles-6b5e9435.js";import"./Icon-5a6c8159.js";import"./ImageCrop-007addf0.js";import"./ImageGallery-d6ac06a8.js";import"./InternalLink-949536b4.js";import"./LinkifyText-f6c7c6e0.js";import"./Loader-1ed8e2d8.js";import"./MapMemberCard-cb4ee94b.js";import"./MapWithDraggablePin-a640304c.js";import"./MemberBadge-fbf89a79.js";import"./Modal-843dd831.js";import"./ModerationStatus-231f16b8.js";import"./MoreContainer-05717c31.js";import"./NotificationItem-045b1e93.js";import"./NotificationList-d77d7bc2.js";import"./OsmGeocoding-8fff0549.js";import"./ProfileLink-8fa96492.js";import"./ResearchEditorOverview-8d66fbbd.js";import"./Select-0c8f901e.js";import"./emotion-styled.browser.esm-935c8bd8.js";import"./Tag-1f6633c7.js";import"./TagList-4e1a1002.js";import"./TextNotification-93ba0fb6.js";import"./Tooltip-60f84766.js";import{U as s}from"./UsefulStatsButton-36b2057d.js";import"./UserStatistics-ff21a5d5.js";import"./VideoPlayer-097a28ce.js";import"./BlockedRoute-eb1ff20d.js";import"./ContentStatistics-7b9a1157.js";import"./TabbedContent-f42af098.js";import"./DiscussionContainer-12f2fea1.js";import"./DiscussionTitle-75757833.js";import{U as e}from"./UserEngagementWrapper-f5d927c9.js";import"./IconCountWithTooltip-adff77b5.js";import"./DonationRequest-578d988e.js";import"./DonationRequestModal-0a912d4f.js";import{B as r,a as l}from"./theme-ui-components.esm-600c41e5.js";import"./index-2506bfc3.js";import"./emotion-element-6a883da9.browser.esm-becb4ad2.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-11801e73.js";import"./index-54304c9c.js";import"./index-0dfb046a.js";import"./index-150eb7c2.js";import"./objectWithoutPropertiesLoose-4f48578a.js";import"./CharacterCount-56c209d3.js";import"./supporter-29075ee8.js";import"./index-4d501b15.js";import"./emotion-react.browser.esm-ac7fc07b.js";import"./hoist-non-react-statics.cjs-dd442a32.js";import"./theme-ui-core-jsx-runtime.esm-9bc3c749.js";import"./theme-ui-css.esm-a1197288.js";import"./icon-arrow-down-33f070d5.js";import"./icon-star-active-309631d9.js";import"./iframe-b37a299f.js";import"../sb-preview/runtime.js";import"./assertThisInitialized-d894dc54.js";import"./inheritsLoose-66f0a91e.js";import"./createClass-020e4422.js";import"./isNativeReflectConstruct-e378569d.js";const $o={title:"Components/UserEngagementWrapper",component:e},t=()=>o(r,{sx:{maxWidth:"1000px",margin:"0 auto"},children:o(e,{children:o(r,{sx:{margin:3},children:n(a,{author:{userName:"howto._createdBy",countryCode:"howto.creatorCountry",isVerified:!0},children:[o(l,{sx:{fontSize:2},onClick:()=>null,children:"Leave a comment"}),o(s,{votedUsefulCount:28,hasUserVotedUseful:!1,isLoggedIn:!1,onUsefulClick:()=>new Promise(()=>{})})]})})})});var i,m,p;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`() => <Box sx={{
+  maxWidth: '1000px',
+  margin: '0 auto'
+}}>
+    <UserEngagementWrapper>
+      <Box sx={{
+      margin: 3
+    }}>
+        <ArticleCallToAction author={{
+        userName: 'howto._createdBy',
+        countryCode: 'howto.creatorCountry',
+        isVerified: true
+      }}>
+          <Button sx={{
+          fontSize: 2
+        }} onClick={() => null}>
+            Leave a comment
+          </Button>
+          <UsefulStatsButton votedUsefulCount={28} hasUserVotedUseful={false} isLoggedIn={false} onUsefulClick={() => new Promise(() => {})} />
+        </ArticleCallToAction>
+      </Box>
+    </UserEngagementWrapper>
+  </Box>`,...(p=(m=t.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};const bo=["Default"];export{t as Default,bo as __namedExportsOrder,$o as default};
+//# sourceMappingURL=UserEngagementWrapper.stories-fd328ac7.js.map

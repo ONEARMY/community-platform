b153546957304cf6eaff6b21a9e5c0f6df41a40b
Ben Furber <ben.furber@googlemail.com>
2024-05-27 15:06:33 +0100

    feat: add unique title check to questions

--- src/pages/Question/Content/Common/FormFields/QuestionTitle.field.tsx
+++ src/pages/Question/Content/Common/FormFields/QuestionTitle.field.tsx
@@ -1,16 +1,33 @@
 import { Field } from 'react-final-form'
 import { FieldInput } from 'oa-components'
+import { useCommonStores } from 'src/common/hooks/useCommonStores'
 import { FormFieldWrapper } from 'src/pages/Howto/Content/Common'
 import {
   QUESTION_MAX_TITLE_LENGTH,
   QUESTION_MIN_TITLE_LENGTH,
 } from 'src/pages/Question/constants'
 import { fields } from 'src/pages/Question/labels'
-import { composeValidators, minValue, required } from 'src/utils/validators'
+import {
+  composeValidators,
+  minValue,
+  required,
+  validateTitle,
+} from 'src/utils/validators'
 
-export const QuestionTitleField = () => {
+import type { MainFormAction } from 'src/common/Form/types'
+
+interface IProps {
+  parentType: MainFormAction
+  formValues: any
+}
+
+export const QuestionTitleField = (props: IProps) => {
+  const { parentType, formValues } = props
+  const { questionStore } = useCommonStores().stores
+
   const { placeholder, title } = fields.title
   const name = 'title'
+  const questionId = (!!formValues && formValues._id) ?? formValues._id
 
   return (
     <FormFieldWrapper htmlFor={name} text={title} required>
@@ -21,6 +38,7 @@ export const QuestionTitleField = () => {
         validate={composeValidators(
           required,
           minValue(QUESTION_MIN_TITLE_LENGTH),
+          validateTitle(parentType, questionId, questionStore),
         )}
         component={FieldInput}
         placeholder={placeholder}
--- src/pages/Research/Content/Common/ResearchUpdate.form.tsx
+++ src/pages/Research/Content/Common/ResearchUpdate.form.tsx
@@ -41,6 +41,7 @@ import { ResearchErrors } from './ResearchErrors'
 import { UpdateSubmitStatus } from './SubmitStatus'
 
 import type { ResearchEditorOverviewUpdate } from 'oa-components'
+import type { MainFormAction } from 'src/common/Form/types'
 import type { IResearch } from 'src/models/research.models'
 
 const ImageInputFieldWrapper = styled.div`
@@ -55,7 +56,7 @@ const CONFIRM_DIALOG_MSG =
 
 interface IProps {
   formValues: any
-  parentType: 'create' | 'edit'
+  parentType: MainFormAction
   redirectUrl?: string
 }
 

f6992414606bbfca7aeeb43974344ff6bd58bb6b
Ben Furber <ben.furber@googlemail.com>
2024-06-06 09:52:08 +0100

    test: update jest max test time to 10 seconds

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 10000,
 }
 
 export default config

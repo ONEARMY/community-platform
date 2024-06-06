3abe46f0d2f8876ca9d0f6e810143efbad49303d
Tulio Bandeira Luz <tulio8395@gmail.com>
2024-06-06 11:53:38 -0400

    refactor: test tear

--- packages/cypress/scripts/tear.ts
+++ packages/cypress/scripts/tear.ts
@@ -20,28 +20,4 @@ main()
 
 async function main() {
   console.log('testeeee', process.env.DB_PREFIX)
-  //await deleteDatabase()
-}
-
-async function deleteDatabase() {
-  console.log(`Deleting database prefix ${process.env.DB_PREFIX}`)
-  return new Promise<void>((resolve, reject) => {
-    setTimeout(() => {
-      resolve()
-    }, 10000)
-    TestDB.clearDB()
-      .then(() => {
-        resolve()
-      })
-      .catch((error) => {
-        reject(error)
-      })
-  })
-    .then(() => {
-      console.log('Database deleted successfully')
-    })
-    .catch((error) => {
-      console.error('Database deleting failed:', error)
-      throw error
-    })
 }

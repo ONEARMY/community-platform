db0703350fd1ade2a057f9120cafeabf7d7ebe4e
David Germain <davidispizza@gmail.com>
2024-06-08 18:04:53 +0200

    chore: improve log statement

--- scripts/postBuild.ts
+++ scripts/postBuild.ts
@@ -67,7 +67,7 @@ const platformTheme = process.env.REACT_APP_PLATFORM_THEME
 if (platformTheme) {
   console.log(`Applying theme: ${platformTheme}`)
   console.log(
-    `Copying src/assets/images/themes/${platformTheme}/public to build/`,
+    `Copying "src/assets/images/themes/${platformTheme}/public" to "build/"`,
   )
   fsExtra.copySync(
     '../src/assets/images/themes/' + platformTheme + '/public',

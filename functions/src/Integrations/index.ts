67795992767ce10bee0c80b30d4747c7ef2b4f7a
unknown <goratt12@gmail.com>
2024-06-09 22:56:26 +0300

    fix(functions): error fixing and lint + tested

--- functions/src/Integrations/index.ts
+++ functions/src/Integrations/index.ts
@@ -1,9 +1,6 @@
 import * as IntegrationsSlack from './firebase-slack'
-import * as IntegrationsDiscord from './firebase-discord'
 import * as IntegrationsPatreon from './patreon'
 
 exports.notifyNewPin = IntegrationsSlack.notifyNewPin
 exports.notifyNewHowTo = IntegrationsSlack.notifyNewHowTo
-exports.notifyPinAccepted = IntegrationsDiscord.notifyPinAccepted
-exports.notifyHowToAccepted = IntegrationsDiscord.notifyHowToAccepted
 exports.patreonAuth = IntegrationsPatreon.patreonAuth

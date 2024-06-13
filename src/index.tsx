d25a49124de175cf7873f92a0b3ac627bf1f3381
Mário Nunes <mariojsnunes@gmail.com>
2024-06-13 21:52:35 +0100

    fix: lint

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ import { App } from './App'
 
 initErrorHandler()
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)

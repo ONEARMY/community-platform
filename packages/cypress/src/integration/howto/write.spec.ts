7554700222e6594b70c6056f3a6ab01c87fc0b6a
Mário Nunes <mariojsnunes@gmail.com>
2024-06-16 17:49:05 +0100

    fix: cypress

--- packages/cypress/src/integration/howto/write.spec.ts
+++ packages/cypress/src/integration/howto/write.spec.ts
@@ -85,7 +85,7 @@ describe('[How To]', () => {
     cy.step(`Deleting step [${stepNumber}]`)
     cy.get(`[data-cy=step_${stepIndex}]:visible`)
       .find('[data-cy=delete-step]')
-      .click()
+      .click({ force: true })
     cy.get('[data-cy=confirm]').click()
   }
 

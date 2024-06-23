008e2c720f36439051ed3b9c21143bb7309c7302
David Germain <davidispizza@gmail.com>
2024-06-23 14:51:34 +0200

    refactor: group by intergration

--- functions/src/Integrations/index.ts
+++ functions/src/Integrations/index.ts
@@ -3,13 +3,11 @@ import * as Discord from './firebase-discord'
 import * as Patreon from './patreon'
 
 exports.notifyPinAwaitingModeration = Slack.notifyPinAwaitingModeration
-exports.notifyPinPublished = Discord.notifyPinPublished
-
 exports.notifyHowtoAwaitingModeration = Slack.notifyHowtoAwaitingModeration
+
+exports.notifyPinPublished = Discord.notifyPinPublished
 exports.notifyHowtoPublished = Discord.notifyHowtoPublished
-
 exports.notifyQuestionPublished = Discord.notifyQuestionPublished
-
 exports.notifyResearchUpdatePublished = Discord.notifyResearchUpdatePublished
 
 exports.patreonAuth = Patreon.patreonAuth

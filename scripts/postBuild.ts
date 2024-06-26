b252d3e31ce3b7076057e91e6ab3574ee4e114a2
David Germain <davidispizza@gmail.com>
2024-06-26 20:58:39 +0200

    chore: introduce a main function

--- scripts/postBuild.ts
+++ scripts/postBuild.ts
@@ -8,57 +8,63 @@ import { _supportedConfigurationOptions } from '../src/config/constants.ts'
 
 import type { CheerioAPI } from 'cheerio'
 
-dotenv.config({ path: path.resolve('../.env'), debug: true })
+main()
 
-const builtHTML = fs.readFileSync('../build/index.html', { encoding: 'utf-8' })
+function main() {
+  dotenv.config({ path: path.resolve('../.env'), debug: true })
 
-const $ = load(builtHTML, { recognizeSelfClosing: true })
+  const builtHTML = fs.readFileSync('../build/index.html', {
+    encoding: 'utf-8',
+  })
 
-console.log('Step 1) Writing configuration into the global window object...')
-const configuration = getWindowVariableObject()
-setFrontendConfiguration($, configuration)
-console.log('')
+  const $ = load(builtHTML, { recognizeSelfClosing: true })
 
-console.log('Step 2) Applying theme...')
-const platformTheme = process.env.REACT_APP_PLATFORM_THEME
-if (platformTheme) {
-  console.log('theme: ' + platformTheme)
-  console.log('Copying assets.')
-  fsExtra.copySync(
-    '../src/assets/images/themes/' + platformTheme + '/public',
-    '../build',
-  )
-} else {
-  console.log('No theme found, skipping.')
+  console.log('Step 1) Writing configuration into the global window object...')
+  const configuration = getWindowVariableObject()
+  setFrontendConfiguration($, configuration)
+  console.log('')
+
+  console.log('Step 2) Applying theme...')
+  const platformTheme = process.env.REACT_APP_PLATFORM_THEME
+  if (platformTheme) {
+    console.log('theme: ' + platformTheme)
+    console.log('Copying assets.')
+    fsExtra.copySync(
+      '../src/assets/images/themes/' + platformTheme + '/public',
+      '../build',
+    )
+  } else {
+    console.log('No theme found, skipping.')
+  }
+  console.log('')
+
+  console.log('Step 3) Making SEO changes...')
+  const siteName = process.env.SITE_NAME || 'Community Platform'
+  console.log('site name: ' + siteName)
+
+  $('title').text(siteName)
+  $('meta[property="og:title"]').attr('content', siteName)
+  $('meta[name="twitter:title"]').attr('content', siteName)
+
+  if (platformTheme) {
+    const siteDescription =
+      platformTheme === 'precious-plastic'
+        ? 'A series of tools for the Precious Plastic community to collaborate around the world. Connect, share and meet each other to tackle plastic waste.'
+        : 'A platform for the Project Kamp community to collaborate around the world. Connect, share and meet each other to figure out how to live more sustainably'
+
+    console.log('site description: ' + siteDescription)
+
+    $('meta[property="og:description"]').attr('content', siteDescription)
+    $('meta[property="twitter:description"]').attr('content', siteDescription)
+    $('meta[name="description"]').attr('content', siteDescription)
+  }
+  console.log('')
+
+  console.log('Step 4) Saving...')
+  const output = $.html()
+  fs.writeFileSync('../build/index.html', output, { encoding: 'utf-8' })
+  console.log('')
 }
-console.log('')
-
-console.log('Step 3) Making SEO changes...')
-const siteName = process.env.SITE_NAME || 'Community Platform'
-console.log('site name: ' + siteName)
-
-$('title').text(siteName)
-$('meta[property="og:title"]').attr('content', siteName)
-$('meta[name="twitter:title"]').attr('content', siteName)
-
-if (platformTheme) {
-  const siteDescription =
-    platformTheme === 'precious-plastic'
-      ? 'A series of tools for the Precious Plastic community to collaborate around the world. Connect, share and meet each other to tackle plastic waste.'
-      : 'A platform for the Project Kamp community to collaborate around the world. Connect, share and meet each other to figure out how to live more sustainably'
-
-  console.log('site description: ' + siteDescription)
-
-  $('meta[property="og:description"]').attr('content', siteDescription)
-  $('meta[property="twitter:description"]').attr('content', siteDescription)
-  $('meta[name="description"]').attr('content', siteDescription)
-}
-console.log('')
-
-console.log('Step 4) Saving...')
-const output = $.html()
-fs.writeFileSync('../build/index.html', output, { encoding: 'utf-8' })
-console.log('')
 
 function setFrontendConfiguration(webpage: CheerioAPI, configuration) {
   webpage('script#CommunityPlatform').html(

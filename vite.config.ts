74e01e4d87fdba40baa85032ac18311de738a41e
Ben Furber <ben.furber@googlemail.com>
2024-06-28 09:17:05 +0100

    test: fix vitest config for timeout

--- vite.config.ts
+++ vite.config.ts
@@ -17,8 +17,8 @@ const vitestConfig: VitestUserConfigInterface = {
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/test/setup.ts',
-    teardownTimeout: 15000,
-    testTimeout: 15000,
+    teardownTimeout: 15_000,
+    testTimeout: 15_000,
     coverage: {
       provider: 'v8',
       reporter: ['text'],

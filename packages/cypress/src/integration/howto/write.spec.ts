92408512d08f900026a6132adeaed8ddd3a883ce
Tulio Bandeira Luz <tulio8395@gmail.com>
2024-07-08 03:59:12 -0400

    refactor: yarn format

--- packages/cypress/src/integration/howto/write.spec.ts
+++ packages/cypress/src/integration/howto/write.spec.ts
@@ -85,7 +85,7 @@ describe('[How To]', () => {
     cy.step(`Deleting step [${stepNumber}]`)
     cy.get(`[data-cy=step_${stepIndex}]:visible`, { timeout: 20000 })
       .find('[data-cy=delete-step]')
-      .click({force: true})
+      .click({ force: true })
     cy.get('[data-cy=confirm]').click()
   }
 

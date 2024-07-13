9990e54a449743847ed67131e826a873dc818815
Kim Skovhus Andersen <kimskovhusandersen@gmail.com>
2024-07-13 16:58:49 +0200

    feat: card story

--- packages/components/src/ResponsiveCard/ResponsiveCard.stories.tsx
+++ packages/components/src/ResponsiveCard/ResponsiveCard.stories.tsx
@@ -7,4 +7,63 @@ export default {
   component: ResponsiveCard,
 } as Meta<typeof ResponsiveCard>
 
-export const Default: StoryFn<typeof ResponsiveCard> = () => <ResponsiveCard />
+export const Default: StoryFn<typeof ResponsiveCard> = () => (
+  <ResponsiveCard
+    title="Title of the card"
+    dataCy="test-id"
+    dataId="card-id"
+    link=""
+    titleAs="h2"
+  />
+)
+export const WithImage: StoryFn<typeof ResponsiveCard> = () => (
+  <ResponsiveCard
+    title="Title of the card"
+    dataCy="test-id"
+    dataId="card-id"
+    link=""
+    titleAs="h2"
+    imageAlt="Image alt text"
+    imageSrc="https://via.placeholder.com/92"
+  />
+)
+
+export const WithImageAndCategory: StoryFn<typeof ResponsiveCard> = () => (
+  <ResponsiveCard
+    title="Title of the card"
+    dataCy="test-id"
+    dataId="card-id"
+    link=""
+    titleAs="h2"
+    imageAlt="Image alt text"
+    imageSrc="https://via.placeholder.com/92"
+    category={{ label: 'Category' }}
+  />
+)
+
+export const WithImageAndCategoryAndCounts: StoryFn<
+  typeof ResponsiveCard
+> = () => (
+  <ResponsiveCard
+    title="Title of the card"
+    dataCy="test-id"
+    dataId="card-id"
+    link=""
+    titleAs="h2"
+    imageAlt="Image alt text"
+    imageSrc="https://via.placeholder.com/92"
+    category={{ label: 'Category' }}
+    iconCounts={[
+      {
+        count: 1,
+        icon: 'star-active',
+        text: 'listing.usefulness',
+      },
+      {
+        count: 1000000,
+        icon: 'comment',
+        text: 'listing.totalComments',
+      },
+    ]}
+  />
+)

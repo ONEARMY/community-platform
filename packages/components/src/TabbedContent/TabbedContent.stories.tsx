ce9f2b13f67c284298e7fef868be9cf1deebfccd
Ben Furber <ben.furber@googlemail.com>
2024-07-17 15:32:00 +0100

    fix: responsive design of tabs on profile

--- packages/components/src/TabbedContent/TabbedContent.stories.tsx
+++ packages/components/src/TabbedContent/TabbedContent.stories.tsx
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import { Box } from 'theme-ui'
 
 import { Tab, TabPanel, Tabs, TabsList } from './TabbedContent'
 
@@ -10,35 +11,44 @@ export default {
 
 export const Default: StoryFn = () => {
   return (
-    <Tabs defaultValue={0}>
-      <TabsList>
-        <Tab>Tab #1</Tab>
-        <Tab>Tab #2</Tab>
-        <Tab>Tab #3</Tab>
-        <Tab>Tab #4</Tab>
-        <Tab>Tab #5</Tab>
-      </TabsList>
+    <Box
+      sx={{
+        background: 'white',
+        maxWidth: '1000px',
+        margin: '0 auto',
+        padding: 6,
+      }}
+    >
+      <Tabs defaultValue={0}>
+        <TabsList>
+          <Tab>Tab #1</Tab>
+          <Tab>Tab #2</Tab>
+          <Tab>Tab #3</Tab>
+          <Tab>Tab #4</Tab>
+          <Tab>Tab #5</Tab>
+        </TabsList>
 
-      <TabPanel>
-        <p>Tab Panel #1</p>
-        <p>{faker.lorem.paragraphs(3)}</p>
-      </TabPanel>
-      <TabPanel>
-        <p>Tab Panel #2</p>
-        <p>{faker.lorem.paragraphs(3)}</p>
-      </TabPanel>
-      <TabPanel>
-        <p>Tab Panel #3</p>
-        <p>{faker.lorem.paragraphs(3)}</p>
-      </TabPanel>
-      <TabPanel>
-        <p>Tab Panel #4</p>
-        <p>{faker.lorem.paragraphs(3)}</p>
-      </TabPanel>
-      <TabPanel>
-        <p>Tab Panel #5</p>
-        <p>{faker.lorem.paragraphs(3)}</p>
-      </TabPanel>
-    </Tabs>
+        <TabPanel>
+          <p>Tab Panel #1</p>
+          <p>{faker.lorem.paragraphs(3)}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Tab Panel #2</p>
+          <p>{faker.lorem.paragraphs(3)}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Tab Panel #3</p>
+          <p>{faker.lorem.paragraphs(3)}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Tab Panel #4</p>
+          <p>{faker.lorem.paragraphs(3)}</p>
+        </TabPanel>
+        <TabPanel>
+          <p>Tab Panel #5</p>
+          <p>{faker.lorem.paragraphs(3)}</p>
+        </TabPanel>
+      </Tabs>
+    </Box>
   )
 }

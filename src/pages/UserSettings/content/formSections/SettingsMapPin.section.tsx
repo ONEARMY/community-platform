fc64bbdd7d3172b4b2342d2e9c222cd3ee72afd0
Ben Furber <ben.furber@googlemail.com>
2024-07-25 15:30:23 +0100

    fix: map setting erroring when deleting a pin

--- src/pages/UserSettings/content/formSections/SettingsMapPin.section.tsx
+++ src/pages/UserSettings/content/formSections/SettingsMapPin.section.tsx
@@ -115,7 +115,7 @@ const DeleteMapPin = (props: IPropsDeletePin) => {
     try {
       const updatedUser = await userStore.deleteUserLocation(user)
       if (updatedUser) {
-        mapsStore.setUserPin(toJS(updatedUser))
+        await mapsStore.deleteUserPin(toJS(updatedUser))
       }
       setNotification({
         message: mapForm.sucessfulDelete,

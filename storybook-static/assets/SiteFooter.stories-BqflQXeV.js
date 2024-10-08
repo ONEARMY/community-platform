e07283ea6e1ac1c4edb0105e233cf30857716fa6
benfurber <benfurber@users.noreply.github.com>
2024-10-08 13:26:53 +0000

    deploy: 9be727374e333ebcbdefc54e406a9bbf5c440d47

---
+++ storybook-static/assets/SiteFooter.stories-BqflQXeV.js
@@ -0,0 +1,34 @@
+import{j as t}from"./jsx-runtime-CexXSJP5.js";import{n as i}from"./emotion-styled.browser.esm-mtFmFg5E.js";import{E as x}from"./ExternalLink-DFvpNOKB.js";import{I as h}from"./Icon-QQOudn0t.js";import{u as f}from"./emotion-element-43c6fea0.browser.esm-B3fc_abn.js";import{F as g,T as s}from"./theme-ui-components.esm-D5Q63Fx1.js";import"./index-BP8_t0zE.js";import"./extends-CF3RwP-h.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-UWD3gXM2.js";import"./supporter-D7hp958O.js";import"./icon-arrow-down-CJeToCJe.js";import"./icon-star-active-ClN6U5FF.js";import"./theme-ui-core-jsx-runtime.browser.esm-BGkPH8X1.js";import"./hoist-non-react-statics.cjs-CQI8syxn.js";const d=()=>{const e=f(),o=i(x)`
+    color: #fff;
+    text-decoration: underline;
+  `,r=i(g)`
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    margin-top: 45px;
+    line-heigh: 1.5;
+    padding: 45px ${e.space[4]}px;
+    position: relative;
+    text-align: center;
+
+    @media only screen and (min-width: ${e.breakpoints[1]}) and (max-width: ${e.breakpoints[2]}) {
+      align-items: flex-start;
+      padding-top: 35px;
+      padding-bottom: 35px;
+      padding-left: 65px;
+      padding-right: ${310}px;
+      text-align: left;
+    }
+
+    @media only screen and (min-width: ${e.breakpoints[2]}) {
+      flex-direction: row;
+      padding-right: ${310}px;
+      text-align: left;
+    }
+  `,n=i(h)`
+    @media only screen and (min-width: ${e.breakpoints[1]}) and (max-width: ${e.breakpoints[2]}) {
+      position: absolute;
+      top: 45px;
+      left: 30px;
+    }
+  `;return t.jsxs(r,{bg:"#27272c",sx:{alignItems:"center"},style:{marginTop:"45px"},children:[t.jsx(n,{glyph:"star-active",mb:[3,3,0]}),t.jsxs(s,{ml:[0,0,0,3],mr:1,children:[e.name," is a project by"," ",t.jsx(o,{href:"https://onearmy.earth/",children:"One Army"}),"."]}),t.jsxs(s,{mt:[2,2,0],children:["Please"," ",t.jsx(o,{href:"https://www.patreon.com/one_army",children:"sponsor the work"})," ","or"," ",t.jsx(o,{href:"https://platform.onearmy.earth/",children:"help\xa0us\xa0build\xa0the\xa0software"}),"."]})]})},O={title:"Layout/SiteFooter",component:d},o=()=>t.jsx(d,{});var a,p,m;o.parameters={...o.parameters,docs:{...null==(a=o.parameters)?void 0:a.docs,source:{originalSource:"() => <SiteFooter />",...null==(m=null==(p=o.parameters)?void 0:p.docs)?void 0:m.source}}};const v=["Default"];export{o as Default,v as __namedExportsOrder,O as default};

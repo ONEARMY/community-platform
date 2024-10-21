c478ddbe2c4645b9028ecb8564b94ebb339cace9
Mário Nunes <mario.nunes@create.pt>
2024-10-21 14:02:09 +0100

    maybe fix?

--- src/routes/_.how-to.$slug._index.tsx
+++ src/routes/_.how-to.$slug._index.tsx
@@ -1,42 +1,39 @@
-import { json } from '@remix-run/node'
+import { json, redirect } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
-import { FIREBASE_CONFIG } from 'src/config/config'
-// import { Howto } from 'src/pages/Howto/Content/Howto/Howto'
-// import { howtoService } from 'src/pages/Howto/howto.service'
-// import { generateTags, mergeMeta } from 'src/utils/seo.utils'
+import { Howto } from 'src/pages/Howto/Content/Howto/Howto'
+import { howtoService } from 'src/pages/Howto/howto.service'
+import { NotFoundPage } from 'src/pages/NotFound/NotFound'
+import { generateTags, mergeMeta } from 'src/utils/seo.utils'
 
-// import type { LoaderFunctionArgs } from '@remix-run/node'
-// import type { IHowtoDB } from 'oa-shared'
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import type { IHowtoDB } from 'oa-shared'
 
-export async function loader() {
-  // const howto = await howtoService.getBySlug(params.slug as string)
+export async function loader({ params }: LoaderFunctionArgs) {
+  const howto = await howtoService.getBySlug(params.slug as string)
 
-  // if (!howto) {
-  //   const searchSlug = (params.slug || '').replace(/-/gi, ' ')
+  if (!howto) {
+    const searchSlug = (params.slug || '').replace(/-/gi, ' ')
 
-  //   return redirect(`/how-to?search=${searchSlug}&source=how-to-not-found`)
-  // }
+    return redirect(`/how-to?search=${searchSlug}&source=how-to-not-found`)
+  }
 
-  return json({ FIREBASE_CONFIG })
+  return json({ howto })
 }
 
-// export const meta = mergeMeta<typeof loader>(({ data }) => {
-//   const howto = data?.howto as IHowtoDB
-//   const title = `${howto?.title} - How-to - ${import.meta.env.VITE_SITE_NAME}`
+export const meta = mergeMeta<typeof loader>(({ data }) => {
+  const howto = data?.howto as IHowtoDB
+  const title = `${howto.title} - How-to - ${import.meta.env.VITE_SITE_NAME}`
 
-//   return generateTags(title, howto.description, howto.cover_image?.downloadUrl)
-// })
+  return generateTags(title, howto.description, howto.cover_image?.downloadUrl)
+})
 
 export default function Index() {
   const data = useLoaderData<typeof loader>()
-  // const howto = data.howto as IHowtoDB
+  const howto = data.howto as IHowtoDB // there is some inference issue, shouldn't need 'as'
 
-  return (
-    <>
-      <div style={{ display: 'none' }}>
-        {JSON.stringify(data.FIREBASE_CONFIG)}
-      </div>
-      {/* <Howto howto={howto} /> */}
-    </>
-  )
+  if (!howto) {
+    return <NotFoundPage />
+  }
+
+  return <Howto howto={howto} />
 }

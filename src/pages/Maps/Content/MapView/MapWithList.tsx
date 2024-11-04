1b5cdef7aae80fb49b3f5bdd6284d0a6c7aa971c
Matúš Motyka <61076969+motuz0001@users.noreply.github.com>
2024-11-04 21:36:50 +0100

    Update MapWithList.tsx

--- src/pages/Maps/Content/MapView/MapWithList.tsx
+++ src/pages/Maps/Content/MapView/MapWithList.tsx
@@ -285,7 +285,7 @@ export const MapWithList = (props: IProps) => {
             icon="gps-location"
             showIconOnly
           />
-          <Tooltip id="locationButton-tooltip"/>
+          <Tooltip id="locationButton-tooltip" />
 
           {/* Globe button to Zoom out to world view */}
           <Button
@@ -313,7 +313,7 @@ export const MapWithList = (props: IProps) => {
             icon="globe"
             showIconOnly
           />
-          <Tooltip id="globeButton-tooltip/>
+          <Tooltip id="globeButton-tooltip" />
 
           {notification && notification !== '' && (
             <Button sx={{ zIndex: 1000 }} variant="subtle">

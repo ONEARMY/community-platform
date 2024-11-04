735d104e7a454993ce8baa845b63d23192801ab7
Matúš Motyka <61076969+motuz0001@users.noreply.github.com>
2024-11-04 22:38:50 +0100

    Update map.spec.ts
    added click for redirect to the new map

--- packages/cypress/src/integration/map.spec.ts
+++ packages/cypress/src/integration/map.spec.ts
@@ -135,6 +135,7 @@ describe('[Map]', () => {
   it('Test zoom out/ globe button + zoom in to users location button', () => {
     cy.viewport('macbook-16')
     cy.visit('/map')
+    cy.get('[data-cy=Banner]').contains('Test it out!').click()
 
     cy.get('[data-cy="WorldViewButton"]', { timeout: 10000 })
       .should('exist')

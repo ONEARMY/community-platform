dbcc4674bcb52f99287ed7e6968b86e00aec89df
Mário Nunes <mariojsnunes@gmail.com>
2024-11-04 22:43:07 +0000

    feat: page views

---
+++ src/services/pageView.service.ts
@@ -0,0 +1,23 @@
+import { doc, increment, updateDoc } from 'firebase/firestore'
+import { DB_ENDPOINTS } from 'oa-shared'
+import { firestore } from 'src/utils/firebase'
+
+type PageViewCollections = Pick<
+  typeof DB_ENDPOINTS,
+  'users' | 'questions' | 'research' | 'howtos'
+>
+
+const incrementViewCount = async (
+  collectionName: keyof PageViewCollections,
+  id: string,
+) => {
+  const docRef = doc(firestore, DB_ENDPOINTS[collectionName], id)
+
+  return await updateDoc(docRef, {
+    total_views: increment(1),
+  })
+}
+
+export const pageViewService = {
+  incrementViewCount,
+}

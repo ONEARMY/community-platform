d052790c73ac4fd55ea8fef95fd77612ae8a5f50
Mário Nunes <mariojsnunes@gmail.com>
2024-11-05 00:37:19 +0000

    fix: favicon (#3982)

--- src/root.tsx
+++ src/root.tsx
@@ -58,7 +58,6 @@ const Document = withEmotionCache(
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
-          <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="preconnect"

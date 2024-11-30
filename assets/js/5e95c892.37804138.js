cd036c9646f972716bbe4917a9a9be7e5f5c12ef
mariojsnunes <mariojsnunes@users.noreply.github.com>
2024-11-30 17:23:34 +0000

    deploy: 02bfc46896f5e7c98472a36653b391485e88cbc3

---
+++ assets/js/5e95c892.37804138.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkoa_docs=self.webpackChunkoa_docs||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var a=r(8215),c=r(1003),u=r(7559),o=r(2831),d=r(781),n=r(4848);function t(e){return(0,n.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(d.A,{children:(0,o.v)(e.route.routes)})})}}}]);
---
+++ storybook-static/assets/MoreContainer-BKCrSvUB.js
@@ -0,0 +1,35 @@
+import{j as r}from"./jsx-runtime-CexXSJP5.js";import{n as c}from"./emotion-styled.browser.esm-Dz2BNYeh.js";import{u as o}from"./theme-ui-core.browser.esm-CvTdiiil.js";import{a}from"./theme-ui-components.esm-BngKL4Rx.js";const i="data:image/svg+xml,%3c?xml%20version='1.0'%20encoding='utf-8'?%3e%3c!--%20Generator:%20Adobe%20Illustrator%2022.1.0,%20SVG%20Export%20Plug-In%20.%20SVG%20Version:%206.00%20Build%200)%20--%3e%3csvg%20version='1.1'%20id='Layer_1'%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20x='0px'%20y='0px'%20viewBox='0%200%20476.8%20303.4'%20style='enable-background:new%200%200%20476.8%20303.4;'%20xml:space='preserve'%3e%3cstyle%20type='text/css'%3e%20.st0{fill:%23FFFFFF;}%20%3c/style%3e%3cpath%20class='st0'%20d='M1.5,130.3c-2.4,24.9-1.7,19.9-1.5,46.2s5.1,51.4,12,69c6.5,16.7,13,31.5,32.5,43.1%20c19.1,11.3,38.3,17,68.3,13.2c30.1-3.8,52.3-8.9,65-13.3c26.5-9.4,45.3-13.9,86.3-13.9c84,0,64.7,20.4,154.9-8%20c17.8-5.5,40.7-48,44.9-59.2s15.2-43.6,12.2-106.8S435.8,8.8,409,4.3s-92-8.8-150.3,10.8c-61.4,20.5-66.9,7.2-110.2,5.7%20c-39.4-1.2-62.7-8.8-102,17.5C24.3,53,8.6,56.8,1.5,130.3z'/%3e%3c/svg%3e",l="data:image/svg+xml,%3c?xml%20version='1.0'%20encoding='utf-8'?%3e%3c!--%20Generator:%20Adobe%20Illustrator%2022.1.0,%20SVG%20Export%20Plug-In%20.%20SVG%20Version:%206.00%20Build%200)%20--%3e%3csvg%20version='1.1'%20id='Layer_1'%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20x='0px'%20y='0px'%20viewBox='0%200%20476.8%20147.4'%20style='enable-background:new%200%200%20476.8%20147.4;'%20xml:space='preserve'%3e%3cstyle%20type='text/css'%3e%20.st0{fill:%23FFFFFF;}%20%3c/style%3e%3cpath%20class='st0'%20d='M1.5,63.4C-0.9,75.4-0.2,73.1,0,85.7s5.1,24.9,12,33.4c6.5,8.1,13,15.3,32.5,20.9c19.1,5.5,38.3,8.2,68.3,6.4%20c30.1-1.8,52.3-4.3,65-6.4c26.5-4.5,45.3-6.7,86.3-6.7c84,0,64.7,9.9,154.9-3.8c17.8-2.7,40.7-23.3,44.9-28.6%20c4.2-5.4,15.2-21.1,12.2-51.7S435.8,4.5,409,2.4s-92-4.3-150.3,5.3c-61.4,9.9-66.9,3.5-110.2,2.7c-39.4-0.6-62.7-4.3-102,8.5%20C24.3,25.9,8.6,27.8,1.5,63.4z'/%3e%3c/svg%3e",x="data:image/svg+xml,%3c?xml%20version='1.0'%20encoding='utf-8'?%3e%3c!--%20Generator:%20Adobe%20Illustrator%2022.1.0,%20SVG%20Export%20Plug-In%20.%20SVG%20Version:%206.00%20Build%200)%20--%3e%3csvg%20version='1.1'%20id='Layer_1'%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20x='0px'%20y='0px'%20viewBox='0%200%20476.8%20123.4'%20style='enable-background:new%200%200%20476.8%20123.4;'%20xml:space='preserve'%3e%3cstyle%20type='text/css'%3e%20.st0{fill:%23FFFFFF;}%20%3c/style%3e%3cpath%20class='st0'%20d='M1.5,53.2C-0.9,63.3-0.2,61.3,0,71.9s5.1,20.9,12,28c6.5,6.8,13,12.8,32.5,17.5c19.1,4.6,38.3,6.9,68.3,5.4%20c30.1-1.5,52.3-3.6,65-5.4c26.5-3.8,45.3-5.6,86.3-5.6c84,0,64.7,8.3,154.9-3.2c17.8-2.3,40.7-19.5,44.9-24s15.2-17.7,12.2-43.3%20S435.8,3.9,409,2.1s-92-3.6-150.3,4.4c-61.4,8.3-66.9,2.9-110.2,2.3c-39.4-0.5-62.7-3.6-102,7.1C24.3,21.8,8.6,23.4,1.5,53.2z'/%3e%3c/svg%3e",d="data:image/svg+xml,%3c?xml%20version='1.0'%20encoding='utf-8'?%3e%3c!--%20Generator:%20Adobe%20Illustrator%2022.1.0,%20SVG%20Export%20Plug-In%20.%20SVG%20Version:%206.00%20Build%200)%20--%3e%3csvg%20version='1.1'%20id='Layer_1'%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%20x='0px'%20y='0px'%20viewBox='0%200%20476.8%20123.4'%20style='enable-background:new%200%200%20476.8%20123.4;'%20xml:space='preserve'%3e%3cstyle%20type='text/css'%3e%20.st0{fill:%23FFFFFF;}%20%3c/style%3e%3cpath%20class='st0'%20d='M1.5,53.2C-0.9,63.3-0.2,61.3,0,71.9s5.1,20.9,12,28c6.5,6.8,13,12.8,32.5,17.5c19.1,4.6,38.3,6.9,68.3,5.4%20c30.1-1.5,52.3-3.6,65-5.4c26.5-3.8,45.3-5.6,86.3-5.6c84,0,64.7,8.3,154.9-3.2c17.8-2.3,40.7-19.5,44.9-24s15.2-17.7,12.2-43.3%20S435.8,3.9,409,2.1s-92-3.6-150.3,4.4c-61.4,8.3-66.9,2.9-110.2,2.3c-39.4-0.5-62.7-3.6-102,7.1C24.3,21.8,8.6,23.4,1.5,53.2z'/%3e%3c/svg%3e",s=e=>{const{theme:s}=o(),t=c(a)`
+    position: relative;
+    max-width: 780px;
+    &:after {
+      content: '';
+      background-image: url(\"${i}\");
+      width: 100%;
+      height: 100%;
+      z-index: ${s.zIndex.behind};
+      background-size: contain;
+      background-repeat: no-repeat;
+      position: absolute;
+      top: 59%;
+      transform: translate(-50%, -50%);
+      left: 50%;
+      max-width: 850px;
+      background-position: center 10%;
+    }
+    @media only screen and (min-width: ${s.breakpoints[0]}) {
+      &:after {
+        background-image: url(\"${l}\");
+      }
+    }
+    @media only screen and (min-width: ${s.breakpoints[1]}) {
+      &:after {
+        background-image: url(\"${x}\");
+      }
+    }
+
+    @media only screen and (min-width: ${s.breakpoints[2]}) {
+      &:after {
+        background-image: url(\"${d}\");
+      }
+    }
+  `;return r.jsx(t,{...e,children:e.children})};try{s.displayName="MoreContainer",s.__docgenInfo={description:"",displayName:"MoreContainer",props:{}}}catch{}export{s as M,l as w};

4d9d177e2a4bd84f221c003011ae620d3fc10e5e
Ben Furber <ben.furber@googlemail.com>
2024-12-02 17:45:01 +0000

    chore: changes after review feedback

--- src/pages/Maps/Maps.client.tsx
+++ src/pages/Maps/Maps.client.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Box } from 'theme-ui'
 
 import { allMapFilterOptions } from './Content/MapView/allMapFilterOptions'
@@ -13,8 +13,6 @@ const STARTING_NOTIFICATION = 'Loading...'
 
 const MapsPage = () => {
   const [allPins, setAllPins] = useState<IMapPin[] | null>(null)
-  const [allToggleFilters, setAllToggleFilters] =
-    useState<MapFilterOptionsList>([])
   const [notification, setNotification] = useState<string>(
     STARTING_NOTIFICATION,
   )
@@ -33,22 +31,24 @@ const MapsPage = () => {
     fetchMapPins()
   }, [])
 
-  useEffect(() => {
-    if (allPins) {
-      const pinDetails = allPins.map(({ creator }) => [
-        creator?.profileType,
-        ...(creator?.tags ? creator.tags.map(({ _id }) => _id) : []),
-        ...(creator?.badges
-          ? Object.keys(creator?.badges).filter((key) => key)
-          : []),
-      ])
-
-      const filtersNeeded = [...new Set(pinDetails.flat())]
-      const allNeededPinFilters = allMapFilterOptions.filter((validFilter) =>
-        filtersNeeded.some((neededfilter) => neededfilter === validFilter._id),
-      )
-      setAllToggleFilters(allNeededPinFilters)
+  const allToggleFilters: MapFilterOptionsList = useMemo(() => {
+    if (!allPins) {
+      return []
     }
+
+    const pinDetails = allPins.map(({ creator }) => [
+      creator?.profileType,
+      ...(creator?.tags ? creator.tags.map(({ _id }) => _id) : []),
+      ...(creator?.badges
+        ? Object.keys(creator?.badges).filter((key) => key)
+        : []),
+    ])
+
+    const filtersNeeded = [...new Set(pinDetails.flat())]
+
+    return allMapFilterOptions.filter((validFilter) =>
+      filtersNeeded.some((neededfilter) => neededfilter === validFilter._id),
+    )
   }, [allPins])
 
   return (

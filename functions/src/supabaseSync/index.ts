b7258101e0b39c6a2143e663e17d227727c93842
Mário Nunes <mariojsnunes@gmail.com>
2024-12-07 18:49:03 +0000

    fix/profile-creation (#4030)

--- functions/src/supabaseSync/index.ts
+++ functions/src/supabaseSync/index.ts
@@ -67,11 +67,6 @@ async function insertOrUpdateProfile(
     .eq('firebase_auth_id', user._authID)
     .single()
 
-  if (profileRequest.error) {
-    functions.logger.log(profileRequest.error)
-    return
-  }
-
   if (profileRequest.data) {
     // Update
     const { error } = await client

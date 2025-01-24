c076628a184bfae33af1c4f9f3636ff6f34aef16
CrowsVeldt <zechy.mahler@gmail.com>
2025-01-24 12:43:25 -0700

    feat: add wrapper Flex component to SpaceProfile

--- src/pages/User/content/SpaceProfile.tsx
+++ src/pages/User/content/SpaceProfile.tsx
@@ -82,135 +82,150 @@ export const SpaceProfile = ({ user, docs }: IProps) => {
   const defaultValue = useLocationHook?.hash?.slice(1) || 'profile'
 
   return (
-    <Card data-cy="SpaceProfile">
-      <Box>
-        {coverImage.length ? (
-          <ImageGallery
-            images={formatImagesForGallery(coverImage) as any}
-            hideThumbnails={true}
-            showNextPrevButton={true}
-          />
-        ) : (
-          <AspectRatio ratio={24 / 3}>
-            <Flex
-              sx={{
-                width: '100%',
-                height: '100%',
-                background: '#ddd',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              No images available.
-            </Flex>
-          </AspectRatio>
-        )}
-      </Box>
-      <Flex
-        sx={{
-          padding: [2, 4],
-          borderTop: '2px solid',
-        }}
-      >
-        <Box sx={{ width: '100%' }}>
-          <Box sx={{ position: 'relative' }}>
-            <Box
-              sx={{
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                right: 0,
-                transform: 'translateY(-50%)',
-              }}
-            >
-              <Box sx={{ display: ['none', 'none', 'block'] }}>
-                <MemberBadge size={150} profileType={profileType} />
+    <Flex>
+      <Card data-cy="SpaceProfile">
+        <Box>
+          {coverImage.length ? (
+            <ImageGallery
+              images={formatImagesForGallery(coverImage) as any}
+              hideThumbnails={true}
+              showNextPrevButton={true}
+            />
+          ) : (
+            <AspectRatio ratio={24 / 3}>
+              <Flex
+                sx={{
+                  width: '100%',
+                  height: '100%',
+                  background: '#ddd',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+              >
+                No images available.
+              </Flex>
+            </AspectRatio>
+          )}
+        </Box>
+        <Flex
+          sx={{
+            padding: [2, 4],
+            borderTop: '2px solid',
+          }}
+        >
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ position: 'relative' }}>
+              <Box
+                sx={{
+                  display: 'block',
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  transform: 'translateY(-50%)',
+                }}
+              >
+                <Box sx={{ display: ['none', 'none', 'block'] }}>
+                  <MemberBadge size={150} profileType={profileType} />
+                </Box>
+                <Box sx={{ display: ['none', 'block', 'none'] }}>
+                  <MemberBadge size={100} profileType={profileType} />
+                </Box>
+                <Box sx={{ display: ['block', 'none', 'none'] }}>
+                  <MemberBadge size={75} profileType={profileType} />
+                </Box>
               </Box>
-              <Box sx={{ display: ['none', 'block', 'none'] }}>
-                <MemberBadge size={100} profileType={profileType} />
-              </Box>
-              <Box sx={{ display: ['block', 'none', 'none'] }}>
-                <MemberBadge size={75} profileType={profileType} />
-              </Box>
+
+              <Flex
+                sx={{ gap: 2, alignItems: 'center', paddingBottom: [2, 4] }}
+              >
+                {userImage?.downloadUrl && (
+                  <Avatar
+                    data-cy="userImage"
+                    src={cdnImageUrl(userImage.downloadUrl, { width: 50 })}
+                    sx={{
+                      objectFit: 'cover',
+                      width: '50px',
+                      height: '50px',
+                    }}
+                  />
+                )}
+                <Flex sx={{ flexDirection: 'column' }}>
+                  <Username
+                    user={{
+                      ...user,
+                      countryCode: getUserCountry(user),
+                    }}
+                  />
+                  <Heading
+                    as="h1"
+                    color={'black'}
+                    style={{ wordBreak: 'break-word' }}
+                    data-cy="userDisplayName"
+                  >
+                    {displayName}
+                  </Heading>
+                </Flex>
+              </Flex>
             </Box>
 
-            <Flex sx={{ gap: 2, alignItems: 'center', paddingBottom: [2, 4] }}>
-              {userImage?.downloadUrl && (
-                <Avatar
-                  data-cy="userImage"
-                  src={cdnImageUrl(userImage.downloadUrl, { width: 50 })}
-                  sx={{
-                    objectFit: 'cover',
-                    width: '50px',
-                    height: '50px',
-                  }}
-                />
-              )}
-              <Flex sx={{ flexDirection: 'column' }}>
-                <Username
-                  user={{
-                    ...user,
-                    countryCode: getUserCountry(user),
-                  }}
-                />
-                <Heading
-                  as="h1"
-                  color={'black'}
-                  style={{ wordBreak: 'break-word' }}
-                  data-cy="userDisplayName"
-                >
-                  {displayName}
-                </Heading>
-              </Flex>
-            </Flex>
-          </Box>
-
-          <Tabs defaultValue={defaultValue}>
-            <TabsList>
-              <Tab value="profile">Profile</Tab>
-              {hasContributed && (
-                <Tab data-cy="ContribTab" value="contributions">
-                  Contributions
+            <Tabs defaultValue={defaultValue}>
+              <TabsList>
+                <Tab value="profile">Profile</Tab>
+                {hasContributed && (
+                  <Tab data-cy="ContribTab" value="contributions">
+                    Contributions
+                  </Tab>
+                )}
+                {hasImpacted && isPreciousPlastic() && (
+                  <Tab data-cy="ImpactTab" value="impact">
+                    {heading}
+                  </Tab>
+                )}
+                <Tab data-cy="contact-tab" value="contact">
+                  Contact
                 </Tab>
-              )}
-              {hasImpacted && isPreciousPlastic() && (
-                <Tab data-cy="ImpactTab" value="impact">
-                  {heading}
-                </Tab>
-              )}
-              <Tab data-cy="contact-tab" value="contact">
-                Contact
-              </Tab>
-            </TabsList>
-            <TabPanel value="profile">
-              <Box sx={{ mt: 1 }}>
-                <Flex
-                  sx={{
-                    flexDirection: ['column', 'column', 'row'],
-                    justifyContent: 'space-between',
-                    alignItems: 'flex-start',
-                    gap: [0, 0, 6],
-                  }}
-                >
+              </TabsList>
+              <TabPanel value="profile">
+                <Box sx={{ mt: 1 }}>
                   <Flex
                     sx={{
-                      flexDirection: 'column',
-                      width: ['100%', '100%', '80%'],
-                      gap: 2,
+                      flexDirection: ['column', 'column', 'row'],
+                      justifyContent: 'space-between',
+                      alignItems: 'flex-start',
+                      gap: [0, 0, 6],
                     }}
                   >
-                    {tags && <ProfileTags tagIds={tags} />}
-                    {about && <Paragraph>{about}</Paragraph>}
-                  </Flex>
-                  <Box
-                    sx={{
-                      width: ['auto', 'auto', '20%'],
-                      mt: [3, 3, 0],
-                    }}
-                  >
-                    <AuthWrapper
-                      roleRequired={UserRole.BETA_TESTER}
-                      fallback={
+                    <Flex
+                      sx={{
+                        flexDirection: 'column',
+                        width: ['100%', '100%', '80%'],
+                        gap: 2,
+                      }}
+                    >
+                      {tags && <ProfileTags tagIds={tags} />}
+                      {about && <Paragraph>{about}</Paragraph>}
+                    </Flex>
+                    <Box
+                      sx={{
+                        width: ['auto', 'auto', '20%'],
+                        mt: [3, 3, 0],
+                      }}
+                    >
+                      <AuthWrapper
+                        roleRequired={UserRole.BETA_TESTER}
+                        fallback={
+                          <UserStatistics
+                            userName={userName}
+                            country={location?.country}
+                            isVerified={user.verified}
+                            isSupporter={!!user.badges?.supporter}
+                            libraryCount={docs?.library.length || 0}
+                            usefulCount={user.totalUseful || 0}
+                            researchCount={docs?.research.length || 0}
+                            totalViews={0}
+                          />
+                        }
+                      >
                         <UserStatistics
                           userName={userName}
                           country={location?.country}
@@ -219,55 +234,44 @@ export const SpaceProfile = ({ user, docs }: IProps) => {
                           libraryCount={docs?.library.length || 0}
                           usefulCount={user.totalUseful || 0}
                           researchCount={docs?.research.length || 0}
-                          totalViews={0}
+                          totalViews={user.total_views || 0}
                         />
-                      }
-                    >
-                      <UserStatistics
-                        userName={userName}
-                        country={location?.country}
-                        isVerified={user.verified}
-                        isSupporter={!!user.badges?.supporter}
-                        libraryCount={docs?.library.length || 0}
-                        usefulCount={user.totalUseful || 0}
-                        researchCount={docs?.research.length || 0}
-                        totalViews={user.total_views || 0}
+                      </AuthWrapper>
+                    </Box>
+                  </Flex>
+                </Box>
+              </TabPanel>
+              {hasContributed && (
+                <TabPanel value="contributions">
+                  <UserCreatedDocuments docs={docs} />
+                </TabPanel>
+              )}
+              {hasImpacted && isPreciousPlastic() && (
+                <TabPanel value="impact">
+                  <Impact impact={impact} user={user} />
+                </TabPanel>
+              )}
+              <TabPanel value="contact">
+                <Box>
+                  <ClientOnly fallback={<></>}>
+                    {() => (
+                      <UserAction
+                        loggedIn={<UserContactForm user={user} />}
+                        loggedOut={
+                          <UserContactNotLoggedIn
+                            displayName={user.displayName}
+                          />
+                        }
                       />
-                    </AuthWrapper>
-                  </Box>
-                </Flex>
-              </Box>
-            </TabPanel>
-            {hasContributed && (
-              <TabPanel value="contributions">
-                <UserCreatedDocuments docs={docs} />
+                    )}
+                  </ClientOnly>
+                  <UserContactAndLinks links={userLinks} />
+                </Box>
               </TabPanel>
-            )}
-            {hasImpacted && isPreciousPlastic() && (
-              <TabPanel value="impact">
-                <Impact impact={impact} user={user} />
-              </TabPanel>
-            )}
-            <TabPanel value="contact">
-              <Box>
-                <ClientOnly fallback={<></>}>
-                  {() => (
-                    <UserAction
-                      loggedIn={<UserContactForm user={user} />}
-                      loggedOut={
-                        <UserContactNotLoggedIn
-                          displayName={user.displayName}
-                        />
-                      }
-                    />
-                  )}
-                </ClientOnly>
-                <UserContactAndLinks links={userLinks} />
-              </Box>
-            </TabPanel>
-          </Tabs>
-        </Box>
-      </Flex>
-    </Card>
+            </Tabs>
+          </Box>
+        </Flex>
+      </Card>
+    </Flex>
   )
 }

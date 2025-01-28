5459631c0ce89bb73fa51fd38e0278fc4cbbd685
Ben Furber <ben.furber@googlemail.com>
2025-01-28 11:28:03 +0000

    fix: ReturnPathLink actually going somewhere

--- packages/components/src/ReturnPathLink/ReturnPathLink.tsx
+++ packages/components/src/ReturnPathLink/ReturnPathLink.tsx
@@ -6,7 +6,7 @@ import type { RefAttributes } from 'react'
 type IProps = RemixLinkProps & RefAttributes<HTMLAnchorElement>
 
 export const ReturnPathLink = (props: IProps) => {
-  const to = `/${props.to}?returnUrl=${encodeURIComponent(location?.pathname)}`
+  const to = `${props.to}?returnUrl=${encodeURIComponent(location?.pathname)}`
 
   return (
     <Link {...props} to={to}>

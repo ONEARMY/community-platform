4a2f6b73f08f273b75aa6ea4d2698ced79366b11
Ben Furber <ben.furber@googlemail.com>
2025-03-17 15:13:59 +0000

    fix: messaging email fields

--- src/.server/templates/RecieverMessage.tsx
+++ src/.server/templates/RecieverMessage.tsx
@@ -2,32 +2,41 @@ import Layout from './Layout'
 import { MessageSettings } from '../models/messageSettings'
 
 type RecieverMessageArgs = {
-  email: string
-  fromUser: string
+  messengerEmailAddress: string
+  messengerName: string | undefined
+  messengerUsername: string
+  receiverName: string
   settings: MessageSettings
   text: string
-  toUserName: string
-  name: string | undefined
 }
 
 export default function RecieverMessage(props: RecieverMessageArgs) {
-  const { email, fromUser, settings, text, toUserName } = props
+  const {
+    receiverName,
+    settings,
+    text,
+    messengerEmailAddress,
+    messengerName,
+    messengerUsername,
+  } = props
 
-  const name = props.name !== 'undefined' ? props.name : fromUser
+  const isMessengerName = messengerName && messengerName !== 'undefined'
+  const name = isMessengerName ? messengerName : messengerUsername
+
   const preview = `${name} wants to chat to you!`
 
   return (
     <Layout preview={preview} settings={settings}>
-      <p>Hey {toUserName},</p>
+      <p>Hey {receiverName},</p>
       <p>
-        <a href={`${settings.siteUrl}/u/${fromUser}`}>{name}</a> has sent you a
-        message through{' '}
+        <a href={`${settings.siteUrl}/u/${messengerUsername}`}>{name}</a> has
+        sent you a message through{' '}
         <a href={settings.siteUrl} target="_blank">
           {settings.siteName}
         </a>
         .
       </p>
-      <p>Please reply directly to their email: {email}.</p>
+      <p>Please reply directly to their email: {messengerEmailAddress}.</p>
       ---
       <p>
         <strong>{text}</strong>

14b532d1ce8b741c8d9acdd4d80307f84f584370
Ben Furber <ben.furber@googlemail.com>
2025-04-07 16:44:43 +0100

    feat: update seo tags for index

--- src/routes/_.news.tsx
+++ src/routes/_.news.tsx
@@ -3,10 +3,22 @@ import { Outlet } from '@remix-run/react'
 import { isModuleSupported, MODULE } from 'src/modules'
 import { EnvironmentContext } from 'src/pages/common/EnvironmentContext'
 import Main from 'src/pages/common/Layout/Main'
+import { generateTags, mergeMeta } from 'src/utils/seo.utils'
 
 export async function loader() {
   return null
 }
+
+export function HydrateFallback() {
+  return <div></div>
+}
+
+export const meta = mergeMeta<typeof loader>(() => {
+  const title = `News - ${import.meta.env.VITE_SITE_NAME}`
+  // const imageUrl = news.heroImage?.publicUrl
+
+  return generateTags(title)
+})
 
 export default function Index() {
   const env = useContext(EnvironmentContext)

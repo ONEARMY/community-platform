a84b15005fc3ba73d4b7160f006c425518e676f9
Bart Enkelaar <benkelaar@gmail.com>
2025-04-28 22:26:31 +0200

    feat: Test visitor tags in ProfileTagsList

--- packages/components/src/ProfileTagsList/ProfileTagsList.test.tsx
+++ packages/components/src/ProfileTagsList/ProfileTagsList.test.tsx
@@ -1,16 +1,56 @@
 import '@testing-library/jest-dom/vitest'
 
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 
 import { render } from '../test/utils'
 import { Default } from './ProfileTagsList.stories'
 
 import type { IProps } from './ProfileTagsList'
+import { ProfileTagsList } from './ProfileTagsList'
 
 describe('ProfileTagsList', () => {
-  it('validates the component behaviour', () => {
+  it('shows the electronics tag from default arguments', () => {
     const { getByText } = render(<Default {...(Default.args as IProps)} />)
 
     expect(getByText('Electronics')).toBeInTheDocument()
+  })
+
+  it('shows nothing when no tags or visitor info present', () => {
+    const { getByTestId } = render(<ProfileTagsList tags={[]} />)
+
+    expect(getByTestId('ProfileTagsList')).toBeEmptyDOMElement()
+  })
+
+  it('shows open when open for visitors', () => {
+    const { getByText } = render(
+      <ProfileTagsList tags={[]} openToVisitors={{ policy: 'open' }} />,
+    )
+
+    expect(getByText('Open to visitors ⓘ')).toBeInTheDocument()
+  })
+
+  it('shows appointment when visits by appointment', () => {
+    const { getByText } = render(
+      <ProfileTagsList tags={[]} openToVisitors={{ policy: 'appointment' }} />,
+    )
+
+    expect(getByText('Visitors after appointment ⓘ')).toBeInTheDocument()
+  })
+
+  it('triggers callback when clicking closed visitor tag', () => {
+    const callback = vi.fn()
+    const { getByText } = render(
+      <ProfileTagsList
+        tags={[]}
+        openToVisitors={{ policy: 'closed' }}
+        showVisitorModal={callback}
+      />,
+    )
+
+    const visitorTag = getByText('Visits currently not possible ⓘ')
+    expect(visitorTag).toBeInTheDocument()
+    visitorTag.click()
+
+    expect(callback).toBeCalled()
   })
 })

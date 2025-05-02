aca0fc04cfa2add31f7d2e98ea18388a35f7fa71
Your Name <you@example.com>
2025-05-02 11:32:41 +0200

    fix: hide contact tab on users's own profile

---
+++ src/pages/User/content/UserProfile.test.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { useLocation } from '@remix-run/react'
+import { Provider } from 'mobx-react'
+import { useCommonStores } from 'src/common/hooks/useCommonStores'
+import { FactoryUser } from 'src/test/factories/User'
+import { describe, expect, it, Mock, vi } from 'vitest'
+
+import { contact } from '../labels'
+import { testingThemeStyles } from 'src/test/utils/themeUtils'
+import { UserProfile } from './UserProfile'
+import { Router } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@theme-ui/core'
+
+const Theme = testingThemeStyles
+
+vi.mock('src/common/hooks/useCommonStores', () => {
+  return {
+    useCommonStores: () => ({
+      stores: {
+        userStore: {
+          getUserEmail: () => vi.fn().mockReturnValue('Bob@email.com'),
+          activeUser: () => vi.fn().mockReturnValue(true),
+        },
+      },
+    }),
+  }
+})
+
+vi.mock('@remix-run/react', async () => ({
+  ...(await vi.importActual('@remix-run/react')),
+  useLocation: vi.fn(),
+}))
+
+
+describe('UserContactForm', () => {
+  const profileUser = FactoryUser({ isContactableByPublic: true });
+  (useLocation as Mock).mockImplementation(() => ({
+    pathname: '/initial',
+  }))
+
+  it('Don\'t render contact tab if seeing own profile', () => {
+    const { container } = render(
+      <Provider {...useCommonStores().stores}>
+        <BrowserRouter>
+          <ThemeProvider theme={Theme}>
+            <UserProfile user={profileUser} isViewingOwnProfile={true} docs={{ library: [], research: [] }}></UserProfile>
+          </ThemeProvider>
+        </BrowserRouter>
+      </Provider>,
+    )
+    expect(container.innerHTML).not.contains('contact-tab')
+  })
+})

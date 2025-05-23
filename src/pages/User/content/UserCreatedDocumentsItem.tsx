f20ee583525e2ee92f1985d9f45808c2be5ce97c
Ian Webster <ian@duskwood.design>
2025-05-23 09:25:26 +0100

    question click through fixed

--- src/pages/User/content/UserCreatedDocumentsItem.tsx
+++ src/pages/User/content/UserCreatedDocumentsItem.tsx
@@ -3,7 +3,7 @@ import { Icon } from 'oa-components'
 import { Flex, Heading, Text } from 'theme-ui'
 
 interface IProps {
-  type: 'library' | 'research' | 'question'
+  type: 'library' | 'research' | 'questions'
   item: {
     id: string | number
     title: string

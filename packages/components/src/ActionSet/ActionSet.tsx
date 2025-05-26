6a840bc87291345c682f6bf9920df4ad4a549b5d
Ben Furber <ben.furber@googlemail.com>
2025-05-26 12:03:43 +0100

    Feat: Move comment actions hide modal

---
+++ packages/components/src/ActionSet/ActionSet.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import { Card, Flex } from 'theme-ui'
+
+import { Button } from '../Button/Button'
+
+import type { ReactNode } from 'react'
+
+interface IProps {
+  children: ReactNode[]
+}
+
+export const ActionSet = ({ children }: IProps) => {
+  const [show, setShow] = useState<boolean>(false)
+
+  const toDisplay = children.filter((child) => !!child)
+  if (!children || toDisplay.length === 0) {
+    return <></>
+  }
+
+  const onClick = () => setShow((show) => !show)
+
+  return (
+    <>
+      <Flex
+        sx={{
+          display: 'inline-block',
+          position: 'relative',
+          gap: 2,
+        }}
+      >
+        <Button
+          icon="more-vert"
+          onClick={onClick}
+          variant="subtle"
+          showIconOnly
+        >
+          Show Actions
+        </Button>
+
+        {show && (
+          <Card
+            sx={{
+              position: 'absolute',
+              right: 0,
+              zIndex: 1,
+              alignItems: 'stretch',
+              flexDirection: 'column',
+              padding: 2,
+              gap: 2,
+            }}
+          >
+            {...children}
+          </Card>
+        )}
+      </Flex>
+    </>
+  )
+}

7dc096db181803142001d26bc98418ca827d274d
Ben Furber <ben.furber@googlemail.com>
2025-06-03 17:00:06 +0100

    feat: add user settings for notifications

--- src/pages/UserSettings/labels.ts
+++ src/pages/UserSettings/labels.ts
@@ -138,6 +138,7 @@ export const fields: ILabels = {
 export const form = {
   defaultError: 'Make sure this field is filled correctly',
   saveSuccess: 'Yay! Impact data saved.',
+  saveNotificationPreferences: 'Whoop. Preferences updated.',
 }
 
 export const headings = {

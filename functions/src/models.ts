ae8ed9bffa5a338f847d31bde9041c1970d87be7
kilavvy <140459108+kilavvy@users.noreply.github.com>
2025-06-12 15:54:35 +0200

    Update models.ts

--- functions/src/models.ts
+++ functions/src/models.ts
@@ -1,7 +1,7 @@
 // Import and Re-export models from the main platform if required by functions
 // NOTE - as of yarn 3.3 we need to import relatively instead of from a workspace import.
 // This is because the functions code sits at same level of main platform package.json, creating cyclic symlinks
-// functions -> node_modules -> community-plaform -> functions -> node_modules ....
+// functions -> node_modules -> community-platform -> functions -> node_modules ....
 // Importing from outside the src code is still fine because we make single builds with webpack
 // which can resolve at build time, but would not work if deploying direct to firebase functions.
 // Alternative fix would be to put the platform code one level further nested e.g. <root>/platform/src

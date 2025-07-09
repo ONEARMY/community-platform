7f859dfe1a64af016852d0a6f01009437a5f57bb
Ben Furber <ben.furber@googlemail.com>
2025-07-09 11:19:07 +0100

    test: add unit tests

---
+++ src/test/factories/notificationsPreferences.ts
@@ -0,0 +1,14 @@
+import type { DBNotificationsPreferences } from 'oa-shared'
+
+export const factoryNotificationsPreferences = (
+  userOverloads: Partial<DBNotificationsPreferences> = {},
+): DBNotificationsPreferences =>
+  ({
+    id: 1,
+    user_id: 123,
+    comments: true,
+    replies: false,
+    research_updates: true,
+    is_unsubscribed: false,
+    ...userOverloads,
+  }) as DBNotificationsPreferences

9d8a949fb9de313ab10ea93b0b6200ca23e08879
Mário Nunes <mariojsnunes@gmail.com>
2025-07-14 21:00:39 +0700

    wip

--- src/routes/api.profile.ts
+++ src/routes/api.profile.ts
@@ -1,6 +1,8 @@
 import { Profile } from 'oa-shared'
 import { createSupabaseServerClient } from 'src/repository/supabase.server'
 
+import type { ActionFunctionArgs } from '@remix-run/node'
+
 export const loader = async ({ request }) => {
   const { client, headers } = createSupabaseServerClient(request)
 
@@ -24,4 +26,20 @@ export const loader = async ({ request }) => {
   const profile = Profile.fromDB(data)
 
   return Response.json(profile, { headers, status: 200 })
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { client, headers } = createSupabaseServerClient(request)
+
+  const {
+    data: { user },
+  } = await client.auth.getUser()
+
+  if (!user) {
+    return Response.json({}, { headers, status: 401 })
+  }
+
+  // TODO
+
+  return Response.json({}, { headers, status: 200 })
 }
--- src/services/profileService.ts
+++ src/services/profileService.ts
@@ -18,7 +18,57 @@ const update = async (value: ProfileFormData) => {
   try {
     const url = new URL('/api/profile', window.location.origin)
     const data = new FormData()
-    // TODO
+
+    data.append('', value.displayName)
+    data.append('', value.about)
+    if (value.country) {
+      data.append('', value.country)
+    }
+    data.append('', value.type)
+
+    if (
+      value.existingCoverImageIds &&
+      value.existingCoverImageIds?.length > 0
+    ) {
+      for (const image of value.existingCoverImageIds) {
+        if (image) {
+          data.append('existingCoverImageIds', image)
+        }
+      }
+    }
+
+    data.append('', value.existingImageId || '')
+
+    data.append('', value.isContactable ? 'true' : 'false')
+
+    if (value.links && value.links?.length > 0) {
+      for (let i = 0; i < value.links.length; i++) {
+        const link = value.links[0]
+        if (link) {
+          data.append(`links.[${i}].label`, link.label)
+          data.append(`links.[${i}].url`, link.url)
+        }
+      }
+    }
+
+    data.append('showVisitorPolicy', value.showVisitorPolicy.toString())
+
+    if (value.showVisitorPolicy) {
+      if (value.visitorPolicy?.details) {
+        data.append('visitorPolicyDetails', value.visitorPolicy.details)
+      }
+      if (value.visitorPolicy?.policy) {
+        data.append('visitorPolicy', value.visitorPolicy.policy)
+      }
+    }
+
+    if (value.tagIds && value.tagIds?.length > 0) {
+      for (const tagId of value.tagIds) {
+        if (tagId) {
+          data.append('tags', tagId.toString())
+        }
+      }
+    }
 
     const response = await fetch(url, {
       body: data,

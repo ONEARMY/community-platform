29b23d419dad03c7fa2f384a6984c21094a83afe
mariojsnunes <mariojsnunes@users.noreply.github.com>
2025-07-16 05:35:29 +0000

    deploy: 2fb9ac6caf8c4c6d449a64cd5ac86a2be440a18b

---
+++ storybook-static/assets/TabbedContent-CNlGcUrY.js
@@ -0,0 +1,53 @@
+import{n as r}from"./emotion-styled.browser.esm-DWZzmYQO.js";import{a,T as n}from"./TabsList-BwXQ2XD8.js";const c=r(a)`
+  background-color: transparent;
+  border: none;
+  border-top-left-radius: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px;
+  border-top-right-radius: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px;
+  box-shadow: 0 2px 0px 0px transparent;
+  cursor: pointer;
+  font-family: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.text)?void 0:o.heading.fontFamily}};
+  font-size: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.fontSizes)?void 0:o[2]}}px;
+  padding: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px
+    ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[3]}}px;
+  margin-right: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[3]}}px;
+  color: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.colors)?void 0:o.grey}};
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media (min-width: 52em) {
+    &:first-of-type {
+      margin-left: 0;
+      position: relative;
+    }
+  }
+
+  @media (max-width: 52em) {
+    border: 1px solid ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.colors)?void 0:o.grey}};
+    border-radius: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px;
+    margin-bottom: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px;
+  }
+
+  &.base--selected {
+    background-color: ${r=>{var e;return null==(e=r.theme)?void 0:e.colors.background}};
+    text-decoration: none;
+    border: none;
+    color: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.colors)?void 0:o.black}};
+
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      left: 0;
+      top: 100%;
+      height: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[3]}}px;
+      width: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[3]}}px;
+      background-color: ${r=>{var e;return null==(e=r.theme)?void 0:e.colors.background}};
+    }
+  }
+`,i=r(n)`
+  background-color: ${r=>{var e;return null==(e=r.theme)?void 0:e.colors.background}};
+  border-radius: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[2]}}px;
+  padding: ${r=>{var e,o;return null==(o=null==(e=r.theme)?void 0:e.space)?void 0:o[3]}}px;
+`;export{c as T,i as a};
---
+++ storybook-static/assets/client-DtwqOgqp.js
@@ -0,0 +1,1 @@
+import{q as r}from"./iframe-Cnv6e--Z.js";var t,o=r;t=o.createRoot,o.hydrateRoot;export{t as c};

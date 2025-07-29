60c58661fd2e925be63ad1d0659e50130f7e058d
benfurber <benfurber@users.noreply.github.com>
2025-07-29 11:21:34 +0000

    deploy: 047b7a9734856563ffad001a09e588efe0cf7358

---
+++ storybook-static/assets/InternalLink-PgP1C5rx.js
@@ -0,0 +1,1 @@
+import{r as e,j as i,L as s}from"./iframe-DeSZYuJ6.js";import{L as t}from"./theme-ui-components.esm-DJ3E_AZV.js";const r=e.forwardRef(((e,r)=>i.jsx(t,{as:s,ref:r,...e,children:e.children})));r.displayName="InternalLink",r.__docgenInfo={description:"",methods:[],displayName:"InternalLink"};export{r as I};

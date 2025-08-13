7bd3d8a82ecb1c454f8c66df36663212bf3b8245
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-08-13 21:12:04 +0200

    New api route for contentType vote count

---
+++ src/routes/api.useful.$contentType.$id.count.ts
@@ -0,0 +1,46 @@
+import { createSupabaseServerClient } from 'src/repository/supabase.server'
+
+import type { LoaderFunctionArgs } from '@remix-run/node'
+import type { ContentType } from 'shared/lib/models/common'
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const { client, headers } = createSupabaseServerClient(request)
+
+  const { contentType, id } = params
+
+  const allowedContentTypes = [
+    'questions',
+    'projects',
+    'research',
+    'news',
+    'comment',
+  ] as const satisfies readonly ContentType[]
+
+  if (!allowedContentTypes.includes(contentType as ContentType)) {
+    return Response.json({ error: 'Unsupported content type' }, { status: 400 })
+  }
+
+  const commentId = Number(id)
+
+  if (!commentId || commentId <= 0 || !Number.isInteger(commentId)) {
+    return Response.json({ error: 'Invalid comment ID' }, { status: 400 })
+  }
+
+  const { data, error } = await client
+    .from('useful_votes')
+    .select('content_id')
+    .eq('content_type', contentType)
+    .eq('content_id', commentId)
+
+  if (error) {
+    console.error('Supabase error:', error)
+    return Response.json(
+      { error: 'Error fetching votes' },
+      { status: 500, headers },
+    )
+  }
+
+  const count = Array.isArray(data) ? data.length : 0
+
+  return Response.json({ count }, { headers, status: 200 })
+}

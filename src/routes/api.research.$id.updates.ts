41c1f7e2271d72a750ff6c6747048592f76784cd
Ben Furber <ben.furber@googlemail.com>
2025-08-18 10:54:47 +0100

    refactor: research update crud flow

--- src/routes/api.research.$id.updates.ts
+++ src/routes/api.research.$id.updates.ts
@@ -12,8 +12,11 @@ import type { DBProfile, DBResearchItem } from 'oa-shared'
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   try {
+    const { client, headers } = createSupabaseServerClient(request)
+
     const researchId = Number(params.id)
     const formData = await request.formData()
+
     const data = {
       title: formData.get('title') as string,
       description: formData.get('description') as string,
@@ -21,12 +24,13 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
       fileUrl: formData.get('fileUrl') as string,
       isDraft: formData.get('draft') === 'true',
     }
-
-    const { client, headers } = createSupabaseServerClient(request)
-
-    const {
-      data: { user },
-    } = await client.auth.getUser()
+    const uploadedImages = formData.getAll('images') as File[]
+    const uploadedFiles = formData.getAll('files') as File[]
+    const userData = await client.auth.getUser()
+    const profile = await profileServiceServer.getByAuthId(
+      userData.data.user!.id,
+      client,
+    )
 
     const researchResult = await client
       .from('research')
@@ -34,36 +38,18 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
       .eq('id', researchId)
       .single()
     const research = researchResult.data as unknown as DBResearchItem
-    const profile = await profileServiceServer.getByAuthId(user!.id, client)
-
-    if (!profile) {
-      return Response.json({}, { status: 400, statusText: 'User not found' })
-    }
 
     const { valid, status, statusText } = validateRequest(
       request,
-      user,
+      userData.data.user,
       data,
       research,
       profile,
+      uploadedImages,
     )
 
     if (!valid) {
       return Response.json({}, { status, statusText })
-    }
-
-    const uploadedImages = formData.getAll('images') as File[]
-    const uploadedFiles = formData.getAll('files') as File[]
-    const imageValidation = validateImages(uploadedImages)
-
-    if (!imageValidation.valid) {
-      return Response.json(
-        {},
-        {
-          status: 400,
-          statusText: imageValidation.errors.join(', '),
-        },
-      )
     }
 
     const updateResult = await client
@@ -74,7 +60,7 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
         video_url: data.videoUrl,
         is_draft: data.isDraft,
         research_id: researchId,
-        created_by: profile.id,
+        created_by: profile!.id,
         tenant_id: process.env.TENANT_ID,
       })
       .select('*,research:research(id,collaborators,created_by,is_draft,slug)')
@@ -88,39 +74,53 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
     const researchUpdate = ResearchUpdate.fromDB(dbResearchUpdate, [])
     researchUpdate.research = updateResult.data.research
 
-    await uploadAndUpdateImages(
-      uploadedImages,
-      `research/${researchId}/updates/${researchUpdate.id}`,
-      researchUpdate,
-      client,
-    )
+    try {
+      await uploadAndUpdateImages(
+        uploadedImages,
+        `research/${researchId}/updates/${researchUpdate.id}`,
+        researchUpdate,
+        client,
+      )
 
-    await uploadAndUpdateFiles(
-      uploadedFiles,
-      `research/${researchId}/updates/${researchUpdate.id}`,
-      researchUpdate,
-      client,
-    )
+      await uploadAndUpdateFiles(
+        uploadedFiles,
+        `research/${researchId}/updates/${researchUpdate.id}`,
+        researchUpdate,
+        client,
+      )
 
-    await subscribersServiceServer.addResearchUpdateSubscribers(
-      researchUpdate,
-      profile.id,
-      client,
-    )
+      await subscribersServiceServer.addResearchUpdateSubscribers(
+        researchUpdate,
+        profile!.id,
+        client,
+      )
 
-    broadcastCoordinationServiceServer.researchUpdate(
-      researchUpdate,
-      profile,
-      client,
-      request,
-    )
+      broadcastCoordinationServiceServer.researchUpdate(
+        researchUpdate,
+        profile,
+        client,
+        request,
+      )
 
-    return Response.json({ researchUpdate }, { headers, status: 201 })
+      return Response.json({ researchUpdate }, { headers, status: 201 })
+    } catch (error) {
+      console.error(error)
+
+      await client.from('research_updates').delete().eq('id', researchUpdate.id)
+
+      return Response.json(
+        {},
+        {
+          status: 500,
+          statusText: 'Error after trying to create research update',
+        },
+      )
+    }
   } catch (error) {
     console.error(error)
     return Response.json(
       {},
-      { status: 500, statusText: 'Error creating research' },
+      { status: 500, statusText: 'Error creating research update' },
     )
   }
 }
@@ -132,24 +132,18 @@ async function uploadAndUpdateImages(
   client: SupabaseClient,
 ) {
   if (files.length > 0) {
-    const mediaResult = await storageServiceServer.uploadImage(
+    const imagesResult = await storageServiceServer.uploadImage(
       files,
       path,
       client,
     )
 
-    if (mediaResult?.media && mediaResult.media.length > 0) {
-      const result = await client
+    if (imagesResult?.media && imagesResult.media.length > 0) {
+      return await client
         .from('research_updates')
-        .update({
-          images: mediaResult.media,
-        })
+        .update({ images: imagesResult.media })
         .eq('id', researchUpdate.id)
         .select()
-
-      if (result.data) {
-        researchUpdate.images = result.data[0].images
-      }
     }
   }
 }
@@ -168,17 +162,13 @@ async function uploadAndUpdateFiles(
     )
 
     if (mediaResult?.media && mediaResult.media.length > 0) {
-      const result = await client
+      return await client
         .from('research_updates')
         .update({
           files: mediaResult.media,
         })
         .eq('id', researchUpdate.id)
         .select()
-
-      if (result.data) {
-        researchUpdate.files = result.data[0].files
-      }
     }
   }
 }
@@ -201,6 +191,7 @@ function validateRequest(
   data: any,
   research: DBResearchItem | null,
   profile: DBProfile | null,
+  images: File[],
 ) {
   if (!user) {
     return { status: 401, statusText: 'unauthorized' }
@@ -231,7 +222,16 @@ function validateRequest(
     !research.collaborators?.includes(profile.username) &&
     !profile.roles?.includes(UserRole.ADMIN)
   ) {
-    return { status: 403, statusText: 'Forbidden' }
+    return { status: 401, statusText: 'unauthorized to edit' }
+  }
+
+  const imageValidation = validateImages(images)
+
+  if (!imageValidation.valid) {
+    return {
+      status: 400,
+      statusText: imageValidation.errors.join(', '),
+    }
   }
 
   return { valid: true }

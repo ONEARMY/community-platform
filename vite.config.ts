77da132312d1adeeca505357931e657f7cd137c7
Mário Nunes <mariojsnunes@gmail.com>
2025-09-30 10:57:38 +0100

    fix: improve support for older ios versions

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     global: 'globalThis',
   },
   build: {
-    target: 'es2020',
+    target: ['es2019', 'safari14'],
     sourcemap: process.env.NODE_ENV !== 'production', // to enable local server-side debugging
     commonjsOptions: {
       transformMixedEsModules: true,

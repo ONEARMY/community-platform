9a62bb032bf007fcc51a3b1044a41beb84cd2d9f
Ben Furber <ben.furber@googlemail.com>
2025-10-01 19:48:06 +0100

    fix: drop style wrapper

--- packages/components/src/DisplayMarkdown/DisplayMarkdown.tsx
+++ packages/components/src/DisplayMarkdown/DisplayMarkdown.tsx
@@ -4,39 +4,22 @@ import remarkBreaks from 'remark-breaks'
 import remarkGfm from 'remark-gfm'
 import remarkYoutube from 'remark-youtube'
 
-import { DisplayMarkdownStylingWrapper } from './DisplayMarkdownStylingWrapper'
-
 export interface IProps {
   body: string
 }
 
 export const DisplayMarkdown = ({ body }: IProps) => {
   return (
-    <DisplayMarkdownStylingWrapper
-      sx={{
-        img: {
-          borderRadius: 2,
-          maxWidth: ['105%', '105%', '120%'],
-          marginLeft: ['-2.5%', '-2.5%', '-10%'],
-        },
-        iframe: {
-          maxWidth: ['105%', '105%', '120%'],
-          maxHeight: ['300px', '370px', '420px'],
-          marginLeft: ['-2.5%', '-2.5%', '-10%'],
-        },
-      }}
+    <Markdown
+      remarkPlugins={[
+        remarkBreaks,
+        remarkGfm,
+        [remarkYoutube, { width: 760, height: 420 }],
+      ]}
+      rehypePlugins={[rehypeRaw]}
+      skipHtml={true}
     >
-      <Markdown
-        remarkPlugins={[
-          remarkBreaks,
-          remarkGfm,
-          [remarkYoutube, { width: 760, height: 420 }],
-        ]}
-        rehypePlugins={[rehypeRaw]}
-        skipHtml={true}
-      >
-        {body}
-      </Markdown>
-    </DisplayMarkdownStylingWrapper>
+      {body}
+    </Markdown>
   )
 }

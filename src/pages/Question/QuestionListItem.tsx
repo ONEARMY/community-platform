386f81d7f1ecb7ee2d62ea11957305609c6e8351
Jacob Roberson <jprobersondev@gmail.com>
2025-10-23 07:37:56 -0500

    ran lint fix

--- src/pages/Question/QuestionListItem.tsx
+++ src/pages/Question/QuestionListItem.tsx
@@ -4,6 +4,7 @@ import {
   InternalLink,
   // ModerationStatus,
 } from 'oa-components'
+import { FollowButtonAction } from 'src/common/FollowButtonAction'
 import { Highlighter } from 'src/common/Highlighter'
 import { Box, Card, Flex, Heading } from 'theme-ui'
 
@@ -11,7 +12,6 @@ import { UserNameTag } from '../common/UserNameTag/UserNameTag'
 import { listing } from './labels'
 
 import type { Question } from 'oa-shared'
-import { FollowButtonAction } from 'src/common/FollowButtonAction'
 
 interface IProps {
   question: Question

5f2dbab3973f6f7bbc83d841c7b301e0d8d42e0b
Mário Nunes <mariojsnunes@gmail.com>
2025-11-27 08:03:07 +0000

    fix

--- src/routes/api.donation-settings.$profileId.ts
+++ src/routes/api.donation-settings.$profileId.ts
@@ -1,19 +1,16 @@
-import { createSupabaseServerClient } from "src/repository/supabase.server";
+import { createSupabaseServerClient } from 'src/repository/supabase.server';
 
-import type { LoaderFunctionArgs } from "react-router";
+import type { LoaderFunctionArgs } from 'react-router';
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const { client, headers } = createSupabaseServerClient(request);
 
   try {
-    const donationSettingsResult = await client.from("tenant_settings")
-      .select(
-        "donation_settings",
-      );
+    const donationSettingsResult = await client.from('tenant_settings').select('donation_settings');
     const data = donationSettingsResult.data?.at(0);
 
     const donationSettings = {
-      spaceName: "",
+      spaceName: '',
       campaignId: data?.donation_settings?.defaultCampaignId,
       description: data?.donation_settings?.defaultDescription,
       imageUrl: data?.donation_settings?.defaultImageUrl,
@@ -21,38 +18,29 @@ export async function loader({ request, params }: LoaderFunctionArgs) {
 
     if (params.profileId) {
       try {
-        const profileResult = await client.from("profiles").select(
-          "display_name,cover_images,donations_enabled",
-        ).eq(
-          "id",
-          params.profileId,
-        ).single();
+        const profileResult = await client
+          .from('profiles')
+          .select('display_name,cover_images,donations_enabled')
+          .eq('id', params.profileId)
+          .single();
 
         if (profileResult.data?.donations_enabled) {
           donationSettings.spaceName = profileResult.data.display_name;
-          donationSettings.campaignId =
-            `${process.env.TENANT_ID}-${params.profileId}`;
-          donationSettings.description = data?.donation_settings
-            ?.spaceDescription;
+          donationSettings.campaignId = `${process.env.TENANT_ID}-${params.profileId}`;
+          donationSettings.description = data?.donation_settings?.spaceDescription;
 
           if (profileResult.data.cover_images?.at(0)?.url) {
             donationSettings.imageUrl = profileResult.data.cover_images[0].url;
           }
-        } else {
-          return Response.json({}, {
-            headers,
-            status: 400,
-            statusText: "Donations are not enabled for this profile",
-          });
         }
       } catch (error) {
-        console.error("Error fetching profile donation settings:", error);
+        console.error('Error fetching profile donation settings:', error);
       }
     }
 
     return Response.json(donationSettings, { headers, status: 200 });
   } catch (error) {
-    console.error("Error fetching donation settings:", error);
+    console.error('Error fetching donation settings:', error);
     return Response.json({}, { headers, status: 500 });
   }
 }

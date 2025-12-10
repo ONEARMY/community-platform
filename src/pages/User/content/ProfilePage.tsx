6cf00a7032ca20139e48e74b2092aec4b1ea2d45
Jacob Roberson <jprobersondev@gmail.com>
2025-12-09 18:41:25 -0600

    Testing in the go pro button, some changes rendering

--- src/pages/User/content/ProfilePage.tsx
+++ src/pages/User/content/ProfilePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useMemo, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-import { Button, InternalLink } from 'oa-components';
+import { Button, ExternalLink, InternalLink } from 'oa-components';
 // eslint-disable-next-line import/no-unresolved
 import { ClientOnly } from 'remix-utils/client-only';
 import { trackEvent } from 'src/common/Analytics';
@@ -57,55 +57,67 @@ export const ProfilePage = observer((props: IProps) => {
         marginTop: isViewingOwnProfile ? 4 : [6, 8],
       }}
     >
-      {isViewingOwnProfile && (
-        <Flex
-          sx={{
-            alignSelf: ['center', 'flex-end'],
-            marginBottom: 6,
-            zIndex: 2,
-            gap: 2,
-            flexDirection: ['column', 'row'],
-          }}
-        >
-          {shouldShowUpgrade && (
-            <Button
-              as="a"
-              href={upgradeBadge.actionUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-              type="button"
-              variant="outline"
-              data-cy="UpgradeBadge"
-              onClick={() => {
-                trackEvent({
-                  category: 'profiles',
-                  action: 'upgradeBadgeClicked',
-                  label: upgradeBadge.actionLabel,
-                });
-              }}
-              sx={{
-                backgroundColor: 'white',
-              }}
-            >
-              <Flex sx={{ alignItems: 'center', gap: 2 }}>
-                {upgradeBadge.badge?.imageUrl && (
-                  <Image
-                    src={upgradeBadge.badge.imageUrl}
-                    sx={{ height: 20, width: 20, flexShrink: 0 }}
-                    alt={upgradeBadge.badge.displayName || 'badge'}
-                  />
+      <ClientOnly fallback={<></>}>
+        {() => (
+          <>
+            {isViewingOwnProfile && (
+              <Flex
+                sx={{
+                  alignSelf: ['center', 'flex-end'],
+                  marginBottom: 6,
+                  zIndex: 2,
+                  gap: 2,
+                  flexDirection: ['column', 'row'],
+                }}
+              >
+                {shouldShowUpgrade && (
+                  <ExternalLink
+                    href={upgradeBadge.actionUrl}
+                    data-cy="UpgradeBadge"
+                    onClick={() => {
+                      trackEvent({
+                        category: 'profiles',
+                        action: 'upgradeBadgeClicked',
+                        label: upgradeBadge.actionLabel,
+                      });
+                    }}
+                    sx={{
+                      textDecoration: 'none',
+                      '&:hover': {
+                        textDecoration: 'none',
+                      },
+                    }}
+                  >
+                    <Button
+                      type="button"
+                      variant="outline"
+                      sx={{
+                        backgroundColor: 'white',
+                      }}
+                    >
+                      <Flex sx={{ alignItems: 'center', gap: 2 }}>
+                        {upgradeBadge.badge?.imageUrl && (
+                          <Image
+                            src={upgradeBadge.badge.imageUrl}
+                            sx={{ height: 20, width: 20, flexShrink: 0 }}
+                            alt={upgradeBadge.badge.displayName || 'badge'}
+                          />
+                        )}
+                        {upgradeBadge.actionLabel}
+                      </Flex>
+                    </Button>
+                  </ExternalLink>
                 )}
-                {upgradeBadge.actionLabel}
+                <InternalLink to="/settings">
+                  <Button type="button" data-cy="EditYourProfile">
+                    Edit Your Profile
+                  </Button>
+                </InternalLink>
               </Flex>
-            </Button>
-          )}
-          <InternalLink to="/settings">
-            <Button type="button" data-cy="EditYourProfile">
-              Edit Your Profile
-            </Button>
-          </InternalLink>
-        </Flex>
-      )}
+            )}
+          </>
+        )}
+      </ClientOnly>
 
       <ClientOnly fallback={<></>}>
         {() => (

be7426c6d303a95224ea65099459f7a3783eb0a0
Jacob Roberson <jprobersondev@gmail.com>
2025-12-09 18:56:14 -0600

    server side caching

--- src/routes/api.upgrade-badges.ts
+++ src/routes/api.upgrade-badges.ts
@@ -1,11 +1,26 @@
+import Keyv from 'keyv';
 import { UpgradeBadge } from 'oa-shared';
+import { isProductionEnvironment } from 'src/config/config';
 import { createSupabaseServerClient } from 'src/repository/supabase.server';
 
 import type { DBUpgradeBadge } from 'oa-shared';
 import type { LoaderFunctionArgs } from 'react-router';
 
+const cache = new Keyv<UpgradeBadge[]>({ ttl: 1800000 }); // ttl: 30 minutes
+
 export async function loader({ request }: LoaderFunctionArgs) {
   const { client, headers } = createSupabaseServerClient(request);
+
+  const cachedUpgradeBadges = await cache.get('upgradeBadges');
+
+  if (
+    cachedUpgradeBadges &&
+    Array.isArray(cachedUpgradeBadges) &&
+    cachedUpgradeBadges.length &&
+    isProductionEnvironment()
+  ) {
+    return Response.json(cachedUpgradeBadges, { headers, status: 200 });
+  }
 
   const { data } = await client.from('upgrade_badge').select(
     `
@@ -15,6 +30,10 @@ export async function loader({ request }: LoaderFunctionArgs) {
   );
 
   const upgradeBadges = data?.map((badge) => UpgradeBadge.fromDB(badge as DBUpgradeBadge));
+
+  if (upgradeBadges && upgradeBadges.length > 0) {
+    cache.set('upgradeBadges', upgradeBadges, 1800000);
+  }
 
   return Response.json(upgradeBadges, { headers, status: 200 });
 }

9c5e31f0eb531b626b63602c6a4078ab9274517e
Mário Nunes <mariojsnunes@gmail.com>
2026-01-09 19:29:13 +0000

    fix unit tests

--- src/pages/User/impact/constants.ts
+++ src/pages/User/impact/constants.ts
@@ -1,8 +1,9 @@
 import type { IImpactYear } from 'oa-shared';
 
 export const IMPACT_REPORT_LINKS = {
-  2024: 'https://www.preciousplastic.com/impact/2024',
-  2023: 'https://www.preciousplastic.com/impact/2023',
+  2024: 'https://www.preciousplastic.com/impact/2025',
+  2023: 'https://www.preciousplastic.com/impact/2024',
+  2022: 'https://www.preciousplastic.com/impact/2023',
   2019: 'https://www.preciousplastic.com/impact',
 };
 

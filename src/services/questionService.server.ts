c29d7e8918b36673ca230a3a982c4ecd231d741b
Ben Furber <ben.furber@googlemail.com>
2026-01-18 16:58:11 +0000

    feat: update profile contributions display

--- src/services/questionService.server.ts
+++ src/services/questionService.server.ts
@@ -1,3 +1,5 @@
+import { ImageServiceServer } from './imageService.server';
+
 import type { SupabaseClient } from '@supabase/supabase-js';
 import type { DBQuestion, Question } from 'oa-shared';
 
@@ -45,17 +47,22 @@ const getQuestionsByUser = async (
   client: SupabaseClient,
   username: string,
 ): Promise<Partial<Question>[]> => {
+  const imageService = new ImageServiceServer(client);
   const functionResult = await client.rpc('get_user_questions', {
     username_param: username,
   });
 
   if (functionResult.error || functionResult.count === 0) {
+    console.error('Error fetching user questions:', functionResult.error);
     return [];
   }
 
   const items = functionResult.data.map((x) => {
+    const images = x.images ? imageService.getPublicUrls(x.images) : null;
     return {
       id: x.id,
+      commentCount: x.comment_count,
+      images,
       title: x.title,
       slug: x.slug,
       usefulCount: x.total_useful,

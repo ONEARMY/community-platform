f54fd696de2a6f2551cc35d53593bd3c95d309c5
Mário Nunes <mariojsnunes@gmail.com>
2026-02-07 15:11:08 +0000

    fix academy seo

--- src/routes/_.academy.$.tsx
+++ src/routes/_.academy.$.tsx
@@ -1,14 +1,18 @@
 import Academy from 'src/pages/Academy/Academy';
 import Main from 'src/pages/common/Layout/Main';
+import { generateTags, mergeMeta } from 'src/utils/seo.utils';
 
-export async function clientLoader() {
+export async function loader() {
   return null;
 }
+
+export const meta = mergeMeta(() => {
+  return generateTags(`Academy - ${import.meta.env.VITE_SITE_NAME}`);
+});
 
 export default function Index() {
   return (
     <Main style={{ flex: 1, overflow: 'hidden' }} ignoreMaxWidth={true}>
-      {/* <SeoTagsUpdateComponent title="Academy" /> */}
       <Academy />
       <style
         dangerouslySetInnerHTML={{ __html: `html { overflow-y: hidden !important; }` }}

e582b516a5b9ccd7c1330706de901d43f4a37e0a
Jacob Roberson <jprobersondev@gmail.com>
2026-02-11 10:21:03 -0600

    fix: published_at display and sorting

--- src/routes/api.research.$id.ts
+++ src/routes/api.research.$id.ts
@@ -60,6 +60,8 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
 
     const isFirstPublish = oldResearch.is_draft && !data.isDraft && !oldResearch.published_at;
 
+    const now = new Date();
+
     const researchResult = await client
       .from('research')
       .update({
@@ -71,8 +73,9 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
         previous_slugs: previousSlugs,
         is_draft: data.isDraft,
         collaborators: data.collaborators,
+        modified_at: now,
         ...(!data.existingImage && { image: null }),
-        ...(isFirstPublish && { published_at: new Date() }),
+        ...(isFirstPublish && { published_at: now }),
       })
       .eq('id', id)
       .select()

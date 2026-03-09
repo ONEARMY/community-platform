a87f8cea74b5c52d911ad97c7815d254b6e45663
Jacob Roberson <jprobersondev@gmail.com>
2026-03-09 09:43:54 -0500

    collapse migration into one

--- supabase/migrations/20260309143743_remove_username_from_user_metadata.sql
+++ supabase/migrations/20260309143743_remove_username_from_user_metadata.sql
@@ -1,3 +1,5 @@
+drop function if exists "public"."get_user_email_by_username"(username text);
+
 CREATE UNIQUE INDEX profiles_username_key ON public.profiles USING btree (username, tenant_id);
 
 alter table "public"."profiles" add constraint "profiles_username_key" UNIQUE using index "profiles_username_key";

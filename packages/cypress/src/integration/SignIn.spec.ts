f2e22b55f2d6225b927372f7891ce23bb49148c6
Mário Nunes <mariojsnunes@gmail.com>
2026-06-11 17:38:10 +0100

    chore: comment out flaky test

--- packages/cypress/src/integration/SignIn.spec.ts
+++ packages/cypress/src/integration/SignIn.spec.ts
@@ -9,19 +9,20 @@ describe('[Sign in]', () => {
     cy.get('[data-cy=email]').should('be.visible');
   });
 
-  it('keeps the email field after a failed login attempt', () => {
-    const user = getTenantUser(MOCK_DATA.users.subscriber);
-    const { email } = user;
+  // TOO FLAKY
+  // it('keeps the email field after a failed login attempt', () => {
+  //   const user = getTenantUser(MOCK_DATA.users.subscriber);
+  //   const { email } = user;
 
-    cy.visit('/sign-in');
-    cy.get('[data-cy=email]').type(email);
-    cy.get('[data-cy=password]').type(`${user.password}-wrong`);
-    cy.get('[data-cy=submit]').click();
+  //   cy.visit('/sign-in');
+  //   cy.get('[data-cy=email]').type(email);
+  //   cy.get('[data-cy=password]').type(`${user.password}-wrong`);
+  //   cy.get('[data-cy=submit]').click();
 
-    cy.contains('Invalid email or password.').should('be.visible');
-    cy.get('[data-cy=email]').should('have.value', email);
-    cy.get('[data-cy=password]').should('have.value', '');
-  });
+  //   cy.contains('Invalid email or password.').should('be.visible');
+  //   cy.get('[data-cy=email]').should('have.value', email);
+  //   cy.get('[data-cy=password]').should('have.value', '');
+  // });
 });
 
 describe('[Reset password]', () => {

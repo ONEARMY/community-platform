a8647441c0e20e393f8ac717a8edc4aa0b07a820
Mário Nunes <mariojsnunes@gmail.com>
2026-08-11 16:38:46 +0700

    chore: upgrade tslog (#4833)

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,6 +1,7 @@
 export interface ISentryConfig {
   dsn: string;
   environment: string;
+  sampleRate: number;
 }
 
 export type siteVariants = 'dev_site' | 'test-ci' | 'staging' | 'production' | 'preview';

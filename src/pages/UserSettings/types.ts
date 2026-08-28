0f6583a3990982b7a185cd8a24f771f740c7c832
Musfiqur Rahman Saimon <britsync07@gmail.com>
2026-08-28 19:22:23 +0600

    fix: add missing settings page titles (#4867)

--- src/pages/UserSettings/types.ts
+++ src/pages/UserSettings/types.ts
@@ -1,10 +1,11 @@
 import type { availableGlyphs } from 'oa-components';
 import type { ComponentType } from 'react';
+import type { SettingsRoute } from './settingsTabs';
 
 export interface ISettingsTab {
   header?: React.ReactNode;
   body: ComponentType;
   glyph: availableGlyphs;
   title: string;
-  route: string;
+  route: SettingsRoute;
 }

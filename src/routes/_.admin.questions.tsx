13994914e4d685781d017c0b40630f698a8b80c6
Zechariah Mahler <zechy.mahler@gmail.com>
2026-09-01 19:25:18 -0600

    feat: add admin questions route

---
+++ src/routes/_.admin.questions.tsx
@@ -0,0 +1,31 @@
+import type { DBQuestion } from 'oa-shared';
+import { Question } from 'oa-shared';
+import type { LoaderFunctionArgs } from 'react-router';
+import { useLoaderData } from 'react-router';
+import { QuestionsPage } from 'src/pages/Admin/Questions/QuestionsPage';
+import { createSupabaseServerClient } from 'src/repository/supabase.server';
+
+export const handle = { breadcrumb: 'Questions' };
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { client } = createSupabaseServerClient(request);
+
+  const { data } = await client
+    .from('questions')
+    .select(
+      'title,slug,created_by,category,tags,moderation,is_draft,published_at,comment_count,deleted,id,created_at,modified_at,previous_slugs,images,description',
+    )
+    .order('type');
+
+  const questions = (data || []).map((question) =>
+    Question.fromDB(question as DBQuestion, question.tags, question.images),
+  );
+
+  return { questions };
+}
+
+export default function Index() {
+  const { questions } = useLoaderData<typeof loader>();
+
+  return <QuestionsPage categories={questions} />;
+}

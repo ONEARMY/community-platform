a251bababdb0cceb109bb212fd3e1caa2c3185fe
Zechariah Mahler <zechy.mahler@gmail.com>
2026-09-03 07:21:29 -0600

    feat: add moderation cell to table on questions page

--- src/pages/Admin/Questions/QuestionsPage.tsx
+++ src/pages/Admin/Questions/QuestionsPage.tsx
@@ -23,34 +23,26 @@ export function QuestionsPage({ questions }: IProps) {
         <TableHeader>
           <TableRow>
             <TableHead className="w-0">Title</TableHead>
-            <TableHead>Description</TableHead>
             <TableHead>Created By</TableHead>
             <TableHead>Category</TableHead>
-            <TableHead>Tags</TableHead>
             <TableHead>Moderation</TableHead>
             <TableHead>Is Draft</TableHead>
             <TableHead>Published At</TableHead>
             <TableHead>Comment Count</TableHead>
-            <TableHead>Deleted</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {questions.map((question) => (
             <TableRow key={question.id}>
               <TableCell>{question.title}</TableCell>
-              <TableCell className="max-w-xs truncate text-muted-foreground">
-                {question.description}
-              </TableCell>
               <TableCell>{question.createdBy.toString()}</TableCell>
-              <TableCell>{question.category !== null ? question.category : ''}</TableCell>
-              <TableCell>{question.tags.join()}</TableCell>
-              <TableCell>{}</TableCell>
+              <TableCell>{question.category !== null ? question.category.name : ''}</TableCell>
+              <TableCell>{question.moderation}</TableCell>
               <TableCell>{question.isDraft}</TableCell>
               <TableCell>
                 {question.publishedAt !== null ? question.publishedAt.toLocaleDateString() : ''}
               </TableCell>
               <TableCell>{question.commentCount}</TableCell>
-              <TableCell>{question.deleted}</TableCell>
             </TableRow>
           ))}
         </TableBody>

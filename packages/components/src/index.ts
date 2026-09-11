ab3fd7ab271d0df4394d3452bfebe814e5a659e6
Mário Nunes <mariojsnunes@gmail.com>
2026-09-11 11:21:43 +0700

    chore: drop morecontainer component (#4888)

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -60,7 +60,6 @@ export { MemberBadge } from './MemberBadge/MemberBadge';
 export { MemberHistory } from './MemberHistory/MemberHistory';
 export { Modal } from './Modal/Modal';
 export { ModerationRecord, ModerationStatus } from './ModerationStatus/ModerationStatus';
-export { MoreContainer } from './MoreContainer/MoreContainer';
 export { NotificationListSupabase } from './NotificationListSupabase/NotificationListSupabase';
 export { NotificationsModal } from './NotificationsModal/NotificationsModal';
 export { OsmGeocoding } from './OsmGeocoding/OsmGeocoding';

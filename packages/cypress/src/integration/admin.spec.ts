c619f2a396d2f128bee808fe7d0d53086f0f80c3
Mário Nunes <mariojsnunes@gmail.com>
2026-09-22 16:18:14 +0100

    chore: fix more tests

--- packages/cypress/src/integration/admin.spec.ts
+++ packages/cypress/src/integration/admin.spec.ts
@@ -86,9 +86,9 @@ describe('[Admin]', () => {
 
     cy.visit('/admin/questions')
     cy.wait(500)
-    cy.get('button[aria-label*="Delete"]').first().click()
+    cy.get('button[aria-label*="Delete"]').first().click({ force: true })
     cy.contains('Delete Question')
-    cy.get('button').contains("Delete").click()
+    cy.get('button').contains("Delete").click({ force: true })
     cy.get('body').should("not.contain.text", "Delete Question")
     cy.get('tr').should("have.class", "bg-gray-100")
 

5af8ce244f57fd9d4bb04e2a0b05efe3d51ff643
Mário Nunes <mariojsnunes@gmail.com>
2026-09-25 10:06:24 +0100

    fix: CSP page views and duplicate page views

--- src/common/Analytics/GoogleAnalytics.tsx
+++ src/common/Analytics/GoogleAnalytics.tsx
@@ -1,10 +1,8 @@
 import { useContext, useEffect } from 'react';
 import ReactGA from 'react-ga4';
-import { useLocation } from 'react-router';
 import { TenantContext } from 'src/pages/common/TenantContext';
 
 export const GoogleAnalytics = () => {
-  const location = useLocation();
   const env = useContext(TenantContext);
   const trackingIds = (env?.gaTrackingId ?? '')
     .split(',')
@@ -16,17 +14,6 @@ export const GoogleAnalytics = () => {
       ReactGA.initialize(trackingIds.map((trackingId) => ({ trackingId })));
     }
   }, []);
-
-  useEffect(() => {
-    if (trackingIds.length) {
-      sendPageView(location);
-    }
-  }, [location]);
-
-  const sendPageView = (location: any) => {
-    ReactGA.set({ page: location.pathname });
-    ReactGA.send({ hitType: 'pageview', page: location.pathname });
-  };
 
   return null;
 };
